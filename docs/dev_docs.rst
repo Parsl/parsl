5d9f07679c67908ef75bf81ff5f41490d7061e67
Yadu Nand B <yadudoc1729@gmail.com>
2017-02-22 15:55:36 -0600

    Adding a setup script. Pending tests

---
+++ docs/dev_docs.rst
@@ -0,0 +1,12 @@
+Developer Documentation
+=======================
+
+.. automodule:: parsl
+    :members:
+
+
+.. autoclass:: parsl.app.app
+    :members:
+
+.. automodule:: parsl.dataflow.dflow
+    :members:
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='parsl',
+    version='0.1.0',
+    description='Simple data dependent workflows in Python',
+    long_description='Simple and easy parallel workflows system for Python',
+    url='https://github.com/swift-lang/swift-e-lab',
+    author='Yadu Nand Babuji',
+    author_email='yadu@uchicago.edu',
+    license='Apache 2.0',
+    package_data={'': ['LICENSE']},
+    packages=['parsl'],
+    install_requires=[],
+)
