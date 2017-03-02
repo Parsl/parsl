905dd617ad4602755bc0674fe29c635f9bb16138
Yadu Nand B <yadudoc1729@gmail.com>
2017-03-02 17:45:32 -0600

    Updates

---
+++ parsl/dataflow/states.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+
+class States(IntEnum):
+    ''' Map states for tasks to an Int
+    '''
+
+    pending  = 0
+    runnable = 1
+    running  = 2
+    done     = 3
+    failed   = 4
+    dep_fail = 5
+
+
+if __name__ == "__main__":
+    print(States.pending)
+    print(States.done)
+    print(3 == States.done)
