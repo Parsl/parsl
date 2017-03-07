c9f340cb3e030daa4ef5be806d5f5e660410350c
Yadu Nand B <yadudoc1729@gmail.com>
2017-03-07 13:52:06 -0600

    Removing the setup/teardown functions and adding them to .travis.yml

--- parsl/tests/test_ipp/__init__.py
+++ parsl/tests/test_ipp/__init__.py
@@ -2,17 +2,16 @@ import os
 import time
 import subprocess
 
-def setup_package():
+def setup():
     print("Starting cluster")
-    os.system("ipcluster start -n 4 &")
-    proc = subprocess.Popen(["ipcluster start -n 4 &"], shell=True, executable="/bin/bash")
-    print("Sleeping to allow for slow start")
-    time.sleep(10)
+    #proc = subprocess.Popen(["ipcluster start -n 4 --daemon"], shell=True, executable="/bin/bash")
+    #print("Sleeping to allow for slow start")
+    #time.sleep(20)
 
-def teardown_package():
+def teardown():
     print("Terminating cluster")
-    os.system("ipcluster stop")
-    time.sleep(3)
+    #os.system("ipcluster stop")
+    #time.sleep(3)
 
 if __name__ == "__main__" :
 
