76060b593fdacdddbb87f4b379ddac6e7ef679cb
Yadu Nand B <yadudoc1729@gmail.com>
2017-03-12 19:57:46 -0500

    Lint fixes

--- parsl/executors/swift_t.py
+++ parsl/executors/swift_t.py
@@ -1,24 +1,19 @@
 ''' Sample Executor for integration with SwiftT.
 
-This follows the model used by `EMEWS <http://www.mcs.anl.gov/~wozniak/papers/Cancer2_2016.pdf>`_ to some extent.
+This follows the model used by `EMEWS <http://www.mcs.anl.gov/~wozniak/papers/Cancer2_2016.pdf>`_
+to some extent.
 
 '''
-import concurrent.futures
 from concurrent.futures import Future
 import logging
 import uuid
 import threading
-import weakref
-import time
-import sys, errno
 import queue
-from queue import Queue
 import multiprocessing as mp
 
 from ipyparallel.serialize import pack_apply_message, unpack_apply_message
 from ipyparallel.serialize import serialize_object, deserialize_object
 
-import parsl
 from parsl.executors.base import ParslExecutor
 
 logger = logging.getLogger(__name__)
@@ -89,7 +84,7 @@ def runner(incoming_q, outgoing_q):
             exec(code, user_ns, user_ns)
 
         except Exception as e:
-            logger.warn("Caught errors but will not handled %s", e)
+            logger.warning("Caught errors but will not handled %s", e)
             raise e
 
         else :
@@ -104,23 +99,23 @@ def runner(incoming_q, outgoing_q):
             msg = incoming_q.get(block=True, timeout=10)
             #logger.debug("[RUNNER] Got message : %s", msg)
 
-        except queue.Empty as e:
+        except queue.Empty:
             # Handle case where no items were on queue
             logger.debug("[RUNNER] got nothing")
-            pass
 
-        except IOError as e:
-            logger.debug("[RUNNER] broken pipe, error: %s", e)
+        except IOError as ioerror:
+            logger.debug("[RUNNER] broken pipe, error: %s", ioerror)
             try:
                 # Attempt to send a stop notification to the management thread
                 outgoing_q.put(None)
-            except Exception as e:
+
+            except Exception :
                 pass
+
             break
 
         except Exception as e:
             logger.debug("[RUNNER] caught unknown exception : %s", e)
-            pass
 
         else:
             # Handle received message
@@ -137,8 +132,8 @@ def runner(incoming_q, outgoing_q):
                     response = {"task_id" : msg["task_id"],
                                 "result"  : serialize_object(response_obj)}
 
-                    logger.warn("[RUNNER] Returing result : %s",
-                                deserialize_object(response["result"]) )
+                    logger.warning("[RUNNER] Returing result : %s",
+                                   deserialize_object(response["result"]) )
 
                 except Exception as e:
                     logger.debug("[RUNNER] Caught task exception")
@@ -161,12 +156,12 @@ class TurbineExecutor(ParslExecutor):
                         |  Data   |  Executor   |   IPC      | External Process(es)
                         |  Flow   |             |            |
                    Task | Kernel  |             |            |
-                 +----->|-------->|------------>|Outgoing_Q -|-> Worker_Process
+                 +----->|-------->|------------>|outgoing_q -|-> Worker_Process
                  |      |         |             |            |    |         |
            Parsl<---Fut-|         |             |            |  result   exception
                      ^  |         |             |            |    |         |
                      |  |         |   Q_mngmnt  |            |    V         V
-                     |  |         |    Thread<--|Incoming_Q<-|--- +---------+
+                     |  |         |    Thread<--|incoming_q<-|--- +---------+
                      |  |         |      |      |            |
                      |  |         |      |      |            |
                      +----update_fut-----+
@@ -207,7 +202,7 @@ class TurbineExecutor(ParslExecutor):
         while True:
             logger.debug("[MTHREAD] Management thread active")
             try:
-                msg = self.Incoming_Q.get(block=True, timeout=1)
+                msg = self.incoming_q.get(block=True, timeout=1)
 
             except queue.Empty as e:
                 # timed out.
@@ -222,18 +217,20 @@ class TurbineExecutor(ParslExecutor):
                 pass
 
             else:
-                if msg == None:
+
+                if msg is None:
                     logger.debug("[MTHREAD] Got None")
                     return
+
                 else:
                     logger.debug("[MTHREAD] Got message : %s", msg)
                     task_fut = self.tasks[msg['task_id']]
                     if 'result' in msg:
-                        result, remainder = deserialize_object(msg['result'])
+                        result, _ = deserialize_object(msg['result'])
                         task_fut.set_result(result)
 
                     elif 'exception' in msg:
-                        exception, remainder = deserialize_object(msg['exception'])
+                        exception, _ = deserialize_object(msg['exception'])
                         task_fut.set_exception(exception)
 
             if not self.isAlive:
@@ -241,7 +238,7 @@ class TurbineExecutor(ParslExecutor):
 
     # When the executor gets lost, the weakref callback will wake up
     # the queue management thread.
-    def weakref_cb(_, q=None):
+    def weakref_cb(self, q=None):
         ''' We do not use this yet
         '''
 
@@ -254,11 +251,12 @@ class TurbineExecutor(ParslExecutor):
         '''
 
         logging.debug("In _start %s", "*"*40)
-        if self._queue_management_thread == None:
+        if self._queue_management_thread is None:
             logging.debug("Starting management thread ")
             self._queue_management_thread = threading.Thread (target=self._queue_management_worker)
             self._queue_management_thread.daemon = True
             self._queue_management_thread.start()
+            
         else:
             logging.debug("Management thread already exists, returning")
 
@@ -268,7 +266,7 @@ class TurbineExecutor(ParslExecutor):
 
         self.isAlive = False
         logging.debug("Waking management thread")
-        self.Incoming_Q.put(None) # Wake up the thread
+        self.incoming_q.put(None) # Wake up the thread
         self._queue_management_thread.join() # Force join
         logging.debug("Exiting thread")
         self.worker.join()
@@ -285,22 +283,22 @@ class TurbineExecutor(ParslExecutor):
 
         logger.debug("In __init__")
         self.mp_manager = mp.Manager()
-        self.Outgoing_Q = self.mp_manager.Queue()
-        self.Incoming_Q = self.mp_manager.Queue()
+        self.outgoing_q = self.mp_manager.Queue()
+        self.incoming_q = self.mp_manager.Queue()
         self.isAlive   = True
 
         self._queue_management_thread = None
         self._start_queue_management_thread()
         logger.debug("Created management thread : %s", self._queue_management_thread)
 
-        self.worker  = mp.Process(target=runner, args = (self.Outgoing_Q, self.Incoming_Q))
+        self.worker  = mp.Process(target=runner, args = (self.outgoing_q, self.incoming_q))
         self.worker.start()
         logger.debug("Created worker : %s", self.worker)
         self.tasks   = {}
 
     def submit (self, func, *args, **kwargs):
-        ''' Submits work to the the Outgoing_Q, an external process listens on this queue for new work.
-        This method is simply pass through and behaves like a submit call as described
+        ''' Submits work to the the outgoing_q, an external process listens on this queue for new
+        work. This method is simply pass through and behaves like a submit call as described
         here `Python docs: <https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor>`_
 
         Args:
@@ -327,7 +325,7 @@ class TurbineExecutor(ParslExecutor):
                "buffer"  : fn_buf }
 
         # Post task to the the outgoing queue
-        self.Outgoing_Q.put(msg)
+        self.outgoing_q.put(msg)
 
         # Return the future
         return self.tasks[task_id]
@@ -342,7 +340,7 @@ class TurbineExecutor(ParslExecutor):
              NotImplemented exception
         '''
 
-        raise NotImplemented
+        raise NotImplementedError
 
     def scale_in (self, workers=1):
         ''' Scale in the number of active workers by 1
@@ -352,7 +350,7 @@ class TurbineExecutor(ParslExecutor):
              NotImplemented exception
         '''
 
-        raise NotImplemented
+        raise NotImplementedError
 
 
 
@@ -360,5 +358,5 @@ class TurbineExecutor(ParslExecutor):
 if __name__ == "__main__" :
 
     print("Start")
-    tex = TurbineExecutor()
+    turb_x = TurbineExecutor()
     print("Done")
