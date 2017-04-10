3cd0a6a0e0dc625306c25fd835fd0c7d9a1afa5f
Yadu Nand B <yadudoc1729@gmail.com>
2017-04-10 09:12:19 -0500

    Restructuring folders, moving execution_providers out of execution. Adding support for jetstream.

---
+++ parsl/execution_provider/execution_provider_base.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta, abstractmethod
+
+class ExecutionProvider(metaclass=ABCMeta):
+    """ Define the strict interface for all Execution Provider
+    """
+
+    @abstractmethod
+    def submit(self, *args, **kwargs):
+        ''' We haven't yet decided on what the args to this can be,
+        whether it should just be func, args, kwargs or be the partially evaluated
+        fn
+        '''
+
+        pass
+
+    @abstractmethod
+    def scale_out(self, *args, **kwargs):
+        ''' Scale out method. We should have the scale out method simply take resource object
+        which will have the scaling methods, scale_out itself should be a coroutine, since
+        scaling tasks can be slow.
+        '''
+        pass
+
+    @abstractmethod
+    def scale_in(self, *args, **kwargs):
+        ''' Scale in method. We should have the scale in method simply take resource object
+        which will have the scaling methods, scale_in itself should be a corinine, since
+        scaling tasks can be slow.
+        '''
+
+        pass
+
+
+class ResourceAbstrator(object):
+
+    def __init__ (self, name):
+        self.name = name
+
+
+    def register_
