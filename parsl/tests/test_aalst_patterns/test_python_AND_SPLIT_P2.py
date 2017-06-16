ec5c7baee5c91b4633c65d9fa54a3d769991ee67
Yadu Nand B <yadudoc1729@gmail.com>
2017-06-16 16:38:12 -0500

    Adding the first few Aalst workflows

---
+++ parsl/tests/test_aalst_patterns/test_python_AND_SPLIT_P2.py
@@ -0,0 +1,49 @@
+''' Testing bash apps
+'''
+import parsl
+from parsl import *
+
+import os
+import time
+import shutil
+import argparse
+
+#parsl.set_stream_logger()
+
+workers = ThreadPoolExecutor(max_workers=4)
+dfk = DataFlowKernel(workers)
+
+@App('python', dfk)
+def increment(x):
+    return x+1
+
+@App('python', dfk)
+def slow_increment(x, dur):
+    import time
+    time.sleep(dur)
+    return x+1
+
+
+def test_and_split(depth=5):
+    ''' Test simple pipeline A->B...->N
+    '''
+    futs = {}
+    for i in range(depth):
+        futs[i] = increment(i)
+
+    print([ futs[i].result() for i in futs])
+
+
+if __name__ == '__main__' :
+
+    parser   = argparse.ArgumentParser()
+    parser.add_argument("-w", "--width", default="10", help="width of the pipeline")
+    parser.add_argument("-d", "--debug", action='store_true', help="Count of apps to launch")
+    args   = parser.parse_args()
+
+    if args.debug:
+        pass
+        parsl.set_stream_logger()
+
+    #test_increment(depth=int(args.width))
+    test_and_split(depth=int(args.width))
---
+++ parsl/tests/test_aalst_patterns/test_python_SEQUENCE_P1.py
@@ -0,0 +1,58 @@
+''' Testing bash apps
+'''
+import parsl
+from parsl import *
+
+import os
+import time
+import shutil
+import argparse
+
+#parsl.set_stream_logger()
+
+workers = ThreadPoolExecutor(max_workers=4)
+dfk = DataFlowKernel(workers)
+
+@App('python', dfk)
+def increment(x):
+    return x+1
+
+@App('python', dfk)
+def slow_increment(x, dur):
+    import time
+    time.sleep(dur)
+    return x+1
+
+
+def test_increment(depth=5):
+    ''' Test simple pipeline A->B...->N
+    '''
+    futs = {0:0}
+    for i in range(1,depth):
+        futs[i] = increment( futs[i-1] )
+
+    print([ futs[i].result() for i in futs if type(futs[i]) != int ])
+
+def test_increment_slow(depth=4):
+    ''' Test simple pipeline A->B...->N with delay
+    '''
+    futs = {0:0}
+    for i in range(1,depth):
+        futs[i] = slow_increment( futs[i-1] , 0.5)
+
+    print(futs[i])
+    print([ futs[i].result() for i in futs if type(futs[i]) != int ])
+
+if __name__ == '__main__' :
+
+    parser   = argparse.ArgumentParser()
+    parser.add_argument("-w", "--width", default="5", help="width of the pipeline")
+    parser.add_argument("-d", "--debug", action='store_true', help="Count of apps to launch")
+    args   = parser.parse_args()
+
+    if args.debug:
+        pass
+        parsl.set_stream_logger()
+
+    #test_increment(depth=int(args.width))
+    test_increment_slow(depth=int(args.width))
