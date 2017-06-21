98e70210a1eb20f39c491d493fab89c92dc3f070
Monica Lewis <monicalewis@Monicas-MacBook-Air-3.local>
2017-06-21 14:25:43 -0500

    IPyParallel fibonacci tests

---
+++ parsl/tests/test_ipp/test_python_fibonacci_recursive.py
@@ -0,0 +1,31 @@
+import parsl
+from parsl import *
+import argparse
+
+workers = IPyParallelExecutor()
+dfk = DataFlowKernel(workers)
+
+@App('python', dfk)
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 2 or n ==1:
+        return 1
+    else:
+        return fibonacci(n - 1).result() + fibonacci(n - 2).result()
+    
+def test_fibonacci(x = 5):
+    results = []
+    for i in range(x):
+        results.append(fibonacci(i))
+    for j in range(len(results)):
+        while results[j].done() != True: 
+            pass
+        print(results[j].result())
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--x", default = '5', action = "store", dest = "b", type = int)
+    args = parser.parse_args()
+    test_fibonacci(args.b)
+    
