21f349b9e0d0397ad46c0ad3e26c093dbc05af8a
Monica Lewis <monicalewis@Monicas-MacBook-Air-3.local>
2017-06-21 11:18:23 -0500

    Fix fibonacci tests

--- parsl/tests/test_threads/test_python_fibonacci_iterative.py
+++ parsl/tests/test_threads/test_python_fibonacci_iterative.py
@@ -10,30 +10,31 @@ dfk = DataFlowKernel(workers)
 def get_num(first, second):
     return first + second 
 
-x1 = 0
-x2 = 1
-num = 5
-counter = 0
+def test_fibonacci(num = 5):
+    x1 = 0
+    x2 = 1
+    counter = 0
+    results = [ ]
+    results.append(0)
+    results.append(1)
+    while counter < num - 2:
+        counter += 1 
+        results.append(get_num(x1, x2))
+        temp = x2
+        x2 = get_num(x1, x2)
+        x1 = temp
+    for i in range(len(results)):
+        if type(results[i]) is int:
+            print(results[i])
+        else:
+            print(results[i].result())
+
 if __name__ == '__main__':   
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--num", default = "5", action = "store", dest = "a", type = int)
     args = parser.parse_args()
-    num = args.a
-counter = 0
-results = [ ]
-results.append(0)
-results.append(1)
-while counter < num - 2:
-    counter += 1 
-    results.append(get_num(x1, x2))
-    temp = x2
-    x2 = get_num(x1, x2)
-    x1 = temp
-for i in range(len(results)):
-    if type(results[i]) is int:
-        print(results[i])
-    else:
-        print(results[i].result())
+    test_fibonacci(args.a)
+
         
     
 
--- parsl/tests/test_threads/test_python_fibonacci_recursive.py
+++ parsl/tests/test_threads/test_python_fibonacci_recursive.py
@@ -1,11 +1,11 @@
 import parsl
 from parsl import *
 import argparse
-workers = ThreadPoolExecutor(max_workers = 30)
+
+workers = ThreadPoolExecutor(max_workers = 5)
 dfk = DataFlowKernel(workers)
 
 @App('python', dfk)
-
 def fibonacci(n):
     if n == 0:
         return 0
@@ -13,15 +13,15 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1).result() + fibonacci(n - 2).result()
-x = 5
+def test_fibonacci(x = 5):
+    results = []
+    for i in range(x):
+        results.append(fibonacci(i))
+    for j in range(len(results)):
+        print(results[j].result())
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--num", default = '5', action = "store", dest = "a", type = int)
     args = parser.parse_args()
-    x = args.a
-results = []
-for i in range(x):
-    results.append(fibonacci(i))
-    
-for j in range(len(results)):
-        print(results[j].result())
+    test_fibonacci(args.a)
