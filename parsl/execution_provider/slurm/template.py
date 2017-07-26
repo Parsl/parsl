1e031db4ddc6985dff058eb2b392346b83a5ef90
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2017-07-26 10:58:46 -0500

    Updates to template

--- parsl/execution_provider/slurm/template.py
+++ parsl/execution_provider/slurm/template.py
@@ -1,13 +1,12 @@
 template_string = '''#!/bin/bash
 
 #SBATCH --job-name=$jobname
-#SBATCH --output=$submit_dir/parsl-$job_id.submit.stdout
-#SBATCH --error=$submit_dir/parsl-$job_id.submit.stderr
+#SBATCH --output=$submit_script_dir/parsl-$jobname.submit.stdout
+#SBATCH --error=$submit_script_dir/parsl-$jobname.submit.stderr
 #SBATCH --nodes=$nodes
 #SBATCH --partition=$queue
 #SBATCH --time=$walltime
 #SBATCH --ntasks-per-node=$tasks_per_node
-#SBATCH --cpus-per-task=$cpus_per_task
 #SBATCH --account=$account
 $slurm_overrides
 
