3321c2b2c4baafae0459e774f138483a20fc5d8f
Yadu Nand B <yadudoc1729@gmail.com>
2017-08-17 16:17:24 -0500

    Pylint cleanup

--- parsl/app/app_factory.py
+++ parsl/app/app_factory.py
@@ -5,37 +5,39 @@ Centralize app object creation.
 '''
 import logging
 from inspect import signature
-logger = logging.getLogger(__name__)
-
 from parsl.app.app import BashApp
 from parsl.app.app import PythonApp
+from parsl.app.errors import InvalidAppTypeError
 
+logger = logging.getLogger(__name__)
 
 class AppFactory(object):
+    ''' AppFactory streamlines creation of apps
+    '''
 
-    def __init__ (self, app_class, executor, func, **kwargs):
+    def __init__(self, app_class, executor, func, **kwargs):
         ''' Construct an AppFactory for a particular app_class
 
         Args:
-            app_class (Class) : An app class
-            executor (Executor) : An executor object which will handle app execution
-            func (Function) : The function to execute
+            app_class(Class) : An app class
+            executor(Executor) : An executor object which will handle app execution
+            func(Function) : The function to execute
 
         Kwargs:
-            walltime (int) : Walltime in seconds, default=60
+            walltime(int) : Walltime in seconds, default=60
 
         Returns:
             An AppFactory Object
         '''
-        self.__name__   = func.__name__
-        self.app_class  = app_class
-        self.executor   = executor
-        self.func       = func
-        self.status     = 'created'
-        self.walltime   = kwargs.get('walltime', 60)
-        sig = signature(func)
+        self.__name__ = func.__name__
+        self.app_class = app_class
+        self.executor = executor
+        self.func = func
+        self.status = 'created'
+        self.walltime = kwargs.get('walltime', 60)
+        self.sig = signature(func)
 
-    def __call__ (self, *args, **kwargs):
+    def __call__(self, *args, **kwargs):
         ''' Create a new object of app_class with the args,
         execute the app_object and return the futures
 
@@ -46,11 +48,10 @@ class AppFactory(object):
              Arbitrary kwargs to the decorated function
 
         Returns:
-             (App_Future, [Data_Futures...])
+            (App_Future, [Data_Futures...])
 
         The call is mostly pass through
         '''
-        logger.debug("In factory call : %s %s" % (args, kwargs))
         # Create and call the new App object
         app_obj = self.app_class(self.func,
                                  self.executor,
@@ -60,42 +61,42 @@ class AppFactory(object):
     def __repr__(self):
         return self.__str__()
 
-    def __str__ (self):
-        return '<class %s"%s for %s>' % (self.app_class.__name__,
-                                         self.__class__.__name__,
-                                         self.__name__)
+    def __str__(self):
+        return '<class %s"%s for %s>' %(self.app_class.__name__,
+                                        self.__class__.__name__,
+                                        self.__name__)
 
 
-class AppFactoryFactory(object) :
+class AppFactoryFactory(object):
     ''' An instance AppFactoryFactory will be factory that creates object of a particular kind.
     AppFactoryFactory has the various apps registered with it, and it will return an AppFactory
     that constructs objects of a specific kind.
 
 
     '''
-    def __init__ (self, name):
+    def __init__(self, name):
         ''' Constructor
 
         Args:
-             name (string) : Name for the appfactory
+             name(string) : Name for the appfactory
 
         Returns:
-             object (AppFactory)
+             object(AppFactory)
         '''
-        self.name  = name
-        self.apps  =  { 'bash'   : BashApp,
-                        'python' : PythonApp }
+        self.name = name
+        self.apps = {'bash' : BashApp,
+                     'python' : PythonApp}
 
-    def make (self, kind, executor, f, **kwargs):
+    def make(self, kind, executor, func, **kwargs):
         ''' Creates a new App of the kind specified
 
         Args:
-            kind (string) : For now only (bash|python)
-            executor (Executor) : An executor object which will handle app execution
-            f (Function) : The function to execute
+            kind(string) : For now only(bash|python)
+            executor(Executor) : An executor object which will handle app execution
+            func(Function) : The function to execute
 
         Kwargs:
-            Walltime (int) : Walltime in seconds
+            Walltime(int) : Walltime in seconds
             Arbritrary kwargs passed onto the AppFactory
 
         Raises:
@@ -108,10 +109,12 @@ class AppFactoryFactory(object) :
         if kind in self.apps:
             return AppFactory(self.apps[kind],
                               executor,
-                              f,
+                              func,
                               **kwargs)
 
         else:
-            logger.error("AppFactory:%s Invalid app kind requested : %s ", self.name, kind)
-            raise InvalidAppTypeError("AppFactory:%s Invalid app kind requested : %s ", self.name, kind)
-
+            logger.error("AppFactory:%s Invalid app kind requested : %s ",
+                         self.name, kind)
+            raise InvalidAppTypeError(
+                "AppFactory:%s Invalid app kind requested : %s ",
+                self.name, kind)
