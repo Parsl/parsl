03f219c954915c10d49235ba97440be4a838b089
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2017-09-25 17:48:08 -0500

    New test that statically scales nodes on midway and launches mpi apps on     multi-node allocations. This is tested.

---
+++ parsl/tests/test_scaling/midway_mpi/test_mpi.py
@@ -0,0 +1,69 @@
+import parsl
+from parsl import *
+from parsl.execution_provider.slurm.slurm import Slurm
+from parsl.execution_provider.provider_factory import ExecProviderFactory
+
+from  jinja2 import Template, Environment
+from jinja2.loaders import FileSystemLoader
+import os
+
+parsl.set_stream_logger()
+WORKING_DIR = "working"
+
+# Let's create a pool of threads to execute our functions
+#workers = ThreadPoolExecutor(max_workers=4)
+#dfk = DataFlowKernel(workers)
+config = {  "site" : "midway_westmere",
+            "execution" :
+            {  "executor" : "ipp",
+               "provider" : "slurm",
+               "channel"  : "local",
+               "options" :
+               {"init_parallelism" : 2,
+                "max_parallelism" : 2,
+                "min_parallelism" : 0,
+                "tasks_per_node"  : 1,
+                "node_granularity" : 2,
+                "partition" : "sandyb",
+                "walltime" : "00:10:00",
+                "account" : "pi-chard",
+                "submit_script_dir" : ".scripts"
+                }
+            }}
+
+
+import subprocess
+import random
+print ("starting ipcontroller")
+
+rand_port =random.randint(55000, 59000)
+p = subprocess.Popen(['ipcontroller','--port={0}'.format(rand_port),'--ip=*'], stdout=None, stderr=None)
+
+epf = ExecProviderFactory()
+resource = epf.make(config)
+dfk = DataFlowKernel(resource)
+
+
+# Helper function to create an input file from a template
+def create_template(template_name, output_name, contents):
+    fs_loader = FileSystemLoader('templates')
+    env = Environment(loader=fs_loader)
+    template = env.get_template(template_name)
+    packmol_file = open(output_name, 'w')
+    packmol_file.write(template.render(contents))
+    packmol_file.close()
+
+
+#################
+# App definitions
+#################
+@App('bash', dfk)
+def mpi_hello(ranks, inputs=[], outputs=[], stdout=None, stderr=None, mock=False ):
+    cmd_line = "mpirun -n {0} mpi_hello"
+
+
+x = mpi_hello(4, stdout="hello.out", stderr="hello.err")
+print("Launched the mpi_hello app")
+x.result()
+
+p.terminate()
