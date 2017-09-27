184109f60ead4bab8fcf52a440981bf24bfbba9c
Yadu Nand B <yadudoc1729@gmail.com>
2017-09-27 15:44:49 -0500

    Updating to match new app return style

--- parsl/tests/test_threads/test_tutorial_1.py
+++ parsl/tests/test_threads/test_tutorial_1.py
@@ -28,8 +28,9 @@ def test_data_future_result () :
     ''' Testing the behavior of a result call on DataFutures
     '''
     # We call sim_mol_dyn with
-    sim_fut, data_futs = sim_mol_dyn(5, 0, outputs=['sim.out'],
-                                     stdout='stdout.txt', stderr='stderr.txt')
+    sim_fut = sim_mol_dyn(5, 0, outputs=['sim.out'],
+                          stdout='stdout.txt', stderr='stderr.txt')
+    data_futs = sim_fut.outputs
     print("Launching and waiting on data_futs")
     print("Done?   : ", data_futs[0].done())
     print("Result? : ", data_futs[0].result(timeout=1))
@@ -40,8 +41,9 @@ def test_app_future_result () :
     ''' Testing the behavior of a result call on AppFutures
     '''
     # We call sim_mol_dyn with
-    sim_fut, data_futs = sim_mol_dyn(5, 0.5, outputs=['sim.out'],
-                                     stdout='stdout.txt', stderr='stderr.txt')
+    sim_fut = sim_mol_dyn(5, 0.5, outputs=['sim.out'],
+                          stdout='stdout.txt', stderr='stderr.txt')
+    data_futs = sim_fut.outputs
     print("Launching and waiting on data_futs")
     print("Done?   : ", sim_fut.done())
     print("Result? : ", sim_fut.result(timeout=1))
