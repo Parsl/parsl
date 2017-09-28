bb18bccbf1e479cedb39ccd643650a55b3717772
Yadu Nand B <yadudoc1729@gmail.com>
2017-09-28 11:41:26 -0500

    Adding README and setup script

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from parsl.version import VERSION
+
+install_requires = [
+    'ipyparallel',
+    'boto3',
+    'azure'
+    ]
+
+tests_require = [
+    'ipyparallel',
+    'mock>=1.0.0',
+    'nose',
+    'pytest'
+    ]
+
+setup(
+    name='libsubmit',
+    version=VERSION,
+    description='Uniform interface to clouds, clusters, grids and supercomputers.',
+    long_description='Submit, track and cancel arbitrary bash scripts on computate resources',
+    url='https://github.com/Parsl/libsubmit',
+    author='Yadu Nand Babuji',
+    author_email='yadu@uchicago.edu',
+    license='Apache 2.0',
+    download_url = 'https://github.com/Parsl/libsubmit/archive/master.zip',
+    package_data={'': ['LICENSE']},
+    packages=['libsubmit'],
+    install_requires=install_requires,
+    classifiers = [
+        # Maturity
+        'Development Status :: 3 - Alpha',
+        # Intended audience
+        'Intended Audience :: Developers',
+        # Licence, must match with licence above
+        'License :: OSI Approved :: Apache Software License',
+        # Python versions supported
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = ['Workflows', 'Scientific computing'],
+)
