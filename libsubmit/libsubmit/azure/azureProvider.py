ac2808f175f75772a44168b2211d7a3659a32878
Yadu Nand B <yadudoc1729@gmail.com>
2017-09-28 14:44:45 -0500

    Adding azure and local

---
+++ libsubmit/libsubmit/azure/azureProvider.py
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+import os
+import pprint
+import json
+import time
+import logging
+import atexit
+
+from azure.common.credentials import UserPassCredentials
+from libsubmit.execution_provider_base import ExecutionProvider
+from azureDeployer import Deployer
+
+translate_table = {'PD': 'PENDING',
+                   'R': 'RUNNING',
+                   'CA': 'CANCELLED',
+                   'CF': 'PENDING',  # (configuring),
+                   'CG': 'RUNNING',  # (completing),
+                   'CD': 'COMPLETED',
+                   'F': 'FAILED',  # (failed),
+                   'TO': 'TIMEOUT',  # (timeout),
+                   'NF': 'FAILED',  # (node failure),
+                   'RV': 'FAILED',  # (revoked) and
+                   'SE': 'FAILED'}  # (special exit state
+
+template_string = """
+cd ~
+sudo apt-get update -y
+sudo apt-get install -y python3 python3-pip ipython
+sudo pip3 install ipyparallel parsl
+"""
+
+
+class AzureProvider(ExecutionProvider):
+    def __init__(self, config):
+        """Initialize Azure provider. Uses Azure python sdk to provide execution resources"""
+        self.config = self.read_configs(config)
+        self.config_logger()
+        credentials = UserPassCredentials(
+            self.config['username'], self.config['pass'])
+        subscription_id = self.config['subscriptionId']
+
+        # self.resource_client = ResourceManagementClient(credentials, subscription_id)
+        # self.storage_client = StorageManagementClient(credentials, subscription_id)
+
+        self.resource_group_name = 'my_resource_group'
+        self.deployer = Deployer(
+            subscription_id,
+            self.resource_group_name,
+            self.read_configs(config))
+
+    def config_logger(self):
+        """Configure Logger"""
+        logger = logging.getLogger("AzureProvider")
+        logger.setLevel(logging.INFO)
+        if not os.path.isfile(self.config['logFile']):
+            with open(self.config['logFile'], 'w') as temp_log:
+                temp_log.write("Creating new log file.\n")
+        fh = logging.FileHandler(self.config['logFile'])
+        fh.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        self.logger = logger
+
+    def _read_conf(self, config_file):
+        """read config file"""
+        config = json.load(open(config_file, 'r'))
+        return config
+
+    def pretty_configs(self, configs):
+        """prettyprint config"""
+        printer = pprint.PrettyPrinter(indent=4)
+        printer.pprint(configs)
+
+    def read_configs(self, config_file):
+        """Read config file"""
+        config = self._read_conf(config_file)
+        return config
+
+    def ipyparallel_configuration(self):
+        config = ''
+        try:
+            with open(os.path.expanduser(self.config['iPyParallelConfigFile'])) as f:
+                config = f.read().strip()
+        except Exception as e:
+            self.logger.error(e)
+            self.logger.info(
+                "Couldn't find user ipyparallel config file. Trying default location.")
+            with open(os.path.expanduser("~/.ipython/profile_parallel/security/ipcontroller-engine.json")) as f:
+                config = f.read().strip()
+        else:
+            self.logger.error(
+                "Cannot find iPyParallel config file. Cannot proceed.")
+            return -1
+        ipptemplate = """
+cat <<EOF> ipengine.json
+{}
+EOF
+
+mkdir -p '.ipengine_logs'
+sleep 5
+ipengine --file=ipengine.json &> .ipengine_logs/ipengine.log""".format(config)
+        return ipptemplate
+
+    def submit(self):
+        """Uses AzureDeployer to spin up an instance and connect it to the ipyparallel controller"""
+        self.deployer.deploy()
+
+    def status(self):
+        """Get status of azure VM. Not implemented yet."""
+        raise NotImplemented
+
+    def cancel(self):
+        """Destroy an azure VM"""
+        self.deployer.destroy()
+
+    def scale_in(self):
+        raise NotImplemented
+
+    def scale_out(self):
+        raise NotImplemented
+
+
+if __name__ == '__main__':
+    config = "azureconf.json"
+    provider = AzureProvider(config)
+    provider.submit()
---
+++ libsubmit/libsubmit/local/local.py
@@ -0,0 +1,167 @@
+import os
+import logging
+import subprocess
+import math
+import time
+from string import Template
+from libsubmit.execution_provider_base import ExecutionProvider
+import libsubmit.error as ep_error
+
+logger = logging.getLogger(__name__)
+
+def execute_no_wait (cmd, walltime):
+    ''' Synchronously execute a commandline string on the shell.
+    Args:
+         - cmd (string) : Commandline string to execute
+         - walltime (int) : walltime in seconds, this is not really used now.
+
+    Returns:
+         A tuple of the following:
+         retcode : Return code from the execution, -1 on fail
+         stdout  : stdout string
+         stderr  : stderr string
+
+    Raises:
+         None.
+    '''
+    retcode = -1
+    stdout = None
+    stderr = None
+    try :
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        pid = proc.pid
+
+    except Exception as e:
+        print("Caught exception : {0}".format(e))
+        logger.warn("Execution of command [%s] failed due to \n %s ",  (cmd, e))
+
+    return pid, proc
+
+
+translate_table = { 'PD' :  'PENDING',
+                    'R'  :  'RUNNING',
+                    'CA' : 'CANCELLED',
+                    'CF' : 'PENDING', #(configuring),
+                    'CG' : 'RUNNING', # (completing),
+                    'CD' : 'COMPLETED',
+                    'F'  : 'FAILED', # (failed),
+                    'TO' : 'TIMEOUT', # (timeout),
+                    'NF' : 'FAILED', # (node failure),
+                    'RV' : 'FAILED', #(revoked) and
+                    'SE' : 'FAILED' } # (special exit state
+
+
+class Local(ExecutionProvider):
+    ''' Slurm Execution Provider
+
+    This provider uses sbatch to submit, squeue for status and scancel to cancel jobs.
+    '''
+
+    def __init__ (self, config):
+        ''' Initialize the Slurm class
+        Args:
+             - Config (dict): Dictionary with all the config options.
+        '''
+
+        self.config = config
+        self.sitename = config['site']
+        self.current_blocksize = 0
+
+        # Dictionary that keeps track of jobs, keyed on job_id
+        self.resources = {}
+
+    ###########################################################################################################
+    # Status
+    ###########################################################################################################
+    def status (self, job_ids):
+        '''  Get the status of a list of jobs identified by their ids.
+        Args:
+            - job_ids (List of ids) : List of identifiers for the jobs
+
+        Returns:
+            - List of status codes.
+
+        '''
+        for job_id in self.resources:
+            poll_code = self.resources[job_id]['proc'].poll()
+            if poll_code == None :
+                self.resources[job_id]['status'] = 'RUNNING'
+            elif poll_code == 0 and self.resources[job_id]['status'] != 'RUNNING':
+                self.resources[job_id]['status'] = 'COMPLETED'
+            elif poll_code < 0 and self.resources[job_id]['status'] != 'RUNNING' :
+                self.resources[job_id]['status'] = 'FAILED'
+
+        return [self.resources[jid]['proc'].poll() for jid in job_ids]
+
+
+    ###########################################################################################################
+    # Submit
+    ###########################################################################################################
+    def submit (self, cmd_string, blocksize, job_name="parsl.auto"):
+        ''' Submits the cmd_string onto an Local Resource Manager job of blocksize parallel elements.
+        Submit returns an ID that corresponds to the task that was just submitted.
+
+        If tasks_per_node <  1:
+             1/tasks_per_node is provisioned
+
+        If tasks_per_node == 1:
+             A single node is provisioned
+
+        If tasks_per_node >  1 :
+             tasks_per_node * blocksize number of nodes are provisioned.
+
+        Args:
+             - cmd_string  :(String) Commandline invocation to be made on the remote side.
+             - blocksize   :(float) - Not really used for local
+
+        Kwargs:
+             - job_name (String): Name for job, must be unique
+
+        Returns:
+             - None: At capacity, cannot provision more
+             - job_id: (string) Identifier for the job
+
+        '''
+
+        job_id, proc = execute_no_wait(cmd_string, 3)
+        logging.debug("Started pid:%s proc:%s ", job_id, proc)
+        self.resources[job_id] = {'job_id' : job_id,
+                                  'status' : 'RUNNING',
+                                  'blocksize' : blocksize,
+                                  'proc' : proc }
+
+        return job_id
+
+    ###########################################################################################################
+    # Cancel
+    ###########################################################################################################
+    def cancel(self, job_ids):
+        ''' Cancels the jobs specified by a list of job ids
+
+        Args:
+        job_ids : [<job_id> ...]
+
+        Returns :
+        [True/False...] : If the cancel operation fails the entire list will be False.
+        '''
+
+        for job in job_ids:
+            print("Killing : ", job)
+            self.resources[job]['proc'].kill()
+            self.resources[job]['status'] = 'CANCELLED'
+        rets = [True for i in job_ids]
+
+        return rets
+
+    @property
+    def scaling_enabled(self):
+        return True
+
+    @property
+    def current_capacity(self):
+        return len(self.resources)
+
+
+if __name__ == "__main__" :
+
+    print("None")
