5437f541414afcee43ebdff6352f8bb3bb918e88
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2017-09-29 16:19:09 -0500

    Updating mpi test

--- parsl/tests/test_scaling/midway_mpi/test_mpi.py
+++ parsl/tests/test_scaling/midway_mpi/test_mpi.py
@@ -37,7 +37,7 @@ import random
 print ("starting ipcontroller")
 
 rand_port =random.randint(55000, 59000)
-p = subprocess.Popen(['ipcontroller','--port={0}'.format(rand_port),'--ip=*'], stdout=None, stderr=None)
+p = subprocess.Popen(['ipcontroller', '--reuse', '--port={0}'.format(rand_port),'--ip=*'], stdout=None, stderr=None)
 
 epf = ExecProviderFactory()
 resource = epf.make(config)
@@ -61,8 +61,23 @@ def create_template(template_name, output_name, contents):
 def mpi_hello(ranks, inputs=[], outputs=[], stdout=None, stderr=None, mock=False ):
     cmd_line = "mpirun -n {0} mpi_hello"
 
+#################
+# App definitions
+#################
+@App('bash', dfk)
+def mpi_test(ranks, inputs=[], outputs=[], stdout=None, stderr=None, mock=False ):
+    cmd_line = '''module load amber/16+cuda-8.0
+    mpirun -n 6 mpi_hello
+    mpirun -np 6 pmemd.MPI -O -i config_files/min.in -o min.out -c prot.rst7 -p prot.parm7 -r min.rst7 -ref prot.rst7
+    '''
 
-x = mpi_hello(4, stdout="hello.out", stderr="hello.err")
+
+#x = mpi_hello(4, stdout="hello.out", stderr="hello.err")
+#print("Launched the mpi_hello app")
+#x.result()
+
+
+x = mpi_test(4, stdout="hello.out", stderr="hello.err")
 print("Launched the mpi_hello app")
 x.result()
 
