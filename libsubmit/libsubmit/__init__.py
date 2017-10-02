5e68d06a441473b3af5230a35dc96f2adb02a08c
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-02 16:55:01 -0500

    Updating package

---
+++ libsubmit/libsubmit/__init__.py
@@ -0,0 +1,19 @@
+'''
+Libsubmit
+=========
+
+Uniform interface to diverse and multi-lingual set of computational resources.
+
+'''
+import logging
+
+logger = logging.getLogger(__name__)
+
+from libsubmit import errors
+from libsubmit.slurm.slurm import Slurm
+from libsubmit.aws.aws import EC2Provider
+from libsubmit.azure.azureProvider import AzureProvider
+from libsubmit.jetstream.jetstream import Jetstream
+from libsubmit.midway.midway import Midway
+from libsubmit.local.local import Local
+
