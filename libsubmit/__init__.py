b700105a1a7b9c4280c52da154028ef3dde8f66a
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-05 15:29:02 -0500

    Fixing issue with version info missing from package init

--- libsubmit/__init__.py
+++ libsubmit/__init__.py
@@ -6,9 +6,9 @@ Uniform interface to diverse and multi-lingual set of computational resources.
 
 '''
 import logging
-
 logger = logging.getLogger(__name__)
 
+from libsubmit.version import VERSION
 from libsubmit.error import *
 from libsubmit.slurm.slurm import Slurm
 from libsubmit.aws.aws import EC2Provider
@@ -17,3 +17,67 @@ from libsubmit.jetstream.jetstream import Jetstream
 from libsubmit.midway.midway import Midway
 from libsubmit.local.local import Local
 
+__author__ = 'Yadu Nand Babuji'
+__version__ = VERSION
+
+__all__ = ['Slurm', 'EC2Provider', 'AzureProvider', 'Jetstream', 'Midway', 'Local']
+
+def set_stream_logger(name='libsubmit', level=logging.DEBUG, format_string=None):
+    '''
+    Add a stream log handler
+
+    Args:
+         - name (string) : Set the logger name.
+         - level (logging.LEVEL) : Set to logging.DEBUG by default.
+         - format_string (sting) : Set to None by default.
+
+    Returns:
+         - None
+    '''
+
+    if format_string is None:
+        format_string = "%(asctime)s %(name)s [%(levelname)s]  %(message)s"
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+def set_file_logger(filename, name='libsubmit', level=logging.DEBUG, format_string=None):
+    ''' Add a stream log handler
+
+    Args:
+        - filename (string): Name of the file to write logs to
+        - name (string): Logger name
+        - level (logging.LEVEL): Set the logging level.
+        - format_string (string): Set the format string
+
+    Returns:
+       -  None
+    '''
+
+    if format_string is None:
+        format_string = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.FileHandler(filename)
+    handler.setLevel(level)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+class NullHandler(logging.Handler):
+    ''' Setup default logging to /dev/null since this is library.
+
+    '''
+
+    def emit(self, record):
+        pass
+
+
+logging.getLogger('libsubmit').addHandler(NullHandler())
