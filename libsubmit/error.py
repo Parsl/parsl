495c653a31b68516938ac44cd361f8536811f5a9
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-04 20:44:31 -0500

    Adding new exception to describe failure to load provider due to missing python modules

--- libsubmit/error.py
+++ libsubmit/error.py
@@ -27,3 +27,14 @@ class ScriptPathError(ExecutionProviderExceptions):
         return "Unable to write submit script:{0} Reason:{1}".format(self.script_path,
                                                                      self.reason)
 
+class OptionalModuleMissing(ExecutionProviderExceptions):
+    ''' Error raised a required module is missing for a optional/extra provider
+    '''
+    def __init__ (self, module_names, reason):
+        self.module_names = module_names
+        self.reason = reason
+
+    def __repr__ (self):
+        return "Unable to Initialize provider.Missing:{0},  Reason:{1}".format(self.module_names,
+                                                                               self.reason)
+
