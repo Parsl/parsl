d617db5cca431ba34208ebd67a5154e24f68893c
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2017-10-05 15:41:03 -0500

    Relocating shared code

--- libsubmit/slurm/slurm.py
+++ libsubmit/slurm/slurm.py
@@ -6,41 +6,11 @@ import time
 from string import Template
 from libsubmit.execution_provider_base import ExecutionProvider
 from libsubmit.slurm.template import template_string
+from libsubmit.exec_utils import execute_wait
+
 import libsubmit.error as ep_error
 
 logger = logging.getLogger(__name__)
-
-def execute_wait (cmd, walltime):
-    ''' Synchronously execute a commandline string on the shell.
-    Args:
-         - cmd (string) : Commandline string to execute
-         - walltime (int) : walltime in seconds, this is not really used now.
-
-    Returns:
-         A tuple of the following:
-         retcode : Return code from the execution, -1 on fail
-         stdout  : stdout string
-         stderr  : stderr string
-
-    Raises:
-         None.
-    '''
-    retcode = -1
-    stdout = None
-    stderr = None
-    try :
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        proc.wait(timeout=walltime)
-        stdout = proc.stdout.read()
-        stderr = proc.stderr.read()
-        retcode = proc.returncode
-
-    except Exception as e:
-        print("Caught exception : {0}".format(e))
-        logger.warn("Execution of command [%s] failed due to \n %s ",  (cmd, e))
-
-    return (retcode, stdout.decode("utf-8"), stderr.decode("utf-8"))
-
 
 translate_table = { 'PD' :  'PENDING',
                     'R'  :  'RUNNING',
