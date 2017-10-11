fd4620e19e0ebcd5d738c1f72741d3fd2da9c016
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-11 17:30:04 -0500

    Updating channel base to include execute_no_wait and file push

--- libsubmit/libsubmit/channels/channel_base.py
+++ libsubmit/libsubmit/channels/channel_base.py
@@ -6,13 +6,47 @@ class Channel (metaclass=ABCMeta):
     .. code:: python
 
          execute_wait...
-    
+
     """
     @abstractmethod
     def execute_wait(self, cmd, walltime, *args, **kwargs):
         '''
+        Args:
+            - cmd (string): Command string to execute over the channel
+            - walltime (int) : Timeout in seconds
+
+        Returns:
+            - (exit_code, stdout, stderr) (int, string, string)
         '''
         pass
+
+    @abstractmethod
+    def execute_no_wait(self, cmd, walltime, *args, **kwargs):
+        ''' Optional. THis is infrequently used.
+
+        Args:
+            - cmd (string): Command string to execute over the channel
+            - walltime (int) : Timeout in seconds
+
+        Returns:
+            - (exit_code(None), stdout, stderr) (int, io_thing, io_thing)
+        '''
+        pass
+
+    @abstractmethod
+    def push_file(self, source, dest_dir):
+        ''' Channel will take care of moving the file from source to the destination
+        directory
+
+        Args:
+            source (string) : Full filepath of the file to be moved
+            dest_dir (string) : Absolute path of the directory to move to
+
+        Returns:
+            status (Bool) : True if the transfer succeeded.
+        '''
+        pass
+
 
     @abstractmethod
     def close(self):
