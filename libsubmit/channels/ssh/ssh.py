330dfc4bad74ed24b3a0e9d7f7c355e6bd05213a
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-12 17:10:56 -0500

    Adding support for channel.script_dir property, and fixed paramiko exception handling

--- libsubmit/channels/ssh/ssh.py
+++ libsubmit/channels/ssh/ssh.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import paramiko
+import getpass
 
 from libsubmit.channels.channel_base import Channel
 from libsubmit.channels.errors import *
@@ -17,9 +18,19 @@ class SshChannel ():
 
     '''
 
-    def __init__ (self, hostname, username=None, password=None, **kwargs):
+    def __init__ (self, hostname, username=None, password=None,
+                  channel_script_dir=None, **kwargs):
         ''' Initialize a persistent connection to the remote system.
         We should know at this point whether ssh connectivity is possible
+
+        Args:
+            - hostname (String) : Hostname
+
+        KWargs:
+            - username (string) : Username on remote system
+            - password (string) : Password for remote system
+            - channel_script_dir (string) : Full path to a script dir where
+              generated scripts could be sent to.
 
         Raises:
         '''
@@ -32,6 +43,12 @@ class SshChannel ():
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.load_system_host_keys()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if channel_script_dir:
+            self.channel_script_dir = channel_script_dir
+        else:
+            self.channel_script_dir = "/tmp/{0}/scripts/".format(getpass.getuser())
+
         try :
             self.ssh_client.connect(hostname,
                                     username=username,
@@ -41,11 +58,21 @@ class SshChannel ():
             self.sftp_client = paramiko.SFTPClient.from_transport(t)
 
         except paramiko.BadHostKeyException as e:
-            raise BadHostKeyException(e)
+            raise BadHostKeyException(e, self.hostname)
 
         except paramiko.AuthenticationException as e:
-            raise AuthException(e)
-        #self.sftp_client = paramiko.SFTPClient.from_transport(self.transport)
+            raise AuthException(e, self.hostname)
+
+        except paramiko.SSHException as e:
+            raise SSHException(e, self.hostname)
+
+        except Exception as e:
+            raise SSHException(e, self.hostname)
+
+
+    @property
+    def script_dir(self):
+        return self.channel_script_dir
 
     def execute_wait(self, cmd, walltime=2, envs={}):
 
