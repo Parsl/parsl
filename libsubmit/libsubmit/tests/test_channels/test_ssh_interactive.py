adc938833ea19c26580296aa8a8c90f02607875f
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-13 14:02:12 -0500

    Adding test for ssh_interactive_logon

---
+++ libsubmit/libsubmit/tests/test_channels/test_ssh_interactive.py
@@ -0,0 +1,22 @@
+import libsubmit
+from libsubmit.channels.ssh_il.ssh_il import SshILChannel as Ssh
+
+def connect_and_list(hostname, username):
+    conn = Ssh(hostname, username=username)
+    ec, out, err = conn.execute_wait("echo $HOSTNAME")
+    conn.close()
+    return out
+
+
+def test_cooley ():
+    ''' Test ssh channels to midway
+    '''
+    url = 'cooley.alcf.anl.gov'
+    uname = 'yadunand'
+    out = connect_and_list(url, uname)
+    print("Sitename :{0}  hostname:{1}".format(url, out))
+    return
+
+if __name__ == "__main__":
+    libsubmit.set_stream_logger()
+    test_cooley()
