699e880e2afb59c50d3d1b5ef65cd6b953a17e3b
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-15 17:10:46 -0500

    Adding a new test

---
+++ libsubmit/tests/test_channels/test_channels.py
@@ -0,0 +1,21 @@
+from libsubmit.channels.ssh.ssh import SshChannel
+from libsubmit.channels.local.local import LocalChannel
+import os
+
+def test_local ():
+
+    channel = LocalChannel(None, None)
+
+    ec, out, err = channel.execute_wait('echo "pwd: $PWD"', 2)
+
+    assert ec == 0, "Channel execute failed"
+    print("Stdout: ", out)
+    print("Stderr: ", err)
+
+
+
+
+
+if __name__ == "__main__" :
+
+    test_local()
