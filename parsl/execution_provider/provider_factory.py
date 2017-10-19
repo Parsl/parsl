b5318c4a3786d9c1ecefa21d7b7708b3e93c7bdb
Yadu Nand B <yadudoc1729@gmail.com>
2017-10-19 00:30:07 -0500

    Support for configs and tests

--- parsl/execution_provider/provider_factory.py
+++ parsl/execution_provider/provider_factory.py
@@ -5,6 +5,7 @@ Centralize creation of execution providers and executors.
 '''
 
 import logging
+import libsubmit
 
 logger = logging.getLogger(__name__)
 
@@ -12,14 +13,12 @@ logger = logging.getLogger(__name__)
 from parsl.executors.ipp import IPyParallelExecutor
 from parsl.executors.swift_t import TurbineExecutor
 from parsl.executors.threads import ThreadPoolExecutor
+from parsl.execution_provider.errors import *
 
-# Execution Providers
-from parsl.execution_provider.slurm.slurm import Slurm
-from parsl.execution_provider.aws.aws import EC2Provider
-from parsl.execution_provider.local.local import Local
+# Execution Providers and channels
+from libsubmit import *
 
-# Channels
-from parsl.channels.ssh_cl import SshClClient
+
 
 class ExecProviderFactory (object):
 
@@ -36,39 +35,79 @@ class ExecProviderFactory (object):
 
         self.executors = { 'ipp' : IPyParallelExecutor,
                            'swift_t' : TurbineExecutor,
-                           'threads' : ThreadPoolExecutor }
+                           'threads' : ThreadPoolExecutor,
+                           None : lambda *args, **kwargs : None }
 
         self.execution_providers = { 'slurm' : Slurm,
                                      'local' : Local,
-                                     'aws' : EC2Provider }
+                                     'aws' : EC2Provider,
+                                     None : lambda *args, **kwargs : None }
 
-        self.channels = { 'ssh-cl' : SshClClient,
-                          'local' : None }
+        self.channels = { 'ssh' : SshChannel,
+                          'ssh-il' : SshILChannel,
+                          'local' : LocalChannel,
+                          None : lambda *args, **kwargs : None }
 
+
+    def validate_config (self, config):
+        ''' Validate_config validates config
+        There is no logic implemented here yet.
+        This might be a good first task for a new dev.
+
+        Args:
+             - config (dict) : Config data structure
+        Returns:
+             - Bool: validity of config
+        '''
+        return True
 
     def make (self, config):
         ''' Construct the appropriate provider, executors and channels and link them together.
         '''
-        sitename = config['site']
-        provider = config['execution']['provider']
-        executor = config['execution']['executor']
-        channel = config['execution']['channel']
 
-        if provider not in self.execution_providers:
-            logger.debug("[ERROR] %s is not a known execution_provider", provider)
-            exit(-1)
-        if executor not in self.executors:
-            logger.debug("[ERROR] %s is not a known executor", executor)
-            exit(-1)
-        if channel not in self.channels:
-            logger.debug("[ERROR] %s is not a known channel", channel)
+        sites = {}
+        for site in config.get("sites"):
+            channel_name = site["auth"]["channel"]
 
-        print("Foo")
-        ep = self.execution_providers[provider](config)
+            if channel_name in self.channels:
+                channel_opts = site["auth"].copy()
+                if "channel" in channel_opts:
+                    del channel_opts["channel"]
+                channel = self.channels[channel_name](**channel_opts)
 
-        executor = self.executors[executor](execution_provider=ep, config=config)
-        return executor
+            else:
+                logger.error("Site:{0} requests an invalid channel:{0}".format(site["site"],
+                                                                               channel_name))
+                raise BadConfig(site["site"],
+                                "invalid channel:{0} requested".format(channel_name))
 
+            logger.debug("Created channel : {0}".format(channel))
 
+            provider_name = site["execution"]["provider"]
+            if provider_name in self.execution_providers:
+                provider = self.execution_providers[provider_name](site,
+                                                                   channel=channel)
 
+            else:
+                logger.error("Site:{0} requests an invalid provider:{0}".format(site["site"],
+                                                                               provider_name))
+                raise BadConfig(site["site"],
+                                "invalid provider:{0} requested".format(provider_name))
 
+            logger.debug("Created execution_provider : {0}".format(provider))
+
+            executor_name = site["execution"]["executor"]
+            if executor_name in self.executors:
+                executor = self.executors[executor_name](execution_provider=provider, config=site)
+
+            else:
+                logger.error("Site:{0} requests an invalid executor:{0}".format(site["site"],
+                                                                               executor_name))
+                raise BadConfig(site["site"],
+                                "invalid executor:{0} requested".format(executor_name))
+
+            logger.debug("Created executor : {0}".format(executor))
+            
+            sites[site["site"]] = executor
+
+        return sites
