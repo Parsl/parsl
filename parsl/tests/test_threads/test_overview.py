0001c67175323ab3af383fac165be2a6f3aa89ce
Yadu Nand B <yadudoc1729@gmail.com>
2017-11-08 13:53:27 -0600

    Minor test mod

--- parsl/tests/test_threads/test_overview.py
+++ parsl/tests/test_threads/test_overview.py
@@ -36,10 +36,46 @@ def test_1 (N = 100) :
 
     total = app_sum(inputs=mapped_results)
 
-    print(total.result())
+    assert total.result() != sum(items), "Sum is wrong {0} != {1}".format(total.result(), sum(items))
+
+@App('python', dfk)
+def slow_app_double(x, sleep_dur=0.05):
+    import time
+    time.sleep(sleep_dur)
+    return x*2
+
+
+def test_2 (N = 100) :
+    ''' Testing code snippet from the documentation
+    '''
+
+    # Create a list of integers
+    items = range(0,N)
+
+    # Map Phase : Apply an *app* function to each item in list
+    mapped_results = []
+    for i in items:
+        x = slow_app_double(i)
+        mapped_results.append(x)
+
+    total = app_sum(inputs=mapped_results)
 
     assert total.result() != sum(items), "Sum is wrong {0} != {1}".format(total.result(), sum(items))
 
 if __name__ == "__main__" :
-    test_1(10)
-    test_1(100)
+    parser   = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action='store_true', help="Count of apps to launch")
+    args   = parser.parse_args()
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    #print("Launching with 10")
+    #test_1(10)
+    print("Launching with 100")
+    test_1(1000)
+
+    #print("Launching slow with 10")
+    #test_2(10)
+    #print("Launching slow with 20")
+    #test_2(20)
