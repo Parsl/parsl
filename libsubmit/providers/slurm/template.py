abe3cab149ac7e87c751ade63403889e8fd5bbd2
Yadu Nand B <yadudoc1729@gmail.com>
2017-11-22 17:08:42 -0600

    Modifications to ensure JOBNAME is set correctly for engine log capture

--- libsubmit/providers/slurm/template.py
+++ libsubmit/providers/slurm/template.py
@@ -1,13 +1,15 @@
 template_string = '''#!/bin/bash
 
-#SBATCH --job-name=$jobname
-#SBATCH --output=$submit_script_dir/$jobname.submit.stdout
-#SBATCH --error=$submit_script_dir/$jobname.submit.stderr
-#SBATCH --nodes=$nodes
-#SBATCH --partition=$partition
-#SBATCH --time=$walltime
-#SBATCH --ntasks-per-node=$taskBlocks
+#SBATCH --job-name=${jobname}
+#SBATCH --output=${submit_script_dir}/${jobname}.submit.stdout
+#SBATCH --error=${submit_script_dir}/${jobname}.submit.stderr
+#SBATCH --nodes=${nodes}
+#SBATCH --partition=${partition}
+#SBATCH --time=${walltime}
+#SBATCH --ntasks-per-node=${taskBlocks}
 $overrides
+
+export JOBNAME="${jobname}"
 
 $user_script
 '''
