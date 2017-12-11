672a888ca4678f6861065714f519ad7a64feb501
Yadu Nand B <yadudoc1729@gmail.com>
2017-12-11 11:00:00 -0600

    Fixing issue with test init

--- parsl/tests/test_ipp/__init__.py
+++ parsl/tests/test_ipp/__init__.py
@@ -2,7 +2,7 @@
 def setup_package():
     import subprocess
     import time
-    proc = subprocess.Popen("ipcluster start -n 4")
+    proc = subprocess.Popen(["ipcluster", "start", "-n", "4"])
     time.sleep(2)
     print("Started ipcluster with pid:{0}".format(proc))
     return proc
@@ -10,7 +10,7 @@ def setup_package():
 def teardown_package():
     import subprocess
     import time
-    proc = subprocess.Popen("ipcluster stop")
+    proc = subprocess.Popen(["ipcluster", "stop"])
     print("Stopping ipcluster")
     time.sleep(2)
     return proc
