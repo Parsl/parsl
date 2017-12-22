4094d25604209e3585953799124ab0e7cb3437ce
Yadu Nand B <yadudoc1729@gmail.com>
2017-12-22 13:05:33 -0600

    Updating libsubmit version requirements. Removing old tests. Updating midway test to use /scratch/midway2/ env paths

--- parsl/tests/test_scaling/midway/test_midway_ipp.ssh.slurm.py
+++ parsl/tests/test_scaling/midway/test_midway_ipp.ssh.slurm.py
@@ -2,22 +2,23 @@ from parsl import *
 import parsl
 import libsubmit
 
-#parsl.set_stream_logger()
+parsl.set_stream_logger()
 print(parsl.__version__)
 print(libsubmit.__version__)
 
+MIDWAY_USERNAME = "yadunand"
 config = {
     "sites" : [
         { "site" : "Local_IPP",
           "auth" : {
               "channel" : "ssh",
               "hostname" : "swift.rcc.uchicago.edu",
-              "username" : "yadunand",
-              "scriptDir" : "/scratch/midway/yadunand/parsl_scripts"
+              "username" : MIDWAY_USERNAME,
+              "scriptDir" : "/scratch/midway2/{0}/parsl_scripts".format(MIDWAY_USERNAME)
           },
           "execution" : {
               "executor" : "ipp",
-              "provider" : "slurm",  # LIKELY SHOULD BE BOUND TO SITE
+              "provider" : "slurm",
               "script_dir" : ".scripts",
               "block" : { # Definition of a block
                   "nodes" : 1,            # of nodes in that block
@@ -29,15 +30,14 @@ config = {
                   "scriptDir" : ".",
                   "options" : {
                       "partition" : "westmere",
-                      "overrides" : '''module load python/3.5.2+gcc-4.8; source /scratch/midway/yadunand/parsl_env_3.5.2_gcc/bin/activate'''
+                      "overrides" : '''module load python/3.5.2+gcc-4.8; source /scratch/midway2/yadunand/parsl_env_3.5.2_gcc/bin/activate'''
                   }
               }
           }
         }
         ],
     "globals" : {   "lazyErrors" : True },
-    #"controller" : { "publicIp" : '128.135.250.229' }
-    "controller" : { "publicIp" : '*' }
+    #"controller" : { "publicIp" : '*' }
 }
 
 dfk = DataFlowKernel(config=config)
@@ -76,4 +76,4 @@ def test_bash():
 if __name__ == "__main__" :
 
     test_python()
-    test_bash()
+    #test_bash()
