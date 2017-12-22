8cedaa96dea401ea7e051041d9797e614d6eb1ca
Yadu Nand B <yadudoc1729@gmail.com>
2017-12-22 15:41:23 -0600

    Updated OSG tests

--- parsl/tests/test_scaling/osg/test_osg_ipp.ssh.condor.py
+++ parsl/tests/test_scaling/osg/test_osg_ipp.ssh.condor.py
@@ -3,46 +3,10 @@ import parsl
 import libsubmit
 #libsubmit.set_stream_logger()
 #parsl.set_stream_logger()
+import os
 
-config = {
-    "sites" : [
-        { "site" : "OSG_Remote",
-          "auth" : {
-              "channel" : "ssh",
-              "hostname" : "login.osgconnect.net",
-              "username" : "yadunand",
-              "scriptDir" : "/home/yadunand/parsl_scripts/"
-              #"scriptDir" : "/tmp/parsl_scripts/"
-          },
-          "execution" : {
-              "executor" : "ipp",
-              "provider" : "condor",  # LIKELY SHOULD BE BOUND TO SITE
-              "scriptDir" : ".scripts",
-              "block" : { # Definition of a block
-                  "nodes" : 1,            # of nodes in that block
-                  "taskBlocks" : 1,       # total tasks in a block
-                  "walltime" : "00:05:00",
-                  "initBlocks" : 4,
-                  "minBlocks" : 0,
-                  "maxBlocks" : 1,
-                  "scriptDir" : ".",
-                  "options" : {
-                      "partition" : "debug",
-                      #"overrides" : "requirements = (HAS_CVMFS_oasis_opensciencegrid_org =?= TRUE)",
-                      "overrides" : 'Requirements = OSGVO_OS_STRING == "RHEL 6" && Arch == "X86_64" &&  HAS_MODULES == True',
-                      "workerSetup" : """module load python/3.5.2;
-python3 -m venv parsl_env;
-source parsl_env/bin/activate;
-pip3 install ipyparallel"""
-                  }
-              }
-          }
-        }
-    ],
-    "globals" : {"lazyErrors" : True },
-    "controller" : { "publicIp" : "*" }
-}
-
+os.environ["OSG_USERNAME"] = "yadunand"
+from osg import multiNode as config
 dfk = DataFlowKernel(config=config)
 
 @App("python", dfk)
