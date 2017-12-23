fc98f196b2b9297366760771d9469cec8bac4d31
Yadu Nand B <yadudoc1729@gmail.com>
2017-12-22 23:31:07 -0600

    Updated cooley test

--- parsl/tests/test_scaling/cooley/test_cooley_ipp.ssh-il.cobalt.py
+++ parsl/tests/test_scaling/cooley/test_cooley_ipp.ssh-il.cobalt.py
@@ -1,42 +1,11 @@
 from parsl import *
 import parsl
 import libsubmit
+import os
 
-print("WARNING: Make sure the username and remote dirs are setup")
-
-config = {
-    "sites": [{
-        "site": "ALCF_Cooley_SSH_Remote",
-        "auth" : {
-            "channel" : "ssh-il",
-            "hostname" : "cooleylogin1.alcf.anl.gov",
-            "username" : "yadunand",
-            "scriptDir" : "/home/yadunand/parsl_scripts/"
-        },
-        "execution":  {
-            "executor": "ipp",
-            "provider": "cobalt",
-            "scriptDir": ".scripts",
-            "block": {
-                "nodes": 1,
-                "walltime": "00:20:00",
-                "initBlocks": 1,
-                "maxBlocks": 1,
-                "minBlocks": 0,
-                "taskBlocks": "$CORES",
-                "scriptDir": ".",
-                "options": {
-                    "overrides": "soft add anaconda; source activate /home/yadunand/.conda/envs/parsl_env_3.5"
-                },
-            },
-        },
-    }],
-    "controller": {"publicIp": "*"},
-    "globals": {"lazyErrors": True},
-}
-
+os.environ["COOLEY_USERNAME"] = "yadunand"
+from cooley import singleNode as config
 dfk = DataFlowKernel(config=config)
-
 
 @App("python", dfk)
 def test():
