ff0994e23828ed1b77fcf38efd6d8015f1e11d12
Yadu Nand B <yadudoc1729@gmail.com>
2018-01-05 13:26:45 -0600

    Updating midway config

--- parsl/tests/test_scaling/configs/midway.py
+++ parsl/tests/test_scaling/configs/midway.py
@@ -3,7 +3,7 @@ USERNAME = os.environ['MIDWAY_USERNAME']
 
 singleNode = {
     "sites" : [
-        { "site" : "Local_IPP",
+        { "site" : "Midway_Remote_IPP",
           "auth" : {
               "channel" : "ssh",
               "hostname" : "swift.rcc.uchicago.edu",
@@ -29,9 +29,36 @@ singleNode = {
     "globals" : {   "lazyErrors" : True }
 }
 
+singleNodeLocal = {
+    "sites" : [
+        { "site" : "Midway_Local_IPP",
+          "auth" : {
+              "channel" : "local",
+              "scriptDir" : "/scratch/midway2/{0}/parsl_scripts".format(USERNAME)
+          },
+          "execution" : {
+              "executor" : "ipp",
+              "provider" : "slurm",
+              "block" : { # Definition of a block
+                  "nodes" : 1,            # of nodes in that block
+                  "taskBlocks" : 1,       # total tasks in a block
+                  "initBlocks" : 1,
+                  "maxBlocks" : 1,
+                  "options" : {
+                      "partition" : "westmere",
+                      "overrides" : '''module load python/3.5.2+gcc-4.8; source /scratch/midway2/yadunand/parsl_env_3.5.2_gcc/bin/activate'''
+                  }
+              }
+          }
+        }
+        ],
+    "globals" : {   "lazyErrors" : True }
+}
+
+
 multiCore = {
     "sites" : [
-        { "site" : "Local_IPP",
+        { "site" : "Midway_N_Workers_IPP",
           "auth" : {
               "channel" : "ssh",
               "hostname" : "swift.rcc.uchicago.edu",
@@ -60,7 +87,7 @@ multiCore = {
 
 multiNode = {
     "sites" : [
-        { "site" : "Local_IPP",
+        { "site" : "Midway_MultiNode",
           "auth" : {
               "channel" : "ssh",
               "hostname" : "swift.rcc.uchicago.edu",
