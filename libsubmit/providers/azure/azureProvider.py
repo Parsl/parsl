c3812cea43887ce1b0cca53d46b11b2f8a68dffa
Benjamin Glick <glick@lclark.edu>
2018-01-09 16:07:31 -0600

    Fix travis build failing

--- libsubmit/providers/azure/azureProvider.py
+++ libsubmit/providers/azure/azureProvider.py
@@ -37,118 +37,6 @@ sudo pip3 install ipyparallel parsl
 
 
 class AzureProvider(ExecutionProvider):
-     '''
-    Here's a sample config for the Azure provider:
-
-    .. code-block:: python
-
-         { "auth" : { # Definition of authentication method for AWS. One of 3 methods are required to authenticate
-                      # with AWS : keyfile, profile or env_variables. If keyfile or profile is not set Boto3 will
-                      # look for the following env variables :
-                      # AWS_ACCESS_KEY_ID : The access key for your AWS account.
-                      # AWS_SECRET_ACCESS_KEY : The secret key for your AWS account.
-                      # AWS_SESSION_TOKEN : The session key for your AWS account.
-
-              #{Description: Path to json file that contains 'AWSAccessKeyId' and 'AWSSecretKey'
-              "keyfile"    :
-                             # Type : String,
-                             # Required : False},
-
-              #{Description: Specify the profile to be used from the standard aws config file
-              "profile"    :
-                             # ~/.aws/config.
-                             # Type : String,
-                             # Expected : "default", # Use the 'default' aws profile
-                             # Required : False},
-
-            },
-
-           "execution" : { # Definition of all execution aspects of a site
-
-              "executor"   : #{Description: Define the executor used as task executor,
-                             # Type : String,
-                             # Expected : "ipp",
-                             # Required : True},
-
-              "provider"   : #{Description : The provider name, in this case ec2
-                             # Type : String,
-                             # Expected : "aws",
-                             # Required :  True },
-
-              "block" : { # Definition of a block
-
-                  "nodes"      : #{Description : # of nodes to provision per block
-                                 # Type : Integer,
-                                 # Default: 1},
-
-                  "taskBlocks" : #{Description : # of workers to launch per block
-                                 # as either an number or as a bash expression.
-                                 # for eg, "1" , "$(($CORES / 2))"
-                                 # Type : String,
-                                 #  Default: "1" },
-
-                  "walltime"  :  #{Description : Walltime requested per block in HH:MM:SS
-                                 # Type : String,
-                                 # Default : "00:20:00" },
-
-                  "initBlocks" : #{Description : # of blocks to provision at the start of
-                                 # the DFK
-                                 # Type : Integer
-                                 # Default : ?
-                                 # Required :    },
-
-                  "minBlocks" :  #{Description : Minimum # of blocks outstanding at any time
-                                 # WARNING :: Not Implemented
-                                 # Type : Integer
-                                 # Default : 0 },
-
-                  "maxBlocks" :  #{Description : Maximum # Of blocks outstanding at any time
-                                 # WARNING :: Not Implemented
-                                 # Type : Integer
-                                 # Default : ? },
-
-                  "options"   : {  # Scheduler specific options
-
-
-                      #{Description : Instance type t2.small|t2...
-                      "instanceType" :
-                                       # Type : String,
-                                       # Required : False
-                                       # Default : t2.small },
-
-                      #{"Description : String to append to the #SBATCH blocks
-                      "imageId"      :
-                                       # in the submit script to the scheduler
-                                       # Type : String,
-                                       # Required : False },
-
-                      "region"       : #{"Description : AWS region to launch machines in
-                                       # in the submit script to the scheduler
-                                       # Type : String,
-                                       # Default : 'us-east-2',
-                                       # Required : False },
-
-                      #{"Description : Name of the AWS private key (.pem file)
-                      "keyName"      :
-                                       # that is usually generated on the console to allow ssh access
-                                       # to the EC2 instances, mostly for debugging.
-                                       # in the submit script to the scheduler
-                                       # Type : String,
-                                       # Required : True },
-
-                      #{"Description : If requesting spot market machines, specify
-                      "spotMaxBid"   :
-                                       # the max Bid price.
-                                       # Type : Float,
-                                       # Required : False },
-                  }
-              }
-            }
-         }
-    '''
-    def __repr__(self):
-        return "<Azure Execution Provider for site:{0}>".format(self.sitename)
- 
     def __init__(self, config):
         """Initialize Azure provider. Uses Azure python SDK to provide execution resources"""
         self.config = self.read_configs(config)
@@ -171,7 +59,6 @@ class AzureProvider(ExecutionProvider):
             self.resource_group_name,
             self.read_configs(config))
 
-        
         self.channel = channel
         if not _boto_enabled:
             raise OptionalModuleMissing(
@@ -210,6 +97,9 @@ class AzureProvider(ExecutionProvider):
             logger.info(
                 "No State File. Cannot load previous options. Creating new infrastructure")
             self.write_state_file()
+
+    def __repr__(self):
+        return "<Azure Execution Provider for site:{0}>".format(self.sitename)
 
     @property
     def channels_required(self):
