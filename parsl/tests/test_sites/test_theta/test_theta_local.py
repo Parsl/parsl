d4d4ca29ee8989dd7657d817960a49557b64f37c
Yadu Nand B <yadudoc1729@gmail.com>
2018-01-25 11:15:58 -0600

    Reorganizing theta tests, with minor improvement to test case.
    Theta tests were renamed with test_ prefix. Test now reports
    number of unique nodes the apps ran on as an indirect test
    for aprun launches.

--- parsl/tests/test_sites/test_theta/test_theta_local.py
+++ parsl/tests/test_sites/test_theta/test_theta_local.py
@@ -8,20 +8,24 @@ from theta import multiNode as config
 dfk = DataFlowKernel(config=config)
 
 @App("python", dfk)
-def test(dur=1):
+def platform_info(dur=1):
     import platform
     import time
     time.sleep(dur)
     return "Hello from {0}".format(platform.uname())
 
 
-def test_all (dur = 1):
+def test_all (N=20, dur=1):
+    ''' Testing local:cobalt:aprun, with checks on number of nodes
+    '''
     results = {}
     for i in range(0,5):
-        results[i] = test(dur=dur)
+        results[i] = platform_info(dur=dur)
 
     print("Waiting ....")
-    print(results[0].result())
+    uniq_set = set([results[rid].result() for rid in results])
+    print("Unique nodes : ", len(uniq_set))
+    
 
 
 if __name__ == "__main__": 
