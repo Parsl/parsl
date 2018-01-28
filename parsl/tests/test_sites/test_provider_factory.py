074d016359d5fa7f353831bff64fefb609c9dbc1
Yadu Nand B <yadudoc1729@gmail.com>
2018-01-28 15:01:32 -0600

    PEP8 conformance fixes. Fixing E122. #72

--- parsl/tests/test_sites/test_provider_factory.py
+++ parsl/tests/test_sites/test_provider_factory.py
@@ -10,28 +10,28 @@ def test_factory_1():
 
     config = {
         "sites": [
-        {"site": "RCC_Midway_Remote",
-          "auth": {
-              "channel": "ssh",
-              "hostname": "swift.rcc.uchicago.edu",
-              "username": "yadunand"
-          },
-          "execution": {
-              "executor": "ipp",
-              "provider": "slurm",  # LIKELY SHOULD BE BOUND TO SITE
-              "scriptDir": ".scripts",
-              "block": { # Definition of a block
-                  "nodes": 1,            # of nodes in that block
-                  "taskBlocks": 1,        # total tasks in a block
-                  "walltime": "00:05:00",
-                  "Options": {
-                      "partition": "debug",
-                      "account": "pi-wilde",
-                      "overrides": "#SBATCH--constraint=haswell"
-                  }
-              }
-          }
-        }
+            {"site": "RCC_Midway_Remote",
+             "auth": {
+                 "channel": "ssh",
+                 "hostname": "swift.rcc.uchicago.edu",
+                 "username": "yadunand"
+             },
+             "execution": {
+                 "executor": "ipp",
+                 "provider": "slurm",  # LIKELY SHOULD BE BOUND TO SITE
+                 "scriptDir": ".scripts",
+                 "block": { # Definition of a block
+                     "nodes": 1,            # of nodes in that block
+                     "taskBlocks": 1,        # total tasks in a block
+                     "walltime": "00:05:00",
+                     "Options": {
+                         "partition": "debug",
+                         "account": "pi-wilde",
+                         "overrides": "#SBATCH--constraint=haswell"
+                     }
+                 }
+             }
+            }
         ],
         "globals": {
             "lazyErrors": True
@@ -63,22 +63,22 @@ def test_factory_3():
 
     config = {
         "sites": [
-        {"site": "Local",
-          "auth": {
-              "channel": None
-          },
-          "execution": {
-              "executor": "threads",
-              "provider": None,  # LIKELY SHOULD BE BOUND TO SITE
-              "maxThreads": 4
-          }
-        }
+            {"site": "Local",
+             "auth": {
+                 "channel": None
+             },
+             "execution": {
+                 "executor": "threads",
+                 "provider": None,  # LIKELY SHOULD BE BOUND TO SITE
+                 "maxThreads": 4
+             }
+            }
         ],
         "globals": {
             "lazyErrors": True
         }
     }
-
+    
     epf = ExecProviderFactory()
     executor = epf.make(config)
 
