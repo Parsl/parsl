67f89c767db4f687be10ca5422db5f4b3e0565f5
Yadu Nand B <yadudoc1729@gmail.com>
2018-01-28 14:59:43 -0600

    PEP8 conformance fix. Fixing E114. #72

--- parsl/app/executors.py
+++ parsl/app/executors.py
@@ -5,7 +5,6 @@ logger = logging.getLogger(__name__)
 
 def ThreadPoolExecutor(max_workers=None, thread_name_prefix=''):
     return concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
-                                                 #thread_name_prefix=thread_name_prefix)
 
 def ProcessPoolExecutor(max_workers=None):
     return concurrent.futures.ProcessPoolExecutor(max_workers=max_workers)
