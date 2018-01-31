ee964bfd27b1bb55a58e643803fc6d56a209d651
Benjamin Glick <glick@lclark.edu>
2018-01-30 17:10:54 -0800

    Add Sun Grid Engine provider and modify .gitignore

--- libsubmit/providers/googlecloud/googlecloud.py
+++ libsubmit/providers/googlecloud/googlecloud.py
@@ -10,17 +10,16 @@ from string import Template
 from libsubmit.providers.provider_base import ExecutionProvider
 from libsubmit.launchers import Launchers
 from libsubmit.error import *
-from libsubmit.providers.aws.template import template_string
 
 logger = logging.getLogger(__name__)
 
 try:
-    import os
+    import googleapiclient
 
 except ImportError:
-    _ge_enabled = False
+    _google_enabled = False
 else:
-    _ge_enabled = True
+    _google_enabled = True
 
 translate_table = {'qw': 'PENDING',
                    'r': 'RUNNING',
@@ -31,8 +30,8 @@ translate_table = {'qw': 'PENDING',
                    }
 
 
-class GridEngine(ExecutionProvider):
-    """ Define the Grid Engine provider
+class GoogleCloud():  # ExcecutionProvider):
+    """ Define the Google Cloud provider
 
     .. code:: python
 
@@ -64,31 +63,25 @@ class GridEngine(ExecutionProvider):
         self.channel = channel
         self.config = config
         self.sitename = config['site']
-        self.current_blocksize = 0
+        self.client = None
         launcher_name = self.config["execution"]["block"].get("launcher",
                                                               "singleNode")
         self.launcher = Launchers.get(launcher_name, None)
         self.scriptDir = self.config["execution"]["scriptDir"]
         if not os.path.exists(self.scriptDir):
             os.makedirs(self.scriptDir)
-        self.has_ipcontroller = False
+
         # Dictionary that keeps track of jobs, keyed on job_id
         self.resources = {}
+        self.current_blocksize = 0
         atexit.register(self.bye)
 
-    def __repr__(self):
-        return "<Grid Engine Execution Provider for site:{0} with channel:{1}>".format(
-            self.sitename, self.channel)
+    def get_google_client():
+        pass
 
-    def create_cmd_string(self, path="/local/cluster/bin/:$PATH", lib_path="/local/cluster/lib/"):
-        return """qsub -e /dev/null -o /dev/null -terse << EOF
-PATH={}
-export PATH
-LD_LIBRARY_PATH={}
-export LD_LIBRARY_PATH
-ipengine
-EOF
-""".format(path, lib_path)
+    def __repr__(self):
+        return "<Google Cloud Platform Execution Provider for site:{0}>".format(
+            self.sitename, self.channel)
 
     def submit(self, cmd_string=None, blocksize=1, job_name="parsl.auto"):
         ''' The submit method takes the command string to be executed upon
@@ -108,29 +101,7 @@ EOF
         Raises:
              - ExecutionProviderExceptions or its subclasses
         '''
-        job_id = None
-        try:
-            qsub_pilot = """qsub -e /dev/null -o /dev/null -terse << EOF
-PATH=/local/cluster/bin
-export PATH
-LD_LIBRARY_PATH=/local/cluster/lib
-export LD_LIBRARY_PATH
-ipengine
-EOF
-"""
-            job_id = os.popen(qsub_pilot).read().strip()
-            logger.debug("Provisioned a slot")
-            new_slot = {job_id: {"job_name": job_name,
-                                 "job_id": job_id,
-                                 "status": translate_table.get('qw', "PENDING")}}
-            self.resources.update(new_slot)
-        except Exception as e:
-            logger.error("Failed to provision a slot")
-            logger.error(e)
-            raise e
-        logger.debug("Provisioned {} slots. Started ipengines.")
-        self.current_blocksize += 1
-        return job_id
+        pass
 
     def status(self, job_ids):
         ''' Get the status of a list of jobs identified by the job identifiers
@@ -162,17 +133,7 @@ EOF
         Raises:
              - ExecutionProviderExceptions or its subclasses
         '''
-        stati = []
-        for job_id in job_ids:
-            try:
-                outp = os.popen("qdel {}".format(job_id)).read()
-            except Exception as e:
-                logger.error("failed to cancel job {}".format(job_id))
-                outp = "False"
-            status = True if "has registered the job" in outp else False
-            stati.append(status)
-            self.current_blocksize -= 1
-        return stati
+        pass
 
     @property
     def scaling_enabled(self):
@@ -193,29 +154,12 @@ EOF
 
     @property
     def channels_required(self):
-        ''' GridEngine does not require a channel
+        '''Google Compute does not require a channel
 
         Returns:
               - Status (Bool)
         '''
         return False
 
-    def start_ipcontroller(self, ip="*", options='--quiet'):
-        '''
-        Start the IPController so that the worker nodes can connect
-        Args:
-            :param ip (str) what ip the ipcontroller should listen on
-            :param options (str) options for ipcontroller
-        '''
-        out = os.popen('ipcontroller --ip="{}" {} &'.format(ip, options))
-        self.has_ipcontroller = True
-        time.sleep(5)
-
-    def stop_ipcontroller(self):
-        os.popen("killall ipcontroller")
-        self.has_ipcontroller = False
-
     def bye(self):
         self.cancel([i for i in list(self.resources)])
-        if self.has_ipcontroller:
-            self.stop_ipcontroller()
