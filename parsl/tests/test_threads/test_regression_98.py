e1d4714596e1722ceb052e340fb4b0fd5261fe95
Yadu Nand B <yadudoc1729@gmail.com>
2018-02-13 13:53:33 -0600

    Adding regression test for issue #98

---
+++ parsl/tests/test_threads/test_regression_98.py
@@ -0,0 +1,45 @@
+''' Testing python apps
+'''
+import parsl
+from parsl import *
+from nose.tools import nottest
+import os
+import time
+import argparse
+import copy
+import json
+
+def test_immutable_config(n=2):
+    """ Regression test for immutable config #98
+    """
+
+    localThreads = {
+        "sites": [
+            {"site": "Local_Threads",
+             "auth": {"channel": None},
+             "execution": {
+                 "executor": "threads",
+                 "provider": None,
+                 "maxThreads": 4
+             }
+            }],
+        "globals": {"lazyErrors": True}
+    }
+
+    original = json.dumps(localThreads, sort_keys=True)
+    dfk = DataFlowKernel(config=localThreads)
+    after = json.dumps(localThreads, sort_keys=True)
+
+    assert original == after, "Config modified"
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Count of apps to launch")
+    args = parser.parse_args()
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    x = test_immutable_config()
