c0e8ccb8c0dc35d3c7e94218fbecf8015e1ed894
Anna Elizabeth Woodard <awoodard@nd.edu>
2018-03-17 11:34:46 -0500

    Require `PARSL_TESTING=True` for nosetests
    Fixes #136.

--- parsl/tests/test_data/__init__.py
+++ parsl/tests/test_data/__init__.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+if str(os.environ.get('PARSL_TESTING', False)).lower() != 'true':
+    raise RuntimeError("must first run 'export PARSL_TESTING=True'")
 
 def setup_package():
     import os
@@ -14,3 +19,4 @@ def setup_package():
 
 def teardown_package():
     pass
+
