fec5f7b0ea48eb3d58d998f00ec226a63290cf81
Yadu Nand B <yadudoc1729@gmail.com>
2018-03-19 22:13:10 -0500

    PEP8 fixes

--- parsl/tests/test_scaling/test_latency/test_midway_latency.py
+++ parsl/tests/test_scaling/test_latency/test_midway_latency.py
@@ -1,10 +1,8 @@
 from parsl import *
-import parsl
 import os
 import time
 import argparse
 
-#parsl.set_stream_logger()
 os.environ['MIDWAY_USERNAME'] = 'yadunand'
 from midway import multiNode as config
 dfk = DataFlowKernel(config=config)
@@ -15,18 +13,19 @@ def python_app():
     import platform
     return "Hello from {0}".format(platform.uname())
 
+
 @App("python", dfk)
 def python_noop():
     return
-
 
 
 @App("bash", dfk)
 def bash_app(stdout=None, stderr=None):
     return 'echo "Hello from $(uname -a)" ; sleep 2'
 
+
 def average(l):
-    return sum(l)/len(l)
+    return sum(l) / len(l)
 
 
 def test_python(count):
@@ -34,26 +33,25 @@ def test_python(count):
 
     print("Priming the system")
     items = []
-    for i in range(0,100):
+    for i in range(0, 100):
         items.extend([python_app()])
     print("Launched primer application")
     for i in items:
         i.result()
     print("Primer done")
 
-
     latency = []
     rtt = []
-    for i in range(0,100):
+    for i in range(0, 100):
         pre = time.time()
         results[i] = python_noop()
         post = time.time()
         results[i].result()
         final = time.time()
-        print("Latency : ", final-post)
-        print("Roundtrip time : ", final-pre)
-        latency.append(final-post)
-        rtt.append(final-pre)
+        print("Latency : ", final - post)
+        print("Roundtrip time : ", final - pre)
+        latency.append(final - post)
+        rtt.append(final - pre)
 
     print("Latency   |   Min:{0:5} Max:{1:5} Average:{2:5}".format(min(latency),
                                                                    max(latency),
@@ -62,9 +60,6 @@ def test_python(count):
     print("Roundtrip |   Min:{0:5} Max:{1:5} Average:{2:5}".format(min(rtt),
                                                                    max(rtt),
                                                                    average(rtt)))
-
-
-
 
 
 def test_bash():
@@ -84,8 +79,8 @@ if __name__ == "__main__":
                         help="Count of apps to launch")
     args = parser.parse_args()
 
-    #if args.debug:
-    #    parsl.set_stream_logger()
+    # if args.debug:
+    #     parsl.set_stream_logger()
 
     test_python(int(args.count))
     # test_bash()
