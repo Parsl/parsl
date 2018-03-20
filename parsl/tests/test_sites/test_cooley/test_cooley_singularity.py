efa1e7aa02cd7eab2b4c029f956ebde7d68d3370
Yadu Nand B <yadudoc1729@gmail.com>
2018-03-20 18:42:30 -0500

    PEP8 fixes

--- parsl/tests/test_sites/test_cooley/test_cooley_singularity.py
+++ parsl/tests/test_sites/test_cooley/test_cooley_singularity.py
@@ -10,12 +10,14 @@ os.environ["COOLEY_USERNAME"] = getpass.getuser()
 from cooley import singleNodeLocal as config
 dfk = DataFlowKernel(config=config)
 
+
 @App("bash", dfk)
 def freesurfer(stdout=None, stderr=None):
     return '''singularity exec ~madduri/freesurfer.simg recon-all
     '''
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
 
     N = 4
     results = {}
@@ -27,7 +29,7 @@ if __name__ == "__main__" :
         results[i].result()
 
     print("Waiting ....")
-    try :
+    try:
         print(results[0].result())
     except Exception as e:
         print("Caught an exception, but this is not a problem")
@@ -35,6 +37,3 @@ if __name__ == "__main__" :
     print("STDOUT from 0th run :")
     print(open(results[0].stdout, 'r').read())
     print(open(results[0].stderr, 'r').read())
-
-    
-    
