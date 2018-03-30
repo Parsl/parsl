4d9e9eaaf2ea7000178ee0052a17ecdb4ee5d269
Yadu Nand B <yadudoc1729@gmail.com>
2018-03-30 18:16:27 -0500

    Updating config override to install parsl instead of ipyparallel

--- parsl/tests/test_sites/configs/osg.py
+++ parsl/tests/test_sites/configs/osg.py
@@ -1,6 +1,43 @@
 import os
 USERNAME = os.environ["OSG_USERNAME"]
 
+
+singleNode = {
+    "sites": [
+        {"site": "OSG_Remote",
+         "auth": {
+             "channel": "ssh",
+             "hostname": "login.osgconnect.net",
+             "username": USERNAME,
+             "scriptDir": "/home/{}/parsl_scripts/".format(USERNAME)
+         },
+         "execution": {
+             "executor": "ipp",
+             "provider": "condor",
+             "block": {                 # Definition of a block
+                 "nodes": 1,            # of nodes in that block
+                 "taskBlocks": 1,       # total tasks in a block
+                 "initBlocks": 1,
+                 "maxBlocks": 1,
+                 "options": {
+                     "partition": "debug",
+                     # The following override is used to specify condor class-ads
+                     # to ensure that we only get machines with cvmfs, modules and
+                     # consequently python3
+                     "overrides": 'Requirements = OSGVO_OS_STRING == "RHEL 6" && Arch == "X86_64" &&  HAS_MODULES == True',
+                     "workerSetup": """module load python/3.5.2;
+python3 -m venv parsl_env;
+source parsl_env/bin/activate;
+pip3 install parsl"""
+                 }
+             }
+         }
+         }
+    ],
+    "globals": {"lazyErrors": True}
+}
+
+
 multiNode = {
     "sites": [
         {"site": "OSG_Remote",
@@ -27,7 +64,7 @@ multiNode = {
                      "workerSetup": """module load python/3.5.2;
 python3 -m venv parsl_env;
 source parsl_env/bin/activate;
-pip3 install ipyparallel"""
+pip3 install parsl"""
                  }
              }
          }
