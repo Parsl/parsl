6425fb9042c175fa5d8ff2249cee1e91123e0f5a
Yadu Nand B <yadudoc1729@gmail.com>
2018-04-18 15:48:49 -0500

    Adding an additional stress test

---
+++ parsl/tests/test_stress/test_python_threads.py
@@ -0,0 +1,41 @@
+''' Testing bash apps
+'''
+import parsl
+from parsl import *
+
+import time
+import argparse
+
+from parsl.configs.local import localThreads as config
+dfk = DataFlowKernel(config=config)
+
+
+@App('python', dfk)
+def increment(x):
+    return x + 1
+
+
+def test_stress(count=1000):
+    """Threaded app launch stress test"""
+    start = time.time()
+    x = {}
+    for i in range(count):
+        x[i] = increment(i)
+    end = time.time()
+    print("Launched {0} tasks in {1} s".format(count, end - start))
+    dfk.cleanup()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", default="100",
+                        help="width of the pipeline")
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Count of apps to launch")
+    args = parser.parse_args()
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    test_stress(count=int(args.count))
