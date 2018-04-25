fd8ce2ed03f570952348b3150742ce160122d41b
Yadu Nand B <yadudoc1729@gmail.com>
2018-04-25 13:56:49 -0500

    Updating test

--- parsl/tests/test_checkpointing/test_regression_233.py
+++ parsl/tests/test_checkpointing/test_regression_233.py
@@ -1,12 +1,13 @@
 from parsl import *
-set_stream_logger()
+# set_stream_logger()
 
 
 def run_checkpointed(cpts):
-
+    # set_stream_logger()
     from parsl.configs.local import localThreads as config
     config["globals"]["checkpointMode"] = "task_exit"
-    dfk = DataFlowKernel(config=config, checkpointFiles=cpts)
+    config["globals"]["checkpointFiles"] = cpts
+    dfk = DataFlowKernel(config=config)
 
     @App('python', dfk, cache=True)
     def cached_rand(x):
@@ -19,7 +20,7 @@ def run_checkpointed(cpts):
         items.append(x)
 
     dfk.cleanup()
-    return [i.result for i in items]
+    return [i.result() for i in items]
 
 
 def run_race(sleep_dur):
@@ -45,21 +46,31 @@ def run_race(sleep_dur):
 
 
 def test_regress_234():
+    """Test task_exit checkpointing with fast tasks"""
     run_race(0)
-    run_race(0.1)
+
+
+def test_slower_apps():
+    """Test task_exit tests with slow apps"""
+    run_race(0.5)
 
 
 def test_checkpoint_availability():
+    import os
 
     original = run_checkpointed([])
-    # last_checkpoint = os.path.abspath('runinfo/{0}/checkpoint'.format(sorted(os.listdir('runinfo/'))[-1]))
-    # cached = run_checkpointed([last_checkpoint])
+    last_checkpoint = os.path.abspath('runinfo/{0}/checkpoint'.format(sorted(os.listdir('runinfo/'))[-1]))
+    print(last_checkpoint)
+    cached = run_checkpointed([last_checkpoint])
 
-    # assert cached == original, "All tasks were not cached"
+    print(cached)
     print(original)
+
+    assert cached == original, "All tasks were not cached"
 
 
 if __name__ == "__main__":
 
-    # test_checkpoint_availability()
-    test_regress_234()
+    test_checkpoint_availability()
+    # test_regress_234()
+    # test_slower_apps()
