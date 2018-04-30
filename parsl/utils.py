23e530c734e645f55a48ff08f3dd269703064a5e
Anna Elizabeth Woodard <awoodard@nd.edu>
2018-04-30 13:54:14 -0500

    Skip check for install location
    Return the same version string regardless of install method. This
    ensures that if the user has modified the code and then run `python
    setup.py install`, it will be reflected in the version. This also
    ensures that if the printed version matches parsl.__version__, we
    know there was an exception raised when determining the code state.
    Caught by @yadudoc.

--- parsl/utils.py
+++ parsl/utils.py
@@ -11,17 +11,16 @@ logger = logging.getLogger(__name__)
 
 def get_version():
     version = parsl.__version__
-    if 'site-packages' not in __file__:
-        work_tree = os.path.dirname(os.path.dirname(__file__))
-        git_dir = os.path.join(work_tree, '.git')
-        env = {'GIT_WORK_TREE': work_tree, 'GIT_DIR': git_dir}
-        try:
-            cmd = shlex.split('git rev-parse --short HEAD')
-            head = subprocess.check_output(cmd, env=env).strip().decode('utf-8')
-            diff = subprocess.check_output(shlex.split('git diff HEAD'), env=env)
-            status = 'dirty' if diff else 'clean'
-            version = '{v}-{head}-{status}'.format(v=VERSION, head=head, status=status)
-        except Exception as e:
-            logger.exception("Unable to determine code state")
+    work_tree = os.path.dirname(os.path.dirname(__file__))
+    git_dir = os.path.join(work_tree, '.git')
+    env = {'GIT_WORK_TREE': work_tree, 'GIT_DIR': git_dir}
+    try:
+        cmd = shlex.split('git rev-parse --short HEAD')
+        head = subprocess.check_output(cmd, env=env).strip().decode('utf-8')
+        diff = subprocess.check_output(shlex.split('git diff HEAD'), env=env)
+        status = 'dirty' if diff else 'clean'
+        version = '{v}-{head}-{status}'.format(v=VERSION, head=head, status=status)
+    except Exception as e:
+        logger.exception("Unable to determine code state")
 
     return version
