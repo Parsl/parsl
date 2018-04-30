de8ad2af92fe78eba5deea79605bafc82151aed8
Anna Elizabeth Woodard <awoodard@nd.edu>
2018-04-30 11:54:57 -0500

    Set `GIT_WORK_TREE`
    This solves a problem reported by @benclifford, where clean/dirty was
    not being set correctly if the PWD is outside the parsl repo.
    
    An additional change introduced here is to catch any exceptions when
    trying to determine the code state; if one is raised it will be logged
    along with the stacktrace, and the version returned will be
    parsl.__version__ (with no additional information about the code state.)
    
    A final minor change is that I removed a redundant `else` clause.

--- parsl/utils.py
+++ parsl/utils.py
@@ -1,20 +1,27 @@
+import logging
 import os
-import subprocess
 import shlex
+import subprocess
 
 import parsl
 from parsl.version import VERSION
 
+logger = logging.getLogger(__name__)
+
 
 def get_version():
-    if 'site-packages' in __file__:
-        version = parsl.__version__
-    else:
-        git_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.git')
-        env = {'GIT_DIR': git_dir}
-        cmd = shlex.split('git rev-parse --short HEAD')
-        head = subprocess.check_output(cmd, env=env).strip().decode('utf-8')
-        diff = subprocess.check_output(shlex.split('git diff HEAD'), env=env)
-        status = 'dirty' if diff else 'clean'
-        version = '{v}-{head}-{status}'.format(v=VERSION, head=head, status=status)
+    version = parsl.__version__
+    if 'site-packages' not in __file__:
+        work_tree = os.path.dirname(os.path.dirname(__file__))
+        git_dir = os.path.join(work_tree, '.git')
+        env = {'GIT_WORK_TREE': work_tree, 'GIT_DIR': git_dir}
+        try:
+            cmd = shlex.split('git rev-parse --short HEAD')
+            head = subprocess.check_output(cmd, env=env).strip().decode('utf-8')
+            diff = subprocess.check_output(shlex.split('git diff HEAD'), env=env)
+            status = 'dirty' if diff else 'clean'
+            version = '{v}-{head}-{status}'.format(v=VERSION, head=head, status=status)
+        except Exception as e:
+            logger.exception("Unable to determine code state")
+
     return version
