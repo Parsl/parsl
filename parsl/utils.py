a4e044c8f92cf26b855231869ea3ca46db8b3246
Anna Elizabeth Woodard <awoodard@nd.edu>
2018-04-30 10:25:09 -0500

    Minor naming change
    Also: flake8 fix.

--- parsl/utils.py
+++ parsl/utils.py
@@ -5,15 +5,16 @@ import shlex
 import parsl
 from parsl.version import VERSION
 
+
 def get_version():
     if 'site-packages' in __file__:
         version = parsl.__version__
     else:
-            version = '{major}-{head}-{status}'.format(major=VERSION, head=head, status=status)
         git_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.git')
         env = {'GIT_DIR': git_dir}
         cmd = shlex.split('git rev-parse --short HEAD')
         head = subprocess.check_output(cmd, env=env).strip().decode('utf-8')
         diff = subprocess.check_output(shlex.split('git diff HEAD'), env=env)
         status = 'dirty' if diff else 'clean'
+        version = '{v}-{head}-{status}'.format(v=VERSION, head=head, status=status)
     return version
