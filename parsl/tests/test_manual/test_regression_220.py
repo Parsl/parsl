ce7cdcb89f5587cd910004fc9d9f047cfef256bf
Yadu Nand B <yadudoc1729@gmail.com>
2018-05-10 17:27:22 -0500

    Trimming down #220 regression test

--- parsl/tests/test_manual/test_regression_220.py
+++ parsl/tests/test_manual/test_regression_220.py
@@ -1,32 +1,24 @@
 from parsl import DataFlowKernel, set_stream_logger
-import socket
 import time
 from parsl.configs.local import localThreads as config
 
 
-def internet_on(host="8.8.8.8", port=53, timeout=3):
-    """
-    Host: 8.8.8.8 (google-public-dns-a.google.com)
-    OpenPort: 53/tcp
-    Service: domain (DNS/TCP)
-    """
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except Exception as ex:
-        return False
-
-
 def test_220():
-    print("This test should be run only with no internet connectivity")
-    x = internet_on()
-    assert x is False, "Internet is available test fails"
+    """Test async usage_tracking behavior for issue #220 """
+
+    print("This test assumes /etc/resolv being misconfigured")
+    with open("/etc/resolv.conf", 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            print("Line: [{}]".format(line))
+            if line.startswith("nameserver") and line != "nameserver 2.2.2.2":
+                assert False, "/etc/resolv.conf should be misconfigured"
 
     start = time.time()
     set_stream_logger()
     dfk = DataFlowKernel(config=config)
     delta = time.time() - start
+    print("Time taken : ", delta)
     assert delta < 1, "DFK took too much time to start, delta:{}".format(delta)
     dfk.cleanup()
 
