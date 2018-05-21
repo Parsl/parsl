fbad621cf45b577d0631b102258a7859668069ed
Ben Clifford <benc@hawaga.org.uk>
2018-05-21 09:50:31 +0100

    De-abbreviate AppFut in log message

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -582,7 +582,7 @@ class DataFlowKernel(object):
                                                           tid=task_id,
                                                           stdout=task_stdout,
                                                           stderr=task_stderr)
-                logger.debug("Task {} launched with AppFut:{}".format(task_id,
+                logger.debug("Task {} launched with AppFuture: {}".format(task_id,
                                                                       task_def['app_fu']))
 
             else:
@@ -608,7 +608,7 @@ class DataFlowKernel(object):
                                                       stdout=task_stdout,
                                                       stderr=task_stderr)
             self.tasks[task_id]['status'] = States.pending
-            logger.debug("Task {} launched with AppFut:{}".format(task_id,
+            logger.debug("Task {} launched with AppFuture: {}".format(task_id,
                                                                   task_def['app_fu']))
         return task_def['app_fu']
 
