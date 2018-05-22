196c48b78e6ee141c724020040bc4545c0fb5423
Anna Elizabeth Woodard <awoodard@nd.edu>
2018-05-22 14:17:04 -0500

    Mark test of cleanup behavior as local
    This informs pytest not to try to load its own configuration, but rather
    run what is loaded locally in the test.

--- parsl/tests/test_regression/test_221.py
+++ parsl/tests/test_regression/test_221.py
@@ -1,6 +1,11 @@
+import pytest
+
 import parsl
 from parsl.app.app import App
-from parsl.tests.configs.local_threads import config
+from parsl.tests.configs.local_ipp import config
+
+parsl.clear()
+dfk = parsl.load(config)
 
 
 @App('python')
@@ -9,14 +14,12 @@ def slow_double(x, dur=0.1):
     time.sleep(dur)
     return x * 5
 
-
+@pytest.mark.local
 def test_cleanup_behavior_221():
     """ A1 A2 A3   -> cleanup
         B1 B2 B3
 
     """
-    parsl.clear()
-    dfk = parsl.load(config)
 
     round_1 = []
     for i in range(0, 10):
