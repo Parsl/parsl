82282f88aaaa17c641593e418996a3e2b987e702
Ben Clifford <benc@hawaga.org.uk>
2018-06-05 20:13:27 +0100

    Revert "Assorted debug playing with race conditions on exception handling"
    This reverts commit fd3c768b44c539097dd077a4576a548c5fe7f07b.

--- parsl/dataflow/futures.py
+++ parsl/dataflow/futures.py
@@ -40,20 +40,6 @@ class AppFuture(Future):
 
     """
 
-    def set_result(self, result):
-        logger.debug("{} setting result".format(self))
-        self.commit(1)
-        if self.parent is not None:
-            raise ValueException("trying to operate on super when we have a parent callback")
-        super().set_result(result)
-
-    def set_exception(self, e):
-        logger.debug("{} setting exception".format(self))
-        self.commit(1)
-        if self.parent is not None:
-            raise ValueException("trying to operate on super when we have a parent callback")
-        super().set_exception(e)
-
     def parent_callback(self, executor_fu):
         """Callback from executor future to update the parent.
 
@@ -66,19 +52,6 @@ class AppFuture(Future):
         Updates the super() with the result() or exception()
         """
         # print("[RETRY:TODO] parent_Callback for {0}".format(executor_fu))
-
-        logger.debug("{} parent callback to update super()".format(self))
-
-        self.commit(2)
-
-        # this is an unexpected situation, I think
-        if executor_fu != self.parent:
-            raise ValueException("parent_callback received an executor_fu {} that differs from self.parent {}".format(executor_fu, self.parent))
-
-        if self.parent is not None:
-            raise ValueException("trying to operate on super when we have a parent callback")
-
-
         if executor_fu.done() is True:
             try:
                 super().set_result(executor_fu.result())
@@ -108,8 +81,6 @@ class AppFuture(Future):
         self._outputs = []
         self._stdout = stdout
         self._stderr = stderr
-        self._commit = 0
-        self._commit_lock = threading.Lock()
 
     @property
     def stdout(self):
@@ -129,10 +100,6 @@ class AppFuture(Future):
         This handles the case where the user has called result on the AppFuture
         before the parent exists.
         """
-        if self.parent is not None:
-            raise ValueException("Can't set parent multiple times")
-        self.commit(2)
-        logger.debug("Future {} updating parent from {} to {}".format(self,self.parent, fut))
         # with self._parent_update_lock:
         self.parent = fut
         fut.add_done_callback(self.parent_callback)
@@ -147,34 +114,19 @@ class AppFuture(Future):
         """
         try:
             if self.parent:
-                self.commit(2)
-
-                # but what if we have a result already in *this* future?
-                if super().done():
-                    raise ValueException("super() is done, but we're trying to wait for a result from self.parent")
-
-
-                logger.debug("{} waiting for result from self.parent {}".format(self, self.parent))
                 res = self.parent.result(timeout=timeout)
             else:
-                self.commit(1)
-                logger.debug("{} waiting for result from super() as no parent".format(self))
                 res = super().result(timeout=timeout)
-
             if isinstance(res, RemoteException):
                 res.reraise()
             return res
 
         except Exception as e:
-            logger.debug("{} exception handling, this app future has parent of {}".format(self, self.parent))
-            if self.parent and self.parent.retries_left > 0:
-                self.commit(2)
-                logger.debug("{} exception handling: {} retries remaining, so waiting again for result".format(self, self.parent.retries_left))
+            if self.parent.retries_left > 0:
                 self._parent_update_event.wait()
                 self._parent_update_event.clear()
                 return self.result(timeout=timeout)
             else:
-                # no parent, or no retries left
                 if isinstance(e, RemoteException):
                     e.reraise()
                 else:
@@ -182,21 +134,18 @@ class AppFuture(Future):
 
     def cancel(self):
         if self.parent:
-            self.commit(2)
             return self.parent.cancel
         else:
             return False
 
     def cancelled(self):
         if self.parent:
-            self.commit(2)
             return self.parent.cancelled()
         else:
             return False
 
     def running(self):
         if self.parent:
-            self.commit(2)
             return self.parent.running()
         else:
             return False
@@ -212,38 +161,25 @@ class AppFuture(Future):
               - False : Pending resolution
         """
         if self.parent:
-            self.commit(2)
             return self.parent.done()
         else:
             return False
 
     def exception(self, timeout=None):
         if self.parent:
-            self.commit(2)
             return self.parent.exception(timeout=timeout)
         else:
             return False
 
     def add_done_callback(self, fn):
         if self.parent:
-            self.commit(2)
             return self.parent.add_done_callback(fn)
         else:
-            raise ValueException("Attempted to add_done_callback, but discarding instead")
+            return None
 
     @property
     def outputs(self):
         return self._outputs
-
-    def commit(self, n):
-        with self._commit_lock:
-            if(self._commit == 0):
-                logger.debug("{} fresh-committing to path {}".format(self, n))
-                self._commit = n
-            elif self._commit == n:
-                logger.debug("{} re-committing to path {}".format(self, n))
-            else:
-                logger.debug("COMMIT FAILURE {} cannot commit to path {} as already committed to {}".format(self, n, self._commit))
 
     def __repr__(self):
         if self.parent:
