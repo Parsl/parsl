61997fde2fbd51e614ac788122629ab78e0b5fa8
Yadu Nand B <yadudoc1729@gmail.com>
2018-06-27 21:12:08 +0000

    Adding manual test runner

---
+++ parsl/tests/manual_tests/site_testing_matrix.rst
@@ -0,0 +1,11 @@
+
+Site Testing
+============
+
+Sites that we will test in this exercise:
+
+* Local IPP
+* Midway RCC at UChicago
+* Open Science Grid (OSG)
+* Cori at NERSC
+* Swan at Cray 
---
+++ parsl/tests/manual_tests/test_basic.py
@@ -0,0 +1,164 @@
+import argparse
+import os
+import time
+
+import parsl
+
+# Tested. Confirmed. Local X Local X SingleNodeLauncher
+# from parsl.tests.configs.local_ipp import config
+
+# Tested. Confirmed. ssh X Slurm X SingleNodeLauncher
+# from parsl.tests.configs.midway_ipp import config
+
+# Tested. Confirmed. ssh X Slurm X SingelNodeLauncher
+# from parsl.tests.configs.midway_ipp_multicore import config
+
+# Tested. Confirmed. ssh X Slurm X SrunLauncher
+#from parsl.tests.configs.midway_ipp_multinode import config
+
+# OSG requires python3.5 for testing. This test is inconsitent,
+# breaks often depending on where the test lands
+# Tested. Confirmed. ssh X Condor X single_node
+# from parsl.tests.configs.osg_ipp_multinode import config
+
+# Tested. Confirmed. ssh X Torque X AprunLauncher
+# from parsl.tests.configs.swan_ipp import config
+
+# Tested. Confirmed. ssh X Torque X AprunLauncher
+# from parsl.tests.configs.swan_ipp_multinode import config
+
+# Tested. Confirmed. ssh X Slurm X SingleNodeLauncher
+#from parsl.tests.configs.cori_ipp_single_node import config
+
+# Tested. Confirmed. ssh X Slurm X srun
+# from parsl.tests.configs.cori_ipp_multinode import config
+
+from parsl.tests.configs.cooley_ssh_il_single_node import config
+
+from parsl.app.app import App
+parsl.load(config)
+#parsl.set_stream_logger()
+
+@App('python')
+def double(x):
+    return x * 2
+
+
+@App('python')
+def echo(x, string, stdout=None):
+    print(string)
+    return x * 5
+
+
+@App('python')
+def import_echo(x, string, stdout=None):
+    import time
+    time.sleep(0)
+    print(string)
+    return x * 5
+
+@App('python')
+def platform(sleep=10, stdout=None):
+    import platform
+    import time
+    time.sleep(sleep)
+    return platform.uname()
+
+
+def test_simple(n=2):
+    start = time.time()
+    x = double(n)
+    print("Result : ", x.result())
+    assert x.result() == n * \
+        2, "Expected double to return:{0} instead got:{1}".format(
+            n * 2, x.result())
+    print("Duration : {0}s".format(time.time() - start))
+    print("[TEST STATUS] test_parallel_for [SUCCESS]")
+    return True
+
+
+def test_imports(n=2):
+    start = time.time()
+    x = import_echo(n, "hello world")
+    print("Result : ", x.result())
+    assert x.result() == n * \
+        5, "Expected double to return:{0} instead got:{1}".format(
+            n * 2, x.result())
+    print("Duration : {0}s".format(time.time() - start))
+    print("[TEST STATUS] test_parallel_for [SUCCESS]")
+    return True
+
+def test_platform(n=2):
+    start = time.time()
+
+    # sync
+    x = platform(sleep=0)
+    print(x.result())
+
+    d = []
+    for i in range(0, n):
+        x = platform(sleep=5)
+        d.append(x)
+
+    print(set([i.result()for i in d]))
+
+    return True
+
+
+def test_parallel_for(n=2):
+    d = {}
+    start = time.time()
+    for i in range(0, n):
+        d[i] = double(i)
+        # time.sleep(0.01)
+
+    assert len(
+        d.keys()) == n, "Only {0}/{1} keys in dict".format(len(d.keys()), n)
+
+    [d[i].result() for i in d]
+    print("Duration : {0}s".format(time.time() - start))
+    print("[TEST STATUS] test_parallel_for [SUCCESS]")
+    return d
+
+
+def test_custom_exception():
+    from globus_sdk import GlobusError
+
+    with pytest.raises(GlobusError):
+        x = custom_exception()
+        x.result()
+
+
+def demonstrate_custom_exception():
+    x = custom_exception()
+    print(x.result())
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--sitespec", default=None)
+    parser.add_argument("-c", "--count", default="10",
+                        help="Count of apps to launch")
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Count of apps to launch")
+    args = parser.parse_args()
+
+    if args.sitespec:
+        try:
+            exec("import parsl; from {} import config".format(args.sitespec))
+            parsl.load(config)
+        except Exception as e:
+            print("Failed to load the requested config : ", args.sitespec)
+            exit(0)
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    # demonstrate_custom_exception()
+    x = test_simple(int(args.count))
+    # x = test_imports()
+    # x = test_parallel_for()
+    # x = test_parallel_for(int(args.count))
+    # x = test_stdout()
+    x = test_platform()
