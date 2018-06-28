35ca324465140152fa7a43ba97e33eac794cd249
Yadu Nand B <yadudoc1729@gmail.com>
2018-06-28 22:15:08 +0000

    Adding tested config for SSHInteractiveLogin for Cooley All channels are now tested.

---
+++ parsl/tests/configs/cooley_ssh_il_single_node.py
@@ -0,0 +1,38 @@
+# Untested
+from libsubmit.channels import SSHInteractiveLoginChannel
+from libsubmit.providers import CobaltProvider
+from libsubmit.launchers import SingleNodeLauncher
+
+from parsl.config import Config
+from parsl.executors.ipp import IPyParallelExecutor
+from parsl.executors.ipp_controller import Controller
+from parsl.tests.user_opts import user_opts
+from parsl.tests.utils import get_rundir
+
+
+config = Config(
+    executors=[
+        IPyParallelExecutor(
+            label='cooley_ssh_il_local_single_node',
+            provider=CobaltProvider(
+                channel=SSHInteractiveLoginChannel(
+                    hostname='cooley.alcf.anl.gov',
+                    username=user_opts['cooley']['username'],
+                    script_dir="/home/{}/parsl_scripts/".format(user_opts['cooley']['username'])
+                ),
+                nodes_per_block=1,
+                tasks_per_node=1,
+                init_blocks=1,
+                max_blocks=1,
+                walltime="00:05:00",
+                overrides=user_opts['cooley']['overrides'],
+                queue='pubnet-debug',
+                account=user_opts['cooley']['account'],
+                launcher=SingleNodeLauncher(),
+            ),
+            controller=Controller(public_ip=user_opts['public_ip'])
+        )
+
+    ],
+    run_dir=get_rundir(),
+)
