9c36746d27c85295faeb1daaa34d9a0344368414
Yadu Nand B <yadudoc1729@gmail.com>
2018-07-11 14:11:34 -0500

    Adding tests for staging examples from docs

---
+++ parsl/tests/test_staging/test_docs_1.py
@@ -0,0 +1,24 @@
+from parsl import App, File
+import parsl
+from parsl.configs.local_threads import config
+parsl.load(config)
+
+
+@App('python')
+def convert(inputs=[], outputs=[]):
+    with open(inputs[0].filepath, 'r') as inp:
+        content = inp.read()
+        with open(outputs[0].filepath, 'w') as out:
+            out.write(content.upper())
+
+
+def test():
+    # create an remote Parsl file
+    inp = File('ftp://www.iana.org/pub/mirror/rirstats/arin/ARIN-STATS-FORMAT-CHANGE.txt')
+
+    # create a local Parsl file
+    out = File('file:///tmp/ARIN-STATS-FORMAT-CHANGE.txt')
+
+    # call the convert app with the Parsl file
+    f = convert(inputs=[inp], outputs=[out])
+    f.result()
---
+++ parsl/tests/test_staging/test_docs_2.py
@@ -0,0 +1,20 @@
+from parsl import App, File
+import parsl
+from parsl.configs.local_threads import config
+parsl.load(config)
+
+
+@App('bash')
+def cat(inputs=[], stdout='stdout.txt'):
+    return 'cat %s' % (inputs[0])
+
+
+def test():
+    # create a test file
+    open('/tmp/test.txt', 'w').write('Hello\n')
+
+    # create the Parsl file
+    parsl_file = File('file:///tmp/test.txt')
+
+    # call the cat app with the Parsl file
+    cat(inputs=[parsl_file])
