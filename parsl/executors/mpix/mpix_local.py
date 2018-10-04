581e734c6f12dda15120c6ee028de20c48643c52
Yadu Nand B <yadudoc1729@gmail.com>
2018-10-04 20:41:58 +0000

    Committing bad configs, that won't really work

--- parsl/executors/mpix/mpix_local.py
+++ parsl/executors/mpix/mpix_local.py
@@ -8,10 +8,12 @@ config = Config(
     executors=[
         MPIExecutor(
             label="local_ipp",
+            # These urls here are *NOT* used
             jobs_q_url="tcp://127.0.0.1:50005",
             results_q_url="tcp://127.0.0.1:50006",
             # launch_cmd="mpiexec -np {tasks_per_node} /home/yadu/src/parsl/parsl/executors/mpix/fabric.py {debug}",
-            launch_cmd="./cleanup.sh ; mpiexec -np {tasks_per_node} /home/yadu/src/parsl/parsl/executors/mpix/fabric_threaded.py {debug}",
+            # launch_cmd="./cleanup.sh ; mpiexec -np {tasks_per_node} /home/yadu/src/parsl/parsl/executors/mpix/fabric_threaded.py {debug}",
+            launch_cmd="./cleanup.sh ; python3 fabric_single_node.py {debug} --task_url={task_url} --result_url={result_url}",
             # launch_cmd="sleep 600",
             # engine_debug=True,
             provider=LocalProvider(
