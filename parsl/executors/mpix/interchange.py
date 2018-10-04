cf51b63d07e72a584a2bd76afda04191ac48658c
Yadu Nand B <yadudoc1729@gmail.com>
2018-10-04 20:34:45 +0000

    Updated logging to send messages to separate log. Minor fix to a major bug in handling timed-out workers

--- parsl/executors/mpix/interchange.py
+++ parsl/executors/mpix/interchange.py
@@ -9,6 +9,7 @@ import multiprocessing
 
 from ipyparallel.serialize import serialize_object
 
+LOOP_SLOWDOWN = 0.0 # in seconds
 
 class ShutdownRequest(Exception):
     ''' Error raised a required module is missing for a optional/extra provider
@@ -49,7 +50,9 @@ class Interchange(object):
                  interchange_address="127.0.0.1",
                  client_ports=(50055, 50056),
                  interchange_ports=(50097, 50098),
-                 heartbeat_period=10):
+                 heartbeat_period=10,
+                 logging_level=logging.INFO, 
+             ):
         """
         Parameters
         ----------
@@ -61,7 +64,12 @@ class Interchange(object):
 
         heartbeat_period : int
              Heartbeat period expected from workers
+
+        logging_level : int
+             Logging level as defined in the logging module. Default : logging.INFO (20)
+
         """
+        start_file_logger("interchange.logs", level=logging_level)
         logger.debug("****************************************")
         logger.debug("Starting Interchange process v0.2")
         logger.debug("****************************************")
@@ -70,7 +78,7 @@ class Interchange(object):
         self.interchange_address = interchange_address
         self.identity = uuid.uuid4()
 
-        logger.debug("Attempting connection to client at:{} port:{},{}".format(
+        logger.info("Attempting connection to client at:{} port:{},{}".format(
             client_address, client_ports[0], client_ports[1]))
         self.context = zmq.Context()
         self.task_incoming = self.context.socket(zmq.DEALER)
@@ -80,13 +88,16 @@ class Interchange(object):
         self.results_outgoing.connect("tcp://{}:{}".format(client_address, client_ports[1]))
         logger.debug("Connected to client")
 
-        logger.debug("Binding to ports:{},{} for incoming worker connections".format(
+        print("[INTERCHANGE] Binding to ports:{},{} for incoming worker connections".format(
+            interchange_ports[0], interchange_ports[1]))
+ 
+        logger.info("Binding to ports:{},{} for incoming worker connections".format(
             interchange_ports[0], interchange_ports[1]))
         self.task_outgoing = self.context.socket(zmq.ROUTER)
         self.task_outgoing.bind("tcp://*:{}".format(interchange_ports[0]))
         self.results_incoming = self.context.socket(zmq.ROUTER)
         self.results_incoming.bind("tcp://*:{}".format(interchange_ports[1]))
-        logger.debug("Incoming ports bound")
+        logger.info("Incoming ports bound")
 
         self._task_queue = []
         self._ready_worker_queue = {}
@@ -115,7 +126,7 @@ class Interchange(object):
         """
         # Listen for tasks
         tasks = []
-        logger.debug("Listening for {} tasks".format(count))
+        logger.debug("[GET_TASKS] Listening for {} tasks".format(count))
         for c in range(count):
             if self.task_incoming in self.socks and self.socks[self.task_incoming] == zmq.POLLIN:
                 if len(self._task_queue) < self.max_task_queue_size:
@@ -133,7 +144,8 @@ class Interchange(object):
                     else:
                         tasks.append(msg)
             else:
-                logger.debug("Returning with {} tasks".format(c))
+                logger.debug("[GET_TASKS] Returning with {} tasks".format(c))
+                break
 
         return tasks
 
@@ -159,7 +171,7 @@ class Interchange(object):
 
         while True:
             self.socks = dict(poller.poll(timeout=poll_period))
-
+            
             # Listen for requests for work
             if self.task_outgoing in self.socks and self.socks[self.task_outgoing] == zmq.POLLIN:
                 message = self.task_outgoing.recv_multipart()
@@ -167,9 +179,9 @@ class Interchange(object):
                 tasks_requested = int.from_bytes(message[1], "little")
                 worker = int.from_bytes(message[0], "little")
 
-                logger.debug("Worker[{}] requested {} tasks".format(worker, tasks_requested))
+                logger.debug("[MAIN] Worker[{}] requested {} tasks".format(worker, tasks_requested))
                 if worker not in self._ready_worker_queue:
-                    logger.debug("Adding worker to ready queue")
+                    logger.debug("[MAIN] Adding worker to ready queue")
                     self._ready_worker_queue[worker] = {'last': time.time(),
                                                         'wtime': 60,
                                                         'free_slots': tasks_requested,
@@ -185,7 +197,7 @@ class Interchange(object):
                     if tasks:
                         self.task_outgoing.send_multipart([message[0], b'', pickle.dumps(tasks)])
                         tids = [t['task_id'] for t in tasks]
-                        logger.debug("Sent tasks: {} to {}".format(tids, worker))
+                        logger.debug("[MAIN] Sent tasks: {} to {}".format(tids, worker))
                         self._ready_worker_queue[worker]['free_slots'] -= len(tasks)
                         self._ready_worker_queue[worker]['tasks'].extend(tids)
                 else:
@@ -196,26 +208,26 @@ class Interchange(object):
                 b_worker, b_message = self.results_incoming.recv_multipart()
                 worker = int.from_bytes(b_worker, "little")
                 if worker not in self._ready_worker_queue:
-                    logger.debug("Received a result from a un-registered worker:{}".format(worker))
+                    logger.debug("[MAIN] Received a result from a un-registered worker:{}".format(worker))
                 else:
                     r = pickle.loads(b_message)
-                    logger.debug("Received result for task {} from {}".format(r['task_id'], worker))
-                    logger.debug("Current tasks : {}".format(self._ready_worker_queue[worker]['tasks']))
+                    logger.debug("[MAIN] Received result for task {} from {}".format(r['task_id'], worker))
+                    logger.debug("[MAIN] Current tasks : {}".format(self._ready_worker_queue[worker]['tasks']))
                     self._ready_worker_queue[worker]['tasks'].remove(r['task_id'])
                     self.results_outgoing.send(b_message)
 
             bad_workers = [worker for worker in self._ready_worker_queue if
                            time.time() - self._ready_worker_queue[worker]['last'] > self.heartbeat_thresh]
             for worker in bad_workers:
-                logger.debug("Last:{} Current:{}".format(self._ready_worker_queue[worker]['last'], time.time()))
-                logger.warning("Too many heartbeats missed for worker:{}".format(worker))
+                logger.debug("[MAIN] Last:{} Current:{}".format(self._ready_worker_queue[worker]['last'], time.time()))
+                logger.warning("[MAIN] Too many heartbeats missed for worker:{}".format(worker))
                 e = WorkerLost(worker)
                 for tid in self._ready_worker_queue[worker]['tasks']:
                     result_package = {'task_id': tid, 'exception': serialize_object(e)}
                     pkl_package = pickle.dumps(result_package)
                     self.results_outgoing.send(pkl_package)
-                    logger.warning("Sent failure reports, unregistering worker")
-                    self._ready_worker_queue.pop(worker, 'None')
+                    logger.warning("[MAIN] Sent failure reports, unregistering worker")
+                self._ready_worker_queue.pop(worker, 'None')
 
             if not start:
                 start = time.time()
@@ -244,11 +256,8 @@ def start_file_logger(filename, name='parsl.executors.mpix.interchange', level=l
         format_string = "%(asctime)s %(name)s:%(lineno)d [%(levelname)s]  %(message)s"
 
     global logger
-
-    logger = multiprocessing.get_logger()
-    return
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
     handler = logging.FileHandler(filename)
     handler.setLevel(level)
     formatter = logging.Formatter(format_string, datefmt='%Y-%m-%d %H:%M:%S')
@@ -261,7 +270,6 @@ def starter(*args, **kwargs):
     to start a new process. The args, kwargs match that of the Interchange.__init__
     """
     # logger = multiprocessing.get_logger()
-    start_file_logger("interchange.logs")
     ic = Interchange(*args, **kwargs)
     ic.start()
 
