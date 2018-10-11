cd3a67bda35c2b98364bf155f25afa57209a8bf8
Yadu Nand B <yadudoc1729@gmail.com>
2018-10-11 16:48:05 -0500

    Renaming executor files to executor.py

--- parsl/executors/__init__.py
+++ parsl/executors/__init__.py
@@ -1,7 +1,7 @@
 from parsl.executors.threads import ThreadPoolExecutor
 from parsl.executors.ipp import IPyParallelExecutor
-from parsl.executors.high_throughput.htex import HighThroughputExecutor
-from parsl.executors.extreme_scale.exex import ExtremeScaleExecutor
+from parsl.executors.high_throughput.executor import HighThroughputExecutor
+from parsl.executors.extreme_scale.executor import ExtremeScaleExecutor
 
 __all__ = ['IPyParallelExecutor',
            'ThreadPoolExecutor',
