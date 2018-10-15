641158170f71877c2dda628c37001e59bcf7facb
Yadu Nand B <yadudoc1729@gmail.com>
2018-10-15 13:46:31 -0500

    Updating config with cores_per_worker option

--- parsl/tests/configs/htex_local.py
+++ parsl/tests/configs/htex_local.py
@@ -10,11 +10,12 @@ config = Config(
         HighThroughputExecutor(
             label="htex_Local",
             engine_debug=True,
+            cores_per_worker=1,
             provider=LocalProvider(
                 channel=LocalChannel(),
                 init_blocks=1,
                 max_blocks=1,
-                tasks_per_node=4,
+                tasks_per_node=1,  # For HighThroughputExecutor, this option should in most cases be 1
                 launcher=SimpleLauncher(),
             ),
         )
