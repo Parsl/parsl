2e13910f126c65f03cc6d62ce686c037464cc000
Yadu Nand B <yadudoc1729@gmail.com>
2018-10-24 14:02:10 -0500

    Better rate limiting, increasing tput while reducing risk of overflow

--- parsl/executors/high_throughput/zmq_pipes.py
+++ parsl/executors/high_throughput/zmq_pipes.py
@@ -31,11 +31,24 @@ class TasksOutgoing(object):
         self.poller.register(self.zmq_socket, zmq.POLLOUT)
 
     def put(self, message):
+        """ This function needs to be fast at the same time aware of the possibility of
+        ZMQ pipes overflowing.
+
+        The timeout increases slowly if contention is detected on ZMQ pipes.
+        We could set copy=False and get slightly better latency but this results
+        in ZMQ sockets reaching a broken state once there are ~10k tasks in flight.
+        This issue can be magnified if each the serialized buffer itself is larger.
+        """
+        timeout_ms = 0
         while True:
-            socks = dict(self.poller.poll(timeout=1))
+            socks = dict(self.poller.poll(timeout=timeout_ms))
             if self.zmq_socket in socks and socks[self.zmq_socket] == zmq.POLLOUT:
+                # The copy option adds latency but reduces the risk of ZMQ overflow
                 self.zmq_socket.send_pyobj(message, copy=True)
                 return
+            else:
+                timeout_ms += 1
+                logger.debug("Not sending due full zmq pipe, timeout: {} ms".format(timeout_ms))
 
     def close(self):
         self.zmq_socket.close()
