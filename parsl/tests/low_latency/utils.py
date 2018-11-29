c23e1ab10927f4fa9a1e43c569110d57affed0f1
Rohan Kumar <rohankumar@cs.uchicago.edu>
2018-11-29 12:20:45 -0600

    worker.py now measures ping time to job-sender

--- parsl/tests/low_latency/utils.py
+++ parsl/tests/low_latency/utils.py
@@ -1,6 +1,7 @@
 import socket
 import fcntl
 import struct
+import subprocess
 
 
 def get_ip_address(ifname):
@@ -15,3 +16,19 @@ def get_ip_address(ifname):
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', bytes(ifname[:15], 'utf-8'))
     )[20:24])
+
+
+def ping_time(ip, n=5):
+    """
+    Returns the average ping time in microseconds.
+
+    Note: This function is inherently platform specific. 
+    It currently works on Midway.
+    """
+    cmd = "ping {} -c {}".format(ip, n)
+    p = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
+    output = str(p.communicate()[0])
+    stats = output.split("\n")[-1].split(" = ")[-1].split("/")
+    avg_ping_time = float(stats[1]) # In ms
+    return avg_ping_time * 1000
+    
