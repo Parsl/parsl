6d441fd9510c3ec9124c5cddec05fb35bd91ef7b
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2018-12-03 16:28:10 -0600

    Typo fixes

--- parsl/providers/local/local.py
+++ parsl/providers/local/local.py
@@ -224,15 +224,16 @@ class LocalProvider(ExecutionProvider, RepresentationMixin):
         for job in job_ids:
             logger.debug("Terminating job/proc_id : {0}".format(job))
             # Here we are assuming that for local, the job_ids are the process id's
-            if self.resouces[job]['proc'] :
+            if self.resources[job]['proc'] :
                 proc = self.resources[job]['proc']
                 os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
                 self.resources[job]['status'] = 'CANCELLED'
-            elif self.resource[job]['remote_pid']:
-                cmd = "kill -- -$(ps -o pgid={} | grep -o '[0-9]*')".format(self.resource[job]['remote_pid'])
+
+            elif self.resources[job]['remote_pid']:
+                cmd = "kill -- -$(ps -o pgid={} | grep -o '[0-9]*')".format(self.resources[job]['remote_pid'])
                 retcode, stdout, stderr = self.channel.execute_wait(cmd, self.cmd_timeout)
                 if retcode != 0:
-                    logger.warning("Failed to kill PID:{} and child processes on {}".format(self.resource[job]['remote_pid'],
+                    logger.warning("Failed to kill PID:{} and child processes on {}".format(self.resources[job]['remote_pid'],
                                                                                             self.label))
 
         rets = [True for i in job_ids]
