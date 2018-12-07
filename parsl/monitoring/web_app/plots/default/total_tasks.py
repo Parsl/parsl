99ff3aa7bfc07de03c595b8dbe9c50d1d0d69be9
Juan David Garrido <juandagarrido@hotmail.com>
2018-12-07 12:16:31 -0600

    Refactoring. Increased runtime speed

--- parsl/monitoring/web_app/plots/default/total_tasks.py
+++ parsl/monitoring/web_app/plots/default/total_tasks.py
@@ -56,7 +56,7 @@ class TotalTasksPlot(BasePlot):
         time_step = 60 * minutes + seconds
 
         x_axis = []
-        for i in range(min_time, max_time, time_step):
+        for i in range(min_time, max_time + time_step, time_step):
             x_axis.append(num_to_timestamp(i).strftime(DB_DATE_FORMAT))
 
         # Fill up dict "apps" like: {app1: [#task1, #task2], app2: [#task4], app3: [#task3]}
@@ -72,21 +72,24 @@ class TotalTasksPlot(BasePlot):
             items = []
             for app, tasks in apps_dict.items():
                 tmp = []
+                task = df_status[df_status['task_id'].isin(tasks)]
                 for i in range(len(x_axis) - 1):
-                    task = df_status[df_status['task_id'].isin(tasks)]
                     x = task['timestamp'] >= x_axis[i]
                     y = task['timestamp'] < x_axis[i + 1]
-                    tmp.append(sum(task.loc[[a and b for a, b in zip(x, y)]]['task_status_name'] == value))
+                    tmp.append(sum(task.loc[x & y]['task_status_name'] == value))
                 items = np.sum([items, tmp], axis=0)
 
             return items
 
+        y_axis_done = y_axis_setup('done')
+        y_axis_failed = y_axis_setup('failed')
+
         return go.Figure(data=[go.Bar(x=x_axis[:-1],
-                                      y=y_axis_setup('done'),
-                                      name='done'),
+                                      y=y_axis_done,
+                                      name='done' + ' (' + str(sum(y_axis_done)) + ')'),
                                go.Bar(x=x_axis[:-1],
-                                      y=y_axis_setup('failed'),
-                                      name='failed')],
+                                      y=y_axis_failed,
+                                      name='failed' + ' (' + str(sum(y_axis_failed)) + ')')],
                          layout=go.Layout(xaxis=dict(tickformat='%m-%d\n%H:%M:%S',
                                                      autorange=True,
                                                      title='Time'),
