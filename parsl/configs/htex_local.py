1f6f486d291de213ac81af193be7f498dc6ba929
Yadu Nand B <yadudoc1729@gmail.com>
2018-12-11 11:25:48 -0600

    Simplifying htex_local config

--- parsl/configs/htex_local.py
+++ parsl/configs/htex_local.py
@@ -1,6 +1,5 @@
 from parsl.providers import LocalProvider
 from parsl.channels import LocalChannel
-from parsl.launchers import SimpleLauncher
 
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
@@ -8,17 +7,13 @@ from parsl.executors import HighThroughputExecutor
 config = Config(
     executors=[
         HighThroughputExecutor(
-            label="htex_Local",
-            worker_debug=True,
+            label="htex_local",
             cores_per_worker=1,
             provider=LocalProvider(
                 channel=LocalChannel(),
                 init_blocks=1,
                 max_blocks=1,
-                tasks_per_node=1,  # For HighThroughputExecutor, this option should in most cases be 1
-                launcher=SimpleLauncher(),
             ),
         )
     ],
-    strategy=None,
 )
