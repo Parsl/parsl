0ea5d98b58844b3a24e1061537541c8cba6a20a6
Juan David Garrido <juandagarrido@hotmail.com>
2018-12-17 17:12:19 -0600

    Refactored table and made each row navigate to workflow based on run_id

--- parsl/monitoring/web_app/apps/workflows.py
+++ parsl/monitoring/web_app/apps/workflows.py
@@ -6,20 +6,24 @@ from parsl.monitoring.web_app.app import get_db, close_db
 
 
 def dataframe_to_html_table(id, dataframe):
+
+    def href(i):
+        return '/workflows/' + dataframe['run_id'].iloc[i]
+
     return html.Table(id=id, children=(
-            [html.Tr([html.Th(" ".join([x.capitalize() for x in col.split('_')])) for col in dataframe.columns])] +
+            [html.Tr([html.Th(" ".join([x.capitalize() for x in col.split('_')])) for col in dataframe.columns[1:]])] +
 
             # Body
             [html.Tr([
-                html.Td(html.A(children=dataframe.iloc[i]['workflow_name'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['workflow_version'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=num_to_timestamp(float(dataframe.iloc[i]['time_began'])).strftime(DB_DATE_FORMAT) if dataframe.iloc[i]['time_began'] != 'None' else 'None', href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=num_to_timestamp(float(dataframe.iloc[i]['time_completed'])).strftime(DB_DATE_FORMAT) if dataframe.iloc[i]['time_completed'] != 'None' else 'None', href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['tasks_completed_count'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['tasks_failed_count'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['user'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['host'], href='/workflows/' + dataframe['workflow_name'].iloc[i])),
-                html.Td(html.A(children=dataframe.iloc[i]['rundir'], href='/workflows/' + dataframe['workflow_name'].iloc[i]))
+                html.Td(html.A(children=dataframe.iloc[i]['workflow_name'], href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['workflow_version'], href=href(i))),
+                html.Td(html.A(children=num_to_timestamp(float(dataframe.iloc[i]['time_began'])).strftime(DB_DATE_FORMAT) if dataframe.iloc[i]['time_began'] != 'None' else 'None', href=href(i))),
+                html.Td(html.A(children=num_to_timestamp(float(dataframe.iloc[i]['time_completed'])).strftime(DB_DATE_FORMAT) if dataframe.iloc[i]['time_completed'] != 'None' else 'None', href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['tasks_completed_count'], href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['tasks_failed_count'], href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['user'], href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['host'], href=href(i))),
+                html.Td(html.A(children=dataframe.iloc[i]['rundir'], href=href(i)))
             ]) for i in range(len(dataframe))])
                       )
 
@@ -29,7 +33,8 @@ sql_conn = get_db()
 layout = html.Div(children=[
     html.H1("Workflows"),
     dataframe_to_html_table(id='workflows_table',
-                            dataframe=pd.read_sql_query("SELECT workflow_name, "
+                            dataframe=pd.read_sql_query("SELECT run_id, "
+                                                        "workflow_name, "
                                                         "workflow_version, "
                                                         "time_began, "
                                                         "time_completed, "
