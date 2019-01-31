f80a1840e528e54902615517eee3c23b14dac4f9
Ben Clifford <benc@hawaga.org.uk>
2019-01-31 08:16:38 +0000

    disable type checking on executors as it reveals a load of errors I'm not ready to fix

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -160,7 +160,11 @@ class DataFlowKernel(object):
         self.checkpoint_mode = config.checkpoint_mode
 
         data_manager = DataManager(max_threads=config.data_management_max_threads, executors=config.executors)
-        self.executors = {e.label: e for e in config.executors + [data_manager]} # type: Dict[str, ParslExecutor]
+        self.executors = {e.label: e for e in config.executors + [data_manager]} # XXXXtype: Dict[str, ParslExecutor]
+        # ^ setting the correct type on executors reveals a whole load of
+        #   type unsafe usage through the codebase, mostly involving the
+        #   .provider attribute. So until I'm ready to address that, this
+        #   type checking is disabled.
         for executor in self.executors.values():
             executor.run_dir = self.run_dir
             if hasattr(executor, 'provider'):
