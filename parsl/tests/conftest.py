d999be4722499ec6017e8d0a36ddae84df96aec0
Ben Clifford <benc@hawaga.org.uk>
2019-02-11 23:45:38 +0000

    Require parsl to have no DFK already when in pytest.
    This catches any attempt by a test module to globally initialise
    a DFK (rather than, for example, in a separate main block),
    and so attempts to catch when the test is trying to use a specific
    configuration (for example, because certain configuration parameters
    are needed)

--- parsl/tests/conftest.py
+++ parsl/tests/conftest.py
@@ -11,6 +11,7 @@ import _pytest.runner as runner
 from pytest_forked import forked_run_report
 
 import parsl
+from parsl.dataflow.dflow import DataFlowKernelLoader
 from parsl.tests.utils import get_rundir
 
 logger = logging.getLogger('parsl')
@@ -130,6 +131,10 @@ def load_dfk(config):
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
             module.config.run_dir = get_rundir()  # Give unique rundir; needed running with -n=X where X > 1.
+
+            if DataFlowKernelLoader._dfk is not None:
+              raise ValueError("DFK didn't start as None - there was a DFK from somewhere already")
+
             parsl.clear()
             dfk = parsl.load(module.config)
 
@@ -138,6 +143,7 @@ def load_dfk(config):
             if(parsl.dfk() != dfk):
                 raise ValueError("DFK was changed during test run which means the test might not have used to pytest-specified DFK: parsl.dfk={}  local dfk={}".format(parsl.dfk(), dfk))
             dfk.cleanup()
+            parsl.clear()
         except KeyError:
             pytest.skip('options in user_opts.py not configured for {}'.format(config))
     else:
