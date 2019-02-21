3a2c9b62a0c65651ec328945671ca5fbff151d89
Ben Clifford <benc@hawaga.org.uk>
2019-02-21 11:43:18 +0000

    Remove indirection of staging via File objects
    Prior to this, file objects pretty much just forwarded on
    staging requests to the data manager without doing anything
    interesting.
    
    This removes that indirection.
    
    It removes some of the need for a file to be associated
    with a data manager - something which sits awkwardly with
    wanting to reuse Files between DFKs, for example in
    interactive notebook use.
    
    Instead, each DFK has a reference to a data manager directly.

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -155,8 +155,8 @@ class DataFlowKernel(object):
         self._checkpoint_timer = None
         self.checkpoint_mode = config.checkpoint_mode
 
-        data_manager = DataManager(self, max_threads=config.data_management_max_threads)
-        self.executors = {e.label: e for e in config.executors + [data_manager]}
+        self.data_manager = DataManager(self, max_threads=config.data_management_max_threads)
+        self.executors = {e.label: e for e in config.executors + [self.data_manager]}
         for executor in self.executors.values():
             executor.run_dir = self.run_dir
             if hasattr(executor, 'provider'):
@@ -348,7 +348,7 @@ class DataFlowKernel(object):
             for dfu in self.tasks[task_id]['app_fu'].outputs:
                 f = dfu.file_obj
                 if isinstance(f, File) and f.is_remote():
-                    f.stage_out(self.tasks[task_id]['executor'])
+                    self.data_manager.stage_out(f, self.tasks[task_id]['executor'])
 
         return
 
@@ -492,16 +492,16 @@ class DataFlowKernel(object):
         inputs = kwargs.get('inputs', [])
         for idx, f in enumerate(inputs):
             if isinstance(f, File) and f.is_remote():
-                inputs[idx] = f.stage_in(executor)
+                inputs[idx] = self.data_manager.stage_in(f, executor)
 
         for kwarg, f in kwargs.items():
             if isinstance(f, File) and f.is_remote():
-                kwargs[kwarg] = f.stage_in(executor)
+                kwargs[kwarg] = self.data_manager.stage_in(f, executor)
 
         newargs = list(args)
         for idx, f in enumerate(newargs):
             if isinstance(f, File) and f.is_remote():
-                newargs[idx] = f.stage_in(executor)
+                newargs[idx] = self.data_manager.stage_in(f, executor)
 
         return tuple(newargs), kwargs
 
