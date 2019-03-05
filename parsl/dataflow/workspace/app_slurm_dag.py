8fd7d7b4baafbda9d024ca387ab3965d36568625
TakGlobus <42143716+TakGlobus@users.noreply.github.com>
2019-03-05 16:06:10 -0600

    modify for slurm jobs

---
+++ parsl/dataflow/workspace/app_slurm_dag.py
@@ -0,0 +1,238 @@
+#  _*_  coding : utf-8    _*_
+#
+#  Parsl DAG Application for strategy performance
+#  based on app_dag_zhuzhao.py
+#   Author: Takuya Kurihana 
+#
+import os
+import argparse
+import numpy as np
+import psutil
+import parsl
+from parsl import *
+from parsl.config import Config
+from parsl.executors import ThreadPoolExecutor
+from parsl.executors import HighThroughputExecutor
+from parsl.providers import LocalProvider
+from parsl.channels  import LocalChannel
+from parsl.launchers import SingleNodeLauncher
+from parsl.addresses import address_by_hostname
+from parsl.channels  import SSHChannel
+from parsl.providers import SlurmProvider
+from parsl.launchers import SrunLauncher
+import logging
+from parsl.app.app import python_app
+
+# Argument settings
+p = argparse.ArgumentParser()
+# Executor option
+p.add_argument(
+    "--executor",
+    help="executor option for configuration setting ",
+    type=str,
+    default='HighThroughput_Slurm'
+)
+# Strategy option
+p.add_argument(
+    "--strategy",
+    help="strategy option for configuration setting ",
+    type=str,
+    default='simple'
+)
+# outputfile directory
+p.add_argument(
+    "--ofiledir",
+    help="output npy-filename directory ",
+    type=str,
+    default='not-specify'
+)
+# outputfilename
+p.add_argument(
+    "--oname",
+    help="output npy-filename option ",
+    type=str,
+    default='change'
+)
+args = p.parse_args()
+print('Argparse:  --executor='+args.executor, flush=True)
+
+
+# Here we can add htex_strategy for option
+# config
+if args.executor == 'ThreadPool':
+  config = Config(
+      executors=[ThreadPoolExecutor(
+          #label='threads',
+          label='htex_local',
+          max_threads=5)
+      ],
+  )
+elif args.executor == 'HighThroughput_Local':
+  config = Config(
+      executors=[
+          HighThroughputExecutor(
+            label="htex_local",
+            cores_per_worker=1,
+            provider=LocalProvider(
+              channel=LocalChannel(),
+              init_blocks=1,
+              max_blocks=1,
+              # tasks_perss_node=1,  # For HighThroughputExecutor, this option sho<
+              launcher=SingleNodeLauncher(),
+            ),
+        )
+    ],
+    #strategy='htex_aggressive',
+    #strategy='htex_totaltime',
+    #strategy='simple',
+    strategy=args.strategy,
+  )
+elif args.executor == 'HighThroughput_Slurm':
+  config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label="midway_htex",
+            cores_per_worker=1,
+            address=address_by_hostname(),
+            provider=SlurmProvider(
+                'broadwl',    # machine name on midway
+                launcher=SrunLauncher(),
+                scheduler_options='#SBATCH --mem-per-cpu=16000 ',
+                ###scheduler_options='#SBATCH --exclusive',
+                worker_init='module load Anaconda3/5.0.0.1; source activate parsl-dev',
+                init_blocks=1,
+                max_blocks=1,
+                nodes_per_block=1,
+                # tasks_per_node=1,  # For HighThroughputExecutor, this option sho<
+                parallelism=1.0,
+                walltime='12:00:00',
+            ),
+        )
+    ],
+    #strategy='htex_aggressive',
+    #strategy='htex_totaltime',
+    #strategy='simple',
+    strategy=args.strategy,
+  )
+
+# TODO: 
+#try:
+#except:
+#  raise NameError("Invalid parsed argument")  
+
+# Load config
+print(config)
+dfk = parsl.load(config)
+
+
+@App('python', dfk)
+def sleeper(dur=5):
+    import time
+    time.sleep(dur)
+
+
+@App('python', dfk)
+def cpu_stress(dur=30):
+    import time
+    s = 0
+    start = time.time()
+    for i in range(10**8):
+        s += i
+        if time.time() - start >= dur:
+            break
+    return s
+
+@python_app
+def inc(inputs=[], init_time=0):
+    import time
+    import psutil
+    import numpy as np
+    start = time.time()
+    #sleep_duration = 600.0
+    sleep_duration = 60.0
+    _inputs = np.asarray(inputs)
+    mems = [] #_inputs[0].tolist()
+    cpus = [] #_inputs[1].tolist()
+    times =[]
+    x = 0
+    while True:
+        x += 1
+        end = time.time()
+        #if (end - start) % 10 == 0:
+        if (end - start) % 2 == 0:
+            mems += [psutil.virtual_memory().percent]
+            cpus += [psutil.cpu_percent()]
+            time += [time.time() - init_time ]
+        if end - start >= sleep_duration:
+            break
+    mems += [psutil.virtual_memory().percent]
+    cpus += [psutil.cpu_percent()]
+    time += [time.time() - init_time ]
+    return [mems, cpus, time]
+
+@python_app
+def add_inc(inputs=[], init_time=0):
+    import time
+    import psutil
+    import numpy as np
+    start = time.time()
+    #sleep_duration = 300.0
+    sleep_duration = 60.0
+    res = 0
+    _inputs = np.asarray(inputs)
+    mems = [] # _inputs[0].tolist()
+    cpus = [] # _inputs[1].tolist()
+    time = []
+    while True:
+        res += 1
+        end = time.time()
+        #if (end - start) % 10 == 0:
+        if (end - start) % 2 == 0:
+            mems += [psutil.virtual_memory().percent]
+            cpus += [psutil.cpu_percent()]
+            time += [time.time() - init_time ]
+        if end - start >= sleep_duration:
+            break
+    mems += [psutil.virtual_memory().percent]
+    cpus += [psutil.cpu_percent()]
+    time += [time.time() - init_time ]
+    return  [mems, cpus, time]
+
+if __name__ == "__main__":
+
+    #total = 10 
+    total = 100
+    half = int(total / 2)
+    one_third = int(total / 3)
+    two_third = int(total / 3 * 2)
+    mems = [psutil.virtual_memory().percent]
+    cpus = [psutil.cpu_percent()]
+    inputs = [mems, cpus]
+    init_time = time.time()   # initial time for runnnig 
+    futures_1 = [inc(inputs, init_time) for i in range(total)]
+    _outputs1 = [ i.result() for i in futures_1]
+    futures_2 = [add_inc(inputs=futures_1[0:half], init_time), add_inc(inputs=futures_1[half:total], init_time)]  
+    _outputs2 = [ i.result() for i in futures_2]
+    futures_3 = [inc(futures_2[0], init_time) for _ in range(half)] + [inc(futures_2[1], init_time) for _ in range(half)]
+    _outputs3 = [ i.result() for i in futures_3]
+    futures_4 = [add_inc(inputs=futures_3[0:one_third], init_time), 
+                 add_inc(inputs=futures_3[one_third:two_third], init_time), 
+                 add_inc(inputs=futures_3[two_third:total], init_time)
+                ]
+    _outputs4 = [ i.result() for i in futures_4]
+    
+    print([f.result() for f in futures_4])
+    print("Done")
+
+    # save output into file
+    cdir = args.ofiledir
+    try:
+      if  cdir is not 'not-specify': 
+        print("  ### Outputdir : %s " % cdir , flush=True )
+    except:
+      cdir = os.getcwd()
+    
+    for idx ,  iout in enumerate([_outputs1, _outputs2, _outputs3, _outputs4]) :
+        _iout = np.asarray(iout)
+        _idx = idx + 1
+        np.save(cdir+'/'+"outputs_"+str(_idx)+"_slurm-"+args.oname, _iout)
---
+++ parsl/dataflow/workspace/job_slurm_app_parallel.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=app_slurm
+#SBATCH --output=%x-%A-%a.out
+#SBATCH --error=%x-%A-%a.err
+#SBATCH -p broadwl
+#SBATCH --nodes=5
+#SBATCH --time=12:00:00
+###SBATCH --array 4
+#SBATCH --array=1
+
+# Here you should modify for your own conda-env
+module load Anaconda3/5.0.0.1
+source activate parsl-dev
+
+# modify config below program as following points
+# - worker_init:  e.g.worker_init='module load Anaconda3/5.0.0.1; source activate py3501'
+# - 
+
+# Settings
+cdir=`pwd`
+if [ ${SLURM_ARRAY_TASK_ID} -eq 1 ]; then
+  strategy=simple
+fi
+if [ ${SLURM_ARRAY_TASK_ID} -eq 2 ]; then
+  strategy=htex_totaltime
+fi
+if [ ${SLURM_ARRAY_TASK_ID} -eq 3 ]; then
+  strategy=htex_aggressive
+fi
+if [ ${SLURM_ARRAY_TASK_ID} -eq 4 ]; then
+  strategy=htex_gradient
+fi
+oname=${SLURM_JOB_ID}-${strategy}
+
+# Submit job
+time python app_slurm_dag.py \
+  --executor=HighThroughput_Slurm \
+  --strategy=${strategy} \
+  --oname=${oname}  \
+  --ofiledir=${cdir} 
+
