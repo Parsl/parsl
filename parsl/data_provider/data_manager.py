Parsl/parsl

commit 66b423066b199296bd9ba0d19ece22bd73192470

Remove DataManager's duplicated executor list and get it live from DFK (#805)
This is in support of PR #803 which possibly
does not support staging using dynamically
added executors without this patch.

parsl/data_provider/data_manager.py
@@ -56,8 +56,11 @@ class DataManager(ParslExecutor):
 
         return dfk.executors['data_manager']
 
-    def __init__(self, max_threads=10, executors=None):
+    def __init__(self, dfk, max_threads=10):
         """Initialize the DataManager.
+
+        Args:
+           - dfk (DataFlowKernel): The DataFlowKernel that this DataManager is managing data for.
 
         Kwargs:
            - max_threads (int): Number of threads. Default is 10.
@@ -66,9 +69,7 @@ class DataManager(ParslExecutor):
         self._scaling_enabled = False
 
         self.label = 'data_manager'
-        if executors is None:
-            executors = []
-        self.executors = {e.label: e for e in executors}
+        self.dfk = dfk
         self.max_threads = max_threads
         self.files = []
         self.globus = None
@@ -115,7 +116,7 @@ class DataManager(ParslExecutor):
         file.local_path = os.path.join(globus_ep['working_dir'], file.filename)
 
     def _get_globus_endpoint(self, executor_label=None):
-        for executor in self.executors.values():
+        for executor in self.dfk.executors.values():
             if executor_label is None or executor.label == executor_label:
                 for scheme in executor.storage_access:
                     if isinstance(scheme, GlobusScheme):
@@ -153,12 +154,12 @@ class DataManager(ParslExecutor):
         """
 
         if file.scheme == 'ftp':
-            working_dir = self.executors[executor].working_dir
+            working_dir = self.dfk.executors[executor].working_dir
             stage_in_app = self._ftp_stage_in_app(executor=executor)
             app_fut = stage_in_app(working_dir, outputs=[file])
             return app_fut._outputs[0]
         elif file.scheme == 'http' or file.scheme == 'https':
-            working_dir = self.executors[executor].working_dir
+            working_dir = self.dfk.executors[executor].working_dir
             stage_in_app = self._http_stage_in_app(executor=executor)
             app_fut = stage_in_app(working_dir, outputs=[file])
             return app_fut._outputs[0]

parsl/dataflow/dflow.py
@@ -155,7 +155,7 @@ class DataFlowKernel(object):
         self._checkpoint_timer = None
         self.checkpoint_mode = config.checkpoint_mode
 
-        data_manager = DataManager(max_threads=config.data_management_max_threads, executors=config.executors)
+        data_manager = DataManager(self, max_threads=config.data_management_max_threads)
         self.executors = {e.label: e for e in config.executors + [data_manager]}
         for executor in self.executors.values():
             executor.run_dir = self.run_dir

commit a8b091fea8f9269a45e11b1dcfaa424d62489bcd

Make duck-typing stronger for executors in DataManager
This should fix issue #809 introduced by #805

parsl/data_provider/data_manager.py
@@ -117,7 +117,7 @@ class DataManager(ParslExecutor):
 
     def _get_globus_endpoint(self, executor_label=None):
         for executor in self.dfk.executors.values():
-            if executor_label is None or executor.label == executor_label:
+            if (executor_label is None or executor.label == executor_label) and hasattr(executor, "storage_access"):
                 for scheme in executor.storage_access:
                     if isinstance(scheme, GlobusScheme):
                         if executor.working_dir: