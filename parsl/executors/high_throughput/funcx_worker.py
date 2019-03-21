cdd91324f16c8925889dbd801909b28e3cbde6a9
Yadu Nand B <yadudoc1729@gmail.com>
2019-03-21 16:17:12 -0500

    Bunch of cleanups of comments and other minor changes

--- parsl/executors/high_throughput/funcx_worker.py
+++ parsl/executors/high_throughput/funcx_worker.py
@@ -1,31 +1,33 @@
+#!/usr/bin/env python3
+
 import logging
 import argparse
 import zmq
 import os
+import sys
+import pickle
 from ipyparallel.serialize import serialize_object, unpack_apply_message
 
-def funcx_worker(worker_id, pool_id, task_url, result_url, logdir, debug=True):
-    """
+from parsl.app.errors import RemoteExceptionWrapper
 
+
+def funcx_worker(worker_id, pool_id, task_url, logdir, debug=False):
+    """
     Funcx worker will use the REP sockets to:
          task = recv ()
          result = execute(task)
          send(result)
     """
 
+    try:
+        os.makedirs("{}/{}".format(logdir, pool_id))
+    except Exception:
+        pass
 
     start_file_logger('{}/{}/funcx_worker_{}.log'.format(logdir, pool_id, worker_id),
                       worker_id,
                       name="worker_log",
                       level=logging.DEBUG if debug else logging.INFO)
-
-    try:
-        # Later check if filepath cannot exist on filesystem.
-        os.makedirs("{}/{}".format(logdir, pool_id))
-        print("MADE FILE")
-    except:
-        logger.debug("ERROR MAKING DIRS AT {}/{}".format(logdir,pool_id))
-        pass
 
     # Sync worker with master
     logger.info('Worker {} started'.format(worker_id))
@@ -36,38 +38,26 @@ def funcx_worker(worker_id, pool_id, task_url, result_url, logdir, debug=True):
 
     funcx_worker_socket = context.socket(zmq.REP)
     funcx_worker_socket.connect(task_url)
-    logger.debug("Connecting to {}".format(task_url))
+    logger.info("Connecting to {}".format(task_url))
 
     while True:
-        logger.debug("IN TASK-RECEIVE LOOP")
         # This task receiver socket is blocking.
         b_task_id, *buf = funcx_worker_socket.recv_multipart()
         # msg = task_socket.recv_pyobj()
         logger.debug("Got buffer : {}".format(buf))
 
-        try:
-            task_id = int.from_bytes(b_task_id, "little")
-
-            logger.info("Received task {}".format(task_id))
-        except Exception as e:
-            logger.debug("CAUGHT THIS: {}".format(e))
-
-
-        # serialized_result = serialize_object(1)
-        # result_package = {'task_id': task_id, 'result': serialized_result}
+        task_id = int.from_bytes(b_task_id, "little")
+        logger.info("Received task {}".format(task_id))
 
         try:
-            logger.debug("Moving to execute function...")
             result = execute_task(buf)
-
-            logger.debug("Successfully executed task!")
             serialized_result = serialize_object(result)
+
         except Exception:
             result_package = {'task_id': task_id, 'exception': serialize_object(RemoteExceptionWrapper(*sys.exc_info()))}
             logger.debug("Got exception something")
         else:
             result_package = {'task_id': task_id, 'result': serialized_result}
-            logger.debug("DID WE COMPLETE EXECUTION?") 
 
         logger.info("Completed task {}".format(task_id))
         pkl_package = pickle.dumps(result_package)
@@ -107,19 +97,13 @@ def execute_task(bufs):
     code = "{0} = {1}(*{2}, **{3})".format(resultname, fname,
                                            argname, kwargname)
 
-    logger.debug("Code string created: {}".format(code))
     try:
-        # logger.debug("[RUNNER] Executing: {0}".format(code))
-        logger.debug("Arguments parsed. Executing!!")
         exec(code, user_ns, user_ns)
-        logger.debug("Successfully executed!")
 
     except Exception as e:
-        # logger.warning("Caught exception; will raise it: {}".format(e), exc_info=True)
         raise e
 
     else:
-        # logger.debug("[RUNNER] Result: {0}".format(user_ns.get(resultname)))
         return user_ns.get(resultname)
 
 
@@ -153,10 +137,7 @@ if __name__ == "__main__":
     parser.add_argument("--worker_id")
     parser.add_argument("--pool_id")
     parser.add_argument("--task_url")
-    parser.add_argument("--result_url")
     parser.add_argument("--logdir")
 
     args = parser.parse_args()
-
-
-    worker = funcx_worker(args.worker_id, args.pool_id, args.task_url, args.result_url, args.logdir)
+    worker = funcx_worker(args.worker_id, args.pool_id, args.task_url, args.logdir)
