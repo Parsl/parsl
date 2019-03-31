96f550ad9578731fb5fdf3f7ce39fbb8f130aa30
Ben Clifford <benc@hawaga.org.uk>
2019-03-31 15:42:23 +0100

    Fix cleannet tests broken by PR #693 (#843)
    PR #693 made the test framework more aggressive about loading DFKs
    in test code when the test framework should be loading DFKs.
    
    As cleannet tests are not run in pytest, this breakage was not
    discovered by CI: these now-fixed tests are disabled by the
    '-k not cleannet' flag to pytest in CI.

--- parsl/tests/test_staging/test_implicit_staging_ftp.py
+++ parsl/tests/test_staging/test_implicit_staging_ftp.py
@@ -3,10 +3,6 @@ import pytest
 import parsl
 from parsl.app.app import App
 from parsl.data_provider.files import File
-from parsl.tests.configs.local_threads import config
-
-parsl.clear()
-parsl.load(config)
 
 
 @App('python')
