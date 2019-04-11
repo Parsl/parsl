580bd550b34690c933aa6e8c10a8e95b1d847784
Ben Clifford <benc@hawaga.org.uk>
2019-04-11 18:04:46 +0000

    fix exception sending - when tid == -1, 'exception' should contain a serialised exception, not a remote exception wrapper; but when tid != -1, 'exception' should contain a RemoteExceptionWrapper

--- parsl/executors/high_throughput/interchange.py
+++ parsl/executors/high_throughput/interchange.py
@@ -365,7 +365,7 @@ class Interchange(object):
                                 logger.debug("Setting kill event")
                                 self._kill_event.set()
                                 e = ManagerLost(manager)
-                                result_package = {'task_id': -1, 'exception': serialize_object(RemoteExceptionWrapper(e))}
+                                result_package = {'task_id': -1, 'exception': serialize_object(e)}
                                 pkl_package = pickle.dumps(result_package)
                                 self.results_outgoing.send(pkl_package)
                                 logger.warning("[MAIN] Sent failure reports, unregistering manager")
@@ -377,7 +377,7 @@ class Interchange(object):
                         if self.suppress_failure is False:
                             self._kill_event.set()
                             e = BadRegistration(manager, critical=True)
-                            result_package = {'task_id': -1, 'exception': serialize_object(RemoteExceptionWrapper(e))}
+                            result_package = {'task_id': -1, 'exception': serialize_object(e)}
                             pkl_package = pickle.dumps(result_package)
                             self.results_outgoing.send(pkl_package)
                         else:
@@ -446,17 +446,21 @@ class Interchange(object):
                 logger.debug("[MAIN] leaving results_incoming section")
 
             logger.debug("[MAIN] entering bad_managers section")
-            bad_managers = [manager for manager in self._ready_manager_queue if
-                            time.time() - self._ready_manager_queue[manager]['last'] > self.heartbeat_threshold]
+            # bad_managers = [manager for manager in self._ready_manager_queue if
+            #                time.time() - self._ready_manager_queue[manager]['last'] > self.heartbeat_threshold]
+            bad_managers = [manager for manager in self._ready_manager_queue]
             for manager in bad_managers:
                 logger.debug("[MAIN] Last: {} Current: {}".format(self._ready_manager_queue[manager]['last'], time.time()))
                 logger.warning("[MAIN] Too many heartbeats missed for manager {}".format(manager))
-                e = ManagerLost(manager)
+ 
                 for tid in self._ready_manager_queue[manager]['tasks']:
-                    result_package = {'task_id': tid, 'exception': serialize_object(RemoteExceptionWrapper(e))}
-                    pkl_package = pickle.dumps(result_package)
-                    self.results_outgoing.send(pkl_package)
-                    logger.warning("[MAIN] Sent failure reports, unregistering manager")
+                    try:
+                        raise ManagerLost(manager)
+                    except Exception as e:
+                        result_package = {'task_id': tid, 'exception': serialize_object(RemoteExceptionWrapper(e))}
+                        pkl_package = pickle.dumps(result_package)
+                        self.results_outgoing.send(pkl_package)
+                        logger.warning("[MAIN] Sent failure reports, unregistering manager")
                 self._ready_manager_queue.pop(manager, 'None')
             logger.debug("[MAIN] leaving bad_managers section")
             logger.debug("[MAIN] ending one main loop iteration")
