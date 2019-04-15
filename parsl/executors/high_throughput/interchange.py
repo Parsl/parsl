c66c0b011355328ef201dd5d028513866014d63c
Zhuozhao Li <zhuozhl@clemson.edu>
2019-04-15 17:41:51 -0500

    check the version until the micro level

--- parsl/executors/high_throughput/interchange.py
+++ parsl/executors/high_throughput/interchange.py
@@ -357,7 +357,7 @@ class Interchange(object):
                         self._ready_manager_queue[manager].update(msg)
                         logger.info("[MAIN] Registration info for manager {}: {}".format(manager, msg))
 
-                        if (msg['python_v'] != self.current_platform['python_v'] or
+                        if (msg['python_v'].rsplit(".", 1)[0] != self.current_platform['python_v'].rsplit(".", 1)[0] or
                             msg['parsl_v'] != self.current_platform['parsl_v']):
                             logger.warn("[MAIN] Manager {} has incompatible version info with the interchange".format(manager))
 
