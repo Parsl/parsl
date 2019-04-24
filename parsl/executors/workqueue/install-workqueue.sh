17ea34252e778d8fe5548458d065833a7fce8a69
Andrew Litteken <andrew.litteken@gmail.com>
2019-04-23 20:52:21 -0400

    fixing wget commands and path names

--- parsl/executors/workqueue/install-workqueue.sh
+++ parsl/executors/workqueue/install-workqueue.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 wget -O /tmp/workqueue_latest https://api.github.com/repos/cooperative-computing-lab/cctools/releases/latest
 url=$(cat /tmp/cctools_latest | grep "ubuntu" | grep "browser_download_url" | sed -E "s/^ +\"browser_download_url\": \"(.*)\"/\1/g")
-wget -o /tmp/cctools.tar.gz "$url"
-tar zxvf cctools.tar.gz
-export PATH=/tmp/cctools/bin:$PATH
-export PYTHONPATH=/tmp/cctools/lib/python3.6/site-packages:$PYTHONPATH
+wget -O /tmp/cctools.tar.gz "$url"
+tar zxvf /tmp/cctools.tar.gz
