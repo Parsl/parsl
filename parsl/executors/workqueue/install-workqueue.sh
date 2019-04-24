6d44fde5a53988eaf04ee614f75659f5a710c27d
Andrew Litteken <andrew.litteken@gmail.com>
2019-04-23 21:22:34 -0400

    more CI fixes

--- parsl/executors/workqueue/install-workqueue.sh
+++ parsl/executors/workqueue/install-workqueue.sh
@@ -2,4 +2,5 @@
 wget -O /tmp/cctools_latest https://api.github.com/repos/cooperative-computing-lab/cctools/releases/latest
 url=$(cat /tmp/cctools_latest | grep "ubuntu" | grep "browser_download_url" | sed -E "s/^ +\"browser_download_url\": \"(.*)\"/\1/g")
 wget -O /tmp/cctools.tar.gz "$url"
-tar zxvf /tmp/cctools.tar.gz
+mkdir $PWD/cctools
+tar -C $PWD/cctools -zxvf /tmp/cctools.tar.gz --strip-components=1
