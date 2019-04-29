f4e3fa46a088cee238984f2ae8d1346a8c82735e
Andrew Litteken <andrew.litteken@gmail.com>
2019-04-29 08:11:20 -0400

    adding extra step for key

--- parsl/executors/workqueue/install-workqueue.sh
+++ parsl/executors/workqueue/install-workqueue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -O /tmp/cctools_latest https://api.github.com/repos/cooperative-computing-lab/cctools/releases/latest?access_token=$CCTOOLS_KEY
-url=$(cat /tmp/cctools_latest | grep  "ubuntu" | grep "browser_download_url" | sed -E "s/^ +\"browser_download_url\": \"(.*)\"/\1/g")
+url=$(cat /tmp/cctools_latest | grep "ubuntu" | grep "browser_download_url" | sed -E "s/^ +\"browser_download_url\": \"(.*)\"/\1/g")
 wget -O /tmp/cctools.tar.gz "$url"
 mkdir /tmp/cctools
 tar -C /tmp/cctools -zxvf /tmp/cctools.tar.gz --strip-components=1
