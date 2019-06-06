Parsl/parsl

commit 4c32c46a442cb3603c6b77583daafe87b40d7b23

Make exex wrap exceptions, and cope with exceptions even when they aren't wrapped (#993)
* port commit a3c773bdec251fba8ceea31e5a60dbfd86f32c06 to extreme scale workers

* Handle the case where what I was expecting to be a wrapped exception is actually just an exception

* add missing remote exception wrapper

parsl/executors/high_throughput/executor.py
@@ -362,10 +362,15 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
                             try:
                                 s, _ = deserialize_object(msg['exception'])
                                 # s should be a RemoteExceptionWrapper... so we can reraise it
-                                try:
-                                    s.reraise()
-                                except Exception as e:
-                                    task_fut.set_exception(e)
+                                if isinstance(s, RemoteExceptionWapper):
+                                    try:
+                                        s.reraise()
+                                    except Exception as e:
+                                        task_fut.set_exception(e)
+                                elif isinstance(s, Exception):
+                                    task_fut.set_exception(s)
+                                else:
+                                    raise ValueError("Unknown exception-like type received: {}".format(type(s)))
                             except Exception as e:
                                 # TODO could be a proper wrapped exception?
                                 task_fut.set_exception(

parsl/executors/extreme_scale/mpi_worker_pool.py
@@ -16,6 +16,7 @@ import json
 
 from mpi4py import MPI
 
+from parsl.app.errors import RemoteExceptionWrapper
 from parsl.version import VERSION as PARSL_VERSION
 from ipyparallel.serialize import unpack_apply_message  # pack_apply_message,
 from ipyparallel.serialize import serialize_object
@@ -402,7 +403,7 @@ def worker(comm, rank):
         try:
             result = execute_task(req['buffer'])
         except Exception as e:
-            result_package = {'task_id': tid, 'exception': serialize_object(e)}
+            result_package = {'task_id': tid, 'exception': serialize_object(RemoteExceptionWrapper(*sys.exc_info()))}
             logger.debug("No result due to exception: {} with result package {}".format(e, result_package))
         else:
             result_package = {'task_id': tid, 'result': serialize_object(result)}

commit a6ad19fe2405fd5a4e0961a0b27139b18b8115e9

Importing and fixing typo on RemoteExceptionWrapper (#1021)
This breakage was introduced by PR #993

parsl/executors/high_throughput/executor.py
@@ -13,6 +13,7 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 from ipyparallel.serialize import pack_apply_message  # ,unpack_apply_message
 from ipyparallel.serialize import deserialize_object  # ,serialize_object
 
+from parsl.app.errors import RemoteExceptionWrapper
 from parsl.executors.high_throughput import zmq_pipes
 from parsl.executors.high_throughput import interchange
 from parsl.executors.errors import *
@@ -362,7 +363,7 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
                             try:
                                 s, _ = deserialize_object(msg['exception'])
                                 # s should be a RemoteExceptionWrapper... so we can reraise it
-                                if isinstance(s, RemoteExceptionWapper):
+                                if isinstance(s, RemoteExceptionWrapper):
                                     try:
                                         s.reraise()
                                     except Exception as e: