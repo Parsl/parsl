b1432fafb367a418aa078a22434030f815a97e8f
Ben Clifford <benc@hawaga.org.uk>
2019-06-10 08:25:30 +0000

    Change visualization --external to --listen
    This allows arbitrary IP addresses to be bound for listening, rather
    than only allowing a choice between 0.0.0.0 and 127.0.0.1
    
    This fits more in line with how most server software allows listening
    address to be chosen (see apache httpd, BIND, postgres).
    
    As a side effect, that also allows the visualization code to listen on
    IPv6.
    
    This should fix issue #1023

--- parsl/monitoring/visualization/app.py
+++ parsl/monitoring/visualization/app.py
@@ -13,8 +13,8 @@ def cli_run():
                         help='Port at which the monitoring Viz Server is hosted. Default: 8080')
     parser.add_argument("-d", "--debug", action='store_true',
                         help="Enable debug logging")
-    parser.add_argument("-e", "--external", action='store_true',
-                        help="Enable hosting on Internet")
+    parser.add_argument("-l", "--listen", type=str, default="127.0.0.1", metavar="ADDRESS",
+                        help="Choose address to listen for connections on. Default: 127.0.0.1. Choose 0.0.0.0 to listen on all addresses.")
     args = parser.parse_args()
 
     app = Flask(__name__)
@@ -26,10 +26,7 @@ def cli_run():
         db.create_all()
         from parsl.monitoring.visualization import views
         views.dummy = False
-        host = '127.0.0.1'
-        if args.external:
-            host = '0.0.0.0'
-        app.run(host=host, port=args.port, debug=args.debug)
+        app.run(host=args.listen, port=args.port, debug=args.debug)
 
 
 if __name__ == "__main__":
