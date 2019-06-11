82f56dbbb0917147963bf860b066e3eb3e7252b9
Yadu Nand B <yadudoc1729@gmail.com>
2019-06-10 23:08:52 -0500

    Adding spaces to appease flake8

--- parsl/configs/midway_htex_multinode.py
+++ parsl/configs/midway_htex_multinode.py
@@ -17,8 +17,8 @@ config = Config(
                 init_blocks=1,
                 min_blocks=1,
                 max_blocks=1,
-                scheduler_options='', # string to prepend to #SBATCH blocks in the submit script to the scheduler
-                worker_init='', # command to run before starting a worker, such as 'source activate env'
+                scheduler_options='',  # string to prepend to #SBATCH blocks in the submit script to the scheduler
+                worker_init='',        # command to run before starting a worker, such as 'source activate env'
                 walltime='00:30:00'
             ),
         )
