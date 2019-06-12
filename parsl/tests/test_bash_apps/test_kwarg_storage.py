99431c6dce7690de2a77ac37e71a22b101ccbb05
Ben Clifford <benc@hawaga.org.uk>
2019-06-12 16:01:23 +0000

    test case to demonstrate issue #1058

--- parsl/tests/test_bash_apps/test_kwarg_storage.py
+++ parsl/tests/test_bash_apps/test_kwarg_storage.py
@@ -1,32 +1,24 @@
-import argparse
 import os
-import shutil
-import time
 
-import parsl
 from parsl.app.app import App
-
-from parsl.tests.configs.local_threads import config
 
 
 @App('bash')
-def foo(x=0, y=1, z=2, stdout=None):
-    return """echo {0} {1} {z}
-    """
+def foo(z=2, stdout=None):
+    return """echo {val} {{z}}
+    """.format(val=z)
 
 
 def test_command_format_1():
     """Testing command format for BashApps
     """
 
-    stdout = os.path.abspath('std.out')
+    stdout = os.path.abspath('std.out.0')
     if os.path.exists(stdout):
         os.remove(stdout)
 
-    app_fu = foo(3, 4, stdout=stdout)
-    app_fu2 = foo(5, 6, stdout=stdout)
+    app_fu = foo(stdout=stdout)
     print("app_fu : ", app_fu)
-    print("app_fu2 : ", app_fu2)
     contents = None
 
     assert app_fu.result() == 0, "BashApp exited with an error code : {0}".format(
@@ -39,6 +31,73 @@ def test_command_format_1():
     if os.path.exists('stdout_file'):
         os.remove(stdout)
 
-    assert contents == '3 4 2\n', 'Output does not match expected string "1 4", Got: "{0}"'.format(
+    assert contents == '2 2\n', 'Output does not match expected string "2 2", Got: "{0}"'.format(
+        contents)
+
+# ===========
+
+    stdout = os.path.abspath('std.out.1')
+    if os.path.exists(stdout):
+        os.remove(stdout)
+
+    app_fu = foo(z=3, stdout=stdout)
+    print("app_fu : ", app_fu)
+    contents = None
+
+    assert app_fu.result() == 0, "BashApp exited with an error code : {0}".format(
+        app_fu.result())
+
+    with open(stdout, 'r') as stdout_f:
+        contents = stdout_f.read()
+        print("Contents : ", contents)
+
+    if os.path.exists('stdout_file'):
+        os.remove(stdout)
+
+    assert contents == '3 3\n', 'Output does not match expected string "3 3", Got: "{0}"'.format(
+        contents)
+
+# ===========
+    stdout = os.path.abspath('std.out.2')
+    if os.path.exists(stdout):
+        os.remove(stdout)
+
+    app_fu = foo(z=4, stdout=stdout)
+    print("app_fu : ", app_fu)
+    contents = None
+
+    assert app_fu.result() == 0, "BashApp exited with an error code : {0}".format(
+        app_fu.result())
+
+    with open(stdout, 'r') as stdout_f:
+        contents = stdout_f.read()
+        print("Contents : ", contents)
+
+    if os.path.exists('stdout_file'):
+        os.remove(stdout)
+
+    assert contents == '4 4\n', 'Output does not match expected string "4 4", Got: "{0}"'.format(
+        contents)
+
+# ===========
+    stdout = os.path.abspath('std.out.3')
+    if os.path.exists(stdout):
+        os.remove(stdout)
+
+    app_fu = foo(stdout=stdout)
+    print("app_fu : ", app_fu)
+    contents = None
+
+    assert app_fu.result() == 0, "BashApp exited with an error code : {0}".format(
+        app_fu.result())
+
+    with open(stdout, 'r') as stdout_f:
+        contents = stdout_f.read()
+        print("Contents : ", contents)
+
+    if os.path.exists('stdout_file'):
+        os.remove(stdout)
+
+    assert contents == '2 2\n', 'Output does not match expected string "2 2", Got: "{0}"'.format(
         contents)
     return True
