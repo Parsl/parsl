af37e041853830f119fa3ade6098ec65df72fc61
Ben Clifford <benc@hawaga.org.uk>
2019-06-14 19:52:54 +0000

    remove output if it exists already

--- parsl/tests/test_docs/test_from_slides.py
+++ parsl/tests/test_docs/test_from_slides.py
@@ -1,5 +1,7 @@
 from parsl.app.app import App
 
+import os
+
 
 @App('bash')
 def echo(message, outputs=[]):
@@ -14,6 +16,9 @@ def cat(inputs=[]):
 
 def test_slides():
     """Testing code snippet from slides """
+
+    if os.path.exists('hello1.txt'):
+        os.remove('hello1.txt')
 
     hello = echo("Hello World!", outputs=['hello1.txt'])
     message = cat(inputs=[hello.outputs[0]])
