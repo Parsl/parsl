359691457c83e5c0e7104d49b28914e0ae6b26b1
Zhuozhao Li <zhuozhao@uchicago.edu>
2019-06-21 12:46:29 -0500

    add reg_time, cpu and memory, rename hostname in manager registration msg (#1087)
    * add reg_time, cpu and memory info to registration msg
    
    * remove if condition to check reg_time

--- parsl/executors/extreme_scale/mpi_worker_pool.py
+++ parsl/executors/extreme_scale/mpi_worker_pool.py
@@ -9,6 +9,7 @@ import platform
 import threading
 import pickle
 import time
+import datetime
 import queue
 import uuid
 import zmq
@@ -98,11 +99,12 @@ class Manager(object):
                                              sys.version_info.minor,
                                              sys.version_info.micro),
                'os': platform.system(),
-               'hname': platform.node(),
+               'hostname': platform.node(),
                'dir': os.getcwd(),
                'prefetch_capacity': 0,
                'worker_count': (self.comm.size - 1),
                'max_capacity': (self.comm.size - 1) + 0,  # (+prefetch)
+               'reg_time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         }
         b_msg = json.dumps(msg).encode('utf-8')
         return b_msg
