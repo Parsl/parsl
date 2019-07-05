fcfb636f17cf6b303983a964167559c16c1f8332
Ben Clifford <benc@hawaga.org.uk>
2019-07-05 22:07:38 +0000

    fix flake8 in test

--- parsl/tests/test_error_handling/test_python_walltime.py
+++ parsl/tests/test_error_handling/test_python_walltime.py
@@ -1,21 +1,23 @@
 import parsl
 from parsl.app.errors import AppTimeout
 
+
 @parsl.python_app
 def my_app(walltime=3):
-  import time
-  # the loop count must be substantially bigger than the walltime
-  # but not infinite - so that the test eventually terminates
-  # even if the walltime doesn't work.
-  for n in range(0, 6):
-    time.sleep(1)    
+    import time
+    # the loop count must be substantially bigger than the walltime
+    # but not infinite - so that the test eventually terminates
+    # even if the walltime doesn't work.
+    for n in range(0, 6):
+        time.sleep(1)
+
 
 def test_python_walltime():
-  f = my_app()
-  try:
-    f.result()
-    raise ValueError("Expected an exception, not a result")
-  except AppTimeout as e:
-    # AppTimeout is the correct result.
-    # Any other exception should propagate upwards
-    pass
+    f = my_app()
+    try:
+        f.result()
+        raise ValueError("Expected an exception, not a result")
+    except AppTimeout:
+        # AppTimeout is the correct result.
+        # Any other exception should propagate upwards
+        pass
