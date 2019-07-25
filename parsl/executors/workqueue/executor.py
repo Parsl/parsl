a2e2c31c194e3eeb541ea7f5467f0babf937bf4b
TJ Dasso <tjdasso@gmail.com>
2019-07-25 15:14:46 -0400

    Merge Resolution

--- parsl/executors/workqueue/executor.py
+++ parsl/executors/workqueue/executor.py
@@ -241,6 +241,8 @@ def WorkQueueSubmitThread(task_queue=multiprocessing.Queue(),
                                 reason += "problem writing out function result"
                             else:
                                 reason += "unable to process wrapper script failure"
+                            reason += "\nTrace:\n" + t.output
+                            logger.debug("WorkQueue runner script failed for task {} because {}\n".format(parsl_tid, reason))
                         # WorkQueue system failure
                         else:
                             reason = "WorkQueue System Failure: "
