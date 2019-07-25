92f7f309646770931c4c4f7da70ead4f714b7b01
Ben Clifford <benc@hawaga.org.uk>
2019-07-25 12:33:50 +0000

    fix flake8

--- parsl/tests/test_error_handling/test_fail.py
+++ parsl/tests/test_error_handling/test_fail.py
@@ -1,11 +1,12 @@
-import parsl
 import pytest
 
 from parsl.app.app import python_app
 
+
 @python_app
 def always_fail():
     raise ValueError("This ValueError should propagate to the app caller in fut.result()")
+
 
 def test_simple():
     with pytest.raises(ValueError):
