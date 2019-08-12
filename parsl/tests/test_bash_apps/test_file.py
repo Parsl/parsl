491bf8d4812121941f5a7d704949b367afc323cb
Ben Clifford <benc@hawaga.org.uk>
2019-08-12 14:02:30 +0000

    add test with no fancy path components, as basic validation before other more complicated tests run

--- parsl/tests/test_bash_apps/test_file.py
+++ parsl/tests/test_bash_apps/test_file.py
@@ -8,6 +8,22 @@ import parsl.app.errors as perror
 from parsl.app.app import App
 from parsl.tests.configs.local_threads import config
 
+def test_stdout_simple():
+
+    out = 't1.out'
+    err = 't1.err'
+    os.system('rm -f ' + out + ' ' + err)
+
+    echo_to_streams('hi', stdout=out, stderr=err).result()
+    len1 = len(open(out).readlines())
+    assert len1 == 1,  "Line count of first output should be 1 but len1={}".format(len1)
+
+    echo_to_streams('hi', stdout=out, stderr=err).result()
+    len2 = len(open(out).readlines())
+    assert len2 == 2,  "Line count of second output should be 2 but len2={}".format(len2)
+
+    os.system('rm -f ' + out + ' ' + err)
+
 
 @App('bash')
 def echo_to_streams(msg, stderr='std.err', stdout='std.out'):
