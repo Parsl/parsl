d1ad899d0fd8508864239aebb3d24dcd33ece6bc
Ben Clifford <benc@hawaga.org.uk>
2019-08-23 18:40:33 +0000

    Revert "remove test that uses outdated DFK param so hasn't been run recently..."
    This reverts commit 78c4089f3bb82737b3925bd889feb49021bb4695.

---
+++ parsl/tests/integration/test_stress/test_bash_apps.py
@@ -0,0 +1,41 @@
+"""Testing bash apps
+"""
+import parsl
+from parsl import App, DataFlowKernel, ThreadPoolExecutor
+
+print("Parsl version: ", parsl.__version__)
+
+import time
+import argparse
+
+workers = ThreadPoolExecutor(max_threads=10)
+dfk = DataFlowKernel(executors=[workers])
+
+
+@App('bash', dfk)
+def sleep_foo(sleepdur, stdout=None):
+    return """sleep {0}"""
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", default="10",
+                        help="Count of apps to launch")
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Count of apps to launch")
+    args = parser.parse_args()
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    fus = {}
+
+    start = time.time()
+    for i in range(int(args.count)):
+
+        fus[i] = sleep_foo(5)
+
+    print([(key, fus[key].result()) for key in fus])
+    end = time.time()
+    print("Total time : ", end - start)
