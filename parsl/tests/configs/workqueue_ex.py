82e46ab809e2630bff9133c8a7d0f148b1001a63
Ben Clifford <benc@hawaga.org.uk>
2019-08-25 18:45:28 +0000

    WQ hack to remove

--- parsl/tests/configs/workqueue_ex.py
+++ parsl/tests/configs/workqueue_ex.py
@@ -2,4 +2,5 @@ from parsl.config import Config
 from parsl.executors import WorkQueueExecutor
 
 config = Config(executors=[WorkQueueExecutor(port=9000,
+                                             see_worker_output=True,
                                              project_name="wq-testing")])
