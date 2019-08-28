0fab19cfc14d7fe2c599228b0c448ed1e3ec5d02
Anna Elizabeth Woodard <awoodard@nd.edu>
2019-08-28 12:49:45 -0500

    Update run script to match current travis invocation

--- parsl/tests/run.sh
+++ parsl/tests/run.sh
@@ -1,55 +1,26 @@
-export PARSL_TESTING="true"
-#pip install -r test-requirements.txt
-
 set -e
-flake8 parsl/
 
-parsl/tests/lint-inits.sh
-set +e
+flake8 parsl/
+bash parsl/tests/lint-inits.sh
 
 # This uses all of the configurations and tests as the base from which to
 # run mypy checks - these are likely to capture most of the code used in
 # parsl
-for test in parsl/tests/configs/*.py parsl/tests/test*/test*
-  do
-    MYPYPATH=$(pwd)/mypy-stubs mypy $test
-    export MypyReturnCode=$?
-    echo mypy return code is $MypyReturnCode
-    if [[ "$MypyReturnCode" != 0 ]] ; then
-      exit 1
-    fi
-  done
+MYPYPATH=$(pwd)/mypy-stubs mypy parsl/tests/configs/
+MYPYPATH=$(pwd)/mypy-stubs mypy parsl/tests/test*/
+MYPYPATH=$(pwd)/mypy-stubs mypy parsl/tests/sites/
 
 # do this before any testing, but not in-between tests
 rm -f .coverage
 
-for test in parsl/tests/test*/test*
-  do
-    pytest $test -k "not cleannet" --config parsl/tests/configs/htex_local.py --cov=parsl --cov-append --cov-report=
-    export PytestReturnCode=$?
-    echo pytest return code is $PytestReturnCode
-    # allow exit code 5; this means pytest did not run a test in the specified file
-    if [[ "$PytestReturnCode" != 0 ]] && [[ "$PytestReturnCode" != 5 ]]; then
-      exit 1
-    fi
-  done
-
-for test in parsl/tests/test*/test*
-  do pytest $test -k "not cleannet" --config parsl/tests/configs/local_threads.py --cov=parsl --cov-append --cov-report=
-    export PytestReturnCode=$?
-    echo pytest return code is $PytestReturnCode
-    # allow exit code 5; this means pytest did not run a test in the specified file
-    if [[ "$PytestReturnCode" != 0 ]] && [[ "$PytestReturnCode" != 5 ]]; then
-      exit 1
-    fi
-  done
+pytest parsl -k "not cleannet" --config parsl/tests/configs/htex_local.py --cov=parsl --cov-append --cov-report= --random-order
+pytest parsl -k "not cleannet" --config parsl/tests/configs/local_threads.py --cov=parsl --cov-append --cov-report= --random-order
 
 # some of the site/ tests require more dependencies. These are installed here as needed,
 # so that the above tests happen with only the basic requirements installed.
 
-set -e
 # workqueue
-bash parsl/executors/workqueue/install-workqueue.sh
+./parsl/executors/workqueue/install-workqueue.sh
 export PATH=$PATH:/tmp/cctools/bin
 export PYTHONPATH=/tmp/cctools/lib/python3.5/site-packages
 
@@ -62,42 +33,18 @@ pip install .[extreme_scale,monitoring]
 
 work_queue_worker localhost 9000 &> /dev/null &
 
-set +e
-for test in parsl/tests/test*/test*
-  do
-    pytest $test -k "not cleannet" --config parsl/tests/configs/workqueue_ex.py --cov=parsl --cov-append --cov-report=
-    export PytestReturnCode=$?
-    echo pytest return code is $PytestReturnCode
-    if [[ "$PytestReturnCode" != 0 ]] && [[ "$PytestReturnCode" != 5 ]]; then
-      exit 1
-    fi
-  done
-
+pytest parsl -k "not cleannet" --config parsl/tests/configs/workqueue_ex.py --cov=parsl --cov-append --cov-report= --random-order --bodge-dfk-per-test
 kill -3 $(ps aux | grep -E -e "[0-9]+:[0-9]+ work_queue_worker" | tr -s ' ' | cut -f 2 -d " ")
 
-# These tests run with specific configs loaded within the tests themselves.
+# these tests run with specific configs loaded within the tests themselves.
 # This mode is enabled with: --config local
-for test in parsl/tests/test*/test* parsl/tests/sites/test*
-  do
-    pytest $test -k "not cleannet" --config local --cov=parsl --cov-append --cov-report=
-    export PytestReturnCode=$?
-    echo pytest return code is $PytestReturnCode
-    if [[ "$PytestReturnCode" != 0 ]] && [[ "$PytestReturnCode" != 5 ]]; then
-      exit 1
-    fi
-  done
+pytest parsl -k "not cleannet" --config local --cov=parsl --cov-append --cov-report= --random-order
 
-set -e
 # run simple worker test. this is unlikely to scale due to
 # a stdout/stderr buffering bug in present master.
-coverage run --append --source=parsl parsl/tests/manual_tests/test_worker_count.py -c 1000
+# - coverage run --append --source=parsl parsl/tests/manual_tests/test_worker_count.py -c 1000
+# TODO: ^ this test has been removed pending investigation ... when restored or removed,
+#       sort out this commented out block appropriately.
 
-# run specific integration tests that need their own configuration
-pytest parsl/tests/integration/test_retries.py -k "not cleannet" --config local --cov=parsl --cov-append --cov-report=
-pytest parsl/tests/integration/test_parsl_load_default_config.py -k "not cleannet" --config local --cov=parsl --cov-append --cov-report=
-
-coverage report
 # prints report of coverage data stored in .coverage
-
-pytest parsl/tests --config parsl/tests/configs/local_threads.py
-pytest parsl/tests --config parsl/tests/configs/local_ipp.py
+coverage report
