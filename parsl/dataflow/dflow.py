31871eefa9ce8c755b08e9f3f6fcd06aedd87de2
Ben Clifford <benc@hawaga.org.uk>
2019-09-09 13:48:25 +0000

    Fix bad reference in monitoring to no-longer-existing internal AppFuture field

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -202,7 +202,7 @@ class DataFlowKernel(object):
             task_log_info['task_fail_history'] = ",".join(self.tasks[task_id]['fail_history'])
         task_log_info['task_depends'] = None
         if self.tasks[task_id]['depends'] is not None:
-            task_log_info['task_depends'] = ",".join([str(t._tid) for t in self.tasks[task_id]['depends']])
+            task_log_info['task_depends'] = ",".join([str(t.tid) for t in self.tasks[task_id]['depends']])
         task_log_info['task_elapsed_time'] = None
         if self.tasks[task_id]['time_returned'] is not None:
             task_log_info['task_elapsed_time'] = (self.tasks[task_id]['time_returned'] -
