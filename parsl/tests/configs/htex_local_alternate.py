7747325c8bd915baddca5033c585844834a5d20c
Ben Clifford <benc@hawaga.org.uk>
2019-09-09 21:09:41 +0000

    try out original htex config as start of slowly adding in the alternate-config changes

--- parsl/tests/configs/htex_local_alternate.py
+++ parsl/tests/configs/htex_local_alternate.py
@@ -21,6 +21,7 @@ import os
 from parsl.providers import LocalProvider
 from parsl.channels import LocalChannel
 from parsl.launchers import SingleNodeLauncher
+from parsl.launchers import SimpleLauncher
 
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
@@ -34,6 +35,23 @@ from parsl.data_provider.file_noop import NoOpFileStaging
 working_dir = os.getcwd() + "/" + "test_htex_alternate"
 
 config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label="htex_Local",
+            worker_debug=True,
+            cores_per_worker=1,
+            provider=LocalProvider(
+                channel=LocalChannel(),
+                init_blocks=1,
+                max_blocks=1,
+                launcher=SimpleLauncher(),
+            ),
+        )
+    ],
+    strategy=None,
+)
+
+big_config = Config(
     executors=[
         HighThroughputExecutor(
             label="htex_Local",
