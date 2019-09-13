af4927c1d3d8232238a06e0b70563ff4008e1866
Ben Clifford <benc@hawaga.org.uk>
2019-09-13 08:08:08 +0000

    Add task definition state assertions to dependency propagation test

--- parsl/tests/test_python_apps/test_depfail_propagation.py
+++ parsl/tests/test_python_apps/test_depfail_propagation.py
@@ -1,6 +1,8 @@
 from parsl import python_app
 from parsl.dataflow.error import DependencyError
 
+from parsl.dataflow.states import States
+
 
 @python_app
 def fails():
@@ -16,8 +18,12 @@ def test_depfail_once():
     """Test the simplest dependency failure case"""
     f1 = fails()
     f2 = depends(f1)
+
     assert isinstance(f1.exception(), Exception)
+    assert f1.task_def['status'] == States.failed
+
     assert isinstance(f2.exception(), DependencyError)
+    assert f2.task_def['status'] == States.dep_fail
 
 
 def test_depfail_chain():
@@ -26,10 +32,18 @@ def test_depfail_chain():
     f2 = depends(f1)
     f3 = depends(f2)
     f4 = depends(f3)
+
     assert isinstance(f1.exception(), Exception)
+    assert f1.task_def['status'] == States.failed
+
     assert isinstance(f2.exception(), DependencyError)
+    assert f2.task_def['status'] == States.dep_fail
+
     assert isinstance(f3.exception(), DependencyError)
+    assert f3.task_def['status'] == States.dep_fail
+
     assert isinstance(f4.exception(), DependencyError)
+    assert f4.task_def['status'] == States.dep_fail
 
 
 def test_depfail_branches():
@@ -39,6 +53,12 @@ def test_depfail_branches():
     f1 = fails()
     f2 = depends(f1)
     f3 = depends(f1)
+
     assert isinstance(f1.exception(), Exception)
+    assert f1.task_def['status'] == States.failed
+
     assert isinstance(f2.exception(), DependencyError)
+    assert f2.task_def['status'] == States.dep_fail
+
     assert isinstance(f3.exception(), DependencyError)
+    assert f3.task_def['status'] == States.dep_fail
