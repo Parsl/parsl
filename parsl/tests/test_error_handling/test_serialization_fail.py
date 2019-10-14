2429a207147ad41f129186a79af5e7701a438d8d
Yadu Nand B <yadudoc1729@gmail.com>
2019-10-14 11:59:40 -0500

    Adding a test

---
+++ parsl/tests/test_error_handling/test_serialization_fail.py
@@ -0,0 +1,24 @@
+import pytest
+
+import threading
+from parsl import python_app
+from parsl.tests.configs.htex_local import fresh_config
+from parsl.executors.errors import SerializationError
+
+local_config = fresh_config()
+local_config.retries = 2
+
+
+@python_app
+def fail_pickling(x):
+    return True
+
+
+@pytest.mark.local
+def test_serialization_error():
+
+    lock = threading.Lock()
+    x = fail_pickling(lock)
+
+    with pytest.raises(SerializationError):
+        x.result()
