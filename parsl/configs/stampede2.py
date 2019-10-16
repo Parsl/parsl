f1fd33e48e0de9cdd80d765d8b34d471b10fec59
Yadu Nand Babuji <yadudoc1729@gmail.com>
2019-10-16 17:48:37 -0500

    Update stampede2 config to use SrunLauncher (#1368)
    * Updating stampede2 config to use SrunLauncher
    
    * Removing the older stampede2 config

--- parsl/configs/stampede2.py
+++ parsl/configs/stampede2.py
@@ -1,5 +1,6 @@
 from parsl.config import Config
 from parsl.providers import SlurmProvider
+from parsl.launchers import SrunLauncher
 from parsl.executors import HighThroughputExecutor
 from parsl.addresses import address_by_hostname
 from parsl.data_provider.globus import GlobusStaging
@@ -10,10 +11,12 @@ config = Config(
         HighThroughputExecutor(
             label='Stampede2_HTEX',
             address=address_by_hostname(),
+            max_workers=2,
             provider=SlurmProvider(
                 nodes_per_block=2,
                 init_blocks=1,
                 min_blocks=1,
+                max_blocks=1,
                 partition='YOUR_PARTITION',
                 # string to prepend to #SBATCH blocks in the submit
                 # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
@@ -21,6 +24,7 @@ config = Config(
                 # Command to be run before starting a worker, such as:
                 # 'module load Anaconda; source activate parsl_env'.
                 worker_init='',
+                launcher=SrunLauncher(),
                 walltime='00:30:00'
             ),
             storage_access=[GlobusStaging(
