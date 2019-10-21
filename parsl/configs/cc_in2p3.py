94de10275a0190fd7e59c5d5c2261fb81feeab72
Yadu Nand Babuji <yadudoc1729@gmail.com>
2019-10-21 16:10:54 -0500

    Restructure site configs (#1372)
    * Removing summit_local_htex.py which was copied instead of moved to summit.py in PR #1362
    
    * Adding updated and tested midway config
    
    * Updating tested Cori config
    
    * Updating CC_IN2P3 config with HTEX.
    
    * Adding updated Ad Hoc cluster config
    
    * Removing IPP based configs and local configs with minor feature mods
    
    Feature options should be covered in user guide and not in the standard site configs list.
    
    * Updating docs to point at new configs for summit, midway, cc_in2p3 and cori
    
    * Updating config link for ad_hoc

---
+++ parsl/configs/cc_in2p3.py
@@ -0,0 +1,24 @@
+from parsl.config import Config
+from parsl.channels import LocalChannel
+from parsl.providers import GridEngineProvider
+from parsl.executors import HighThroughputExecutor
+from parsl.addresses import address_by_query
+
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label='cc_in2p3_htex',
+            address=address_by_query(),
+            max_workers=2,
+            provider=GridEngineProvider(
+                channel=LocalChannel(),
+                nodes_per_block=1,
+                init_blocks=2,
+                max_blocks=2,
+                walltime="00:20:00",
+                scheduler_options='',     # Input your scheduler_options if needed
+                worker_init='',     # Input your worker_init if needed
+            ),
+        )
+    ],
+)
--- parsl/configs/local_ipp.py
+++
@@ -1,4 +0,0 @@
-from parsl.config import Config
-from parsl.executors.ipp import IPyParallelExecutor
-
-config = Config(executors=[IPyParallelExecutor()])
