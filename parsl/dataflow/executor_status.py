4b5dcf9be4e9fe435216d5aba542ce08a279070b
hategan <hategan@mcs.anl.gov>
2019-10-30 16:16:15 -0700

    The core logic

---
+++ parsl/dataflow/executor_status.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from typing import Dict, Any
+
+
+class ExecutorStatus(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def executor(self) -> "parsl.executors.base.ParslExecutor":
+        pass
+
+    @property
+    @abstractmethod
+    def status(self) -> Dict[Any, "parsl.providers.provider_base.JobStatus"]:
+        pass
