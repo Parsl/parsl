6f576453fac83e75597f4b589a0ed910fb734f1e
Ben Clifford <benc@hawaga.org.uk>
2019-10-31 15:22:06 -0300

    Remove outdated HTEX module docstring (#1421)
    There is more description in the docstring for
    HighThroughputExecutor, which is also available
    via parsl.HighThroughputExecutor.

--- parsl/executors/high_throughput/executor.py
+++ parsl/executors/high_throughput/executor.py
@@ -1,6 +1,5 @@
-"""HighThroughputExecutor builds on the Swift/T EMEWS architecture to use MPI for fast task distribution
-"""
-
+from concurrent.futures import Future
+import typeguard
 import logging
 import math
 import pickle
