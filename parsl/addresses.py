5ff95ab6530d513d35fc686df2d45afc5dee8916
Ben Clifford <benc@hawaga.org.uk>
2019-11-01 12:06:24 +0000

    change 'correct' to 'usable'. add module docstring

--- parsl/addresses.py
+++ parsl/addresses.py
@@ -1,3 +1,11 @@
+"""This module contains several helper functions which can be used to
+find an address of the submitting system, for example to use as the
+address parameter for HighThroughputExecutor.
+
+The helper to use depends on the network environment around the submitter,
+so some experimentation will probably be needed to choose the correct one.
+"""
+
 import logging
 import os
 import platform
@@ -13,7 +21,7 @@ def address_by_route() -> str:
     """Finds an address for the local host by querying the local routing table
        for the route to Google DNS.
 
-       This will return an incorrect value when the internet-facing address is
+       This will return an unusable value when the internet-facing address is
        not reachable from workers.
     """
     logger.debug("Finding address by querying local routing table")
@@ -23,8 +31,8 @@ def address_by_route() -> str:
 
 
 def address_by_query() -> str:
-    """Finds an address for the local host by querying ipify. This will
-       return an incorrect value when the host is behind NAT, or when the
+    """Finds an address for the local host by querying ipify. This may
+       return an unusable value when the host is behind NAT, or when the
        internet-facing address is not reachable from workers.
     """
     logger.debug("Finding address by querying remote service")
@@ -36,8 +44,8 @@ def address_by_query() -> str:
 def address_by_hostname() -> str:
     """Returns the hostname of the local host.
 
-       This will return an incorrect value when the hostname cannot be
-       resolved correctly from workers.
+       This will return an unusable value when the hostname cannot be
+       resolved from workers.
     """
     logger.debug("Finding address by using local hostname")
     addr = platform.node()
