3f79583e360fbb8c6cf2b404b2d810df44e71933
Ben Clifford <benc@hawaga.org.uk>
2019-11-10 20:14:59 +0000

    update to work against recent master and fix flake8

--- parsl/tests/test_staging/test_1316.py
+++ parsl/tests/test_staging/test_1316.py
@@ -7,12 +7,14 @@ from parsl.config import Config
 from parsl.data_provider.files import File
 from parsl.data_provider.staging import Staging
 
+
 @python_app
 def observe_input_local_path(f):
     """Returns the local_path that is seen by the app,
     so that the test suite can also see that path.
     """
     return f.local_path
+
 
 @python_app
 def wait_and_create(outputs=[]):
@@ -21,64 +23,6 @@ def wait_and_create(outputs=[]):
     # names and Futures.
     time.sleep(10)
 
-
-class SP1(Staging):
-    def can_stage_in(self, file):
-        return file.scheme == 'sp1'
-
-    def stage_in(self, dm, executor, file, parent_fut):
-        file.local_path = "./test1.tmp"
-        return file  # specify an updated File object to be used on the app side
-
-    def can_stage_out(self, file):
-        return file.scheme == 'sp1'
-
-    def stage_out(self, dm, executor, file, app_fu):
-        file.local_path = "./test1.tmp"
-        return None # no tasks inserted so parsl should use app completion for DataFuture completion
-
-
-@pytest.mark.local
-def test_1316_local_path_on_execution_side():
-    """This test demonstrates the ability of a StagingProvider to set the
-    local_path of a File on the execution side, but that the change does not
-    modify the local_path of the corresponding submit side File, even when
-    running in a single python process.
-    """
-
-    config = Config(executors=[ThreadPoolExecutor(storage_access=[SP1()])])
-
-    file = File("sp1://test")
-
-    parsl.load(config)
-    p = observe_input_local_path(file).result()
-
-    assert p == "./test1.tmp", "File object on the execution side gets the local_path set by the staging provider"
-
-    assert not hasattr(file, 'local_path'), "The local_path on the submit side is not modified"
-
-    parsl.clear()
-
-@pytest.mark.local
-def test_1316_local_path_setting_preserves_dependency():
-    config = Config(executors=[ThreadPoolExecutor(storage_access=[SP1()])])
-
-    file = File("sp1://test")
-
-    parsl.load(config)
-
-    wc_app_future = wait_and_create(outputs=[file])
-    data_future = wc_app_future.outputs[0]
-    
-    p = observe_input_local_path(data_future).result()
-
-    assert wc_app_future.done(), "wait_and_create should finish before observe_input_local_path finishes"
-
-    assert p == "./test1.tmp", "File object on the execution side gets the local_path set by the staging provider"
-
-    assert not hasattr(file, 'local_path'), "The local_path on the submit side is not modified"
-
-    parsl.clear()
 
 class SP2(Staging):
     def can_stage_in(self, file):
@@ -93,7 +37,7 @@ class SP2(Staging):
 
     def stage_out(self, dm, executor, file, app_fu):
         file.local_path = "./test1.tmp"
-        return None # no tasks inserted so parsl should use app completion for DataFuture completion
+        return None  # no tasks inserted so parsl should use app completion for DataFuture completion
 
 
 @pytest.mark.local
@@ -113,9 +57,10 @@ def test_1316_local_path_on_execution_side_sp2():
 
     assert p == "./test1.tmp", "File object on the execution side gets the local_path set by the staging provider"
 
-    assert not hasattr(file, 'local_path'), "The local_path on the submit side is not modified"
+    assert not file.local_path, "The local_path on the submit side should not be set"
 
     parsl.clear()
+
 
 @pytest.mark.local
 def test_1316_local_path_setting_preserves_dependency_sp2():
@@ -127,15 +72,13 @@ def test_1316_local_path_setting_preserves_dependency_sp2():
 
     wc_app_future = wait_and_create(outputs=[file])
     data_future = wc_app_future.outputs[0]
-    
+
     p = observe_input_local_path(data_future).result()
 
     assert wc_app_future.done(), "wait_and_create should finish before observe_input_local_path finishes"
 
     assert p == "./test1.tmp", "File object on the execution side gets the local_path set by the staging provider"
 
-    assert not hasattr(file, 'local_path'), "The local_path on the submit side is not modified"
+    assert not file.local_path, "The local_path on the submit side should not be set"
 
     parsl.clear()
-
-
