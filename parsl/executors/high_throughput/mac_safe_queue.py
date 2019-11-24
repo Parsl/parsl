52efd08fa3239a520f915bb24acd29f8844484da
Yadu Nand B <yadudoc1729@gmail.com>
2019-11-23 19:47:25 -0600

    Fixed and tested mac safe queue

--- parsl/executors/high_throughput/mac_safe_queue.py
+++ parsl/executors/high_throughput/mac_safe_queue.py
@@ -1,6 +1,5 @@
 import multiprocessing
 import multiprocessing.queues
-import threading
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,31 +9,59 @@ class MacSafeQueue(multiprocessing.queues.Queue):
     """ Multiprocessing queues do not have qsize attributes on MacOS.
     This is slower but more portable version of the multiprocessing Queue
     that adds a explicit counter
+
+    Reference : https://github.com/keras-team/autokeras/commit/4ddd568b06b4045ace777bc0fb7bc18573b85a75
     """
 
     def __init__(self, *args, **kwargs):
         if 'ctx' not in kwargs:
             kwargs['ctx'] = multiprocessing.get_context()
         super().__init__(*args, **kwargs)
-        self._lock = threading.Lock()
-        self._counter = 0
+        self._counter = multiprocessing.Value('i', 0)
 
     def put(self, *args, **kwargs):
-        logger.critical("Putting item {}".format(args))
+        # logger.critical("Putting item {}".format(args))
         x = super().put(*args, **kwargs)
-        with self._lock:
-            self._counter += 1
+        with self._counter.get_lock():
+            self._counter.value += 1
         return x
 
     def get(self, *args, **kwargs):
         x = super().get(*args, **kwargs)
-        with self._lock:
-            self._counter -= 1
-        logger.critical("Getting item {}".format(x))
+        with self._counter.get_lock():
+            self._counter.value -= 1
+        # logger.critical("Getting item {}".format(x))
         return x
 
     def qsize(self):
-        return self._counter
+        return self._counter.value
 
     def empty(self):
-        return not self._counter
+        return not self._counter.value
+
+
+def consumer(in_q, out_q, delay=0):
+    import time
+    while True:
+        print("In loop")
+        x = in_q.get()
+        time.sleep(delay)
+        out_q.put(x)
+        if x == 'STOP':
+            break
+
+
+if __name__ == "__main__":
+
+    task_q = MacSafeQueue()
+    result_q = MacSafeQueue()
+
+    p = multiprocessing.Process(target=consumer, args=(task_q, result_q,))
+    p.start()
+    for i in range(10):
+        print(f"Putting {i}")
+        task_q.put(i)
+        x = result_q.get()
+        print(f"Got {x}")
+    task_q.put('STOP')
+    p.terminate()
