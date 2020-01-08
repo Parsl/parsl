bbf882243b8922f34bcfb8c95493592baea48b3f
Logan Ward <ward.logan.t@gmail.com>
2020-01-08 08:30:12 -0600

    Added nodes_per_block special case for SimpleLauncher

--- parsl/dataflow/strategy.py
+++ parsl/dataflow/strategy.py
@@ -3,6 +3,7 @@ import time
 import math
 
 from parsl.executors import IPyParallelExecutor, HighThroughputExecutor, ExtremeScaleExecutor
+from parsl.launchers import SimpleLauncher
 from parsl.providers.provider_base import JobState
 
 logger = logging.getLogger(__name__)
@@ -186,7 +187,13 @@ class Strategy(object):
             elif isinstance(executor, ExtremeScaleExecutor):
                 tasks_per_node = executor.ranks_per_node
 
-            nodes_per_block = executor.provider.nodes_per_block
+            # Determine the number of nodes per block
+            if hasattr(executor.provider, 'launcher') and isinstance(executor.provider.launcher, SimpleLauncher):
+                # Parsl only launches one worker. That one worker can access >1 compute node
+                nodes_per_block = 1
+            else:
+                # Assume that Parsl launchers a single worker on each node
+                nodes_per_block = executor.provider.nodes_per_block
             parallelism = executor.provider.parallelism
 
             running = sum([1 for x in status if x.state == JobState.RUNNING])
