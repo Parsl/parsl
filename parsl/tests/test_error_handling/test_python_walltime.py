76bdb4984264670f4fa09519f14571ea9814bb9e
Yadu Nand B <yadudoc1729@gmail.com>
2020-01-09 14:27:04 -0600

    Updating test for walltime args

--- parsl/tests/test_error_handling/test_python_walltime.py
+++ parsl/tests/test_error_handling/test_python_walltime.py
@@ -3,18 +3,22 @@ import pytest
 import parsl
 from parsl.app.errors import AppTimeout
 
-
+@pytest.mark.local
 @parsl.python_app
-def my_app(walltime=3):
+def my_app(walltime=1):
     import time
-    # the loop count must be substantially bigger than the walltime
-    # but not infinite - so that the test eventually terminates
-    # even if the walltime doesn't work.
-    for n in range(0, 6):
-        time.sleep(1)
-
+    time.sleep(1.2)
+    return True
 
 def test_python_walltime():
     f = my_app()
     with pytest.raises(AppTimeout):
         f.result()
+
+with pytest.raises(TypeError):
+    @pytest.mark.local
+    @parsl.python_app(walltime=1)
+    def my_app_2():
+        import time
+        time.sleep(1.2)
+        return True
