e8ed698b58e299d0511558ea743f999c1a29f873
Yadu Nand B <yadudoc1729@gmail.com>
2020-01-09 14:44:37 -0600

    Fixing tests

--- parsl/tests/test_error_handling/test_python_walltime.py
+++ parsl/tests/test_error_handling/test_python_walltime.py
@@ -3,7 +3,6 @@ import pytest
 import parsl
 from parsl.app.errors import AppTimeout
 
-@pytest.mark.local
 @parsl.python_app
 def my_app(walltime=1):
     import time
@@ -15,10 +14,12 @@ def test_python_walltime():
     with pytest.raises(AppTimeout):
         f.result()
 
-with pytest.raises(TypeError):
-    @pytest.mark.local
-    @parsl.python_app(walltime=1)
-    def my_app_2():
-        import time
-        time.sleep(1.2)
-        return True
+def test_python_bad_decorator_args():
+
+    with pytest.raises(TypeError):
+        @pytest.mark.local
+        @parsl.python_app(walltime=1)
+        def my_app_2():
+            import time
+            time.sleep(1.2)
+            return True
