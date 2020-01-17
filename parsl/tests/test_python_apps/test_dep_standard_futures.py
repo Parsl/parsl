403735ba55a7784dd42b1ed6e2fcb189754efd5f
Ben Clifford <benc@hawaga.org.uk>
2020-01-17 16:01:59 +0000

    add failing tests for using concurrent.futures Futures as dependencies

---
+++ parsl/tests/test_python_apps/test_dep_standard_futures.py
@@ -0,0 +1,35 @@
+import parsl
+from concurrent.futures import Future
+
+@parsl.python_app
+def copy_app(v):
+    return v
+
+def test_future_result_dependency():
+
+    plain_fut = Future()
+
+    parsl_fut = copy_app(plain_fut)
+
+    assert not parsl_fut.done()
+
+    message = "Test"
+
+    plain_fut.set_result(message)
+
+    assert parsl_fut.result() == message
+
+def test_future_fail_dependency():
+
+    plain_fut = Future()
+
+    parsl_fut = copy_app(plain_fut)
+
+    assert not parsl_fut.done()
+
+    plain_fut.set_result(ValueError("Plain failure"))
+
+    # TODO: be tighter on the returned exception: it should be a dependency failure exception
+    assert parsl_fut.exception() is not None
+
+
