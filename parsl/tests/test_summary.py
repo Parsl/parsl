a3ea2ddf1eb64778aa70cf19f6d6b24b07cc4065
Ben Clifford <benc@hawaga.org.uk>
2020-02-06 14:32:49 +0000

    test which passes in parent of #1543

--- parsl/tests/test_summary.py
+++ parsl/tests/test_summary.py
@@ -3,12 +3,27 @@ import pytest
 from parsl.tests.configs.local_threads import fresh_config
 
 
+@parsl.python_app
+def succeed():
+    pass
+
+
+@parsl.python_app
+def fail():
+    raise RuntimeError("Deliberate failure in fail() app")
+
+
 @pytest.mark.local
 def test_summary(caplog):
 
     parsl.load(fresh_config())
+
+    succeed().result()
+    fail().exception()
+
     parsl.dfk().cleanup()
     parsl.clear()
 
     assert "Summary of tasks in DFK:" in caplog.text
-
+    assert "Tasks in state States.done: 0" in caplog.text
+    assert "Tasks in state States.failed: 1" in caplog.text
