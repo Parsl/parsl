d6b3280979a787310c0d18edd8951676d7d8b14e
Yadu Nand B <yadudoc1729@gmail.com>
2020-02-07 13:17:52 -0600

    Test now asserts that a TypeError is reported when the return value is a large numpy array

--- parsl/tests/test_regression/test_1480.py
+++ parsl/tests/test_regression/test_1480.py
@@ -16,8 +16,9 @@ def test_1480(size=1000):
     parsl.load(config)
     x = compute_descript(size=size)
 
-    assert len(x.result()) == size, "Expected len:{} instead got: {}".format(size,
-                                                                             len(x.result()))
+    # This should raise a TypeError: can't pickle memoryview objects
+    with pytest.raises(TypeError):
+        x.result()
 
 
 if __name__ == "__main__":
