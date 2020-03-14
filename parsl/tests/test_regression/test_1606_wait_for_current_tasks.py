79b99168152ce27fe00e4a21ca990a561afd3a01
Ben Clifford <benc@hawaga.org.uk>
2020-03-14 12:11:01 +0000

    add test description

--- parsl/tests/test_regression/test_1606_wait_for_current_tasks.py
+++ parsl/tests/test_regression/test_1606_wait_for_current_tasks.py
@@ -1,13 +1,16 @@
 import parsl
 
+
 @parsl.python_app
 def slow_app(delay):
     import time
     time.sleep(delay)
 
-def test_fake_bad():
+
+def test_wait_for_tasks():
     slow_app(5)
     slow_app(10)
     parsl.dfk().wait_for_current_tasks()
-
-
+    # the regression reported in #1606 is that wait_for_current_tasks
+    # fails due to tasks being removed from the DFK tasks dict as they
+    # complete, introduced in #1543.
