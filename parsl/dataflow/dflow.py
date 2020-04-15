f6a96de772b397c971467eb0db6212fcc648ee8a
Ben Clifford <benc@hawaga.org.uk>
2020-04-15 17:00:56 +0000

    Remove task record retries_left because it is no longer used
    A previous commit replaced its use with fail_count

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -402,7 +402,6 @@ class DataFlowKernel(object):
                             # executor or memoization hash function.
 
                             logger.debug("Got an exception launching task", exc_info=True)
-                            self.tasks[task_id]['retries_left'] = 0
                             exec_fu = Future()
                             exec_fu.set_exception(e)
             else:
@@ -416,7 +415,6 @@ class DataFlowKernel(object):
                     task_log_info = self._create_task_log_info(task_record)
                     self.monitoring.send(MessageType.TASK_INFO, task_log_info)
 
-                self.tasks[task_id]['retries_left'] = 0
                 exec_fu = Future()
                 exec_fu.set_exception(DependencyError(exceptions,
                                                       task_id))
@@ -485,8 +483,6 @@ class DataFlowKernel(object):
             task_log_info = self._create_task_log_info(self.tasks[task_id])
             self.monitoring.send(MessageType.TASK_INFO, task_log_info)
 
-        self.tasks[task_id]['retries_left'] = self._config.retries - \
-            self.tasks[task_id]['fail_count']
         logger.info("Task {} launched on executor {}".format(task_id, executor.label))
         return exec_fu
 
