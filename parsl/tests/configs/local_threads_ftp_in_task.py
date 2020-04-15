e92d1cbd6b8c19c2249ef3276f1827fd0f7bbc72
Ben Clifford <benc@hawaga.org.uk>
2020-04-15 15:57:34 +0000

    Use parsl inbuilt runinfo uniquification, not separate runinfo_XXXXXX for pytest
    there are comments about needing this when running multiple things in parallel
    which is probably a race condition on creating rundir/NNN/ directories that I have
    a year or two ago observed when starting parsl up twice in the same directory
    anyway (as part of a weird import setup)
    
    the solution to whatever that broader problem is should be the solution to this
    
    
    running tests with -nX doesn't work because --config is not passed through

--- parsl/tests/configs/local_threads_ftp_in_task.py
+++ parsl/tests/configs/local_threads_ftp_in_task.py
@@ -2,7 +2,6 @@ from parsl.config import Config
 from parsl.data_provider.file_noop import NoOpFileStaging
 from parsl.data_provider.ftp import FTPInTaskStaging
 from parsl.executors.threads import ThreadPoolExecutor
-from parsl.tests.utils import get_rundir
 
 config = Config(
     executors=[
@@ -10,6 +9,5 @@ config = Config(
             label='local_threads_http_in_task',
             storage_access=[FTPInTaskStaging(), NoOpFileStaging()]
         )
-    ],
-    run_dir=get_rundir()
+    ]
 )
