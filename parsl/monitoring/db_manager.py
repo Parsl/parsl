25a994af11baf8606042432ebd4cb2476f909a12
Ben Clifford <benc@hawaga.org.uk>
2020-04-20 10:49:16 +0000

    Catch exceptions in database init (for example, DB schema errors)
    Before this commit, if an exception happened in the Database __init__ call,
    then the database thread would exit, and parsl would hang at shutdown.
    
    After this commit, exceptions in database creation are handling with the
    existing database exception queue.

--- parsl/monitoring/db_manager.py
+++ parsl/monitoring/db_manager.py
@@ -440,9 +440,9 @@ def dbm_starter(exception_q, priority_msgs, node_msgs, resource_msgs, *args, **k
     The DFK should start this function. The args, kwargs match that of the monitoring config
 
     """
-    dbm = DatabaseManager(*args, **kwargs)
-    logger.info("Starting dbm in dbm starter")
     try:
+        dbm = DatabaseManager(*args, **kwargs)
+        logger.info("Starting dbm in dbm starter")
         dbm.start(priority_msgs, node_msgs, resource_msgs)
     except Exception as e:
         logger.exception("dbm.start exception")
