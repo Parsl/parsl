0ee812f0809ced893ab54780b5d6e3a07996e6ec
Ben Clifford <benc@hawaga.org.uk>
2020-04-21 15:32:00 +0000

    TODO: Dependency exception should not include exception text of upstream exceptions. Perhaps only the task ID of upstream tasks?
    When there is a long chain of dependencies, then an early one failing generates quite awkward to
    read exception text.

--- parsl/dataflow/error.py
+++ parsl/dataflow/error.py
@@ -63,8 +63,7 @@ class DependencyError(DataFlowException):
         self.outputs = outputs
 
     def __repr__(self):
-        return "Dependency failure for task {} from: {}".format(self.task_id,
-                                                                self.dependent_exceptions)
+        return "Dependency failure for task {}".format(self.task_id)
 
     def __str__(self):
         return self.__repr__()
