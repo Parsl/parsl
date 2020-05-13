b7788da1cb19f8ec2c19d92b0cfbe9b18921f071
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-05-13 12:17:37 -0500

    Updating test to account for changes from #1653

--- parsl/tests/test_regression/test_1480.py
+++ parsl/tests/test_regression/test_1480.py
@@ -12,14 +12,30 @@ def compute_descript(size=1000):
 
 
 @pytest.mark.local
-def test_1480(size=1000):
+def test_1480(size=10**6):
     parsl.load(config)
     x = compute_descript(size=size)
 
     # This should raise a TypeError: can't pickle memoryview objects
     with pytest.raises(TypeError):
         x.result()
+    parsl.clear()
+
+
+@pytest.mark.local
+def test_1653():
+    """ Check if #1653 works correctly
+    """
+    parsl.load(config)
+
+    x = compute_descript(size=100).result()
+    assert x.shape() == (100, 0), "Got incorrect numpy shape"
+
+    x = compute_descript(size=1000).result()
+    assert x.shape() == (1000, 0), "Got incorrect numpy shape"
+    parsl.clear()
 
 
 if __name__ == "__main__":
     test_1480()
+    test_1653()
