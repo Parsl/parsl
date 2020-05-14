2070e5d94b2ff7732d77cf2a5ebbd619f4b433fa
Benjamin Tovar <btovar@nd.edu>
2020-05-14 09:20:18 -0400

    test issue363 using shared_fs

---
+++ parsl/tests/configs/workqueue_ex_shared.py
@@ -0,0 +1,6 @@
+from parsl.config import Config
+from parsl.executors import WorkQueueExecutor
+
+config = Config(executors=[WorkQueueExecutor(port=9000,
+                                             project_name="wq-testing",
+                                             shared_fs=True)])
