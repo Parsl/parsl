d616c4f47cd38c412c268802f183e489fdfa3a2b
Yadu Nand B <yadudoc1729@gmail.com>
2020-05-14 22:52:35 +0000

    Updating config to use fresh_config

--- parsl/tests/configs/frontera.py
+++ parsl/tests/configs/frontera.py
@@ -9,30 +9,35 @@ from .user_opts import user_opts
 """ This config assumes that it is used to launch parsl tasks from the login nodes
 of Frontera at TACC. Each job submitted to the scheduler will request 2 nodes for 10 minutes.
 """
-config = Config(
-    executors=[
-        HighThroughputExecutor(
-            label="frontera_htex",
-            address=address_by_hostname(),
-            max_workers=1,
-            provider=SlurmProvider(
-                cmd_timeout=60,     # Add extra time for slow scheduler responses
-                channel=LocalChannel(),
-                nodes_per_block=2,
-                init_blocks=1,
-                min_blocks=1,
-                max_blocks=1,
-                partition='development',  # Replace with partition name
-                scheduler_options=user_opts['frontera']['scheduler_options'],
 
-                # Command to be run before starting a worker, such as:
-                # 'module load Anaconda; source activate parsl_env'.
-                worker_init=user_opts['frontera']['worker_init'],
-                
-                # Ideally we set the walltime to the longest supported walltime.
-                walltime='00:10:00',
-                launcher=SrunLauncher(),
-            ),
-        )
-    ],
-)
+def fresh_config():
+    return Config(
+        executors=[
+            HighThroughputExecutor(
+                label="frontera_htex",
+                address=address_by_hostname(),
+                max_workers=1,
+                provider=SlurmProvider(
+                    cmd_timeout=60,     # Add extra time for slow scheduler responses
+                    channel=LocalChannel(),
+                    nodes_per_block=2,
+                    init_blocks=1,
+                    min_blocks=1,
+                    max_blocks=1,
+                    partition='development',  # Replace with partition name
+                    scheduler_options=user_opts['frontera']['scheduler_options'],
+                    
+                    # Command to be run before starting a worker, such as:
+                    # 'module load Anaconda; source activate parsl_env'.
+                    worker_init=user_opts['frontera']['worker_init'],
+                    
+                    # Ideally we set the walltime to the longest supported walltime.
+                    walltime='00:10:00',
+                    launcher=SrunLauncher(),
+                ),
+            )
+        ],
+    )
+
+
+config = fresh_config()
