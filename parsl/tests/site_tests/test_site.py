460436b0a78838c8de92e21dc93a5969c960f99d
Yadu Nand B <yadudoc1729@gmail.com>
2020-05-14 22:23:47 -0500

    Removing dead code

--- parsl/tests/site_tests/test_site.py
+++ parsl/tests/site_tests/test_site.py
@@ -48,34 +48,15 @@ def test_platform(n=2, sleep_dur=10):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--sitespec", default=None)
     parser.add_argument("-c", "--count", default="4",
                         help="Count of apps to launch")
     parser.add_argument("-t", "--time", default="60",
                         help="Sleep time for each app")
-
     parser.add_argument("-d", "--debug", action='store_true',
                         help="Count of apps to launch")
     args = parser.parse_args()
 
     if args.debug:
         parsl.set_stream_logger()
-
-    if args.sitespec:
-        config = None
-        try:
-            exec("import parsl; from {} import config".format(args.sitespec))
-            # Here we will set workers per node or manager to 1 to make sure we have 2 managers connecting
-            config.executors[0].max_workers = 2
-            # Make sure that we are requesting one 2 node block.
-            assert config.executors[0].provider.nodes_per_block == 2, "Expecting nodes_per_block = 2, got:{}".format(
-                config.executors[0].provider.nodes_per_block)
-            parsl.load(config)
-        except Exception:
-            print("Failed to load the requested config : ", args.sitespec)
-            exit(0)
-    else:
-        from site_config_selector import config
-        parsl.load(config)
 
     x = test_platform(n=int(args.count), sleep_dur=int(args.time))
