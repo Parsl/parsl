bd32259aa81169509292b1c7e15a4730ff98598d
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-05-15 16:55:25 -0500

    Updates for local conda env creation

--- parsl/tests/site_tests/conda_setup.sh
+++ parsl/tests/site_tests/conda_setup.sh
@@ -21,54 +21,67 @@ create_conda() {
 
     if [[ "$(hostname)" =~ .*thetalogin.* ]]
     then
-	echo "On theta"
-	module load miniconda-3/latest
-	conda create -p $CONDA_TARGET --clone $CONDA_PREFIX --yes --force
-	conda activate $CONDA_TARGET 
-	# Theta is weird, we do explicit install
-	pip install -r test-requirements.txt
-	conda install pip psutil --yes
-	python3 setup.py install
-	echo "module load miniconda-3/latest;"           >  ~/setup_parsl_test_env.sh
- 	echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
-	return
+        echo "On theta"
+        module load miniconda-3/latest
+        conda create -p $CONDA_TARGET --clone $CONDA_PREFIX --yes --force
+        conda activate $CONDA_TARGET
+        # Theta is weird, we do explicit install
+        pip install -r test-requirements.txt
+        conda install pip psutil --yes
+        python3 setup.py install
+        echo "module load miniconda-3/latest;"           >  ~/setup_parsl_test_env.sh
+        echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
+        return
 
     elif [[ "$(hostname)" =~ .*frontera.* ]]
     then
-	echo "On Frontera"
-	if [[ -d ~/anaconda3 ]]
-	then
-	    echo "Loading anaconda3 from ~/anaconda3"
-	    source ~/anaconda3/bin/activate
-	else
-	    echo "Please install conda to your home dir at ~/anaconda3"
-	fi
-	conda create -p $CONDA_TARGET python=3.7 --yes --force
-	conda activate $CONDA_TARGET
-	echo "source ~/anaconda3/bin/activate;"          >  ~/setup_parsl_test_env.sh
- 	echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
+        echo "On Frontera"
+        if [[ -d ~/anaconda3 ]]
+        then
+            echo "Loading anaconda3 from ~/anaconda3"
+            source ~/anaconda3/bin/activate
+        else
+            echo "Please install conda to your home dir at ~/anaconda3"
+        fi
+        conda create -p $CONDA_TARGET python=3.7 --yes --force
+        conda activate $CONDA_TARGET
+        echo "source ~/anaconda3/bin/activate;"          >  ~/setup_parsl_test_env.sh
+        echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
 
     elif [[ "$(hostname -f)" =~ .*summit.* ]]
     then
-	echo "On Summit"
+        echo "On Summit"
 
-	module load ibm-wml-ce
-	conda create -p $CONDA_TARGET --yes --force
-	conda activate $PWD/$CONDA_TARGET
-	conda install paramiko>=2.7.1 pip numpy psutil pandas --yes
-	# conda install --file requirements.txt --yes
-	echo "module load ibm-wml-ce"                    >  ~/setup_parsl_test_env.sh
- 	echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
+        module load ibm-wml-ce
+        conda create -p $CONDA_TARGET --yes --force
+        conda activate $PWD/$CONDA_TARGET
+        conda install paramiko>=2.7.1 pip numpy psutil pandas --yes
+        # conda install --file requirements.txt --yes
+        echo "module load ibm-wml-ce"                    >  ~/setup_parsl_test_env.sh
+        echo "conda activate $PWD/$CONDA_TARGET"         >> ~/setup_parsl_test_env.sh
 
     elif [[ "$(hostname)" =~ .*cori.* ]]
     then
-	echo "On Cori"
-	module load python/3.7-anaconda-2019.07
-	conda create -p $CONDA_TARGET python=3.7 --yes --force
-	conda activate $CONDA_TARGET
-	echo "module load python/3.7-anaconda-2019.07;"  >  ~/setup_parsl_test_env.sh
- 	echo "conda activate $CONDA_TARGET"              >> ~/setup_parsl_test_env.sh
+        echo "On Cori"
+        module load python/3.7-anaconda-2019.07
+        conda create -p $CONDA_TARGET python=3.7 --yes --force
+        conda activate $CONDA_TARGET
+        echo "module load python/3.7-anaconda-2019.07;"  >  ~/setup_parsl_test_env.sh
+        echo "conda activate $CONDA_TARGET"              >> ~/setup_parsl_test_env.sh
 
+    else
+        echo "Treating $(hostname) as Local"
+        if [[ -d ~/anaconda3 ]]
+        then
+            echo "Loading anaconda3 from ~/anaconda3"
+            source ~/anaconda3/bin/activate
+        else
+            echo "Please install conda to your home dir at ~/anaconda3"
+        fi
+        conda create -p $CONDA_TARGET python=3.7 --yes --force
+        conda activate $PWD/$CONDA_TARGET
+        echo "source ~/anaconda3/bin/activate;"      >  ~/setup_parsl_test_env.sh
+        echo "conda activate $PWD/$CONDA_TARGET"     >> ~/setup_parsl_test_env.sh
     fi
 
     echo "Installing parsl from $PARSL_ROOT"
