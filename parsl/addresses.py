cdade60944aaa8ced282650346ee74f95c59f063
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-05-18 11:35:56 -0500

    Adding a timeout for the call to get address

--- parsl/addresses.py
+++ parsl/addresses.py
@@ -33,13 +33,18 @@ def address_by_route() -> str:
     return addr
 
 
-def address_by_query() -> str:
+def address_by_query(timeout=30) -> str:
     """Finds an address for the local host by querying ipify. This may
        return an unusable value when the host is behind NAT, or when the
        internet-facing address is not reachable from workers.
+       Parameters:
+       -----------
+
+       timeout : float
+          Timeout for the request in seconds. Can be < 1. Default: 30s
     """
     logger.debug("Finding address by querying remote service")
-    response = requests.get('https://api.ipify.org')
+    response = requests.get('https://api.ipify.org', timeout=30)
 
     if response.status_code == 200:
         addr = response.text
