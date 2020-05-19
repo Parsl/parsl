412ebcb1dddd033c92f0cd46c3a3ed8e095a5d3f
Ben Clifford <benc@hawaga.org.uk>
2020-05-19 17:17:09 +0000

    raise exceptions harder (maybe just for dev?)

--- parsl/monitoring/visualization/plots/default/workflow_resource_plots.py
+++ parsl/monitoring/visualization/plots/default/workflow_resource_plots.py
@@ -132,8 +132,11 @@ def worker_efficiency(task, node):
                              title="Worker efficiency"))
         return plot(fig, show_link=False, output_type="div", include_plotlyjs=False)
     except Exception as e:
-        print(e)
-        return "The worker efficiency plot cannot be generated due to missing data."
+        print("BENC BENC BENC BENC BENC BENC BENC BENC")
+        print("Exception:")
+        print(repr(e))
+        raise
+        # return "The worker efficiency plot cannot be generated due to missing data."
 
 
 def resource_efficiency(resource, node, label='CPU'):
