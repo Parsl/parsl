67fda8c8dab681194b4153c382b29966034f1ade
Benjamin Tovar <btovar@nd.edu>
2020-05-19 13:17:54 -0400

    don't use project name in local config for test
    This forces wq to use a hostname, which is better for local testing, as
    it does not need to connect to the global work queue projects' catalog.

--- parsl/tests/configs/workqueue_ex.py
+++ parsl/tests/configs/workqueue_ex.py
@@ -1,5 +1,4 @@
 from parsl.config import Config
 from parsl.executors import WorkQueueExecutor
 
-config = Config(executors=[WorkQueueExecutor(port=9000,
-                                             project_name="wq-testing")])
+config = Config(executors=[WorkQueueExecutor(port=9000)])
