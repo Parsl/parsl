af97704f83db00a4f13ec2903b9e1ddecdfd0e88
Benjamin Tovar <btovar@nd.edu>
2020-05-22 12:22:38 -0400

    Revert "remove project_name and see_worker_output from wqex_local.py"
    This reverts commit 91e277ce4e4d20a9a833ef7df77e6e893dc11804.
    
    Submitted to incorrect branch.

--- parsl/tests/workqueue_tests/wqex_local.py
+++ parsl/tests/workqueue_tests/wqex_local.py
@@ -3,6 +3,8 @@ from parsl.executors import WorkQueueExecutor
 
 config = Config(
     executors=[WorkQueueExecutor(port=50055,
+                                 project_name="WQexample",
+                                 see_worker_output=True,
                                  source=True,
                                  # init_command='source /home/yadu/src/wq_parsl/setup_parsl_env.sh;
                                  # echo "Ran at $date" > /home/yadu/src/wq_parsl/parsl/tests/workqueue_tests/ran.log',
