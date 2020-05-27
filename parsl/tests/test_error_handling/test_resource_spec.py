2c5b16e96ed8d03993d3d286fa4bcfc18fe64a58
Ben Clifford <benc@hawaga.org.uk>
2020-05-27 12:46:48 +0000

    fix test_resource_spec

--- parsl/tests/test_error_handling/test_resource_spec.py
+++ parsl/tests/test_error_handling/test_resource_spec.py
@@ -1,6 +1,7 @@
 import parsl
 from parsl.app.app import python_app
 from parsl.tests.configs.local_threads import config
+from parsl.tests.logfixtures import permit_severe_log
 from parsl.executors.errors import UnsupportedFeatureError
 from parsl.executors import WorkQueueExecutor
 
@@ -11,20 +12,21 @@ def double(x, parsl_resource_specification={}):
 
 
 def test_resource(n=2):
-    spec = {'cores': 2, 'memory': '1GiB'}
-    fut = double(n, parsl_resource_specification=spec)
-    try:
-        fut.result()
-    except Exception as e:
-        assert isinstance(e, UnsupportedFeatureError)
-    else:
-        executors = parsl.dfk().executors
-        executor = None
-        for label in executors:
-            if label != 'data_manager':
-                executor = executors[label]
-                break
-        assert isinstance(executor, WorkQueueExecutor)
+    with permit_severe_log():
+        spec = {'cores': 2, 'memory': '1GiB'}
+        fut = double(n, parsl_resource_specification=spec)
+        try:
+            fut.result()
+        except Exception as e:
+            assert isinstance(e, UnsupportedFeatureError)
+        else:
+            executors = parsl.dfk().executors
+            executor = None
+            for label in executors:
+                if label != 'data_manager':
+                    executor = executors[label]
+                    break
+            assert isinstance(executor, WorkQueueExecutor)
 
 
 if __name__ == '__main__':
