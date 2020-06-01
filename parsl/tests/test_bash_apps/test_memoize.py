7e944818867a05fefbba41163fbd91a4c0c0f33d
Zhuozhao Li <zhuozhl@clemson.edu>
2020-06-01 21:25:57 +0000

    add a test with mark issue363

--- parsl/tests/test_bash_apps/test_memoize.py
+++ parsl/tests/test_bash_apps/test_memoize.py
@@ -45,6 +45,10 @@ def fail_on_presence_kw(outputs=[], foo={}):
     return 'if [ -f {0} ] ; then exit 1 ; else touch {0}; fi'.format(outputs[0])
 
 
+# This test is an oddity that requires a shared-FS and simply
+# won't work if there's a staging provider.
+# @pytest.mark.sharedFS_required
+@pytest.mark.issue363
 def test_bash_memoization_keywords(n=2):
     """Testing bash memoization
     """
