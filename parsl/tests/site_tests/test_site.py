e93e0fca19277ee6998be0b453f43ab8bef35e7a
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-06-01 17:04:20 -0500

    Site testing (#1707)
    * Adding site config selector
    
    * Removing obsolete file
    
    * Adding site tests. Tested on frontera.
    
    * Setting cleannet requirement flag for http/https staging tests since http staging doesn't work on some theta nodes.
    
    * Updates to site tests
    
    * Updating site config selector to use CONDA_DEFAULT_ENV
    
    * Adding a missing init file and full module import paths
    
    * Adding local config and minor changes
    
    * Updating site tests
    
    * Adding conda setup with updates for theta
    
    * Updates to test
    
    * Updating to use fresh_config
    
    * Using fresh_config
    
    * Updating config to use fresh_config
    
    * Flake8 fixes
    
    * Removing dead code
    
    * Switching to fresh_config for frontera
    
    * Updating conda_setup to write out ~/parsl_setup_test_env.sh
    
    * Adding default worker_init
    
    * flake8 fixes
    
    * Updating with install steps for summit
    
    * Minor updates to tests
    
    * Adding summit to user_opts
    
    * Updates for running site tests on Cori
    
    * Updated to run locally
    
    * Explicitly skip if called on htex_local where there aren't multiple nodes to run from
    
    * Updates for local conda env creation
    
    * Only whitespace. minor flake8 changes
    
    * Testing working now for comet.
    
    * flake8 fixes only
    
    * Adding stampede2 setup
    
    * Adding stampede2 selector
    
    * Fixing format string for py3.5 friendliness
    
    * Adding a readme
    
    * add midway site test
    
    * Updating README
    
    * Adding an error message when conda_setup doesn't recognize the hostname
    
    * Setting min_blocks=0
    
    * Code updates to avoid pciking data_manager by mistake
    
    * Adding configs and setup for cc_in2p3
    
    * Adding markers for issue363:No shared-fs and support in Makefile to deselect with SHARED_FS_OPTIONS env variable
    
    * Updates for summit
    
    * Removing obsolete configs
    
    * add petrelkube test
    
    * mark tests for issue 363
    
    * add some text to README about SHARED_FS_OPTIONS
    
    * flake8 fixes
    
    * add midway config and remove old midway config
    
    * flake8 fixes only
    
    * Updating doc config from midway_htex -> midway
    
    * Adding config variable
    
    * Skipping test againt midway config
    
    * flake8 fixes
    
    * add blue waters
    
    * add blue waters config
    
    * flake8 fix. Only whitespace change
    
    * Adding a staging_required marker (#1734)
    
    * Adding a staging_required marker to isolate tests that should work with staging
    
    * change mark issue363 to staging_required
    
    * correct typo
    
    Co-authored-by: Zhuozhao Li <zhuozhl@clemson.edu>
    
    * add a test with mark issue363
    
    Co-authored-by: Yadu Nand Babuji <yadunand@comet-ln2.sdsc.edu>
    Co-authored-by: Zhuozhao Li <zhuozhl@clemson.edu>
    Co-authored-by: Zhuozhao Li <zhuozhao@uchicago.edu>
    Co-authored-by: Zhuozhao Li <zhuozhao@clemson.edu>
    Co-authored-by: Ben Clifford <benc@hawaga.org.uk>

---
+++ parsl/tests/site_tests/test_site.py
@@ -0,0 +1,67 @@
+import argparse
+import pytest
+import parsl
+from parsl.app.app import python_app  # , bash_app
+from parsl.tests.site_tests.site_config_selector import fresh_config
+
+
+@python_app
+def platform(sleep=10, stdout=None):
+    import platform
+    import time
+    time.sleep(sleep)
+    return platform.uname()
+
+
+@pytest.mark.local
+def test_platform(n=2, sleep_dur=10):
+    """ This should sleep to make sure that concurrent apps will go to different workers
+    on different nodes.
+    """
+    config = fresh_config()
+    if config.executors[0].label == "htex_local":
+        return
+
+    parsl.load(fresh_config())
+
+    dfk = parsl.dfk()
+    name = list(dfk.executors.keys())[0]
+    print("Trying to get executor : ", name)
+
+    x = platform(sleep=0)
+    print(x.result())
+
+    print("Executor : ", dfk.executors[name])
+    print("Connected   : ", dfk.executors[name].connected_workers)
+    print("Outstanding : ", dfk.executors[name].outstanding)
+
+    d = []
+    for i in range(0, n):
+        x = platform(sleep=sleep_dur)
+        d.append(x)
+
+    pinfo = set([i.result()for i in d])
+    assert len(pinfo) == 2, "Expected two nodes, instead got {}".format(pinfo)
+
+    print("Test passed")
+
+    parsl.clear()
+    del dfk
+    return True
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", default="4",
+                        help="Count of apps to launch")
+    parser.add_argument("-t", "--time", default="60",
+                        help="Sleep time for each app")
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Count of apps to launch")
+    args = parser.parse_args()
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+    x = test_platform(n=int(args.count), sleep_dur=int(args.time))
