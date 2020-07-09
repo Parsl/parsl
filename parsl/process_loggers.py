62730b3cfbe8adc55ad02438a2f9bc285a25c7bb
Ben Clifford <benc@hawaga.org.uk>
2020-07-09 09:55:12 +0000

    Remove prints from exception wrapper

--- parsl/process_loggers.py
+++ parsl/process_loggers.py
@@ -8,19 +8,15 @@ logger = logging.getLogger(__name__)
 def wrap_with_logs(fn):
     @functools.wraps(fn)
     def wrapped(*args, **kwargs):
-        print("exception wrapper: start")
         thread = threading.current_thread()
 
         name = "thread {} in process {}".format(thread.name, "UNLOGGED")
 
         try:
-            print("exception wrapper: started for {}".format(name))
             r = fn(*args, **kwargs)
-            print("exception wrapper: normal ending for {}".format(name))
             logger.info("exception wrapper: normal ending for {}".format(name))
             return r
         except Exception:
-            print("exception wrapper: exceptional ending for {}".format(name))
             logger.error("exception wrapper: exceptional ending for {}".format(name))
             logger.exception("exception wrapper: caught an exception for {}".format(name))
             raise
@@ -30,7 +26,6 @@ def wrap_with_logs(fn):
 def hanging_wrap_with_logs(fn):
     @functools.wraps(fn)
     def wrapped(*args, **kwargs):
-        print("exception wrapper: start")
         # logger.info("exception wrapper: start")
         # logger.debug("exception wrapper: about to get current process")
 
@@ -42,14 +37,11 @@ def hanging_wrap_with_logs(fn):
         name = "thread {}".format(thread.name)
 
         try:
-            print("exception wrapper: started for {}".format(name))
             logger.info("exception wrapper: started for {}".format(name))
             r = fn(*args, **kwargs)
-            print("exception wrapper: normal ending for {}".format(name))
             logger.info("exception wrapper: normal ending for {}".format(name))
             return r
         except Exception:
-            print("exception wrapper: exceptional ending for {}".format(name))
             logger.error("exception wrapper: exceptional ending for {}".format(name))
             logger.exception("exception wrapper: caught an exception for {}".format(name))
             raise
