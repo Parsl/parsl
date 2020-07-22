cf59dfd6582926467d45447c9e2abc23017d2fc3
Ben Clifford <benc@hawaga.org.uk>
2020-07-22 16:42:22 +0100

    Tidy interchange start() docstring (#1811)

--- parsl/executors/high_throughput/interchange.py
+++ parsl/executors/high_throughput/interchange.py
@@ -337,7 +337,7 @@ class Interchange(object):
                 continue
 
     def start(self, poll_period=None):
-        """ Start the NeedNameQeueu
+        """ Start the interchange
 
         Parameters:
         ----------
