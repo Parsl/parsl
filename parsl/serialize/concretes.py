8f2d3803fd35ef535074a17cc8b28ca5db8ff4aa
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-07-30 13:06:22 -0500

    Adding docstrings clarifying each serializers capabilities and limitations

--- parsl/serialize/concretes.py
+++ parsl/serialize/concretes.py
@@ -2,17 +2,23 @@ import json
 import dill
 import pickle
 import logging
-import functools
-import inspect
 
 logger = logging.getLogger(__name__)
 from parsl.serialize.base import fxPicker_shared
 
 
-class pickle_base64(fxPicker_shared):
+class PickleSerializer(fxPicker_shared):
+    """ Pickle serialization covers most python objects, with some notable exceptions:
 
-    _identifier = b'10\n'
-    _for_code = False
+    * functions defined in a interpretor/notebook
+    * classes defined in local context and not importable using a fully qualified name
+    * clojures, generators and coroutines
+    * [sometimes] issues with wrapped/decorated functions
+    """
+
+    _identifier = b'01\n'
+    _for_code = True
+    _for_data = True
 
     def __init__(self):
         super().__init__()
@@ -27,10 +33,21 @@ class pickle_base64(fxPicker_shared):
         return data
 
 
-class dill_base64(fxPicker_shared):
+class DillSerializer(fxPicker_shared):
+    """ Dill serialization works on a superset of object including the ones covered by pickle.
+    However for most cases pickle is faster. For most callable objects the additional overhead
+    of dill can be amortized with an lru_cache. Here's items that dill handles that pickle
+    doesn't:
 
-    _identifier = b'11\n'
-    _for_code = False
+    * functions defined in a interpretor/notebook
+    * classes defined in local context and not importable using a fully qualified name
+    * functions that are wrapped/decorated by other functions/classes
+    * clojures
+    """
+
+    _identifier = b'02\n'
+    _for_code = True
+    _for_data = True
 
     def __init__(self):
         super().__init__()
@@ -45,114 +62,28 @@ class dill_base64(fxPicker_shared):
         return data
 
 
-class json_base64(fxPicker_shared):
+class JsonSerializer(fxPicker_shared):
     """ Json based serialization does not guarantee consistency on tuple/list types
-    Should be lower priority that pickle/dill
-    """
+    Should be lower priority that pickle/dill. This is here for legacy reasons.
+    In the funcX + automate case the automate service requires json serialization,
+    and cannot handle pickle/dill.
 
-    _identifier = '12\n'
-    _for_code = False
-
-    def __init__(self):
-        super().__init__()
-
-    def serialize(self, data):
-        x = json.dumps(data)
-        return self.identifier + x
-
-    def deserialize(self, payload):
-        x = json.loads(self.chomp(payload))
-        return x
-
-
-class code_pickle(fxPicker_shared):
-
-    _identifier = b'00\n'
-    _for_code = True
-
-    def __init__(self):
-        super().__init__()
-
-    @functools.lru_cache(maxsize=100)
-    def serialize(self, data):
-        return self.identifier + pickle.dumps(data)
-
-    def deserialize(self, payload):
-        chomped = self.chomp(payload)
-        data = pickle.loads(chomped)
-        return data
-
-
-class code_dill(fxPicker_shared):
-    """ We use dill to get the source code out of the function object
-    and then exec the function body to load it in. The function object
-    is then returned by name.
-    """
-
-    _identifier = b'01\n'
-    _for_code = True
-
-    def __init__(self):
-        super().__init__()
-
-    @functools.lru_cache(maxsize=100)
-    def serialize(self, data):
-        x = dill.dumps(data)
-        return self.identifier + x
-
-    def deserialize(self, payload):
-        chomped = self.chomp(payload)
-        fn = dill.loads(chomped)
-        return fn
-
-
-class code_text_inspect(fxPicker_shared):
-    """ We use dill to get the source code out of the function object
-    and then exec the function body to load it in. The function object
-    is then returned by name.
-    """
-
-    _identifier = b'02\n'
-    _for_code = True
-
-    def __init__(self):
-        super().__init__()
-
-    @functools.lru_cache(maxsize=100)
-    def serialize(self, data):
-        name = data.__name__
-        body = inspect.getsource(data)
-        x = pickle.dumps((name, body))
-        return self.identifier + x
-
-    def deserialize(self, payload):
-        chomped = self.chomp(payload)
-        name, body = pickle.loads(chomped)
-        exec(body)
-        return locals()[name]
-
-
-class code_text_dill(fxPicker_shared):
-    """ We use dill to get the source code out of the function object
-    and then exec the function body to load it in. The function object
-    is then returned by name.
+    Unlike pickle and dill, json is a string based serializer. For consistency
+    this class will do utf-8 encoding of the json-serialized strings to bytes for
+    consistency
     """
 
     _identifier = b'03\n'
-    _for_code = True
+    _for_code = False
+    _for_data = True
 
     def __init__(self):
         super().__init__()
 
-    @functools.lru_cache(maxsize=100)
     def serialize(self, data):
-        name = data.__name__
-        body = dill.source.getsource(data)
-        x = pickle.dumps((name, body))
+        x = json.dumps(data).encode('utf-8')
         return self.identifier + x
 
     def deserialize(self, payload):
-        chomped = self.chomp(payload)
-        name, body = pickle.loads(chomped)
-        exec(body)
-        return locals()[name]
+        x = json.loads(self.chomp(payload).decode('utf-8'))
+        return x
