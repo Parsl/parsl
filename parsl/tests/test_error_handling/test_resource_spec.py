80409a8973cf59ff75028c3955a553ed05969919
Zhuozhao Li <zhuozhl@clemson.edu>
2020-08-12 19:23:09 +0000

    change the test for resource specification

--- parsl/tests/test_error_handling/test_resource_spec.py
+++ parsl/tests/test_error_handling/test_resource_spec.py
@@ -1,7 +1,9 @@
 import parsl
 from parsl.app.app import python_app
-from parsl.tests.configs.local_threads import config
-from parsl.executors.errors import UnsupportedFeatureError
+#from parsl.tests.configs.local_threads import config
+# from parsl.tests.configs.htex_local import config
+from parsl.tests.configs.workqueue_ex import config
+from parsl.executors.errors import UnsupportedFeatureError, ExecutorError
 from parsl.executors import WorkQueueExecutor
 
 
@@ -11,19 +13,47 @@ def double(x, parsl_resource_specification={}):
 
 
 def test_resource(n=2):
-    spec = {'cores': 2, 'memory': '1GiB'}
+    executors = parsl.dfk().executors
+    executor = None
+    for label in executors:
+        if label != 'data_manager':
+            executor = executors[label]
+            break
+
+    # Specify incorrect number of resources
+    spec = {'cores': 2, 'memory': 1000}
     fut = double(n, parsl_resource_specification=spec)
     try:
         fut.result()
+    except UnsupportedFeatureError as e:
+        print(e)
+        assert not isinstance(executor, WorkQueueExecutor)
     except Exception as e:
-        assert isinstance(e, UnsupportedFeatureError)
+        print(e)
+        assert isinstance(e, ExecutorError)
+
+    # Specify resources with wrong types
+    # cpus is incorrect
+    spec = {'cpus': 2, 'memory': 1000, 'disk': 1000}
+    fut = double(n, parsl_resource_specification=spec)
+    try:
+        fut.result()
+    except UnsupportedFeatureError as e:
+        print(e)
+        assert not isinstance(executor, WorkQueueExecutor)
+    except Exception as e:
+        print(e)
+        assert isinstance(e, ExecutorError)
+
+    # Correct specification
+    spec = {'cores': 2, 'memory': 1000, 'disk': 1000}
+    fut = double(n, parsl_resource_specification=spec)
+    try:
+        fut.result()
+    except UnsupportedFeatureError as e:
+        print(e)
+        assert not isinstance(executor, WorkQueueExecutor)
     else:
-        executors = parsl.dfk().executors
-        executor = None
-        for label in executors:
-            if label != 'data_manager':
-                executor = executors[label]
-                break
         assert isinstance(executor, WorkQueueExecutor)
 
 
