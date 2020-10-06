2ab60f2d6fa5c62ac1a213f9ce47d3bd14daf895
Lincoln Bryant <lincolnb@uchicago.edu>
2020-10-06 13:44:44 -0500

    Update osg.py

--- parsl/configs/osg.py
+++ parsl/configs/osg.py
@@ -16,14 +16,14 @@ config = Config(
                 scheduler_options="""
                 +ProjectName = "MyProject"
                 Requirements = HAS_MODULES=?=TRUE
-                """
+                """,
                 # Command to be run before starting a worker, such as:
                 # 'module load Anaconda; source activate parsl_env'.
                 worker_init='unset HOME; unset PYTHONPATH; module load python/3.7.0; python3 -m venv parsl_env; source parsl_env/bin/activate; python3 -m pip install parsl',
                 walltime="00:20:00",
             ),
             worker_logdir_root='$OSG_WN_TMP',
-            worker_ports(31000,31001)
+            worker_ports=(31000,31001)
         )
     ]
 )
