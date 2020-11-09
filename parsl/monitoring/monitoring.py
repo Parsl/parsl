60fc53d6209219bfc85f19e0c946559c9afbe487
Zhuozhao Li <zhuozhl@clemson.edu>
2020-11-09 19:15:15 +0000

    raise monitoring first message exception to task directly

--- parsl/monitoring/monitoring.py
+++ parsl/monitoring/monitoring.py
@@ -301,16 +301,13 @@ class MonitoringHub(RepresentationMixin):
         """
         def wrapped(*args, **kwargs):
             # Send first message to monitoring router
-            try:
-                monitor(os.getpid(),
-                        task_id,
-                        monitoring_hub_url,
-                        run_id,
-                        logging_level,
-                        sleep_dur,
-                        first_message=True)
-            except Exception:
-                pass
+            monitor(os.getpid(),
+                    task_id,
+                    monitoring_hub_url,
+                    run_id,
+                    logging_level,
+                    sleep_dur,
+                    first_message=True)
 
             # create the monitor process and start
             p = Process(target=monitor,
