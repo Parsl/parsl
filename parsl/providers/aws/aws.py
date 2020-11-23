bf6b2025d49fc7fa8c8c465983aa6b502d45b4d3
Sohit Miglani <sohitmiglani@gmail.com>
2020-11-22 18:07:22 -0800

    Fixed mismatch in state_file name

--- parsl/providers/aws/aws.py
+++ parsl/providers/aws/aws.py
@@ -139,7 +139,7 @@ class AWSProvider(ExecutionProvider, RepresentationMixin):
         self.launcher = launcher
         self.linger = linger
         self.resources = {}
-        self.state_file = state_file if state_file is not None else '.ec2_{}.json'.format(self.label)
+        self.state_file = state_file if state_file is not None else 'awsproviderstate.json'
 
         env_specified = os.getenv("AWS_ACCESS_KEY_ID") is not None and os.getenv("AWS_SECRET_ACCESS_KEY") is not None
         if profile is None and key_file is None and not env_specified:
