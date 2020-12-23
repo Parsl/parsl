e60f4b1e2d0690cabfb0d4f0775977d83de8e08e
Zhuozhao Li <zhuozhl@clemson.edu>
2020-12-23 19:00:28 +0000

    update scale_in block id type

--- parsl/dataflow/task_status_poller.py
+++ parsl/dataflow/task_status_poller.py
@@ -68,7 +68,7 @@ class PollItem(ExecutorStatus):
             for id in ids:
                 new_status[id] = JobStatus(JobState.CANCELLED)
                 del self._status[id]
-            self.send_monitoring_info(new_status)
+            self.send_monitoring_info(new_status, block_id_type='internal')
         return ids
 
     def scale_out(self, n):
