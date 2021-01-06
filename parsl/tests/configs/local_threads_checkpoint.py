04cac32ae8b708916f3a18c704b937cf6ea7636f
Ben Clifford <benc@hawaga.org.uk>
2021-01-06 02:45:40 +0000

    Use parsl inbuilt runinfo uniquification, not separate runinfo_XXXXXX for pytest (#1915)
    There is no reason at the moment that the test suite should be doing uniquification in
    a way that parsl cannot do automatically:
    
    1. There are comments about needing this when running multiple tests in parallel. The
    test suite at the moment isn't expected to run in parallel - at the very least:
    
    1.1 there is no separation of working files and global variables between tests
    
    1.2 running pytest with -nX fails immediately because the --config parameter does not
    work
    
    2. There were also problems a year or two ago with a style of writing workflows which
    initialised a DFK as soon as a module was loaded. That meant a DFK would be created on
    workers when deserialisation happened, and that caused a race condition with multiple
    workers trying to create the same rundir/NNN/ when they shouldn't have created a worker
    at all. The solution to this is "don't do that", as in, don't load a DFK by default in
    a module that will be loaded on a worker. The test suite as it is now does not have
    that bad behaviour.
    
    I've had this patch in my LSST dev stack since April 2020 and haven't observed any
    problems.

--- parsl/tests/configs/local_threads_checkpoint.py
+++ parsl/tests/configs/local_threads_checkpoint.py
@@ -1,6 +1,5 @@
 from parsl.config import Config
 from parsl.executors.threads import ThreadPoolExecutor
-from parsl.tests.utils import get_rundir
 
 
 def fresh_config():
@@ -9,8 +8,7 @@ def fresh_config():
             ThreadPoolExecutor(
                 label='local_threads_checkpoint',
             )
-        ],
-        run_dir=get_rundir(),
+        ]
     )
 
 
