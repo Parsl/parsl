dd60aedb807df2bf6f233f0ca96ec45a6f4e3958
Ben Clifford <benc@hawaga.org.uk>
2021-01-28 17:27:21 +0000

    Make monitoring more resilient to spurious network traffic (#1947)
    * Make monitoring more resilient to spurious network traffic
    
    In practice on cori.nersc.gov on large runs, some unexpected network
    traffic arrived at monitoring ports. This would sometimes crash
    the monitoring system. This PR makes monitoring more likely to ignore
    such traffic rather than crashing.
    
    This PR adds some tests to generate spurious traffic.
    
    Notes to reviewers:
    I'm a little unclear about the correct way to report that
    exceptions happened in the database loop: at the moment, this
    ignores such exceptions and then reports a final exception
    when monitoring finishes.
    
    Another of my concerns is that there are other causes of exceptions,
    not only spurious messages, and this may not be the right
    handling for such messages. (for example, does a particular
    socket closed exception result in the loop going into a 100%
    CPU loop?)
    
    Co-authored-by: Zhuozhao Li <zhuozhao@uchicago.edu>

---
+++ parsl/tests/test_monitoring/test_fuzz_zmq.py
@@ -0,0 +1,96 @@
+import logging
+import os
+import parsl
+import pytest
+import socket
+import time
+
+logger = logging.getLogger(__name__)
+
+
+@parsl.python_app
+def this_app():
+    return 5
+
+
+@pytest.mark.local
+def test_row_counts():
+    from parsl.tests.configs.htex_local_alternate import fresh_config
+    import sqlalchemy
+
+    if os.path.exists("monitoring.db"):
+        logger.info("Monitoring database already exists - deleting")
+        os.remove("monitoring.db")
+
+    logger.info("loading parsl")
+    parsl.load(fresh_config())
+
+    logger.info("invoking apps and waiting for result")
+
+    assert this_app().result() == 5
+    assert this_app().result() == 5
+
+    # now we've run some apps, send fuzz into the monitoring ZMQ
+    # socket, before trying to run some more tests.
+
+    # there are different kinds of fuzz:
+    # could send ZMQ messages that are weird
+    # could send random bytes to the TCP socket
+    #   the latter is what i'm most suspicious of in my present investigation
+
+    # dig out the interchange port...
+    hub_address = parsl.dfk().hub_address
+    hub_interchange_port = parsl.dfk().hub_interchange_port
+
+    # this will send a string to a new socket connection
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((hub_address, hub_interchange_port))
+        s.sendall(b'fuzzing\r')
+
+    # this will send a non-object down the DFK's existing ZMQ connection
+    parsl.dfk().monitoring._dfk_channel.send(b'FuzzyByte\rSTREAM')
+
+    # This following attack is commented out, because monitoring is not resilient
+    # to this.
+    # In practice, it works some of the time but in some circumstances,
+    # it would still abandon writing multiple unrelated records to the database,
+    # causing ongoing monitoring data loss.
+
+    # This will send an unusual python object down the
+    # DFK's existing ZMQ connection. this doesn't break the router,
+    # but breaks the db_manager in a way that isn't reported until
+    # the very end of the run, and database writing is abandoned
+    # rather than completing, in this case.
+    # I'm unclear if this is a case we should be trying to handle.
+    # parsl.dfk().monitoring._dfk_channel.send_pyobj("FUZZ3")
+
+    # hopefully long enough for any breakage to happen
+    # before attempting to run more tasks
+    time.sleep(5)
+
+    assert this_app().result() == 5
+    assert this_app().result() == 5
+
+    logger.info("cleaning up parsl")
+    parsl.dfk().cleanup()
+    parsl.clear()
+
+    # at this point, we should find one row in the monitoring database.
+
+    logger.info("checking database content")
+    engine = sqlalchemy.create_engine("sqlite:///monitoring.db")
+    with engine.begin() as connection:
+
+        result = connection.execute("SELECT COUNT(*) FROM workflow")
+        (c, ) = result.first()
+        assert c == 1
+
+        result = connection.execute("SELECT COUNT(*) FROM task")
+        (c, ) = result.first()
+        assert c == 4
+
+        result = connection.execute("SELECT COUNT(*) FROM try")
+        (c, ) = result.first()
+        assert c == 4
+
+    logger.info("all done")
