f6613eb26e5d79433ec8c8612385c29d5afba587
Ben Clifford <benc@hawaga.org.uk>
2021-02-17 15:53:13 +0000

    test fs based monitoring provider using local executor

--- parsl/tests/configs/local_threads_monitoring.py
+++ parsl/tests/configs/local_threads_monitoring.py
@@ -2,8 +2,21 @@ from parsl import ThreadPoolExecutor
 from parsl.config import Config
 from parsl.monitoring import MonitoringHub
 
-config = Config(executors=[ThreadPoolExecutor(label='threads', max_threads=4)],
-                monitoring=MonitoringHub(
+
+# BENC: temp class for dev purposes. should test both UDP and filesystem
+# radiomodes with local executor.
+class TestExecutor(ThreadPoolExecutor):
+    radio_mode = "filesystem"
+
+
+def fresh_config():
+    executor = TestExecutor(label='threads', max_threads=4)
+
+    # BENC: this is to check I'm overriding in subclass properly
+    assert executor.radio_mode == "filesystem"
+
+    return Config(executors=[executor],
+                  monitoring=MonitoringHub(
                     hub_address="localhost",
                     hub_port=55055,
                     resource_monitoring_interval=3,
