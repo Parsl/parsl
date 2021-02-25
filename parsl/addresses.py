e8f1aaa1cf7c07b7478a5f944c36daa32bfeb8cf
radiantone <darren@rapidquest.ai>
2021-02-25 07:42:54 -0500

    Workaround for missing /sbin/ip command
    Some linux distros do not come with /sbin/ip command and thus a workaround is needed for the function address_by_route.

--- parsl/addresses.py
+++ parsl/addresses.py
@@ -29,7 +29,17 @@ def address_by_route() -> str:
        not reachable from workers.
     """
     logger.debug("Finding address by querying local routing table")
-    addr = os.popen("/sbin/ip route get 8.8.8.8 | awk '{print $NF;exit}'").read().strip()
+    
+    if os.path.exists('/sbin/ip'):
+        addr = os.popen("/sbin/ip route get 8.8.8.8 | awk '{print $NF;exit}'").read().strip()
+    else:
+        # original author unknown
+        import socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        addr = s.getsockname()[0]
+        s.close()
+        
     logger.debug("Address found: {}".format(addr))
     return addr
 
