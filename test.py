e22e6648a0c904f0342a1b6917780d85488b6258
Darren Govoni <darren@rapidquest.ai>
2021-03-22 12:02:30 -0400

    Code cleanup and maxworkers arg

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ from parsl.app.app import balsam_app
 from parsl.config import Config
 from parsl.executors.balsam.executor import BalsamExecutor
 
-logging.disable(sys.maxsize)
 
 config = Config(
     executors=[
         BalsamExecutor(
             siteid=1,
+            maxworkers=3,
             project='local'
         )
-    ],
+    ]
 )
 
 parsl.load(config)
@@ -28,22 +28,32 @@ def callback(future,**kwargs):
 
 
 @balsam_app(executors=["BalsamExecutor"])
+def greetings(inputs=[]):
+    return "echo Greetings"
+
+
+@balsam_app(executors=["BalsamExecutor"])
 def hello(inputs=[]):
-    return "echo Hello"
+    return "Hello"
+
+
+@balsam_app(executors=["BalsamExecutor"])
+def combine(inputs=[]):
+    return "echo {} {}".format(inputs[0], inputs[1])
 
 
 @balsam_app(executors=["BalsamExecutor"])
 def world(inputs=[]):
-    return "echo {} World".format(inputs[0])
+    return "echo {} {} World!".format(inputs[0], inputs[1])
+
 
 SITE_ID = 1
 CLASS_PATH = 'parslapprunner.ParslAppRunner'
 
-hello1 = hello(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='bash', workdir='parsl/hello1', appname='hello1', callback=callback)
+hello = hello(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='python', workdir='parsl/hello', appname='hello', callback=callback)
+combine = combine(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='bash', workdir='parsl/combine', appname='combine', callback=callback, inputs=[hello.result(), "There!"])
+greetings = greetings(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='bash', workdir='parsl/greetings', appname='greetings', callback=callback)
 
-world = world(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='bash', workdir='parsl/world', appname='world', callback=callback, inputs=[hello1.result()])
-
-# Callback result: World
+world = world(SITE_ID, CLASS_PATH, numnodes=1, sitedir="git/site1", script='bash', workdir='parsl/world', appname='world', callback=callback, inputs=[combine.result(), greetings.result()])
 
 print(world.result())
-# Hello World
