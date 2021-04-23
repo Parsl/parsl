8c15f3d2f390308da5dd3a23f9a5a771cb8c4872
Darren Govoni <darren@rapidquest.ai>
2021-04-23 11:24:09 -0400

    New package with AppRunner and BashRunner apps

--- examples/balsam/apps/parsl.py
+++ examples/balsam/apps/parsl.py
@@ -1,12 +1,43 @@
 from balsam.site import ApplicationDefinition
 
 
-class ParslAppRunner(ApplicationDefinition):
+class BashRunner(ApplicationDefinition):
+    """
+    Parsl Bash Runner. Place this file in your site directory under apps/ directory.
+    """
+    environment_variables = {}
+    command_template = '/bin/bash -c {{ command }}'
+    parameters = {}
+    transfers = {}
+
+    def preprocess(self):
+        print("COMMAND:",self.command_template)
+        self.job.state = "PREPROCESSED"
+
+    def postprocess(self):
+        from balsam.api import site_config
+
+        workdir = self.job.resolve_workdir(site_config.data_path)
+        stdout = workdir.joinpath("job.out").read_text().strip()
+        self.job.data = {'result': stdout, 'type': 'bash'}
+        self.job.state = "POSTPROCESSED"
+
+    def shell_preamble(self):
+        pass
+
+    def handle_timeout(self):
+        self.job.state = "RESTART_READY"
+
+    def handle_error(self):
+        self.job.state = "FAILED"
+
+
+class AppRunner(ApplicationDefinition):
     """
     Parsl App Runner. Place this file in your site directory under apps/ directory.
     """
     environment_variables = {}
-    command_template = 'singularity exec --bind {{ workdir }}:/work --bind .:/app {{ image }} python /app/app.py'
+    command_template = 'singularity exec --bind {{ workdir }}:/work --bind .:/app {{ image }} {{ command }}'
     parameters = {}
     transfers = {}
 
@@ -43,4 +74,3 @@ class ParslAppRunner(ApplicationDefinition):
 
     def handle_error(self):
         self.job.state = "FAILED"
-
