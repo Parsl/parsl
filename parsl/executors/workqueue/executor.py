436cc2c9f9c388120892cfb08c621f39bbfef3a9
Ben Clifford <benc@hawaga.org.uk>
2021-04-27 09:26:27 +0000

    add docstring

--- parsl/executors/workqueue/executor.py
+++ parsl/executors/workqueue/executor.py
@@ -179,6 +179,12 @@ class WorkQueueExecutor(NoStatusHandlingExecutor):
 
         worker_options: str
             Extra options passed to work_queue_worker. Default is ''.
+
+        worker_executable: str
+            The command used to invoke work_queue_worker. This can be used
+            when the worker needs to be wrapped inside some other command
+            (for example, to run the worker inside a container). Default is
+            'work_queue_worker'.
     """
 
     @typeguard.typechecked
