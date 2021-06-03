e5590f61c64f757cc421319cd699707864b15eae
Ben Clifford <benc@hawaga.org.uk>
2021-06-03 10:56:06 +0000

    Add comment on resource type verification

--- parsl/executors/workqueue/executor.py
+++ parsl/executors/workqueue/executor.py
@@ -356,6 +356,9 @@ class WorkQueueExecutor(NoStatusHandlingExecutor):
                 logger.error(message)
                 raise ExecutorError(self, message)
 
+            # this checks that either all of the required resource types are specified, or
+            # that none of them are: the `required_resource_types` are not actually required,
+            # but if one is specified, then they all must be.
             key_check = required_resource_types.intersection(keys)
             required_keys_ok = len(key_check) == 0 or key_check == required_resource_types
             if not self.autolabel and not required_keys_ok:
