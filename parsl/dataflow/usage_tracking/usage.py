bac4b8a7010676d46fdd140a8cb030940e44508e
Ben Clifford <benc@hawaga.org.uk>
2021-06-29 07:22:14 -0700

    Put usage tracking back to a forked process which was removed by my earlier removal of global forking

--- parsl/dataflow/usage_tracking/usage.py
+++ parsl/dataflow/usage_tracking/usage.py
@@ -10,6 +10,7 @@ import sys
 import platform
 import multiprocessing as mp
 
+from parsl.multiprocessing import ForkProcess
 from parsl.version import VERSION as PARSL_VERSION
 
 logger = logging.getLogger(__name__)
@@ -19,7 +20,7 @@ def async_process(fn):
     """ Decorator function to launch a function as a separate process """
 
     def run(*args, **kwargs):
-        proc = mp.Process(target=fn, args=args, kwargs=kwargs, name="Usage-Tracking")
+        proc = ForkProcess(target=fn, args=args, kwargs=kwargs, name="Usage-Tracking")
         proc.start()
         return proc
 
