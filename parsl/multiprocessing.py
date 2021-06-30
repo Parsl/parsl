99e2a7ca8d935dfdccb0377a6df9240905c99012
Ben Clifford <benc@hawaga.org.uk>
2021-06-30 09:38:39 -0700

    Fix mypy

--- parsl/multiprocessing.py
+++ parsl/multiprocessing.py
@@ -6,7 +6,7 @@ import multiprocessing
 import multiprocessing.queues
 import platform
 
-from typing import Type
+from typing import Callable, Type
 
 logger = logging.getLogger(__name__)
 
@@ -49,6 +49,10 @@ class MacSafeQueue(multiprocessing.queues.Queue):
     def empty(self):
         return not self._counter.value
 
+# SizedQueue should be constructable using the same calling
+# convention as multiprocessing.Queue but that entire signature
+# isn't expressible in mypy 0.790
+SizedQueue: Callable[..., multiprocessing.Queue]
 
 if platform.system() != 'Darwin':
     import multiprocessing
