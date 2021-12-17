07fdf429e4e16ebb69276ed04a225e4fb5b4cadd
kommav <86332757+kommav@users.noreply.github.com>
2021-12-17 09:07:27 -0600

    Update test_fuzz_zmq.py

--- parsl/tests/test_monitoring/test_fuzz_zmq.py
+++ parsl/tests/test_monitoring/test_fuzz_zmq.py
@@ -18,9 +18,9 @@ def test_row_counts():
     from parsl.tests.configs.htex_local_alternate import fresh_config
     import sqlalchemy
 
-    if os.path.exists("monitoring.db"):
+    if os.path.exists("runinfo/monitoring.db"):
         logger.info("Monitoring database already exists - deleting")
-        os.remove("monitoring.db")
+        os.remove("runinfo/monitoring.db")
 
     logger.info("loading parsl")
     parsl.load(fresh_config())
