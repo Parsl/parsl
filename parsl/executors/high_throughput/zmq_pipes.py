98e15ca4e2720565b96de66a4c507860819c72e2
Ben Clifford <benc@hawaga.org.uk>
2022-02-17 14:47:31 +0000

    Use exponential, rather than linear, backoff when interchange task full
    This reduces the logging associated with a queue full event as the
    delay period will get large rapidly.
    
    In exchange this probably makes message delivery during task submission
    a bit slower when a queue full event happens, although I have not benchmarked
    that.

--- parsl/executors/high_throughput/zmq_pipes.py
+++ parsl/executors/high_throughput/zmq_pipes.py
@@ -112,7 +112,7 @@ class TasksOutgoing(object):
         in ZMQ sockets reaching a broken state once there are ~10k tasks in flight.
         This issue can be magnified if each the serialized buffer itself is larger.
         """
-        timeout_ms = 0
+        timeout_ms = 1
         while True:
             socks = dict(self.poller.poll(timeout=timeout_ms))
             if self.zmq_socket in socks and socks[self.zmq_socket] == zmq.POLLOUT:
@@ -120,7 +120,7 @@ class TasksOutgoing(object):
                 self.zmq_socket.send_pyobj(message, copy=True)
                 return
             else:
-                timeout_ms += 1
+                timeout_ms *= 2
                 logger.debug("Not sending due to non-ready zmq pipe, timeout: {} ms".format(timeout_ms))
 
     def close(self):
