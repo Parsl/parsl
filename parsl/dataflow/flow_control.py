5f9b2666cb2f7290a5cdad0bc6caf83f1e5ee050
Ben Clifford <benc@hawaga.org.uk>
2022-05-13 12:14:06 +0000

    Remove kind parameter from places it isn't used or documented

--- parsl/dataflow/flow_control.py
+++ parsl/dataflow/flow_control.py
@@ -88,7 +88,7 @@ class FlowControl(object):
                 return
 
             if prev == self._wake_up_time:
-                self.make_callback(kind='timer')
+                self.make_callback()
             else:
                 print("Sleeping a bit more")
 
@@ -98,18 +98,14 @@ class FlowControl(object):
         self._event_count += 1
         if self._event_count >= self.threshold:
             logger.debug("Eventcount >= threshold")
-            self.make_callback(kind="event")
+            self.make_callback()
 
-    def make_callback(self, kind=None):
+    def make_callback(self):
         """Makes the callback and resets the timer.
-
-        KWargs:
-               - kind (str): Default=None, used to pass information on what
-                 triggered the callback
         """
         self._wake_up_time = time.time() + self.interval
         try:
-            self.callback(tasks=self._event_buffer, kind=kind)
+            self.callback(tasks=self._event_buffer)
         except Exception:
             logger.error("Flow control callback threw an exception - logging and proceeding anyway", exc_info=True)
         self._event_buffer = []
@@ -190,11 +186,11 @@ class Timer(object):
                 return
 
             if prev == self._wake_up_time:
-                self.make_callback(kind='timer')
+                self.make_callback()
             else:
                 print("Sleeping a bit more")
 
-    def make_callback(self, kind=None):
+    def make_callback(self):
         """Makes the callback and resets the timer.
         """
         self._wake_up_time = time.time() + self.interval
