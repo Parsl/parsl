Parsl/parsl

commit 2bece7f897b4b36f5e27373f1375f96ddb24b32a

Only query active slurm jobs (#2259)
When squeue is called with at least one job ID which is active, it returns successfully. If it is called with only jobs that have already completed, it returns an error. This situation happens if no blocks are currently running.

The same logic (checking if the existing status is terminal) is used in the local and kubernetes providers.

Co-authored-by: Alec Clowes <alec.clowes@roivant.com>

parsl/providers/slurm/slurm.py
@@ -138,15 +138,21 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
         Returns:
               [status...] : Status list of all jobs
         '''
-        job_id_list = ','.join(self.resources.keys())
+        job_id_list = ','.join(
+            [jid for jid, job in self.resources.keys() if not job['status'].terminal]
+        )
+        if not job_id_list:
+            logger.debug('No active jobs, skipping status update')
+            return
+
         cmd = "squeue --job {0}".format(job_id_list)
-        logger.debug("Executing sqeueue")
+        logger.debug("Executing %s", cmd)
         retcode, stdout, stderr = self.execute_wait(cmd)
-        logger.debug("sqeueue returned")
+        logger.debug("sqeueue returned %s %s", stdout, stderr)
 
         # Execute_wait failed. Do no update
         if retcode != 0:
-            logger.warning("squeue failed with non-zero exit code {} - see https://github.com/Parsl/parsl/issues/1588".format(retcode))
+            logger.warning("squeue failed with non-zero exit code {}".format(retcode))
             return
 
         jobs_missing = list(self.resources.keys())

commit 46c9b3999ab901be08c8f1381473fbebfda21117

Fix slurm provider type error broken by PR #2259

parsl/providers/slurm/slurm.py
@@ -139,7 +139,7 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
               [status...] : Status list of all jobs
         '''
         job_id_list = ','.join(
-            [jid for jid, job in self.resources.keys() if not job['status'].terminal]
+            [jid for jid, job in self.resources.items() if not job['status'].terminal]
         )
         if not job_id_list:
             logger.debug('No active jobs, skipping status update')