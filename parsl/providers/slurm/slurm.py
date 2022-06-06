94d31bc59c12a0897f436cef9b7f8a5073419761
Ben Clifford <benc@hawaga.org.uk>
2022-06-06 20:04:22 +0000

    Clarify log messages when SlurmProvider gets unknown statuses (#2258)
    * Format slurm to parsl state mapping more usefully
    
    Before:
    [DEBUG]  Updating job 58928398 with slurm status R to parsl status 2
    
    After:
    [DEBUG]  Updating job 58928492 with slurm status R to parsl state JobState.RUNNING
    
    * Emit a warning when slurm job state is unknown

--- parsl/providers/slurm/slurm.py
+++ parsl/providers/slurm/slurm.py
@@ -160,8 +160,11 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
             parts = line.split()
             if parts and parts[0] != 'JOBID':
                 job_id = parts[0]
-                status = translate_table.get(parts[4], JobState.UNKNOWN)
-                logger.debug("Updating job {} with slurm status {} to parsl status {}".format(job_id, parts[4], status))
+                slurm_state = parts[4]
+                if slurm_state not in translate_table:
+                    logger.warning(f"Slurm status {slurm_state} is not known")
+                status = translate_table.get(slurm_state, JobState.UNKNOWN)
+                logger.debug("Updating job {} with slurm status {} to parsl state {!s}".format(job_id, slurm_state, status))
                 self.resources[job_id]['status'] = JobStatus(status)
                 jobs_missing.remove(job_id)
 
