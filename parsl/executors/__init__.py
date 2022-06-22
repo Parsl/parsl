2f8c9937583ce0d4c1bcdea0f07b2ac0095ef166
Ben Clifford <benc@hawaga.org.uk>
2022-06-22 13:31:49 +0000

    Remove remaining IPP code, issue #1548

--- parsl/executors/__init__.py
+++ parsl/executors/__init__.py
@@ -1,5 +1,4 @@
 from parsl.executors.threads import ThreadPoolExecutor
-from parsl.executors.ipp import IPyParallelExecutor
 from parsl.executors.workqueue.executor import WorkQueueExecutor
 from parsl.executors.high_throughput.executor import HighThroughputExecutor
 from parsl.executors.extreme_scale.executor import ExtremeScaleExecutor
