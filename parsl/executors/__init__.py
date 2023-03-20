c539b1e900bfafcb3b6b48d97abe77f2d126e2cf
Ben Clifford <benc@hawaga.org.uk>
2023-03-20 17:48:34 +0100

    Move extreme scale executor to end of life (#2622)
    Remove re-export of ExtremeScaleExecutor from parsl.executors.
    Remove CI tests.
    Remove mypy validation of extreme scale source tree.
    Remove MPI dependency.
    Remove references in documentation.
    
    Fixes #2565
    Fixes #2154
    Fixes #1229

--- parsl/executors/__init__.py
+++ parsl/executors/__init__.py
@@ -1,11 +1,9 @@
 from parsl.executors.threads import ThreadPoolExecutor
 from parsl.executors.workqueue.executor import WorkQueueExecutor
 from parsl.executors.high_throughput.executor import HighThroughputExecutor
-from parsl.executors.extreme_scale.executor import ExtremeScaleExecutor
 from parsl.executors.flux.executor import FluxExecutor
 
 __all__ = ['ThreadPoolExecutor',
            'HighThroughputExecutor',
-           'ExtremeScaleExecutor',
            'WorkQueueExecutor',
            'FluxExecutor']
