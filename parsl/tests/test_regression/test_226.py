2e73a724306c88a4ea09ce54302a431679775404
Ben Clifford <benc@hawaga.org.uk>
2023-05-15 02:51:21 +0000

    Fix-up and re-enable regression test for issue #226

--- parsl/tests/test_regression/test_226.py
+++ parsl/tests/test_regression/test_226.py
@@ -7,9 +7,6 @@ import pandas as pd
 import pytest
 
 from parsl.app.app import bash_app, python_app
-from parsl.tests.configs.local_threads import config
-
-local_config = config
 
 
 class Foo:
@@ -46,26 +43,20 @@ def echo(msg, postfix='there', stdout='std.out'):
     return 'echo {} {}'.format(msg, postfix)
 
 
-blacklist = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs', '*ipp*')
-
-
-# @pytest.mark.blacklist(blacklist, reason='hangs on Travis')
-@pytest.mark.skip("Broke somewhere between PR #525 and PR #652")
 def test_no_eq():
     res = get_foo_x('foo').result()
     assert res == 1, 'Expected 1, returned {}'.format(res)
 
 
-# @pytest.mark.blacklist(blacklist, reason='hangs on Travis')
-@pytest.mark.skip("Broke somewhere between PR #525 and PR #652")
 def test_get_dataframe():
     res = get_dataframe().result()
     assert res.equals(data), 'Unexpected dataframe'
 
 
-# @pytest.mark.blacklist(blacklist, reason='hangs on Travis')
-@pytest.mark.skip("Broke somewhere between PR #525 and PR #652")
 def test_bash_default_arg():
+    if os.path.exists('std.out'):
+        os.remove('std.out')
+
     echo('hello').result()
     with open('std.out', 'r') as f:
         assert f.read().strip() == 'hello there', "Output should be 'hello there'"
