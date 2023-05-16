e4257705f4ecffc1d0411f8d9985e52d738cff26
Ben Clifford <benc@hawaga.org.uk>
2023-05-16 13:12:41 +0000

    flake8/mypy

--- parsl/tests/test_error_handling/test_no_all_executor.py
+++ parsl/tests/test_error_handling/test_no_all_executor.py
@@ -3,8 +3,9 @@ from parsl import ThreadPoolExecutor
 from parsl.config import Config
 from parsl.errors import ConfigurationError
 
+
 @pytest.mark.local
-def test_no_executor_with_all_label():
+def test_no_executor_with_all_label() -> None:
     """Checks that the configuration system rejects configurations with
     an executor labelled 'all', which conflicts with the use of 'all' as
     a magic label to select all executors."""
