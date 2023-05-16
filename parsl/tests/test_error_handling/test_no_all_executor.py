1cf0789257e507727ba8e2e11550a2fb8318586b
Ben Clifford <benc@hawaga.org.uk>
2023-05-16 13:20:48 +0000

    Add more tests, which demonstrate that this PR is broken

--- parsl/tests/test_error_handling/test_no_all_executor.py
+++ parsl/tests/test_error_handling/test_no_all_executor.py
@@ -1,4 +1,5 @@
 import pytest
+import parsl
 from parsl import ThreadPoolExecutor
 from parsl.config import Config
 from parsl.errors import ConfigurationError
@@ -12,3 +13,23 @@ def test_no_executor_with_all_label() -> None:
 
     with pytest.raises(ConfigurationError):
         Config(executors=[ThreadPoolExecutor(label="all")])
+
+
+@pytest.mark.local
+def test_no_add_executor_with_all_label() -> None:
+    """Checks that the configuration system rejects configurations with
+    an executor labelled 'all', which conflicts with the use of 'all' as
+    a magic label to select all executors."""
+
+    c = Config(executors=[ThreadPoolExecutor(label="executor1")])
+    dfk = parsl.load(c)
+
+    # check we can add an executor successfully
+    dfk.add_executors([ThreadPoolExecutor(label="executor2")])
+
+    # check we cannot add an executor called 'all'
+    with pytest.raises(ConfigurationError):
+        dfk.add_executors([ThreadPoolExecutor(label="all")])
+
+    dfk.cleanup()
+    parsl.clear()
