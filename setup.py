b39d45d801bd409ba1d5a417fe0b66ef9c2865a6
Ben Clifford <benc@hawaga.org.uk>
2023-06-04 17:11:33 +0000

    bump pandas to see if that makes disapperaed distutils happier

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extras_require = {
         'networkx>=2.5,<2.6',
         'Flask>=1.0.2',
         'flask_sqlalchemy',
-        'pandas<2',
+        'pandas<3',
         'plotly',
         'python-daemon'
     ],
