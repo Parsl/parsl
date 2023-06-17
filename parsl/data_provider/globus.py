9f5d287240c3691d0e1f547f7aab4890d2f0c334
Mousa Abughosh <mabughosh@hpclab-9.rd.unr.edu>
2023-06-16 18:10:14 -0700

    Globus API changes patch

--- parsl/data_provider/globus.py
+++ parsl/data_provider/globus.py
@@ -81,6 +81,8 @@ class Globus:
         tc = globus_sdk.TransferClient(authorizer=cls.authorizer)
         td = globus_sdk.TransferData(tc, src_ep, dst_ep)
         td.add_item(src_path, dst_path)
+        print('Globus transfer submitted from {}{} to {}{}'.format(
+            src_ep, src_path, dst_ep, dst_path))
         try:
             task = tc.submit_transfer(td)
         except Exception as e:
@@ -94,22 +96,17 @@ class Globus:
         with 60 second timeout limit. If the task is ACTIVE after time runs out 'task_wait' returns False,
         and True otherwise.
         """
-        while not tc.task_wait(task['task_id'], 60, 15):
+        while not tc.task_wait(task['task_id'], timeout=60):
             task = tc.get_task(task['task_id'])
             # Get the last error Globus event
-            events = tc.task_event_list(task['task_id'], num_results=1, filter='is_error:1')
-            try:
-                event = next(events)
-            # No error reported,  the transfer is still running
-            except StopIteration:
-                continue
-            # Print the error event to stderr and Parsl file log if it was not yet printed
-            if event['time'] != last_event_time:
-                last_event_time = event['time']
-                logger.warning('Non-critical Globus Transfer error event for globus://{}{}: "{}" at {}. Retrying...'.format(
-                    src_ep, src_path, event['description'], event['time']))
-                logger.debug('Globus Transfer error details: {}'.format(event['details']))
-
+            task_id = task['task_id']
+            for event in tc.task_event_list(task_id):
+                if event['time'] != last_event_time:
+                    last_event_time = event['time']
+                    logger.warning(
+                        'Non-critical Globus Transfer error event for globus://{}{}: "{}" at {}. Retrying...'.format(
+                            src_ep, src_path, event['description'], event['time']))
+                    logger.debug('Globus Transfer error details: {}'.format(event['details']))
         """
         The Globus transfer job (task) has been terminated (is not ACTIVE). Check if the transfer
         SUCCEEDED or FAILED.
@@ -120,7 +117,7 @@ class Globus:
                 task['task_id'], src_ep, src_path, dst_ep, dst_path))
         else:
             logger.debug('Globus Transfer task: {}'.format(task))
-            events = tc.task_event_list(task['task_id'], num_results=1, filter='is_error:1')
+            events = tc.task_event_list(task['task_id'])
             event = events.data[0]
             raise Exception('Globus transfer {}, from {}{} to {}{} failed due to error: "{}"'.format(
                 task['task_id'], src_ep, src_path, dst_ep, dst_path, event['details']))
