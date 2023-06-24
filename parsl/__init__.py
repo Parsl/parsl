ae6e8cbed3fa230212fe8d9e95a9ec5a9fe777ae
Ben Clifford <benc@hawaga.org.uk>
2023-06-24 17:46:42 +0300

    Fix 'import parsl.multiprocessing' (#2761)
    Due to a duplicate parsl.multiprocessing name, one defined in
    parsl/__init__.py by `import parsl`, and the other defined by
    the module namespace of parsl/multiprocessing.py, behaviour of
    imports of parsl.multiprocessing were inconsistent:
    
    >>> from parsl.multiprocessing import MacSafeQueue
    >>> MacSafeQueue
    <class 'parsl.multiprocessing.MacSafeQueue'>
    >>> import parsl.multiprocessing
    >>> parsl.multiprocessing.MacSafeQueue
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    AttributeError: module 'multiprocessing' has no attribute 'MacSafeQueue'
    
    This PR removes the duplicate definitions of parsl.multiprocessing,
    making the intended parsl module exposed in both import cases, and
    using a private (_underscored) alias for the internal import in
    parsl/__init__.py

--- parsl/__init__.py
+++ parsl/__init__.py
@@ -32,9 +32,9 @@ from parsl.data_provider.files import File
 
 from parsl.dataflow.dflow import DataFlowKernel, DataFlowKernelLoader
 
-import multiprocessing
+import multiprocessing as _multiprocessing
 if platform.system() == 'Darwin':
-    multiprocessing.set_start_method('fork', force=True)
+    _multiprocessing.set_start_method('fork', force=True)
 
 __author__ = 'The Parsl Team'
 __version__ = VERSION
