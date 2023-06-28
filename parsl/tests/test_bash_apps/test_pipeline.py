207c57c833198089ae0ce73f25fcb667f09a7538
Kevin Hunter Kesling <kevin@globus.org>
2023-06-27 22:56:56 -0400

    Remove a number of leftover test files
    Take advantage of pytest fixtures and update all errant tests accordingly.  In
    particular, noting that many tests rely on shared fs, use a temporary directory
    structure rooted inside of the CWD (typically the root Git directory), rather
    than `tmp_path`'s native use of `TMPDIR`.
    
    The only leftover detritus I didn't engineer removal of was the `runinfo/`
    directory, which is where Parsl places logs if `parsl.AUTO_LOGNAME` is in play.
    Given the potential to interplay with a developer's "live" data, I opted to
    leave this for now.

--- parsl/tests/test_bash_apps/test_pipeline.py
+++ parsl/tests/test_bash_apps/test_pipeline.py
@@ -1,17 +1,12 @@
-import argparse
-import os
 import pytest
 
-import parsl
 from parsl.app.app import bash_app
 from parsl.data_provider.files import File
 from parsl.app.futures import DataFuture
 
-from parsl.tests.configs.local_threads import config
-
 
 @bash_app
-def increment(inputs=[], outputs=[], stdout=None, stderr=None):
+def increment(inputs=(), outputs=(), stdout=None, stderr=None):
     cmd_line = """
     if ! [ -f {inputs[0]} ] ; then exit 43 ; fi
     x=$(cat {inputs[0]})
@@ -21,7 +16,7 @@ def increment(inputs=[], outputs=[], stdout=None, stderr=None):
 
 
 @bash_app
-def slow_increment(dur, inputs=[], outputs=[], stdout=None, stderr=None):
+def slow_increment(dur, inputs=(), outputs=(), stdout=None, stderr=None):
     cmd_line = """
     x=$(cat {inputs[0]})
     echo $(($x+1)) > {outputs[0]}
@@ -30,91 +25,60 @@ def slow_increment(dur, inputs=[], outputs=[], stdout=None, stderr=None):
     return cmd_line
 
 
-def cleanup_work(depth):
-    for i in range(0, depth):
-        fn = "test{0}.txt".format(i)
-        if os.path.exists(fn):
-            os.remove(fn)
-
-
 @pytest.mark.staging_required
-def test_increment(depth=5):
+def test_increment(tmpd_cwd, depth=5):
     """Test simple pipeline A->B...->N
     """
+    fpath = tmpd_cwd / "test0.txt"
+    fpath.write_text("0\n")
 
-    cleanup_work(depth)
-
-    # Create the first file
-    open("test0.txt", 'w').write('0\n')
-
-    # Create the first entry in the dictionary holding the futures
-    prev = File("test0.txt")
-    futs = {}
+    prev = [File(str(fpath))]
+    futs = []
     for i in range(1, depth):
-        print("Launching {0} with {1}".format(i, prev))
-        assert isinstance(prev, DataFuture) or isinstance(prev, File)
-        output = File("test{0}.txt".format(i))
-        fu = increment(inputs=[prev],  # Depend on the future from previous call
-                       # Name the file to be created here
-                       outputs=[output],
-                       stdout="incr{0}.out".format(i),
-                       stderr="incr{0}.err".format(i))
-        [prev] = fu.outputs
-        futs[i] = prev
-        print(prev.filepath)
-        assert isinstance(prev, DataFuture)
+        assert isinstance(prev[0], (DataFuture, File))
+        output = File(str(tmpd_cwd / f"test{i}.txt"))
+        f = increment(
+            inputs=prev,
+            outputs=[output],
+            stdout=str(tmpd_cwd / f"incr{i}.out"),
+            stderr=str(tmpd_cwd / f"incr{i}.err"),
+        )
+        prev = f.outputs
+        futs.append((i, prev[0]))
+        assert isinstance(prev[0], DataFuture)
 
-    for key in futs:
-        if key > 0:
-            fu = futs[key]
-            file = fu.result()
-            filename = file.filepath
+    for key, f in futs:
+        file = f.result()
+        expected = str(tmpd_cwd / f"test{key}.txt")
 
-            # this test is a bit close to a test of the specific implementation
-            # of File
-            assert file.local_path is None, "File on local side has overridden local_path, file: {}".format(repr(file))
-            assert file.filepath == "test{0}.txt".format(key), "Submit side filepath has not been preserved over execution"
-
-            data = open(filename, 'r').read().strip()
-            assert data == str(
-                key), "[TEST] incr failed for key: {0} got data: {1} from filename {2}".format(key, data, filename)
-
-    cleanup_work(depth)
+        assert file.local_path is None, "File on local side has overridden local_path, file: {}".format(repr(file))
+        assert file.filepath == expected, "Submit side filepath has not been preserved over execution"
+        data = open(file.filepath).read().strip()
+        assert data == str(key)
 
 
 @pytest.mark.staging_required
-def test_increment_slow(depth=5, dur=0.5):
+def test_increment_slow(tmpd_cwd, depth=5, dur=0.01):
     """Test simple pipeline slow (sleep.5) A->B...->N
     """
 
-    cleanup_work(depth)
+    fpath = tmpd_cwd / "test0.txt"
+    fpath.write_text("0\n")
 
-    # Create the first file
-    open("test0.txt", 'w').write('0\n')
-
-    prev = File("test0.txt")
-    # Create the first entry in the dictionary holding the futures
-    futs = {}
-    print("************** Type: ", type(dur), dur)
+    prev = [File(str(fpath))]
+    futs = []
     for i in range(1, depth):
-        print("Launching {0} with {1}".format(i, prev))
-        output = File("test{0}.txt".format(i))
-        fu = slow_increment(dur,
-                            # Depend on the future from previous call
-                            inputs=[prev],
-                            # Name the file to be created here
-                            outputs=[output],
-                            stdout="incr{0}.out".format(i),
-                            stderr="incr{0}.err".format(i))
-        [prev] = fu.outputs
-        futs[i] = prev
-        print(prev.filepath)
+        output = File(str(tmpd_cwd / f"test{i}.txt"))
+        f = slow_increment(
+            dur,
+            inputs=prev,
+            outputs=[output],
+            stdout=str(tmpd_cwd / f"incr{i}.out"),
+            stderr=str(tmpd_cwd / f"incr{i}.err"),
+        )
+        prev = f.outputs
+        futs.append((i, prev[0]))
 
-    for key in futs:
-        if key > 0:
-            fu = futs[key]
-            data = open(fu.result().filepath, 'r').read().strip()
-            assert data == str(
-                key), "[TEST] incr failed for key: {0} got: {1}".format(key, data)
-
-    cleanup_work(depth)
+    for key, f in futs:
+        data = open(f.result().filepath).read().strip()
+        assert data == str(key)
