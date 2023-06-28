be23854d8fbfd11320c9d610d38e7a867bcb26d6
Kevin Hunter Kesling <kevin@globus.org>
2023-06-28 11:56:28 -0400

    Reduce depth to work with WQ for delayed tests

--- parsl/tests/test_python_apps/test_type5.py
+++ parsl/tests/test_python_apps/test_type5.py
@@ -13,18 +13,16 @@ def add_xy(x, y):
     return x + y
 
 
-@pytest.mark.parametrize("n", (2, 3, 5))
-def test_func_linked_to_previous_result(n):
-    futs = (map_x2(i) for i in range(1, n + 1))
-    for _ in range(1, n - 1):
+def test_func_linked_to_previous_result(depth=2):
+    futs = (map_x2(i) for i in range(1, depth + 1))
+    for _ in range(1, depth - 1):
         futs = (map_x2(fut) for fut in futs)
     futs = [map_x2(fut) for fut in futs]
 
-    assert sum(f.result() for f in futs) == (2 ** n) * sum(range(1, n + 1))
+    assert sum(f.result() for f in futs) == (2 ** depth) * sum(range(1, depth + 1))
 
 
-@pytest.mark.parametrize("width", (2, 4, 8, 32))
-def test_func_linked_to_multiple_previous_results(width):
+def test_func_linked_to_multiple_previous_results(width=4):
     futs = (map_x2(i) for i in range(1, width + 1))
     f_it = iter(futs)
 
