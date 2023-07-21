0100f44b9abc32ffe1a0a233344c2b14b39638b2
Ben Clifford <benc@hawaga.org.uk>
2023-07-21 10:26:38 +0000

    Redo serpent config so that it demonstrates how serpent on its own isn't enough for the whole test suite

--- parsl/tests/test_serialization/config_serpent.py
+++ parsl/tests/test_serialization/config_serpent.py
@@ -22,15 +22,26 @@ from parsl.data_provider.http import HTTPInTaskStaging
 from parsl.data_provider.ftp import FTPInTaskStaging
 from parsl.data_provider.file_noop import NoOpFileStaging
 
-from parsl.serialize.facade import register_method_for_data  # TODO: move this into parsl.serialize root as its user exposed
+from parsl.serialize.facade import methods_for_data, register_method_for_data  # TODO: move this into parsl.serialize root as its user exposed
 
 from parsl.serialize.plugin_serpent import SerpentSerializer
 
 working_dir = os.getcwd() + "/" + "test_htex_alternate"
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def fresh_config():
+    # get rid of the default serializers so that only serpent will be used in
+    # data mode.
+    global methods_for_data
+    logger.error(f"BENC: before reset, methods_for_data = {methods_for_data}")
+    methods_for_data.clear()
+    logger.error(f"BENC: after reset, methods_for_data = {methods_for_data}")
     register_method_for_data(SerpentSerializer())
+    logger.error(f"BENC: after config, methods_for_data = {methods_for_data}")
 
     return Config(
         executors=[
