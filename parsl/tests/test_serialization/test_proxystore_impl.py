385d39adaceac917631fa8f68acb1df03d25d414
Ben Clifford <benc@hawaga.org.uk>
2023-07-21 14:25:55 +0000

    check types of serialized values

--- parsl/tests/test_serialization/test_proxystore_impl.py
+++ parsl/tests/test_serialization/test_proxystore_impl.py
@@ -8,6 +8,7 @@ def policy_example(o):
     """Example policy will proxy only lists."""
     return isinstance(o, list)
 
+
 @pytest.mark.local
 def test_proxystore_nonglobal():
     """Check that values are roundtripped, for both proxied and non-proxied types.
@@ -18,18 +19,21 @@ def test_proxystore_nonglobal():
     from proxystore.store import Store, register_store
     from proxystore.connectors.file import FileConnector
 
-    store = Store(name='parsl_store_'+str(uuid.uuid4()), connector=FileConnector(store_dir="/tmp"))
+    store = Store(name='parsl_store_' + str(uuid.uuid4()), connector=FileConnector(store_dir="/tmp"))
     register_store(store)
 
     s = ProxyStoreSerializer(store=store, should_proxy=policy_example)
 
     # check roundtrip for an int, which will not be proxystored
-    roundtripped_7 = s.deserialize(s.serialize(7))
+    s_7 = s.serialize(7)
+    assert isinstance(s_7, bytes)
+    roundtripped_7 = s.deserialize(s_7)
     assert roundtripped_7 == 7
     assert not isinstance(roundtripped_7, Proxy)
 
-
-    l = [1,2,3]
-    roundtripped_l = s.deserialize(s.serialize(l))
-    assert roundtripped_l == l
-    assert isinstance(roundtripped_l, Proxy)
+    v = [1, 2, 3]
+    s_v = s.serialize(v)
+    assert isinstance(s_7, bytes)
+    roundtripped_v = s.deserialize(s_v)
+    assert roundtripped_v == v
+    assert isinstance(roundtripped_v, Proxy)
