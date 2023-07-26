c1b5429f86f5bb3671d82258b4f816057cb26ec7
Ben Clifford <benc@hawaga.org.uk>
2023-07-26 21:20:32 +0200

    Move serialization error definitions into parsl.serialize (#2839)
    This is in expectation of at least one more exception being added
    related to serialization as part of ongoing serializer plugin
    work.

---
+++ parsl/serialize/errors.py
@@ -0,0 +1,24 @@
+from parsl.errors import ParslError
+
+
+class DeserializationError(ParslError):
+    """Failure at the deserialization of results/exceptions from remote workers.
+    """
+
+    def __init__(self, reason: str) -> None:
+        self.reason = reason
+
+    def __str__(self) -> str:
+        return f"Failed to deserialize objects. Reason: {self.reason}"
+
+
+class SerializationError(ParslError):
+    """Failure to serialize task objects.
+    """
+
+    def __init__(self, fname: str) -> None:
+        self.fname = fname
+        self.troubleshooting = "https://parsl.readthedocs.io/en/latest/faq.html#addressing-serializationerror"
+
+    def __str__(self) -> str:
+        return f"Failed to serialize objects for an invocation of function {self.fname}. Refer {self.troubleshooting}"
