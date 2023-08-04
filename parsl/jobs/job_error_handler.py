3ff7a0cb13c7133054b8d853ce5cb246eac1dc55
Ben Clifford <benc@hawaga.org.uk>
2023-08-04 09:22:20 +0000

    refactor JobErrorHandler class out of existence

--- parsl/jobs/job_error_handler.py
+++ parsl/jobs/job_error_handler.py
@@ -19,36 +19,38 @@ class JobErrorHandler:
         es.executor.handle_errors(self, es.status)
 
     def simple_error_handler(self, executor: ParslExecutor, status: Dict[str, JobStatus], threshold: int):
-        (total_jobs, failed_jobs) = self.count_jobs(status)
+        (total_jobs, failed_jobs) = _count_jobs(status)
         if total_jobs >= threshold and failed_jobs == total_jobs:
-            executor.set_bad_state_and_fail_all(self.get_error(status))
+            executor.set_bad_state_and_fail_all(_get_error(status))
 
-    def count_jobs(self, status: Dict[str, JobStatus]):
-        total = 0
-        failed = 0
-        for js in status.values():
-            total += 1
-            if js.state == JobState.FAILED:
-                failed += 1
-        return total, failed
 
-    def get_error(self, status: Dict[str, JobStatus]) -> Exception:
-        """Concatenate all errors."""
-        err = ""
-        count = 1
-        for js in status.values():
-            if js.message is not None:
-                err = err + "{}. {}\n".format(count, js.message)
-                count += 1
-            stdout = js.stdout_summary
-            if stdout:
-                err = err + "\tSTDOUT: {}\n".format(stdout)
-            stderr = js.stderr_summary
-            if stderr:
-                err = err + "\tSTDERR: {}\n".format(stderr)
+def _count_jobs(status: Dict[str, JobStatus]):
+    total = 0
+    failed = 0
+    for js in status.values():
+        total += 1
+        if js.state == JobState.FAILED:
+            failed += 1
+    return total, failed
 
-        if len(err) == 0:
-            err = "[No error message received]"
-        # wrapping things in an exception here doesn't really help in providing more information
-        # than the string itself
-        return Exception(err)
+
+def _get_error(status: Dict[str, JobStatus]) -> Exception:
+    """Concatenate all errors."""
+    err = ""
+    count = 1
+    for js in status.values():
+        if js.message is not None:
+            err = err + "{}. {}\n".format(count, js.message)
+            count += 1
+        stdout = js.stdout_summary
+        if stdout:
+            err = err + "\tSTDOUT: {}\n".format(stdout)
+        stderr = js.stderr_summary
+        if stderr:
+            err = err + "\tSTDERR: {}\n".format(stderr)
+
+    if len(err) == 0:
+        err = "[No error message received]"
+    # wrapping things in an exception here doesn't really help in providing more information
+    # than the string itself
+    return Exception(err)
