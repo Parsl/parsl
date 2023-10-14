de56ce97ed5c4c67e03b5d3e376c146f69432d4d
Ben Clifford <benc@hawaga.org.uk>
2023-10-14 09:23:34 +0000

    Merge tests into one file

--- parsl/tests/test_python_apps/test_lifted.py
+++ parsl/tests/test_python_apps/test_lifted.py
@@ -1,5 +1,6 @@
 import pytest
 
+from concurrent.futures import Future
 from parsl import python_app
 
 
@@ -63,6 +64,10 @@ def test_returns_a_tuple():
     assert returns_a_tuple()[0].result() == "X"
 
 
+def test_lifted_getitem_on_dict_bad_key():
+    assert isinstance(returns_a_dict()["invalid"].exception(), KeyError)
+
+
 @pytest.mark.skip("returning classes is not supported in WorkQueue or Task Vine - see issue #2908")
 def test_returns_a_class():
 
@@ -76,3 +81,32 @@ def test_returns_a_class():
 
     # when the result is not indexable, a sensible error should
     # appear in the appropriate future
+
+
+@python_app
+def passthrough(v):
+    return v
+
+
+def test_lifted_getitem_ordering():
+    # this should test that lifting getitem has the correct execution
+    # order: that it does not defer the execution of following code
+
+    f_prereq = Future()
+
+    f_top = passthrough(f_prereq)
+
+    f_a = f_top['a']
+
+    # lifted ['a'] should not defer execution here (so it should not
+    # implicitly call result() on f_top). If it does, this test will
+    # hang at this point, waiting for f_top to get a value, which
+    # will not happen until f_prereq gets a value..
+    # which doesn't happen until:
+
+    f_prereq.set_result({"a": "X"})
+
+    # now at this point it should be safe to wait for f_a to get a result
+    # while passthrough and lifted getitem run...
+
+    assert f_a.result() == "X"
