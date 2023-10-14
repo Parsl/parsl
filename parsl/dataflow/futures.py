09865b43a7e10082b4e50f70072203bdf8e81a11
Ben Clifford <benc@hawaga.org.uk>
2023-10-14 13:09:22 +0000

    Remove in-function import and use package level import like in __getitem__

--- parsl/dataflow/futures.py
+++ parsl/dataflow/futures.py
@@ -131,15 +131,13 @@ class AppFuture(Future):
         return deferred_getitem_app(self, key)
 
     def __getattr__(self, name: str) -> AppFuture:
-        from parsl.app.app import python_app
-
         # this will avoid lifting behaviour on private methods and attributes,
         # including __double_underscore__ methods which implement other
         # Python syntax (such as iterators in for loops)
         if name.startswith("_"):
             raise AttributeError()
 
-        deferred_getattr_app = python_app(deferred_getattr, executors=['_parsl_internal'], data_flow_kernel=self.task_def['dfk'])
+        deferred_getattr_app = app.python_app(deferred_getattr, executors=['_parsl_internal'], data_flow_kernel=self.task_def['dfk'])
 
         return deferred_getattr_app(self, name)
 
