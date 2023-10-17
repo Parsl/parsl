7f79060cc51569943f817cd1d91a1eb8a2a31cbc
Thanh Son Phung <tphung@nd.edu>
2023-10-17 15:10:18 -0400

    test fixed cctools

--- parsl/executors/taskvine/install-taskvine.sh
+++ parsl/executors/taskvine/install-taskvine.sh
@@ -7,12 +7,14 @@ if [[ -z $CCTOOLS_VERSION ]]; then
   exit 1
 fi
 
-TARBALL="cctools-$CCTOOLS_VERSION-x86_64-ubuntu20.04.tar.gz"
-[[ -f "/etc/redhat-release" ]] && TARBALL="cctools-$CCTOOLS_VERSION-x86_64-centos8.tar.gz"
+#TARBALL="cctools-$CCTOOLS_VERSION-x86_64-ubuntu20.04.tar.gz"
+TARBALL="cctools-$CCTOOLS_VERSION.parsl.wq.chdir.fix-x86_64-ubuntu20.04.tar.gz"
+[[ -f "/etc/redhat-release" ]] && TARBALL="cctools-$CCTOOLS_VERSION.parsl.wq.chdir.fix-x86_64-centos8.tar.gz"
 
 # If stderr is *not* a TTY, then disable progress bar and show HTTP response headers
 [[ ! -t 1 ]] && NO_VERBOSE="--no-verbose" SHOW_HEADERS="-S"
-wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/download/release/$CCTOOLS_VERSION/$TARBALL"
+#wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/download/release/$CCTOOLS_VERSION/$TARBALL"
+wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/tag/parsl-wq-chdir-fix/$TARBALL"
 
 mkdir -p /tmp/cctools
 tar -C /tmp/cctools -zxf /tmp/cctools.tar.gz --strip-components=1
