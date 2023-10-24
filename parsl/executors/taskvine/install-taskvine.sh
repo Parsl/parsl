1668df8c567fd3594f845bfc854bb497df04c151
Thanh Son Phung <tphung@nd.edu>
2023-10-24 10:08:44 -0400

    fix path

--- parsl/executors/taskvine/install-taskvine.sh
+++ parsl/executors/taskvine/install-taskvine.sh
@@ -14,7 +14,8 @@ TARBALL="cctools-$CCTOOLS_VERSION.parsl.wq.chdir.fix-x86_64-ubuntu20.04.tar.gz"
 # If stderr is *not* a TTY, then disable progress bar and show HTTP response headers
 [[ ! -t 1 ]] && NO_VERBOSE="--no-verbose" SHOW_HEADERS="-S"
 #wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/download/release/$CCTOOLS_VERSION/$TARBALL"
-wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/tag/parsl-wq-chdir-fix/$TARBALL"
+
+wget "$NO_VERBOSE" "$SHOW_HEADERS" -O /tmp/cctools.tar.gz "https://github.com/cooperative-computing-lab/cctools/releases/download/parsl-wq-chdir-fix/$TARBALL"
 
 mkdir -p /tmp/cctools
 tar -C /tmp/cctools -zxf /tmp/cctools.tar.gz --strip-components=1
