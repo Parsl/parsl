11a383a43a7fdc71f205886995a33f71ab05236d
Ben Clifford <benc@hawaga.org.uk>
2023-11-20 14:24:13 +0000

    Revert "More aggressive shutdown handling: should now result in a failed test suite rather than a hang"
    This reverts commit 1b4adc527b1d39b2b5866d56440ea0b6a768b02b.
    
    This commit was causing a different shutdown problem in normal htex operation, so reverting while I debug

--- parsl/executors/high_throughput/zmq_pipes.py
+++ parsl/executors/high_throughput/zmq_pipes.py
@@ -65,18 +65,11 @@ class CommandClient:
             for i in range(max_retries):
                 logger.debug(f"try {i} for command {message}")
                 try:
-                    logger.debug("waiting for command client to be ready")
-                    r = self.zmq_socket.poll(timeout=30000)   # TODO: don't hardcode this timeout
-                    if r == 0:  # timeout
-                        raise RuntimeError("CommandClient poll-before-command timed out")
-                    # TODO: what other values of r are correct?
-                    logger.debug("Sending command client command")
                     self.zmq_socket.send_pyobj(message, copy=True)
                     logger.debug(f"waiting for response from command {message}")
                     r = self.zmq_socket.poll(timeout=30000)   # TODO: don't hardcode this timeout
                     if r == 0:  # timeout
-                        raise RuntimeError("CommandClient poll-before-result timed out")
-                    # TODO: what other values of r are correct?
+                        raise RuntimeError("CommandClient poll timed out")
 
                     reply = self.zmq_socket.recv_pyobj(flags=zmq.NOBLOCK)
                     # Don't block here: we know there's a message because
@@ -147,9 +140,7 @@ class TasksOutgoing:
                 socks = dict(self.poller.poll(timeout=timeout_ms))
                 if self.zmq_socket in socks and socks[self.zmq_socket] == zmq.POLLOUT:
                     # The copy option adds latency but reduces the risk of ZMQ overflow
-                    logger.debug("Sending TasksOutgoing message")
                     self.zmq_socket.send_pyobj(message, copy=True)
-                    logger.debug("Sent TasksOutgoing message")
                     return
                 else:
                     timeout_ms = max(timeout_ms, 1)
@@ -189,12 +180,8 @@ class ResultsIncoming:
         self._lock = threading.Lock()
 
     def get(self):
-        logger.debug("Waiting for ResultsIncoming lock")
         with self._lock:
-            logger.debug("Waiting for ResultsIncoming message")
-            m = self.results_receiver.recv_multipart()
-            logger.debug("Received ResultsIncoming message")
-            return m
+            return self.results_receiver.recv_multipart()
 
     def close(self):
         with self._lock:
