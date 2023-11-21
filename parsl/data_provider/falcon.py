4e9b73354795603fff338e2e18b551eeaea5dd5c
Mousa Abughosh <mabughosh@hpclab-9.rd.unr.edu>
2023-11-20 17:20:45 -0800

    added detailed documentaion

--- parsl/data_provider/falcon.py
+++ parsl/data_provider/falcon.py
@@ -11,6 +11,58 @@ from parsl.utils import RepresentationMixin
 # Initialize the logger
 logger = logging.getLogger(__name__)
 
+"""
+To run Falcon data_provider
+- Setup your virtual environments on both source and destination server
+  and install required python packages.
+- On the destination server, you can stage data files using FalconStaging.
+  The following example shows creation of a Flacon-accessible file
+  e.g. File('falcon://192.168.10.11/parsl/data/file.txt/?8080')
+- On the source server(s), you need to run a sender which:
+        - accept a JSON object from a zmq on port 5555, this JSON object will include:
+            - host: host IP
+            - port: port number for the falcon transfer
+            - directory_path: Path to the director for the transfer
+        - run the command "falcon sender --host 'host' --port 'port' --data_dir
+          'directory_path' --method probe"
+        - sleep for 0.1 seconds (in order for the receiver instance to run before sender instance)
+        e.g. 
+            zmq_context = zmq.Context()
+            zmq_socket = zmq_context.socket(zmq.REP)
+            zmq_socket.bind("tcp://*:5555")
+            
+            # Wait for the next request from the client
+            json_data = zmq_socket.recv_string()
+            print("Received JSON data:", json_data)
+            
+            # Parse the JSON data
+            data = json.loads(json_data)
+            
+            # Extract the message and host from the data
+            host = data.get("host", "No host provided")
+            port = data.get("port", "No port provided")
+            file_path = data.get("file_path", "No file path provided")
+            
+            # Start the sender
+            sender_command = ["falcon", "sender", "--host", host, "--port", port, "--data_dir",
+                              file_path, "--method", "probe"]
+            print(sender_command)
+            time.sleep(0.1)
+            try:
+                subprocess.run(sender_command, check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Command failed with exit code {e.returncode}: {e.stderr}")
+            except FileNotFoundError:
+                print("The 'falcon' command was not found. Make sure it's installed and in your system's PATH.")
+            
+            # Send the response back to the client
+            zmq_socket.send_string("Received")
+            
+            zmq_socket.close()
+            zmq_context.term()
+            
+You can learn about Falcon and how it runs here: https://pypi.org/project/falcon-datamover/
+"""
 
 class FalconStaging(Staging, RepresentationMixin):
     """
