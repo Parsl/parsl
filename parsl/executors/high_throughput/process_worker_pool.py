Parsl/parsl

commit dfcbff8f633f33aa2aeca68ba5b13bfd66bc223a

Support using spawn instead of fork for HTEx (#2433)
Polaris, in particular, has an interaction between MPI and fork which makes HTEx worker pools fail to function when used with an MPILauncher. This PR adds the ability for HTEx to launch workers by spawning a new process rather than forking.

parsl/executors/high_throughput/process_worker_pool.py
@@ -10,6 +10,7 @@ import pickle
 import time
 import queue
 import uuid
+from threading import Thread
 from typing import Sequence, Optional
 
 import zmq
@@ -24,7 +25,8 @@ from parsl.version import VERSION as PARSL_VERSION
 from parsl.app.errors import RemoteExceptionWrapper
 from parsl.executors.high_throughput.errors import WorkerLost
 from parsl.executors.high_throughput.probe import probe_addresses
-from parsl.multiprocessing import ForkProcess as mpProcess
+from parsl.multiprocessing import ForkProcess as mpForkProcess
+from parsl.multiprocessing import SpawnProcess as mpSpawnProcess
 
 from parsl.multiprocessing import SizedQueue as mpQueue
 
@@ -64,7 +66,8 @@ class Manager(object):
                  heartbeat_period=30,
                  poll_period=10,
                  cpu_affinity=False,
-                 available_accelerators: Sequence[str] = ()):
+                 available_accelerators: Sequence[str] = (),
+                 start_method: str = 'fork'):
         """
         Parameters
         ----------
@@ -120,6 +123,11 @@ class Manager(object):
 
         available_accelerators: list of str
             List of accelerators available to the workers. Default: Empty list
+
+        start_method: str
+            What method to use to start new worker processes. Choices are fork, spawn, and thread.
+            Default: fork
+
         """
 
         logger.info("Manager started")
@@ -177,6 +185,17 @@ class Manager(object):
         self.worker_count = min(max_workers,
                                 mem_slots,
                                 math.floor(cores_on_node / cores_per_worker))
+
+        # Determine which start method to use
+        start_method = start_method.lower()
+        if start_method == "fork":
+            self.mpProcess = mpForkProcess
+        elif start_method == "spawn":
+            self.mpProcess = mpSpawnProcess
+        elif start_method == "thread":
+            self.mpProcess = Thread
+        else:
+            raise ValueError(f'HTEx does not support start method: "{start_method}"')
 
         self.pending_task_queue = mpQueue()
         self.pending_result_queue = mpQueue()
@@ -384,15 +403,15 @@ class Manager(object):
                     except KeyError:
                         logger.info("Worker {} was not busy when it died".format(worker_id))
 
-                    p = mpProcess(target=worker, args=(worker_id,
-                                                       self.uid,
-                                                       self.worker_count,
-                                                       self.pending_task_queue,
-                                                       self.pending_result_queue,
-                                                       self.ready_worker_queue,
-                                                       self._tasks_in_progress,
-                                                       self.cpu_affinity
-                                                 ), name="HTEX-Worker-{}".format(worker_id))
+                    p = self.mpProcess(target=worker, args=(worker_id,
+                                                            self.uid,
+                                                            self.worker_count,
+                                                            self.pending_task_queue,
+                                                            self.pending_result_queue,
+                                                            self.ready_worker_queue,
+                                                            self._tasks_in_progress,
+                                                            self.cpu_affinity),
+                                       name="HTEX-Worker-{}".format(worker_id))
                     self.procs[worker_id] = p
                     logger.info("Worker {} has been restarted".format(worker_id))
                 time.sleep(self.heartbeat_period)
@@ -410,16 +429,17 @@ class Manager(object):
 
         self.procs = {}
         for worker_id in range(self.worker_count):
-            p = mpProcess(target=worker, args=(worker_id,
-                                               self.uid,
-                                               self.worker_count,
-                                               self.pending_task_queue,
-                                               self.pending_result_queue,
-                                               self.ready_worker_queue,
-                                               self._tasks_in_progress,
-                                               self.cpu_affinity,
-                                               self.available_accelerators[worker_id] if self.accelerators_available else None),
-                          name="HTEX-Worker-{}".format(worker_id))
+            p = self.mpProcess(target=worker,
+                               args=(worker_id,
+                                     self.uid,
+                                     self.worker_count,
+                                     self.pending_task_queue,
+                                     self.pending_result_queue,
+                                     self.ready_worker_queue,
+                                     self._tasks_in_progress,
+                                     self.cpu_affinity,
+                                     self.available_accelerators[worker_id] if self.accelerators_available else None),
+                               name="HTEX-Worker-{}".format(worker_id))
             p.start()
             self.procs[worker_id] = p
 
@@ -654,6 +674,8 @@ if __name__ == "__main__":
                         help="Whether/how workers should control CPU affinity.")
     parser.add_argument("--available-accelerators", type=str, nargs="*",
                         help="Names of available accelerators")
+    parser.add_argument("--start-method", type=str, choices=["fork", "spawn", "thread"], default="fork",
+                        help="Method used to start new worker processes")
 
     args = parser.parse_args()
 
@@ -682,6 +704,7 @@ if __name__ == "__main__":
         logger.info("Heartbeat period: {}".format(args.hb_period))
         logger.info("CPU affinity: {}".format(args.cpu_affinity))
         logger.info("Accelerators: {}".format(" ".join(args.available_accelerators)))
+        logger.info("Start method: {}".format(args.start_method))
 
         manager = Manager(task_port=args.task_port,
                           result_port=args.result_port,

parsl/executors/high_throughput/executor.py
@@ -32,6 +32,8 @@ from parsl.providers import LocalProvider
 
 logger = logging.getLogger(__name__)
 
+_start_methods = ['fork', 'spawn', 'thread']
+
 
 class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
     """Executor designed for cluster-scale
@@ -148,13 +150,23 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
 
         default: empty list
 
+    start_method: str
+        What method to use to start new worker processes.
+        HTEx supports "spawn," "fork," and "thread" workers.
+        "Spawn" and "fork" workers are launched in separate processes using different mechanisms,
+        which are described in `Python's multiprocessing documentation.
+        <https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods>`_.
+        "Thread" workers are separate threads of the ``process_worker_pool``, which saves on memory but is
+        only recommended for workloads that involving launching other processes (e.g., ``bash_app`` s).
+        Default: fork
+
     prefetch_capacity : int
         Number of tasks that could be prefetched over available worker capacity.
         When there are a few tasks (<100) or when tasks are long running, this option should
         be set to 0 for better load balancing. Default is 0.
 
     address_probe_timeout : int | None
-        Managers attempt connecting over many different addesses to determine a viable address.
+        Managers attempt connecting over many different addresses to determine a viable address.
         This option sets a time limit in seconds on the connection attempt.
         Default of None implies 30s timeout set on worker.
 
@@ -191,6 +203,7 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
                  max_workers: Union[int, float] = float('inf'),
                  cpu_affinity: str = 'none',
                  available_accelerators: Union[int, Sequence[str]] = (),
+                 start_method: str = 'fork',
                  prefetch_capacity: int = 0,
                  heartbeat_threshold: int = 120,
                  heartbeat_period: int = 30,
@@ -215,6 +228,7 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
         self.prefetch_capacity = prefetch_capacity
         self.address = address
         self.address_probe_timeout = address_probe_timeout
+        self.start_method = start_method
         if self.address:
             self.all_addresses = address
         else:
@@ -236,6 +250,14 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
             # If the user provide an integer, create some names for them
             available_accelerators = list(map(str, range(available_accelerators)))
         self.available_accelerators = list(available_accelerators)
+
+        # Raise errors for incompatible settings
+        if start_method not in _start_methods:
+            raise ValueError(f'Start method "{start_method}" not recognized. Expected one of: {", ".join(_start_methods)}')
+        if start_method == "thread" and cpu_affinity != "none":
+            raise ValueError('Thread affinity is not available with start method: "thread"')
+        if start_method == "thread" and len(available_accelerators) > 0:
+            raise ValueError('Accelerator pinning not available with start method: "thread"')
 
         # Determine the number of workers per node
         self._workers_per_node = min(max_workers, mem_slots, cpu_slots)
@@ -273,7 +295,8 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
                                "{address_probe_timeout_string} "
                                "--hb_threshold={heartbeat_threshold} "
                                "--cpu-affinity {cpu_affinity} "
-                               "--available-accelerators {accelerators}")
+                               "--available-accelerators {accelerators} "
+                               "--start-method {start_method}")
 
     radio_mode = "htex"
 
@@ -308,7 +331,8 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
                                        poll_period=self.poll_period,
                                        logdir=worker_logdir,
                                        cpu_affinity=self.cpu_affinity,
-                                       accelerators=" ".join(self.available_accelerators))
+                                       accelerators=" ".join(self.available_accelerators),
+                                       start_method=self.start_method)
         self.launch_cmd = l_cmd
         logger.debug("Launch command: {}".format(self.launch_cmd))
 
@@ -538,7 +562,7 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
                 self.hold_worker(manager['manager'])
 
     def submit(self, func, resource_specification, *args, **kwargs):
-        """Submits work to the the outgoing_q.
+        """Submits work to the outgoing_q.
 
         The outgoing_q is an external process listens on this
         queue for new work. This method behaves like a

parsl/launchers/launchers.py
@@ -203,8 +203,21 @@ class MpiExecLauncher(Launcher):
     - mpiexec is installed and can be located in $PATH
     - The provider makes available the $PBS_NODEFILE environment variable
     """
-    def __init__(self, debug: bool = True):
+    def __init__(self, debug: bool = True, bind_cmd: str = '--bind-to', overrides: str = ''):
+        """
+
+        Parameters
+        ----------
+        bind_cmd: str
+            Name of the argument for binding ranks to CPUs
+
+        overrides: str
+            Additional arguments to add to the invocation
+
+        """
         super().__init__(debug=debug)
+        self.bind_cmd = bind_cmd
+        self.overrides = overrides
 
     def __call__(self, command: str, tasks_per_node: int, nodes_per_block: int) -> str:
         """
@@ -234,12 +247,14 @@ cat << MPIEXEC_EOF > cmd_$JOBNAME.sh
 MPIEXEC_EOF
 chmod u+x cmd_$JOBNAME.sh
 
-mpiexec --bind-to none -n $WORKERCOUNT --hostfile $HOSTFILE /usr/bin/sh cmd_$JOBNAME.sh
+mpiexec {bind_cmd} none {overrides} -n $WORKERCOUNT --hostfile $HOSTFILE /usr/bin/sh cmd_$JOBNAME.sh
 
 [[ "{debug}" == "1" ]] && echo "All workers done"
 '''.format(command=command,
            task_blocks=task_blocks,
-           debug=debug_num)
+           debug=debug_num,
+           overrides=self.overrides,
+           bind_cmd=self.bind_cmd)
         return x
 
 

parsl/multiprocessing.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 # maybe ForkProcess should be: Callable[..., Process] so as to make
 # it clear that it returns a Process always to the type checker?
 ForkProcess: Type = multiprocessing.get_context('fork').Process
+SpawnProcess: Type = multiprocessing.get_context('spawn').Process
 
 
 class MacSafeQueue(multiprocessing.queues.Queue):

parsl/tests/sites/test_start_method.py
@@ -1,0 +1,67 @@
+import os
+from contextlib import contextmanager
+
+from pytest import mark, raises
+
+import parsl
+from parsl.providers import LocalProvider
+from parsl.channels import LocalChannel
+from parsl.config import Config
+from parsl.executors import HighThroughputExecutor
+from parsl import python_app
+
+
+@python_app()
+def test_function():
+    return os.getpid()
+
+
+@contextmanager
+def config(start_method: str, **kwargs):
+    config = Config(
+        executors=[
+            HighThroughputExecutor(
+                label="fork",
+                worker_debug=True,
+                max_workers=2,
+                provider=LocalProvider(
+                    channel=LocalChannel(),
+                    init_blocks=1,
+                    max_blocks=1,
+                ),
+                start_method="spawn"
+            )
+        ],
+        strategy=None,
+    )
+    dfk = parsl.load(config)
+    try:
+        yield dfk
+    finally:
+        dfk.cleanup()
+        parsl.clear()
+
+
+@mark.local
+@mark.parametrize("start_method", ["spawn", "fork", "thread"])
+def test_spawn_method(start_method: str):
+    with config(start_method):
+        # Get the PID for the child function as a way of making sure it launches
+        future = test_function()
+        remote_pid = future.result()
+        assert remote_pid != os.getpid()
+
+
+@mark.local
+def test_htex_config_failures():
+    with raises(ValueError) as exc:
+        HighThroughputExecutor(start_method='thread', available_accelerators=1)
+    assert 'Accelerator' in str(exc)
+
+    with raises(ValueError) as exc:
+        HighThroughputExecutor(start_method='thread', cpu_affinity='block')
+    assert 'Thread affinity' in str(exc)
+
+    with raises(ValueError) as exc:
+        HighThroughputExecutor(start_method='not real', cpu_affinity='block')
+    assert 'Start method "not real" not recognized' in str(exc)

commit 15290a9f4b4395c40e1eb6264533b43aa33e842b

Remove defaults for Manager init, forcing keywords to be specified, and perhaps revealign a bug that start_method is unused (#2973)
This PR removes theoretically-always-unused argument defaults in the process worker pool manager, and forces arguments to be supplied as kwargs.

These defaults have two downsides:

i) users reading the code might believe the defaults will be used as end-Parsl-user defaults. However, generally there will be up to two other layers of defaults that will override the defaults removed by this PR: defaults specified in the argparser of process_worker_pool; and defaults specified in the constructor to the HighThroughputExecutor object.

These defaults, then, are misleading, as they are defaults only in the presence of a coding error such as ii) below.

ii) in the presence of a coding error adding a new parameter, where the new parameter is not wired all the way through, rather than raise an error about a missing parameter, the otherwise-unused default is used. This was a practical problem demonstrated by the threads/spawn/fork start method parameter introduced in #2433 and removed as broken in #2980.

Switching to mandatory kwargs can help with accidentally misarranged parameters in the construction of a Manager object, a similar but different coding error to ii) above.

parsl/executors/high_throughput/process_worker_pool.py
@@ -48,22 +48,22 @@ class Manager:
                 |                          |                IPC-Qeueues
 
     """
-    def __init__(self,
-                 addresses="127.0.0.1",
-                 address_probe_timeout=30,
-                 task_port="50097",
-                 result_port="50098",
-                 cores_per_worker=1,
-                 mem_per_worker=None,
-                 max_workers=float('inf'),
-                 prefetch_capacity=0,
-                 uid=None,
-                 block_id=None,
-                 heartbeat_threshold=120,
-                 heartbeat_period=30,
-                 poll_period=10,
-                 cpu_affinity=False,
-                 available_accelerators: Sequence[str] = ()):
+    def __init__(self, *,
+                 addresses,
+                 address_probe_timeout,
+                 task_port,
+                 result_port,
+                 cores_per_worker,
+                 mem_per_worker,
+                 max_workers,
+                 prefetch_capacity,
+                 uid,
+                 block_id,
+                 heartbeat_threshold,
+                 heartbeat_period,
+                 poll_period,
+                 cpu_affinity,
+                 available_accelerators: Sequence[str]):
         """
         Parameters
         ----------
@@ -72,7 +72,7 @@ class Manager:
 
         address_probe_timeout : int
              Timeout in seconds for the address probe to detect viable addresses
-             to the interchange. Default : 30s
+             to the interchange.
 
         uid : str
              string unique identifier
@@ -82,43 +82,41 @@ class Manager:
 
         cores_per_worker : float
              cores to be assigned to each worker. Oversubscription is possible
-             by setting cores_per_worker < 1.0. Default=1
+             by setting cores_per_worker < 1.0.
 
         mem_per_worker : float
              GB of memory required per worker. If this option is specified, the node manager
              will check the available memory at startup and limit the number of workers such that
              the there's sufficient memory for each worker. If set to None, memory on node is not
              considered in the determination of workers to be launched on node by the manager.
-             Default: None
 
         max_workers : int
              caps the maximum number of workers that can be launched.
-             default: infinity
 
         prefetch_capacity : int
              Number of tasks that could be prefetched over available worker capacity.
              When there are a few tasks (<100) or when tasks are long running, this option should
-             be set to 0 for better load balancing. Default is 0.
+             be set to 0 for better load balancing.
 
         heartbeat_threshold : int
              Seconds since the last message from the interchange after which the
-             interchange is assumed to be un-available, and the manager initiates shutdown. Default:120s
+             interchange is assumed to be un-available, and the manager initiates shutdown.
 
              Number of seconds since the last message from the interchange after which the worker
-             assumes that the interchange is lost and the manager shuts down. Default:120
+             assumes that the interchange is lost and the manager shuts down.
 
         heartbeat_period : int
              Number of seconds after which a heartbeat message is sent to the interchange, and workers
              are checked for liveness.
 
         poll_period : int
-             Timeout period used by the manager in milliseconds. Default: 10ms
+             Timeout period used by the manager in milliseconds.
 
         cpu_affinity : str
              Whether or how each worker should force its affinity to different CPUs
 
         available_accelerators: list of str
-            List of accelerators available to the workers. Default: Empty list
+            List of accelerators available to the workers.
 
         """
 