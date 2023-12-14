Parsl/parsl

commit 24ae9507232b7f6024677d7612b09fecd0a143dd

Add slurmprovider options (#2986)
Adds optional parameters to SlurmProvider for choosing the queue and constraint slurm options. This makes the SlurmProvider easier to work on NERSC systems which uses these options for job submissions. Queue is used to choose the jobs priority (regular, debug, realtime, etc.) and constraint to choose between cpu and gpu nodes on Perlmutter.

parsl/providers/slurm/slurm.py
@@ -47,6 +47,10 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
     account : str
         Slurm account to which to charge resources used by the job. If unspecified or ``None``, the job will use the
         user's default account.
+    qos : str
+        Slurm queue to place job in. If unspecified or ``None``, no queue slurm directive will be specified.
+    constraint : str
+        Slurm job constraint, often used to choose cpu or gpu type. If unspecified or ``None``, no constraint slurm directive will be added.
     channel : Channel
         Channel for accessing this provider. Possible channels include
         :class:`~parsl.channels.LocalChannel` (the default),
@@ -92,6 +96,8 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
     def __init__(self,
                  partition: Optional[str] = None,
                  account: Optional[str] = None,
+                 qos: Optional[str] = None,
+                 constraint: Optional[str] = None,
                  channel: Channel = LocalChannel(),
                  nodes_per_block: int = 1,
                  cores_per_node: Optional[int] = None,
@@ -133,6 +139,11 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
             self.scheduler_options += "#SBATCH --partition={}\n".format(partition)
         if account:
             self.scheduler_options += "#SBATCH --account={}\n".format(account)
+        if qos:
+            self.scheduler_options += "#SBATCH --qos={}\n".format(qos)
+        if constraint:
+            self.scheduler_options += "#SBATCH --constraint={}\n".format(constraint)
+
         self.regex_job_id = regex_job_id
         self.worker_init = worker_init + '\n'
 

commit 4838c73f7c352174bc0b62b17f6f57c59a3add2a

Fix missing qos and constraint attributes that broke SlurmProvider in #2986 (#2997)
Fixes #2994

parsl/providers/slurm/slurm.py
@@ -132,6 +132,8 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
         self.exclusive = exclusive
         self.move_files = move_files
         self.account = account
+        self.qos = qos
+        self.constraint = constraint
         self.scheduler_options = scheduler_options + '\n'
         if exclusive:
             self.scheduler_options += "#SBATCH --exclusive\n"