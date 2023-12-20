730540c4cf79ee1d088384868fd5d4f6f95d3f74
Yadu Babuji <yadudoc1729@gmail.com>
2023-12-20 15:25:27 -0600

    Adding NoopScheduler and MPITaskScheduler

--- parsl/executors/high_throughput/mpi_resource_management.py
+++ parsl/executors/high_throughput/mpi_resource_management.py
@@ -1,10 +1,16 @@
+import logging
+import multiprocessing
+import os
+import pickle
+import queue
 import subprocess
 from enum import Enum
-import os
-import multiprocessing
-from typing import List
-import logging
+from typing import Dict, List
+
 from parsl.multiprocessing import SpawnContext
+from parsl.serialize import (pack_res_spec_apply_message,
+                             unpack_res_spec_apply_message)
+
 logger = logging.getLogger(__name__)
 
 
@@ -60,6 +66,7 @@ def identify_scheduler() -> Scheduler:
 
 class MPIResourceUnavailable(Exception):
     """Raised if there are no free resources available for an MPI request"""
+
     def __init__(self, requested: int, available: int):
         self.requested = requested
         self.available = available
@@ -68,40 +75,114 @@ class MPIResourceUnavailable(Exception):
         return f"MPIResourceUnavailable(requested={self.requested} available={self.available}"
 
 
-class MPIResourceTracker:
+class NoopScheduler:
     def __init__(
         self,
-        nodes_q: multiprocessing.Queue,
-        mpi_launcher: str,
+        pending_task_q: multiprocessing.Queue,
+        pending_result_q: multiprocessing.Queue,
     ):
-        self.nodes_q = nodes_q
-        self.mpi_launcher = mpi_launcher
+        self.pending_task_q = pending_task_q
+        self.pending_result_q = pending_result_q
+
+    def put_task(self, task) -> None:
+        return self.pending_task_q.put(task)
+
+    def get_result(self, block: bool, timeout: float):
+        return self.pending_result_q.get(block, timeout=timeout)
+
+    def put_result(self, result) -> None:
+        return self.pending_result_q.put(result)
+
+
+class MPITaskScheduler(NoopScheduler):
+    def __init__(
+        self,
+        pending_task_q: multiprocessing.Queue,
+        pending_result_q: multiprocessing.Queue,
+    ):
+        super().__init__(pending_task_q, pending_result_q)
         self.scheduler = identify_scheduler()
+        # PriorityQueue is threadsafe
+        self._backlog_queue: multiprocessing.Queue = queue.PriorityQueue()
+        self._map_tasks_to_nodes: Dict[str, List[str]] = {}
         self.available_nodes = get_nodes_in_batchjob(self.scheduler)
-        self._free_node_counter = SpawnContext.Value("i", len(self.available_nodes))
-
-        logger.debug(f"scheduler:{self.scheduler}, nodes:{self.available_nodes}")
-
+        self._free_node_counter: multiprocessing.Value = SpawnContext.Value("i", len(self.available_nodes))
+        self.nodes_q = SpawnContext.Queue()
         for node in self.available_nodes:
             self.nodes_q.put(node)
 
-    def get_nodes(self, num_nodes: int) -> List[str]:
-        logger.debug(f"requesting : {num_nodes=} we have {self._free_node_counter}")
+        logger.info(
+            f"Starting MPITaskScheduler with {len(self.available_nodes)}"
+        )
+
+    def _get_nodes(self, num_nodes: int):
+        logger.debug(
+            f"Requesting : {num_nodes=} we have {self._free_node_counter}"
+        )
         acquired_nodes = []
         with self._free_node_counter.get_lock():
             if num_nodes <= self._free_node_counter.value:
                 self._free_node_counter.value -= num_nodes
             else:
-                raise MPIResourceUnavailable(requested=num_nodes, available=self._free_node_counter.value)
+                raise MPIResourceUnavailable(
+                    requested=num_nodes, available=self._free_node_counter.value
+                )
 
             for i in range(num_nodes):
                 node = self.nodes_q.get()
                 acquired_nodes.append(node)
-
         return acquired_nodes
 
-    def return_nodes(self, nodes: List[str]) -> None:
+    def _return_nodes(self, nodes: List[str]) -> None:
         for node in nodes:
             self.nodes_q.put(node)
         with self._free_node_counter.get_lock():
             self._free_node_counter.value += len(nodes)
+
+    def put_task(self, task_package: dict):
+        user_ns = locals()
+        user_ns.update({"__builtins__": __builtins__})
+        _f, _args, _kwargs, resource_spec = unpack_res_spec_apply_message(
+            task_package["buffer"], user_ns, copy=False
+        )
+
+        nodes_needed = resource_spec.get("NUM_NODES")
+        if nodes_needed:
+            try:
+                allocated_nodes = self._get_nodes(nodes_needed)
+            except MPIResourceUnavailable:
+                logger.warning("Not enough resources, placing task into backlog")
+                self._backlog_queue.put((nodes_needed, task_package))
+                return
+            else:
+                resource_spec["MPI_NODELIST"] = ",".join(allocated_nodes)
+                self._map_tasks_to_nodes[task_package["task_id"]] = allocated_nodes
+                buffer = pack_res_spec_apply_message(_f, _args, _kwargs, resource_spec)
+                task_package["buffer"] = buffer
+
+        self.pending_task_q.put(task_package)
+
+    def _schedule_backlog_tasks(self):
+        try:
+            _nodes_requested, task_package = self._backlog_queue.get(block=False)
+            self.put_task(task_package)
+        except queue.Empty:
+            return
+        except MPIResourceUnavailable:
+            logger.debug("MPIResourceUnavailable: Popping task back onto backlog_queue")
+            self._backlog_queue.put((_nodes_requested, task_package))
+            return
+        else:
+            # Keep attempting to schedule tasks till we are out of resources
+            self._schedule_backlog_tasks()
+
+    def get_result(self, block: bool, timeout: float):
+        result_pkl = self.pending_result_q.get(block, timeout=timeout)
+        result_dict = pickle.loads(result_pkl)
+        if result_dict["type"] == "result":
+            task_id = result_dict["task_id"]
+            nodes_to_reallocate = self._map_tasks_to_nodes[task_id]
+            self._return_nodes(nodes_to_reallocate)
+            self._schedule_backlog_tasks()
+
+        return result_pkl
