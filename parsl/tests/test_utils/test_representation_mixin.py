a71b812b4543c76a8d140c7b9c4e1b5af512e08c
Ben Clifford <benc@hawaga.org.uk>
2024-01-12 15:15:19 +0000

    Test non-validation mode of RepresentationMixin without mutating global state

--- parsl/tests/test_utils/test_representation_mixin.py
+++ parsl/tests/test_utils/test_representation_mixin.py
@@ -41,20 +41,28 @@ def test_repr_bad():
         repr(BadRepr(p1, p2))
 
 
+class NonValidatingRepresentationMixin(RepresentationMixin):
+    """This will override the process level RepresentationMixin which can
+    be set to validating mode by pytest fixtures"""
+    _validate_repr = False
+
+
+class BadReprNonValidating(NonValidatingRepresentationMixin):
+    """This class incorrectly subclasses RepresentationMixin.
+    It does not store the parameter x on self.
+    """
+    def __init__(self, x, y):
+        self.y = y
+
+
 @pytest.mark.local
 def test_repr_bad_unvalidated():
-    old_v = RepresentationMixin._validate_repr
-    RepresentationMixin._validate_repr = False
-
     p1 = "parameter 1"
     p2 = "the second parameter"
 
-    try:
-        # repr should not raise an exception
-        r = repr(BadRepr(p1, p2))
-        # parameter 2 should be found in the representation, but not
-        # parameter 1
-        assert p1 not in r
-        assert p2 in r
-    finally:
-        RepresentationMixin._validate_repr = old_v
+    # repr should not raise an exception
+    r = repr(BadReprNonValidating(p1, p2))
+    # parameter 2 should be found in the representation, but not
+    # parameter 1
+    assert p1 not in r
+    assert p2 in r
