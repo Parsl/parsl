d924ee04d98327d11f85ad43de89fe2e03f8a8d0
Ben Clifford <benc@hawaga.org.uk>
2024-02-12 15:59:19 +0000

    Remove loggers; make arbitrary token more arbitrary

--- parsl/tests/test_monitoring/test_incomplete_futures.py
+++ parsl/tests/test_monitoring/test_incomplete_futures.py
@@ -2,10 +2,9 @@ import logging
 import os
 import parsl
 import pytest
+import random
 
 from concurrent.futures import Future
-
-logger = logging.getLogger(__name__)
 
 
 @parsl.python_app
@@ -20,23 +19,19 @@ def test_future_representation():
     from parsl.tests.configs.htex_local_alternate import fresh_config
 
     if os.path.exists("runinfo/monitoring.db"):
-        logger.info("Monitoring database already exists - deleting")
         os.remove("runinfo/monitoring.db")
 
-    logger.info("loading parsl")
     parsl.load(fresh_config())
 
-    # this is arbitrary
-    TOKEN = 54739
+    # we're going to pass this TOKEN into an app via a pre-requisite Future,
+    # and then expect to see it appear in the monitoring database.
+    TOKEN = random.randint()
 
     # make a Future that has no result yet
     # invoke a task that depends on it
     # inspect and insert something about the monitoring recorded value of that Future
     # make the Future complete
     # inspect and insert something about the monitoring recorded value of that Future
-
-    # potential race condition:
-    # monitoring will take some time to update the record after submitting the task or completing the Future
 
     f1 = Future()
 
@@ -64,5 +59,3 @@ def test_future_representation():
         result = connection.execute(text("SELECT task_inputs FROM task"))
         (task_inputs, ) = result.first()
         assert task_inputs == "[" + repr(TOKEN) + "]"
-
-    logger.info("all done")
