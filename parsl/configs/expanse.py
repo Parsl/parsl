930bb8c0a4ab996051b26be72b3fffd7eebc33f8
Yadu Babuji <yadudoc1729@gmail.com>
2024-02-21 10:49:34 -0600

    Adding Expanse@SDSC to configuration section
    * Comet is now replaced by Expanse

---
+++ parsl/configs/expanse.py
@@ -0,0 +1,29 @@
+from parsl.config import Config
+from parsl.launchers import SrunLauncher
+from parsl.providers import SlurmProvider
+from parsl.executors import HighThroughputExecutor
+
+
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label='Expanse_CPU_Multinode',
+            max_workers=32,
+            provider=SlurmProvider(
+                'compute',
+                account='YOUR_ALLOCATION_ON_EXPANSE',
+                launcher=SrunLauncher(),
+                # string to prepend to #SBATCH blocks in the submit
+                # script to the scheduler
+                scheduler_options='',
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init='',
+                walltime='01:00:00',
+                init_blocks=1,
+                max_blocks=1,
+                nodes_per_block=2,
+            ),
+        )
+    ]
+)
