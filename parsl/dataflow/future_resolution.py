e382e3998d36f73f1dbccaf948a3ab1b800499f8
Andrew S. Rosen <asrosen93@gmail.com>
2024-03-04 01:25:26 -0800

    Adding onto #3111: shallow resolution of lists, sets, and tuples (#3118)

--- parsl/dataflow/future_resolution.py
+++ parsl/dataflow/future_resolution.py
@@ -26,23 +26,32 @@ def _(fut: Future):
 
 
 # The above is the traditional Parsl future/non-future behaviour.
-# Below is an example of shallow traversal of a tuple.
+# Below is an example of shallow traversal of iterables.
 
-@traverse_to_gather.register
-def _(tup: tuple):
+
+@traverse_to_gather.register(tuple)
+@traverse_to_gather.register(list)
+@traverse_to_gather.register(set)
+def _(iterable):
     # a "deep" traversal would instead recursively call traverse_to_gather
-    # here to inspect whatever is inside the tuple
-    return [v for v in tup if isinstance(v, Future)]
+    # here to inspect whatever is inside the sequence
+
+    return [v for v in iterable if isinstance(v, Future)]
 
 
-@traverse_to_unwrap.register
+@traverse_to_unwrap.register(tuple)
+@traverse_to_unwrap.register(list)
+@traverse_to_unwrap.register(set)
 @singledispatch
-def _(tup: tuple):
+def _(iterable):
     def unwrap(v):
         if isinstance(v, Future):
-            assert v.done(), "sequencing error: v should be done by now, otherwise weird hangs in DFK"
+            assert (
+                v.done()
+            ), "sequencing error: v should be done by now, otherwise weird hangs in DFK"
             return v.result()
         else:
             return v
 
-    return tuple(map(unwrap, tup))
+    type_ = type(iterable)
+    return type_(map(unwrap, iterable))
