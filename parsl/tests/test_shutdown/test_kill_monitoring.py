11131f4b4d5dfb0a2a36864a99496bdf6f75488d
Ben Clifford <benc@hawaga.org.uk>
2024-03-04 12:22:05 +0000

    tidyup/fix flake8

--- parsl/tests/test_shutdown/test_kill_monitoring.py
+++ parsl/tests/test_shutdown/test_kill_monitoring.py
@@ -4,6 +4,12 @@ import pytest
 import signal
 import time
 
+from parsl.tests.configs.htex_local_alternate import fresh_config
+
+# This is a very generous upper bound on expected shutdown time of target
+# process after receiving a signal, measured in seconds.
+PERMITTED_SHUTDOWN_TIME_S = 60
+
 
 @parsl.python_app
 def simple_app():
@@ -12,7 +18,6 @@ def simple_app():
 
 @pytest.mark.local
 def test_no_kills():
-    from parsl.tests.configs.htex_local_alternate import fresh_config
     """This tests that we can create a monitoring-enabled DFK and shut it down."""
 
     parsl.load(fresh_config())
@@ -27,15 +32,11 @@ def test_no_kills():
 @pytest.mark.parametrize("sig", [signal.SIGINT, signal.SIGTERM, signal.SIGKILL])  # are we expecting SIGKILL resilience here? Ideally yes
 @pytest.mark.parametrize("process_attr", ["router_proc", "dbm_proc"])
 def test_kill_monitoring_helper_process(sig, process_attr, try_assert):
-    from parsl.tests.configs.htex_local_alternate import fresh_config
     """This tests that we can kill a monitoring process and still have successful shutdown.
-    This emulates behaviour when ctrl-C is pressed: that all of the processes receive a
-    termination signal  - SIGINT for ctrl-C - at once, and so specifically we should be
-    tolerant to monitoring processes going away.
+    SIGINT emulates some racy behaviour when ctrl-C is pressed: that
+    monitoring processes receive a ctrl-C too, and so the other processes
+    need to be tolerant to monitoring processes arbitrarily exiting.
     """
-
-    # This is a very generous upper bound on process shutdown times.
-    expected_target_shutdown_time = 60
 
     parsl.load(fresh_config())
 
@@ -53,9 +54,7 @@ def test_kill_monitoring_helper_process(sig, process_attr, try_assert):
 
     os.kill(target_pid, sig)
 
-    start_time = time.time()
-
-    try_assert(lambda: not target_proc.is_alive(), timeout_ms = expected_target_shutdown_time * 1000)
+    try_assert(lambda: not target_proc.is_alive(), timeout_ms=PERMITTED_SHUTDOWN_TIME_S * 1000)
 
     # now we have broken one piece of the monitoring system, do some app
     # execution and then shut down.
