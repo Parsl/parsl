d0690e69c58fca6b903d112bd534207f8d3252f4
Christelle Mbemba <christelleilunga39@gmail.com>
2024-03-06 12:44:08 +0000

    Updated simpleLauncher Function

--- parsl/tests/sites/test_launchers.py
+++ parsl/tests/sites/test_launchers.py
@@ -9,3 +9,19 @@ def test_wrapped_launcher(caplog):
     w('test', 2, 2)
     assert 'tasks per node' in caplog.text
     assert 'nodes per block' in caplog.text
+    
+def test_simple_launcher_one_node_per_block():
+    # Test that SimpleLauncher works with one node per block specified
+    launcher = SimpleLauncher()
+    assert launcher("", 1, 1) == ""
+
+def test_simple_launcher_exception():
+    # Test that SimpleLauncher raises an exception with some other number of nodes per block
+    launcher = SimpleLauncher()
+    with pytest.raises(ValueError):
+        launcher("", 1, 64)
+
+def test_advanced_users_permit_multiple_nodes():
+    # Test that advanced users can disable the exception when using multiple nodes per block
+    launcher = SimpleLauncher(permit_multiple_nodes=True)
+    assert launcher("", 1, 64) == ""
