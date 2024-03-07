197182f9670ac90fb5d39da2fe8241d0c81a463f
ClaudiaCumberbatch <zhousc@uchicago.edu>
2024-03-07 04:22:56 +0000

    add explanations in diaspora_login.py

--- parsl/tests/test_radio/diaspora_login.py
+++ parsl/tests/test_radio/diaspora_login.py
@@ -1,3 +1,8 @@
+'''
+Before using diaspora radio, the user should first login to the diaspora event service.
+This can not be aggregated into test file, because it needs an authentication token requiring cli
+which pytest does not support.
+'''
 from diaspora_event_sdk import Client as GlobusClient
 c = GlobusClient()
 print(c.retrieve_key())
