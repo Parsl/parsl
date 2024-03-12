0b8676f46f2ebfa3ab9ed83c8cd90c50096a934b
mercybassey <udohmercy911@gmail.com>
2024-03-12 09:34:23 +0100

    Added flux test config

---
+++ parsl/tests/configs/flux_local.py
@@ -0,0 +1,13 @@
+from parsl.config import Config
+from parsl.executors import FluxExecutor
+
+
+def fresh_config():
+    return Config(
+        executors=[FluxExecutor()],
+    )
+
+
+
+config = fresh_config()
+
