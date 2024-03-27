a37b46a9315aa3726d4ab4978025e7fe162641ca
Ben Clifford <benc@hawaga.org.uk>
2024-03-27 12:49:20 +0000

    flake8

--- parsl/tests/test_tc_logan.py
+++ parsl/tests/test_tc_logan.py
@@ -4,7 +4,7 @@ import pytest
 from parsl.dataflow.taskrecord import TaskRecord
 
 
-def l(t: TaskRecord):
+def some_user_supplied_mutator(t: TaskRecord):
     t['kwargs']['s'] = f"hi, magic value for app {t['func_name']} on executor {t['executor']}"
 
 
@@ -15,7 +15,7 @@ def write(s: str, stdout=parsl.AUTO_LOGNAME):
 
 @pytest.mark.local
 def test_tc_logan():
-    parsl.load(parsl.Config(tc_logan=l))
+    parsl.load(parsl.Config(tc_logan=some_user_supplied_mutator))
     f = write()
     f.result()
     parsl.dfk().cleanup()
