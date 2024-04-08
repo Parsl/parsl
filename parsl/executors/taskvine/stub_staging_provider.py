62e99fcaf406c67ec235d1cc7cba52da0fda9227
Ben Clifford <benc@hawaga.org.uk>
2024-04-08 10:47:27 +0000

    Don't stage http(s) urls for taskvine - instead pass through to taskvine executor for it to figure that stuff out itself (which right now fails test)

---
+++ parsl/executors/taskvine/stub_staging_provider.py
@@ -0,0 +1,19 @@
+import logging
+
+from parsl.utils import RepresentationMixin
+from parsl.data_provider.staging import Staging
+
+
+logger = logging.getLogger(__name__)
+
+known_url_schemes = ["file", "http", "https"]
+
+class StubStaging(Staging, RepresentationMixin):
+
+    def can_stage_in(self, file):
+        logger.debug("Task vine staging provider checking passthrough for {}".format(repr(file)))
+        return file.scheme in known_url_schemes
+
+    def can_stage_out(self, file):
+        logger.debug("Task vine staging provider checking passthrough for {}".format(repr(file)))
+        return file.scheme in known_url_schemes
--- parsl/tests/configs/taskvine_ex.py
+++ parsl/tests/configs/taskvine_ex.py
@@ -2,12 +2,11 @@ from parsl.config import Config
 from parsl.executors.taskvine import TaskVineExecutor
 from parsl.executors.taskvine import TaskVineManagerConfig
 
-from parsl.data_provider.http import HTTPInTaskStaging
 from parsl.data_provider.ftp import FTPInTaskStaging
-from parsl.data_provider.file_noop import NoOpFileStaging
+from parsl.executors.taskvine.stub_staging_provider import StubStaging
 
 
 def fresh_config():
     return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(port=9000),
                                               worker_launch_method='factory',
-                                              storage_access=[FTPInTaskStaging(), HTTPInTaskStaging(), NoOpFileStaging()])])
+                                              storage_access=[FTPInTaskStaging(), StubStaging()])])
