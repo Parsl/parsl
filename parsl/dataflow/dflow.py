1140cffcdc324c0f549b846fb9a7b5b86e7bd94f
Ben Clifford <benc@hawaga.org.uk>
2024-04-11 11:42:40 -0500

    Rework monitoring recording of invalid stdout/err streams (#3344)
    This PR stops treating None as an invalid stdout/err: it is a legitimate
    value for stdout/err. Prior to this PR, using None was resulting in
    the stdout/err being recorded in monitoring as a stringified exception,
    like this:
    
    ```
    sqlite> select task_stderr from task;
    type of argument "stdfspec" must be one of (os.PathLike, str, Tuple[str, str], Tuple[os.PathLike, str]); got NoneType instead
    ```
    
    Before this PR, an invalid stdout/err stream was logged at WARNING level
    and the filename as recorded in the monitoring database was set to the
    exception message.
    
    This PR elevates that log to an ERROR, because this is an error,
    including the exception message to give a debugging hint.
    
    In this error case, a blank stdout or stderr will be recorded in the
    monitoring database. Generally the task will fail later due to other
    parts of the code being unable to interpret the specification.
    
    This PR adds a few checks around stdout/err tests that should not have
    any error raised - this test checks for ERROR rather than WARNING because
    Parsl will legitimately output WARNING messages in some configurations
    (such as when checkpointing is enabled but Parsl notices it hasn't
    checkpointed anything)
    
    Co-authored-by: Kevin Hunter Kesling <kevin@globus.org>

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -240,16 +240,29 @@ class DataFlowKernel:
         task_log_info['task_stdin'] = task_record['kwargs'].get('stdin', None)
         stdout_spec = task_record['kwargs'].get('stdout', None)
         stderr_spec = task_record['kwargs'].get('stderr', None)
-        try:
-            stdout_name, _ = get_std_fname_mode('stdout', stdout_spec)
-        except Exception as e:
-            logger.warning("Incorrect stdout format {} for Task {}".format(stdout_spec, task_record['id']))
-            stdout_name = str(e)
-        try:
-            stderr_name, _ = get_std_fname_mode('stderr', stderr_spec)
-        except Exception as e:
-            logger.warning("Incorrect stderr format {} for Task {}".format(stderr_spec, task_record['id']))
-            stderr_name = str(e)
+
+        # stdout and stderr strings are set to the filename if we can
+        # interpret the specification; otherwise, set to the empty string
+        # (on exception, or when not specified)
+
+        if stdout_spec is not None:
+            try:
+                stdout_name, _ = get_std_fname_mode('stdout', stdout_spec)
+            except Exception:
+                logger.exception("Could not parse stdout specification {} for task {}".format(stdout_spec, task_record['id']))
+                stdout_name = ""
+        else:
+            stdout_name = ""
+
+        if stderr_spec is not None:
+            try:
+                stderr_name, _ = get_std_fname_mode('stderr', stderr_spec)
+            except Exception:
+                logger.exception("Could not parse stderr specification {} for task {}".format(stderr_spec, task_record['id']))
+                stderr_name = ""
+        else:
+            stderr_name = ""
+
         task_log_info['task_stdout'] = stdout_name
         task_log_info['task_stderr'] = stderr_name
         task_log_info['task_fail_history'] = ",".join(task_record['fail_history'])
