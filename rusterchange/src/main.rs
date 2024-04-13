71699248b3324bb1ab1c95a43d7fff8f30ed7507
Ben Clifford <benc@hawaga.org.uk>
2024-04-13 10:49:25 +0000

    enqueue task... but do nothing with it after that

--- rusterchange/src/main.rs
+++ rusterchange/src/main.rs
@@ -1,3 +1,14 @@
+use queues::IsQueue;
+
+// use of Queue wants Clone trait, which is a bit suspicious: does that mean we have
+// explicit clones of the (potentially large) buffer in interchange? when ideally we
+// would pass around the buffer linearly without ever duplicating it? TODO
+#[derive(Clone)]
+struct Task {
+    task_id: i64,
+    buffer: Vec<u8>
+}
+
 fn main() {
     println!("the rusterchange");
 
@@ -30,6 +41,8 @@ fn main() {
 
     let zmq_results_workers_to_interchange = zmq_ctx.socket(zmq::SocketType::ROUTER).expect("could not create results_workers_to_interchange socket");
     zmq_results_workers_to_interchange.bind("tcp://127.0.0.1:9004").expect("could not bind results_workers_to_interchange");
+
+    let mut task_queue: queues::Queue<Task> = queues::Queue::new();
 
     loop {
 
@@ -68,11 +81,29 @@ fn main() {
             println!("Unpickled: {}", t);
             // the protocol on this channel gives a dict with two entries:
             // a "buffer" and a "task_id"
+            // if this protocol was declared as a dataclass or typed dict, would I be
+            // able to generate a rust-side image of that class and then deserialize
+            // into something I can interact with? (what does in-process rust binding
+            // do in this situation?)
 
-            let serde_pickle::Value::Dict(task_dict) = t else { panic!("protocol violation"); };
-            let task_id = &task_dict[&serde_pickle::HashableValue::String("task_id".to_string())]; 
+            let serde_pickle::Value::Dict(task_dict) = t else { panic!("protocol violation") };
+            let serde_pickle::Value::I64(task_id) = &task_dict[&serde_pickle::HashableValue::String("task_id".to_string())] else { panic!("protocol violation") }; 
+            let serde_pickle::Value::Bytes(buffer) = &task_dict[&serde_pickle::HashableValue::String("buffer".to_string())] else { panic!("protocol violation") };
 
             println!("Received htex task {}", task_id);
+
+            // now there's two options: either we can match against capacity of a registered manager
+            // or if there is no capacity, we can queue this and match when a manager indicates
+            // it has capacity.
+
+            // manager capacity isn't implemented... so all we can do is queue it here.
+            // perhaps consider that i'll want to do richer matchmaking based on resource stuff contained in protocol (see outreachy internship) and be prepared to use something richer than a queue?
+
+            let task = Task {
+                task_id: *task_id,
+                buffer: buffer.clone() // TODO: awkward clone here of buffer but I guess because of serde_pickle, we have to clone it out of the task_dict value if we're doing shared values... perhaps there is a way to convert the task dict into the buffer forgetting everything else, linearly? TODO
+            };
+            task_queue.add(task).expect("queue broken - eg full?");
         }
 
         // TODO: this isn't polled for, so should be impossible to be reached...
@@ -80,5 +111,7 @@ fn main() {
         if sockets[0].get_revents().contains(zmq::PollEvents::POLLOUT) {
             println!("poll out"); // we can write to this socket. actually probably should avoid polling for this? or perhaps optionally if we want to send a heartbeat without blocking?
         }
+
+
     }
 }
