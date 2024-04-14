b0b0dfcd74271bec2732aeb9129f19eef5160a45
Colin Thomas <cthomas0687@gmail.com>
2024-04-14 16:40:35 -0400

    formatting

--- parsl/executors/taskvine/stub_staging_provider.py
+++ parsl/executors/taskvine/stub_staging_provider.py
@@ -5,15 +5,15 @@ from parsl.data_provider.staging import Staging
 from parsl.data_provider.files import File
 from concurrent.futures import Future
 from parsl.app.futures import DataFuture
-from typing import Optional, Callable
-import os
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
 known_url_schemes = ["file", "http", "https", "taskvinetemp"]
 
+
 class StubStaging(Staging, RepresentationMixin):
-    
+
     def can_stage_in(self, file):
         logger.debug("Task vine staging provider checking passthrough for {}".format(repr(file)))
         return file.scheme in known_url_schemes
@@ -22,13 +22,13 @@ class StubStaging(Staging, RepresentationMixin):
         logger.debug("Task vine staging provider checking passthrough for {}".format(repr(file)))
         return file.scheme in known_url_schemes
 
-    def stage_in(self, dm: "DataManager", executor: str, file: File, parent_fut: Optional[Future]) -> Optional[DataFuture]:
+    def stage_in(self, dm, executor: str, file: File, parent_fut: Optional[Future]) -> Optional[DataFuture]:
         if file.netloc == '':
             file.netloc = file.filename
         file.local_path = file.netloc
         return None
 
-    def stage_out(self, dm: "DataManager", executor: str, file: File, app_fu: Future) -> Optional[Future]:
+    def stage_out(self, dm, executor: str, file: File, app_fu: Future) -> Optional[Future]:
         if file.netloc == '':
             file.netloc = file.filename
         file.local_path = file.netloc
--- parsl/executors/taskvine/utils.py
+++ parsl/executors/taskvine/utils.py
@@ -1,5 +1,4 @@
 from typing import Optional
-import uuid
 
 
 class ParslTaskToVine:
