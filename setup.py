edf444e7f1c749dfbe63da188b9c4947ea4b9843
Ben Clifford <benc@hawaga.org.uk>
2024-04-18 06:19:44 +0000

    Update radical pin to latest

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ extras_require = {
     'workqueue': ['work_queue'],
     'flux': ['pyyaml', 'cffi', 'jsonschema'],
     'proxystore': ['proxystore'],
-    'radical-pilot': ['radical.pilot==1.49'],
+    'radical-pilot': ['radical.pilot==1.52.1'],
     # Disabling psi-j since github direct links are not allowed by pypi
     # 'psij': ['psi-j-parsl@git+https://github.com/ExaWorks/psi-j-parsl']
 }
