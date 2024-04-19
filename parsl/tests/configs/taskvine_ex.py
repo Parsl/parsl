d192344518b98816bb29cab9eb2214ceaf30688b
Colin Thomas <cthomas0687@gmail.com>
2024-04-19 14:13:33 -0400

    test config

--- parsl/tests/configs/taskvine_ex.py
+++ parsl/tests/configs/taskvine_ex.py
@@ -1,10 +1,12 @@
 from parsl.config import Config
-from parsl.data_provider.file_noop import NoOpFileStaging
-from parsl.data_provider.ftp import FTPInTaskStaging
-from parsl.data_provider.http import HTTPInTaskStaging
-from parsl.executors.taskvine import TaskVineExecutor, TaskVineManagerConfig
+from parsl.executors.taskvine import TaskVineExecutor
+from parsl.executors.taskvine import TaskVineManagerConfig
 
+from parsl.data_provider.http import HTTPInTaskStaging
+from parsl.data_provider.ftp import FTPInTaskStaging
+from parsl.executors.taskvine.stub_staging_provider import StubStaging
 
 def fresh_config():
     return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(port=9000),
-                                              worker_launch_method='factory')])
+                                              worker_launch_method='factory',
+                                              storage_access=[FTPInTaskStaging(), StubStaging()])])
