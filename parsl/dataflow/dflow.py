Parsl/parsl

commit 906d5448439bfbb9add1e5e37a6811ad065d054f

Add more cases for std stream logging (#3347)
Previously, stdout/err logs not output if the streams were not set
(i.e. defaulting to None) and logged with the entire stream target
if set.

This PR adds more cases here with case specific logging: if stdout/err
is not redirected, that is now logged. If tuple mode is used, the
tuple is unpacked and logged as separate fields. If an unknown format
is specified an error is logged (but no exception is raised - it is
not the job of this code to validate the stdout/err specifications)

This PR is in preparation for a new case to be added in an upcoming
PR which requires more serious formatting that was not accomodated
by the previous implementation.

parsl/dataflow/dflow.py
@@ -1391,10 +1391,20 @@ class DataFlowKernel:
 
     @staticmethod
     def _log_std_streams(task_record: TaskRecord) -> None:
-        if task_record['app_fu'].stdout is not None:
-            logger.info("Standard output for task {} available at {}".format(task_record['id'], task_record['app_fu'].stdout))
-        if task_record['app_fu'].stderr is not None:
-            logger.info("Standard error for task {} available at {}".format(task_record['id'], task_record['app_fu'].stderr))
+        tid = task_record['id']
+
+        def log_std_stream(name: str, target) -> None:
+            if target is None:
+                logger.info(f"{name} for task {tid} will not be redirected.")
+            elif isinstance(target, str):
+                logger.info(f"{name} for task {tid} will be redirected to {target}")
+            elif isinstance(target, tuple) and len(target) == 2:
+                logger.info(f"{name} for task {tid} will be redirected to {target[0]} with mode {target[1]}")
+            else:
+                logger.error(f"{name} for task {tid} has unknown specification: {target!r}")
+
+        log_std_stream("Standard out", task_record['app_fu'].stdout)
+        log_std_stream("Standard error", task_record['app_fu'].stderr)
 
 
 class DataFlowKernelLoader:

commit 7618686f8eedecb30078d4f14706a6c0033dc12b

Fix stdout/err log errors introduced by PR #3347
PR #3347 attempted to do case-based logging of all the different kinds
of stdout/err specification.

It failed to capture some of the cases involving os.PathLike, and so
after PR #3347, those cases would log an ERROR that the specification
was unknown. This new behavior is only a new ERROR logging message -
PR #3347 did not change other behaviour.

This PR also amends a rich test of stdout/err specification types
introduced in PR #3363 to check that no ERROR messages are logged during
these tests.

parsl/dataflow/dflow.py
@@ -1426,8 +1426,12 @@ class DataFlowKernel:
                 logger.info(f"{name} for task {tid} will not be redirected.")
             elif isinstance(target, str):
                 logger.info(f"{name} for task {tid} will be redirected to {target}")
-            elif isinstance(target, tuple) and len(target) == 2:
+            elif isinstance(target, os.PathLike):
+                logger.info(f"{name} for task {tid} will be redirected to {os.fspath(target)}")
+            elif isinstance(target, tuple) and len(target) == 2 and isinstance(target[0], str):
                 logger.info(f"{name} for task {tid} will be redirected to {target[0]} with mode {target[1]}")
+            elif isinstance(target, tuple) and len(target) == 2 and isinstance(target[0], os.PathLike):
+                logger.info(f"{name} for task {tid} will be redirected to {os.fspath(target[0])} with mode {target[1]}")
             elif isinstance(target, DataFuture):
                 logger.info(f"{name} for task {tid} will staged to {target.file_obj.url}")
             else:

parsl/tests/test_monitoring/test_stdouterr.py
@@ -1,6 +1,7 @@
 """Tests monitoring records app name under various decoration patterns.
 """
 
+import logging
 import os
 import parsl
 import pytest
@@ -55,7 +56,7 @@ class ArbitraryStaging(Staging):
                           (File("file:///tmp/pl5"), "file:///tmp/pl5"),
                           ])
 @pytest.mark.parametrize('stream', ['stdout', 'stderr'])
-def test_stdstream_to_monitoring(stdx, expected_stdx, stream, tmpd_cwd):
+def test_stdstream_to_monitoring(stdx, expected_stdx, stream, tmpd_cwd, caplog):
     """This tests that various forms of stdout/err specification are
        represented in monitoring correctly. The stderr and stdout codepaths
        are generally duplicated, rather than factorised, and so this test
@@ -106,3 +107,6 @@ def test_stdstream_to_monitoring(stdx, expected_stdx, stream, tmpd_cwd):
             assert expected_stdx(c)
         else:
             raise RuntimeError("Bad expected_stdx value")
+
+    for record in caplog.records:
+        assert record.levelno < logging.ERROR