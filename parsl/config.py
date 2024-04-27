67f16d597aa158aae5078592e4847a6770c184b7
nishchaykarle <nishchaykarle@gmail.com>
2024-04-27 02:56:50 -0500

    update usage_tracking to be represented by an int based on level chosen

--- parsl/config.py
+++ parsl/config.py
@@ -60,9 +60,11 @@ class Config(RepresentationMixin, UsageInformation):
         How often the scaling strategy should be executed. Default is 5 seconds.
     max_idletime : float, optional
         The maximum idle time allowed for an executor before strategy could shut down unused blocks. Default is 120.0 seconds.
-    usage_tracking : bool, optional
-        Set this field to True to opt-in to Parsl's usage tracking system. Parsl only collects minimal, non personally-identifiable,
-        information used for reporting to our funding agencies. Default is False.
+    usage_tracking : int, optional
+        Set this field to 1, 2, or 3 to opt-in to Parsl's usage tracking system.
+        The value represents the level of usage tracking detail to be collected.
+        Parsl only collects minimal, non personally-identifiable,
+        information used for reporting to our funding agencies. Default is 0 (disabled).
     initialize_logging : bool, optional
         Make DFK optionally not initialize any logging. Log messages
         will still be passed into the python logging system under the
@@ -94,7 +96,7 @@ class Config(RepresentationMixin, UsageInformation):
                  strategy_period: Union[float, int] = 5,
                  max_idletime: float = 120.0,
                  monitoring: Optional[MonitoringHub] = None,
-                 usage_tracking: bool = False,
+                 usage_tracking: int = 0,
                  initialize_logging: bool = True) -> None:
 
         executors = tuple(executors or [])
@@ -127,7 +129,10 @@ class Config(RepresentationMixin, UsageInformation):
         self.strategy = strategy
         self.strategy_period = strategy_period
         self.max_idletime = max_idletime
+
         self.usage_tracking = usage_tracking
+        self._validate_usage_tracking()
+
         self.initialize_logging = initialize_logging
         self.monitoring = monitoring
 
@@ -145,6 +150,12 @@ class Config(RepresentationMixin, UsageInformation):
         if len(duplicates) > 0:
             raise ConfigurationError('Executors must have unique labels ({})'.format(
                 ', '.join(['label={}'.format(repr(d)) for d in duplicates])))
+
+    def _validate_usage_tracking(self) -> None:
+        if int(self.usage_tracking) not in {0, 1, 2, 3}:
+            raise ConfigurationError(
+                f"Usage Tracking values must be 0, 1, 2, or 3 and not {self.usage_tracking}"
+            )
 
     def get_usage_information(self):
         return {"executors_len": len(self.executors)}
