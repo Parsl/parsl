b1dd197eb9d39428860db3e09603f7457f9a0c08
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2024-05-17 11:05:51 -0700

    Change message to warning

--- parsl/providers/slurm/slurm.py
+++ parsl/providers/slurm/slurm.py
@@ -207,7 +207,7 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
         # squeue does not report on jobs that are not running. So we are filling in the
         # blanks for missing jobs, we might lose some information about why the jobs failed.
         for missing_job in jobs_missing:
-            logger.debug("Updating missing job {} to completed status".format(missing_job))
+            logger.warning("Updating missing job {} to completed status".format(missing_job))
             self.resources[missing_job]['status'] = JobStatus(
                 JobState.COMPLETED, stdout_path=self.resources[missing_job]['job_stdout_path'],
                 stderr_path=self.resources[missing_job]['job_stderr_path'])
