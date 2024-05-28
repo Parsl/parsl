e7ba9996c15c6d0e5b965ebeb15b7981ed587fc9
Colin Thomas <cthomas0687@gmail.com>
2024-05-28 13:15:38 -0400

    explicit data providers

--- parsl/tests/configs/taskvine_ex.py
+++ parsl/tests/configs/taskvine_ex.py
@@ -4,10 +4,12 @@ from parsl.executors.taskvine import TaskVineManagerConfig
 
 from parsl.data_provider.http import HTTPInTaskStaging
 from parsl.data_provider.ftp import FTPInTaskStaging
+from parsl.data_provider.file_noop import NoOpFileStaging
+from parsl.data_provider.zip import ZipFileStaging
 from parsl.executors.taskvine.stub_staging_provider import StubStaging
 
 
 def fresh_config():
     return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(port=9000),
                                               worker_launch_method='factory',
-                                              storage_access=[FTPInTaskStaging(), StubStaging()])])
+                                              storage_access=[FTPInTaskStaging(), StubStaging(), NoOpFileStaging(), ZipFileStaging()])])
