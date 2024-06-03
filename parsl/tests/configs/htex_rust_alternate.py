f2301862bae56d30a54725dbb7529eda5d9581ba
Ben Clifford <benc@hawaga.org.uk>
2024-06-03 19:09:46 +0000

    isort example configs

--- parsl/tests/configs/htex_rust_alternate.py
+++ parsl/tests/configs/htex_rust_alternate.py
@@ -15,23 +15,20 @@ will cause substantially different behaviour on whatever
 those timing parameters control.
 """
 
-# imports for monitoring:
-from parsl.monitoring import MonitoringHub
-
 import os
 
-from parsl.providers import LocalProvider
 from parsl.channels import LocalChannel
+from parsl.config import Config
+from parsl.data_provider.file_noop import NoOpFileStaging
+from parsl.data_provider.ftp import FTPInTaskStaging
+from parsl.data_provider.http import HTTPInTaskStaging
+from parsl.data_provider.zip import ZipFileStaging
+from parsl.executors import HighThroughputExecutor
 from parsl.launchers import SingleNodeLauncher
 
-from parsl.config import Config
-from parsl.executors import HighThroughputExecutor
-
-
-from parsl.data_provider.http import HTTPInTaskStaging
-from parsl.data_provider.ftp import FTPInTaskStaging
-from parsl.data_provider.file_noop import NoOpFileStaging
-from parsl.data_provider.zip import ZipFileStaging
+# imports for monitoring:
+from parsl.monitoring import MonitoringHub
+from parsl.providers import LocalProvider
 
 working_dir = os.getcwd() + "/" + "test_htex_alternate"
 
