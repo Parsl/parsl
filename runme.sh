b56dfd9ce3671e3390d6df15484efd9cd349f36f
Ben Clifford <benc@hawaga.org.uk>
2024-06-10 14:09:21 +0000

    Randomise test order to see if a test failure is specific to a particular test, or something else

--- runme.sh
+++ runme.sh
@@ -2,6 +2,6 @@
 
 source /venv/bin/activate
 
-pytest parsl/tests/ --config ./htex_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x
+pytest parsl/tests/ --config ./htex_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order
 
 
