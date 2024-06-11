0de37c4bb804c3e09491aaea4437f6baf8a07343
Ben Clifford <benc@hawaga.org.uk>
2024-06-11 13:33:03 +0000

    Remove commented-out code

--- parsl/executors/high_throughput/interchange.py
+++ parsl/executors/high_throughput/interchange.py
@@ -673,17 +673,6 @@ def start_file_logger(filename: str, level: int = logging.DEBUG, format_string: 
     logger.addHandler(handler)
 
 
-# @wrap_with_logs(target="interchange")
-# def starter(*args: Any, **kwargs: Any) -> None:
-#    """Start the interchange process
-
-#    The executor is expected to call this function. The args, kwargs match that of the Interchange.__init__
-#    """
-#    setproctitle("parsl: HTEX interchange")
-#    # logger = multiprocessing.get_logger()
-#    ic = Interchange(*args, **kwargs)
-#    ic.start()
-
 if __name__ == "__main__":
     setproctitle("parsl: HTEX interchange")
 
