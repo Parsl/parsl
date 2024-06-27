632890b584da7213b2784a4c7c4f038571975c21
astro-friedel <friedel@illinois.edu>
2024-06-27 15:26:08 -0500

    added flask-wtf to monitoring requirements for form processing

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ extras_require = {
         'networkx>=2.5,<2.6',
         'Flask>=1.0.2',
         'flask_sqlalchemy',
+        'Flask-WTF',
         'pandas<2.2',
         'plotly',
         'python-daemon'
