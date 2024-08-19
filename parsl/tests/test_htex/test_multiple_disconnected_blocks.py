7ad560873c9d290731d5b45bc399556da79f9fbe
Ben Clifford <benc@hawaga.org.uk>
2024-08-19 14:24:50 +0000

    tag questionable test

--- parsl/tests/test_htex/test_multiple_disconnected_blocks.py
+++ parsl/tests/test_htex/test_multiple_disconnected_blocks.py
@@ -9,6 +9,7 @@ from parsl.executors.errors import BadStateException
 from parsl.jobs.states import JobState, JobStatus
 from parsl.providers import LocalProvider
 
+logger = logging.getLogger(__name__)
 
 def local_config():
     """Config to simulate failing blocks without connecting"""
@@ -40,21 +41,29 @@ def double(x):
 
 
 @pytest.mark.local
+@pytest.mark.skip("shouldn't be expecting a bad state exception if we've scaled in block 0 rather than it failing - maybe need to tweak min/max/init blocks?")
 def test_multiple_disconnected_blocks():
     """Test reporting of blocks that fail to connect from HTEX
     When init_blocks == N, error handling expects N failures before
     the run is cancelled
     """
+
+    logger.error("BENC: 1")
     dfk = parsl.dfk()
     executor = dfk.executors["HTEX"]
 
     connected_blocks = executor.connected_blocks()
     assert not connected_blocks, "Expected 0 blocks"
 
+    logger.error("BENC: 2")
     future = double(5)
+    logger.error("BENC: 2.1")
     with pytest.raises(BadStateException):
+        logger.error("BENC: 2.2")
         future.result()
+        logger.error("BENC: 2.3")
 
+    logger.error("BENC: 3")
     assert isinstance(future.exception(), BadStateException)
     exception_body = str(future.exception())
     assert "EXIT CODE: 0" in exception_body
@@ -65,5 +74,7 @@ def test_multiple_disconnected_blocks():
         assert isinstance(status, JobStatus)
         assert status.state == JobState.MISSING
 
+    logger.error("BENC: 4")
     connected_blocks = executor.connected_blocks()
     assert not connected_blocks, "Expected 0 blocks"
+    logger.error("BENC: 5")
