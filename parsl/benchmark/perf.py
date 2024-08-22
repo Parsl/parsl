68b9cb014cbb908b8da8b2224e8a92a21e04cc56
Ben Clifford <benc@hawaga.org.uk>
2024-08-22 09:20:30 +0000

    out-of-memory checkpointing
    goal: results should not (never? in weak small cache?) be stored in an
    in-memory memo table. so that memo table should be not present in this
    implementation. instead all memo questions go to the sqlite3 database.
    
    this drives some blurring between in-memory caching and disk-based
    checkpointing: the previous disk based checkpointed model relied on
    repopulating the in-memory memo table cache...
    
    i hit some thread problems when using one sqlite3 connection across threads
    and the docs are unclear about what I can/cannot do, so i made this open
    the sqlite3 database on every access. that's probably got quite a performance
    hit, but its probably enough for basically validating the idea.
    
    the three usecases: sqlite3, in-memory and traditional on-disk checkpoint,
    maybe needs some thoughts about the completion semantics and data structures
    to go with it.
    
    TODO: this breaks:
      pytest  parsl/tests/test_python_apps/test_memoize_exception.py  --config parsl/tests/configs/htex_local_alternate.py
    because the behaviour asserted there is only behaviour for the default
    memoizer. So it should probably become a --config local test with
    known configuration(s).

--- parsl/benchmark/perf.py
+++ parsl/benchmark/perf.py
@@ -49,7 +49,7 @@ def performance(*, resources: dict, target_t: float, args_extra_size: int, itera
 
     iteration = 1
 
-    args_extra_payload = "x" * args_extra_size
+    # args_extra_payload = "x" * args_extra_size
 
     if isinstance(iterate_mode, list):
         n = iterate_mode[0]
@@ -65,7 +65,13 @@ def performance(*, resources: dict, target_t: float, args_extra_size: int, itera
 
         fs = []
         print("Submitting tasks / invoking apps")
-        for _ in range(n):
+        for index in range(n):
+            # this means there is a different argument for each iteration,
+            # which will make checkpointing/memo behave differently
+            # so this could be switchable in parsl-perf dev branch
+            # args_extra_payload = index   # always a new one (except for run repeats)
+
+            args_extra_payload = index % 10
             fs.append(app(args_extra_payload, parsl_resource_specification=resources))
 
         submitted_t = time.time()
