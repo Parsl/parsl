6a32f0ff3dff61255699fa7a3adfc806f379ff26
Ben Clifford <benc@hawaga.org.uk>
2024-09-05 13:44:28 +0000

    Log more to the console, kubernetes style

--- runme.sh
+++ runme.sh
@@ -3,6 +3,6 @@
 source /venv/bin/activate
 
 pytest parsl/tests/ --config ./htex_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order
-PYTHONPATH=/usr/lib/python3.12/site-packages/ pytest parsl/tests/ --config ./taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order
+PYTHONPATH=/usr/lib/python3.12/site-packages/ pytest parsl/tests/ --config ./taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG
 
 
