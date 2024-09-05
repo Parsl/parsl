21dcae67030f120540ab4fcba5a021b5d64ed318
Ben Clifford <benc@hawaga.org.uk>
2024-09-05 14:03:36 +0000

    force hostname based address config, in line with comment in previous commit

--- taskvine_k8s_kind.py
+++ taskvine_k8s_kind.py
@@ -2,11 +2,12 @@ from parsl.channels import LocalChannel
 from parsl.config import Config
 from parsl.launchers import SimpleLauncher
 from parsl.providers import KubernetesProvider
+from parsl.addresses import address_by_hostname
 
 from parsl.executors.taskvine import TaskVineExecutor, TaskVineManagerConfig
 
 def fresh_config():
-    return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(port=9000),
+    return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(address=address_by_hostname(), port=9000),
                                               worker_launch_method='provider',
                 provider=KubernetesProvider(
                     worker_init=". /venv/bin/activate",
