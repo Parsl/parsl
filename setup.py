530373537a234db5d30b7e407276e6701e9b7151
Ben Clifford <benc@hawaga.org.uk>
2024-10-08 13:25:56 +0000

    Drop support for end-of-life Python 3.8 (#3629)
    Python 3.8 entered the end-of-life phase on 2024-10-07.
    See https://peps.python.org/pep-0569/
    
    This PR removes CI testing for Python 3.8 and relabels
    several occurences of 3.8 to 3.9 in the documentation.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     include_package_data=True,
     package_data={'parsl': ['py.typed']},
     packages=find_packages(),
-    python_requires=">=3.8.0",
+    python_requires=">=3.9.0",
     install_requires=install_requires,
     scripts = ['parsl/executors/high_throughput/process_worker_pool.py',
                'parsl/executors/high_throughput/interchange.py',
@@ -71,7 +71,6 @@ setup(
         # Licence, must match with licence above
         'License :: OSI Approved :: Apache Software License',
         # Python versions supported
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
