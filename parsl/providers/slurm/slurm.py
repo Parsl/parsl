e95036d459b6bd8b2a9fe1eca72aa5156e34499e
Ben Clifford <benc@hawaga.org.uk>
2024-10-24 17:27:55 +0000

    Use slurm provider script dir instead of channel script dir -- these should be the same

--- parsl/providers/slurm/slurm.py
+++ parsl/providers/slurm/slurm.py
@@ -3,7 +3,7 @@ import math
 import os
 import re
 import time
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import typeguard
 
@@ -265,8 +265,8 @@ class SlurmProvider(ClusterProvider, RepresentationMixin):
 
         logger.debug("Requesting one block with {} nodes".format(self.nodes_per_block))
 
-        job_config = {}
-        job_config["submit_script_dir"] = self.channel.script_dir
+        job_config: Dict[str, Any] = {}
+        job_config["submit_script_dir"] = self.script_dir
         job_config["nodes"] = self.nodes_per_block
         job_config["tasks_per_node"] = tasks_per_node
         job_config["walltime"] = wtime_to_minutes(self.walltime)
