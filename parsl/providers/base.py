bdf3364017c0b9ca8c787787579cbd9a7610a939
Ben Clifford <benc@hawaga.org.uk>
2024-10-24 12:45:03 +0000

    Remove unused (multi)channeled marker types (#3653)
    These were introduced to help make channels easier to typecheck in the
    DFK, but the DFK code continued to use hasattr tests for 'channel' and
    'channels' attributes rather than these marker types.
    
    This is part of channel removal, issue #3515
    
    Co-authored-by: Kevin Hunter Kesling <kevin@globus.org>

--- parsl/providers/base.py
+++ parsl/providers/base.py
@@ -2,7 +2,6 @@ import logging
 from abc import ABCMeta, abstractmethod, abstractproperty
 from typing import Any, Dict, List, Optional
 
-from parsl.channels.base import Channel
 from parsl.jobs.states import JobStatus
 
 logger = logging.getLogger(__name__)
@@ -153,19 +152,4 @@ class ExecutionProvider(metaclass=ABCMeta):
 
         :return: the number of seconds to wait between calls to status()
         """
-        pass
-
-
-class Channeled():
-    """A marker type to indicate that parsl should manage a Channel for this provider"""
-    def __init__(self) -> None:
-        self.channel: Channel
-        pass
-
-
-class MultiChanneled():
-    """A marker type to indicate that parsl should manage multiple Channels for this provider"""
-
-    def __init__(self) -> None:
-        self.channels: List[Channel]
         pass
