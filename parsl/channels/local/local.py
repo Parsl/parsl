d6f115e7c951c3b0735d77586477e89c3c5d5f0b
Ben Clifford <benc@hawaga.org.uk>
2024-10-24 17:54:41 +0000

    Rework environment handling in LocalChannel
    Environments are never passed to local channel, so the environment parameter
    is removed from execute_wait by this PR.
    
    This will change the environment behaviour of the local channel: commands
    will now receive a copy of the parent environment at time of command
    invocation. Prior to this PR, commands receive a copy of the parent
    environment at the time of channel initialization and would not see any
    changes made to the process environment after that.
    
    This new behaviour is more consistent with Popen. It is a user-facing
    change, though.
    
    Because it is user facing and not directly part of #3515 channel removal,
    it could be made into a PR now.

--- parsl/channels/local/local.py
+++ parsl/channels/local/local.py
@@ -1,4 +1,3 @@
-import copy
 import logging
 import os
 import subprocess
@@ -15,42 +14,27 @@ class LocalChannel(RepresentationMixin):
 
     def __init__(self):
         ''' Initialize the local channel.
-
-        KwArgs:
-            - envs (dict) : A dictionary of env variables to be set when launching the shell
         '''
-        envs = {}
-        self.envs = envs
-        local_env = os.environ.copy()
-        self._envs = copy.deepcopy(local_env)
-        self._envs.update(envs)
+        pass
 
-    def execute_wait(self, cmd, walltime=None, envs={}):
+    def execute_wait(self, cmd, walltime=None):
         ''' Synchronously execute a commandline string on the shell.
 
         Args:
             - cmd (string) : Commandline string to execute
             - walltime (int) : walltime in seconds, this is not really used now.
 
-        Kwargs:
-            - envs (dict) : Dictionary of env variables. This will be used
-              to override the envs set at channel initialization.
-
         Returns:
             - retcode : Return code from the execution
             - stdout  : stdout string
             - stderr  : stderr string
         '''
-        current_env = copy.deepcopy(self._envs)
-        current_env.update(envs)
-
         try:
             logger.debug("Creating process with command '%s'", cmd)
             proc = subprocess.Popen(
                 cmd,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
-                env=current_env,
                 shell=True,
                 preexec_fn=os.setpgrp
             )
--- parsl/tests/test_channels/test_local_channel.py
+++ parsl/tests/test_channels/test_local_channel.py
@@ -19,22 +19,3 @@ def test_env():
     assert x, "HOME not found"
 
     print("RC:{} \nSTDOUT:{} \nSTDERR:{}".format(rc, stdout, stderr))
-
-
-@pytest.mark.local
-def test_env_mod():
-    ''' Testing for env update at execute time.
-    '''
-
-    lc = LocalChannel()
-    rc, stdout, stderr = lc.execute_wait("env", 1, {'TEST_ENV': 'fooo'})
-
-    stdout = stdout.split('\n')
-    x = [s for s in stdout if s.startswith("PATH=")]
-    assert x, "PATH not found"
-
-    x = [s for s in stdout if s.startswith("HOME=")]
-    assert x, "HOME not found"
-
-    x = [s for s in stdout if s.startswith("TEST_ENV=fooo")]
-    assert x, "User set env missing"
