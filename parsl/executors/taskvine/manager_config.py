3151ca84e083524645e7989c337573f8f773d4cd
Colin Thomas <33940547+colinthomas-z80@users.noreply.github.com>
2024-11-08 12:31:26 -0500

    TaskVine Executor: add vine tune to manager config (#3692)
    # Description
    Make vine_tune config parameters accessible through the manager config
    while using the TaskVine executor
    
    ## Type of change
    
    - New feature

--- parsl/executors/taskvine/manager_config.py
+++ parsl/executors/taskvine/manager_config.py
@@ -156,6 +156,10 @@ class TaskVineManagerConfig:
         Directory to store TaskVine logging facilities.
         Default is None, in which all TaskVine logs will be contained
         in the Parsl logging directory.
+
+    tune_parameters: Optional[dict]
+        Extended vine_tune parameters, expressed in a dictionary
+        by { 'tune-parameter' : value }.
     """
 
     # Connection and communication settings
@@ -181,6 +185,7 @@ class TaskVineManagerConfig:
     autocategory: bool = True
     enable_peer_transfers: bool = True
     wait_for_workers: Optional[int] = None
+    tune_parameters: Optional[dict] = None
 
     # Logging settings
     vine_log_dir: Optional[str] = None
