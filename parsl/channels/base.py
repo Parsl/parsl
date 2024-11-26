37d9a12f0e0266012cc4b83b992165bff9c0d062
Ben Clifford <benc@hawaga.org.uk>
2024-11-26 18:56:28 +0000

    remove last bit of doc string

--- parsl/channels/base.py
+++ parsl/channels/base.py
@@ -2,13 +2,6 @@ from abc import ABCMeta, abstractproperty
 
 
 class Channel(metaclass=ABCMeta):
-    """Channels are abstractions that enable ExecutionProviders to talk to
-    resource managers of remote compute facilities.
-
-    For certain resources such as campus clusters or supercomputers at
-    research laboratories, resource requirements may require authentication.
-    """
-
     @abstractproperty
     def script_dir(self) -> str:
         ''' This is a property. Returns the directory assigned for storing all internal scripts such as
