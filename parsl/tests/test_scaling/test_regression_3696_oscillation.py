74c845573a8d00b288f4d583e3aa542b6d78649d
Ben Clifford <benc@hawaga.org.uk>
2024-12-11 13:25:01 +0000

    add more assertions into test, around scale in call

--- parsl/tests/test_scaling/test_regression_3696_oscillation.py
+++ parsl/tests/test_scaling/test_regression_3696_oscillation.py
@@ -38,11 +38,22 @@ def test_htex_strategy_does_not_oscillate():
     provider.max_blocks = 2
     provider.nodes_per_block = 1
 
-    def f(n):
+    def scale_out(n):
         for _ in range(n):
             statuses[len(statuses)] = JobStatus(state=JobState.PENDING)
 
-    executor.scale_out_facade.side_effect = f
+    executor.scale_out_facade.side_effect = scale_out
+
+    def scale_in(n, max_idletime=None):
+        # find n PENDING jobs and set them to CANCELLED
+        for k in statuses:
+            if statuses[k].state == JobState.PENDING:
+                statuses[k].state = JobState.CANCELLED
+                n -= 1
+            if n == 0:
+                return
+
+    executor.scale_in_facade.side_effect = scale_in
 
     s.add_executors([executor])
 
@@ -51,7 +62,9 @@ def test_htex_strategy_does_not_oscillate():
     s.strategize([executor])
 
     executor.scale_out_facade.assert_called()
+    print(f"BENC: {statuses}")
     assert len(statuses) == 1, "Only one block should have been launched"
+    assert len([k for k in statuses if statuses[k].state == JobState.PENDING]) == 1
     # there might be several calls to scale_out_facade inside strategy,
     # but the end effect should be that exactly one block is scaled out.
 
@@ -59,9 +72,13 @@ def test_htex_strategy_does_not_oscillate():
 
     # In issue #3696, this second strategize does a scale in, because 14 < 48*1
     s.strategize([executor])
-    executor.scale_in_facade.assert_not_called()  # this assert fails due to issue #3696
+
+    # assert that there should still be 1 pending blocks
+    assert len([k for k in statuses if statuses[k].state == JobState.PENDING]) == 1
+    # this assert fails due to issue #3696
 
     # Now check scale in happens with 0 load
     executor.outstanding = 0
     s.strategize([executor])
     executor.scale_in_facade.assert_called()
+    assert len([k for k in statuses if statuses[k].state == JobState.PENDING]) == 0
