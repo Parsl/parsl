d2b82d7c554718a7747b42b6f13a0c0116c8db68
Ben Clifford <benc@hawaga.org.uk>
2025-01-05 18:14:03 +0000

    revert accidental test config change

--- parsl/tests/configs/htex_local.py
+++ parsl/tests/configs/htex_local.py
@@ -9,7 +9,7 @@ def fresh_config():
         executors=[
             HighThroughputExecutor(
                 label="htex_local",
-                loopback_address="127.0.0.1",
+                loopback_address="::1",
                 worker_debug=True,
                 cores_per_worker=1,
                 encrypted=True,
