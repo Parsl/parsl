eebdedca872d0d093780fe0b262ccc996266b023
Ben Clifford <benc@hawaga.org.uk>
2025-01-15 13:08:25 +0000

    restore python version in path

--- runme.sh
+++ runme.sh
@@ -12,4 +12,4 @@ pytest parsl/tests/ --config ./htex_k8s_kind.py -k 'not issue3328 and not stagin
 # which I guess is saying something is making assumptions about the presence of a shared filesystem even when defaulting to shared_fs=False in the taskvine config?
 
 
-PYTHONPATH=/usr/lib/python3/site-packages/ pytest parsl/tests/ --config ./taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG
+PYTHONPATH=/usr/lib/python3.11/site-packages/ pytest parsl/tests/ --config ./taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG
