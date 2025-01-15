88c5f172c38ef02e517d8530f531e732097555c0
Ben Clifford <benc@hawaga.org.uk>
2025-01-15 15:36:07 +0000

    remove a note to self about taskvine vs non-shared fs

--- parsl/tests/ci_k8s/runme.sh
+++ parsl/tests/ci_k8s/runme.sh
@@ -4,12 +4,4 @@ source /venv/bin/activate
 
 pytest parsl/tests/ --config parsl/tests/ci_k8s/htex_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order
 
-
-# I tried letting staging_required tests run here but they do not -- a bit confused about this comment in taskvine:
-#
-#            # Absolute paths are assumed to be in shared filesystem, and thus
-#            # not staged by taskvine.
-# which I guess is saying something is making assumptions about the presence of a shared filesystem even when defaulting to shared_fs=False in the taskvine config?
-
-
 PYTHONPATH=/usr/lib/python3.11/site-packages/ pytest parsl/tests/ --config parsl/tests/ci_k8s/taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG
