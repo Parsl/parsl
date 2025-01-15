5854b9e674753d26a76aec532f3fd74b109df50f
Ben Clifford <benc@hawaga.org.uk>
2025-01-15 14:25:00 +0000

    isort

--- parsl/tests/ci_k8s/taskvine_k8s_kind.py
+++ parsl/tests/ci_k8s/taskvine_k8s_kind.py
@@ -1,9 +1,9 @@
+from parsl.addresses import address_by_hostname
 from parsl.config import Config
+from parsl.executors.taskvine import TaskVineExecutor, TaskVineManagerConfig
 from parsl.launchers import SimpleLauncher
 from parsl.providers import KubernetesProvider
-from parsl.addresses import address_by_hostname
 
-from parsl.executors.taskvine import TaskVineExecutor, TaskVineManagerConfig
 
 def fresh_config():
     return Config(executors=[TaskVineExecutor(manager_config=TaskVineManagerConfig(address=address_by_hostname(), port=9000),
