beecde2d35a7177b13407d53ba53bfbc2e033071
Yadu Babuji <yadudoc1729@gmail.com>
2025-03-10 15:37:24 -0500

    Add alternate cleaner serialization method names

--- parsl/serialize/__init__.py
+++ parsl/serialize/__init__.py
@@ -7,10 +7,20 @@ from parsl.serialize.facade import (
     unpack_res_spec_apply_message,
 )
 
+serialize_task = pack_res_spec_apply_message
+deserialize_task = unpack_res_spec_apply_message
+serialize_result = serialize
+deserialize_result = deserialize
+
+
 __all__ = ['serialize',
            'deserialize',
            'pack_apply_message',
            'unpack_apply_message',
            'unpack_res_spec_apply_message',
-           'pack_res_spec_apply_message'
+           'pack_res_spec_apply_message',
+           'serialize_task',
+           'serialize_result',
+           'deserialize_task',
+           'deserialize_result'
            ]
