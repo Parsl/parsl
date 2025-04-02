66606950d8138bdaa4adc150af1d45d5da6ed4d5
Ben Clifford <benc@hawaga.org.uk>
2025-04-02 14:44:21 +0000

    move filesystem radio receiver to radios directory out of monitoring like UDP PR and ZMQ PR. doesn't move who has responsibility for startup/shutdown.

--- parsl/monitoring/radios/filesystem_router.py
+++ parsl/monitoring/radios/filesystem_router.py
@@ -4,6 +4,7 @@ import logging
 import os
 import pickle
 import time
+from multiprocessing.context import SpawnProcess
 from multiprocessing.queues import Queue
 from multiprocessing.synchronize import Event
 from typing import cast
@@ -11,6 +12,7 @@ from typing import cast
 from parsl.log_utils import set_file_logger
 from parsl.monitoring.radios.multiprocessing import MultiprocessingQueueRadioSender
 from parsl.monitoring.types import TaggedMonitoringMessage
+from parsl.multiprocessing import SpawnEvent, join_terminate_close_proc
 from parsl.process_loggers import wrap_with_logs
 from parsl.utils import setproctitle
 
@@ -52,3 +54,32 @@ def filesystem_router_starter(*, q: Queue[TaggedMonitoringMessage], run_dir: str
 
         time.sleep(1)  # whats a good time for this poll?
     logger.info("Ending filesystem radio receiver")
+
+
+class FilesystemRadioReceiver():
+    def __init__(self, *, process: SpawnProcess, exit_event: Event) -> None:
+        self.process = process
+        self.exit_event = exit_event
+
+    def close(self) -> None:
+        self.exit_event.set()
+        join_terminate_close_proc(self.process)
+
+
+def start_filesystem_receiver(*,
+                              monitoring_messages: Queue,
+                              logdir: str,
+                              debug: bool) -> FilesystemRadioReceiver:
+
+    router_exit_event = SpawnEvent()
+
+    filesystem_proc = SpawnProcess(target=filesystem_router_starter,
+                                   kwargs={"q": monitoring_messages,
+                                           "run_dir": logdir,
+                                           "exit_event": router_exit_event},
+                                   name="Monitoring-Filesystem-Process",
+                                   daemon=True
+                                   )
+    filesystem_proc.start()
+
+    return FilesystemRadioReceiver(process=filesystem_proc, exit_event=router_exit_event)
