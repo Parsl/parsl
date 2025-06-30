Parsl/parsl

commit 9c2ff0e678c1728dc92ef593853015bbfaf3add2

Add pluggable API for monitoring radios (#3892)
Prior to this, the set of radios available for sending results from
in-task monitoring back to the submit side was fixed in a hard-coded if
statement, identified by name.

This PR changes that to a config-object style, along the lines of other
pieces of Parsl pluggable configurability.

This PR also changes the way in which the receiving side of radios is
started: rather than starting all possible receivers (which is not
possible in the new open-world model of radios), it only starts
receivers for the configured radios. This now happens one-per-executor,
which means in some situations there may be more, rather than fewer,
executor receiver processes.

User facing configuration changes:

Executors now have a remote_monitoring_radio configuration parameter,
for this new configuration. In most existing executors, this is
optional, with the executors using the same default radio as they did
before.

The ThreadPoolExecutor no longer has a default, as it previously used
the UDP radio, which is now configured differently. A follow-on PR will
introduce a new thread pool radio to allow the ThreadPoolExecutor to
have a default.

On the MonitoringHub, the hub_address and hub_port parameters will be
ignored after this PR. Before this PR, they were used to configure the
destination for the UDP radio. That is now configured on the new
UDPRadio object directly.

The UDPRadio now exposes its atexit_timeout parameter. This was
previously implemented, but not exposed for user configuration

## Type of change

- New feature

parsl/monitoring/radios/filesystem_router.py
@@ -4,15 +4,15 @@ import logging
 import os
 import pickle
 import time
-from multiprocessing.context import SpawnProcess
 from multiprocessing.queues import Queue
 from multiprocessing.synchronize import Event
 from typing import cast
 
 from parsl.log_utils import set_file_logger
+from parsl.monitoring.radios.base import MonitoringRadioReceiver
 from parsl.monitoring.radios.multiprocessing import MultiprocessingQueueRadioSender
 from parsl.monitoring.types import TaggedMonitoringMessage
-from parsl.multiprocessing import SpawnEvent, join_terminate_close_proc
+from parsl.multiprocessing import SpawnEvent, SpawnProcess
 from parsl.process_loggers import wrap_with_logs
 from parsl.utils import setproctitle
 
@@ -57,30 +57,18 @@ def filesystem_router_starter(*, q: Queue[TaggedMonitoringMessage], run_dir: str
     logger.info("Ending filesystem radio receiver")
 
 
-class FilesystemRadioReceiver():
-    def __init__(self, *, process: SpawnProcess, exit_event: Event) -> None:
-        self.process = process
-        self.exit_event = exit_event
+class FilesystemRadioReceiver(MonitoringRadioReceiver):
+    def __init__(self, resource_msgs: Queue, run_dir: str) -> None:
+        self.exit_event = SpawnEvent()
+        self.process = SpawnProcess(target=filesystem_router_starter,
+                                    kwargs={"q": resource_msgs, "run_dir": run_dir, "exit_event": self.exit_event},
+                                    name="Monitoring-Filesystem-Process",
+                                    daemon=True
+                                    )
+        self.process.start()
+        logger.info("Started filesystem radio receiver process %s", self.process.pid)
 
-    def close(self) -> None:
-        self.exit_event.set()
-        join_terminate_close_proc(self.process)
-
-
-def start_filesystem_receiver(*,
-                              monitoring_messages: Queue,
-                              logdir: str,
-                              debug: bool) -> FilesystemRadioReceiver:
-
-    router_exit_event = SpawnEvent()
-
-    filesystem_proc = SpawnProcess(target=filesystem_router_starter,
-                                   kwargs={"q": monitoring_messages,
-                                           "run_dir": logdir,
-                                           "exit_event": router_exit_event},
-                                   name="Monitoring-Filesystem-Process",
-                                   daemon=True
-                                   )
-    filesystem_proc.start()
-
-    return FilesystemRadioReceiver(process=filesystem_proc, exit_event=router_exit_event)
+    def shutdown(self) -> None:
+        self.process.terminate()
+        self.process.join()
+        self.process.close()

parsl/dataflow/dflow.py
@@ -44,8 +44,10 @@ from parsl.executors.status_handling import BlockProviderExecutor
 from parsl.executors.threads import ThreadPoolExecutor
 from parsl.jobs.job_status_poller import JobStatusPoller
 from parsl.monitoring import MonitoringHub
+from parsl.monitoring.errors import RadioRequiredError
 from parsl.monitoring.message_type import MessageType
 from parsl.monitoring.radios.multiprocessing import MultiprocessingQueueRadioSender
+from parsl.monitoring.radios.udp import UDPRadio
 from parsl.monitoring.remote import monitor_wrapper
 from parsl.process_loggers import wrap_with_logs
 from parsl.usage_tracking.usage import UsageTracker
@@ -186,7 +188,9 @@ class DataFlowKernel:
         self.executors: Dict[str, ParslExecutor] = {}
 
         self.data_manager = DataManager(self)
-        parsl_internal_executor = ThreadPoolExecutor(max_threads=config.internal_tasks_max_threads, label='_parsl_internal')
+        parsl_internal_executor = ThreadPoolExecutor(max_threads=config.internal_tasks_max_threads,
+                                                     label='_parsl_internal',
+                                                     remote_monitoring_radio=UDPRadio(address='127.0.0.1'))
         self.add_executors(config.executors)
         self.add_executors([parsl_internal_executor])
 
@@ -736,17 +740,19 @@ class DataFlowKernel:
         try_id = task_record['fail_count']
 
         if self.monitoring is not None and self.monitoring.resource_monitoring_enabled:
+            if executor.remote_monitoring_radio is None:
+                raise RadioRequiredError()
+
             wrapper_logging_level = logging.DEBUG if self.monitoring.monitoring_debug else logging.INFO
             (function, args, kwargs) = monitor_wrapper(f=function,
                                                        args=args,
                                                        kwargs=kwargs,
                                                        x_try_id=try_id,
                                                        x_task_id=task_id,
-                                                       monitoring_hub_url=self.monitoring.monitoring_hub_url,
+                                                       radio_config=executor.remote_monitoring_radio,
                                                        run_id=self.run_id,
                                                        logging_level=wrapper_logging_level,
                                                        sleep_dur=self.monitoring.resource_monitoring_interval,
-                                                       radio_mode=executor.radio_mode,
                                                        monitor_resources=executor.monitor_resources(),
                                                        run_dir=self.run_dir)
 
@@ -1131,8 +1137,14 @@ class DataFlowKernel:
         for executor in executors:
             executor.run_id = self.run_id
             executor.run_dir = self.run_dir
-            if self.monitoring:
+            if self.monitoring and executor.remote_monitoring_radio is not None:
                 executor.monitoring_messages = self.monitoring.resource_msgs
+                logger.debug("Starting monitoring receiver for executor %s "
+                             "with remote monitoring radio config %s",
+                             executor, executor.remote_monitoring_radio)
+
+                executor.monitoring_receiver = executor.remote_monitoring_radio.create_receiver(resource_msgs=executor.monitoring_messages,
+                                                                                                run_dir=executor.run_dir)
             if hasattr(executor, 'provider'):
                 if hasattr(executor.provider, 'script_dir'):
                     executor.provider.script_dir = os.path.join(self.run_dir, 'submit_scripts')

parsl/executors/base.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import os
 from abc import ABCMeta, abstractmethod
 from concurrent.futures import Future
@@ -8,7 +9,10 @@ from typing import Any, Callable, Dict, Optional
 
 from typing_extensions import Literal, Self
 
+from parsl.monitoring.radios.base import MonitoringRadioReceiver, RadioConfig
 from parsl.monitoring.types import TaggedMonitoringMessage
+
+logger = logging.getLogger(__name__)
 
 
 class ParslExecutor(metaclass=ABCMeta):
@@ -27,10 +31,8 @@ class ParslExecutor(metaclass=ABCMeta):
        label: str - a human readable label for the executor, unique
               with respect to other executors.
 
-    Per-executor monitoring behaviour can be influenced by exposing:
-
-       radio_mode: str - a string describing which radio mode should be used to
-              send task resource data back to the submit side.
+       remote_monitoring_radio: RadioConfig describing how tasks on this executor
+              should report task resource status
 
     An executor may optionally expose:
 
@@ -55,7 +57,6 @@ class ParslExecutor(metaclass=ABCMeta):
     """
 
     label: str = "undefined"
-    radio_mode: str = "udp"
 
     def __init__(
         self,
@@ -65,6 +66,10 @@ class ParslExecutor(metaclass=ABCMeta):
         run_id: Optional[str] = None,
     ):
         self.monitoring_messages = monitoring_messages
+
+        self.remote_monitoring_radio: Optional[RadioConfig] = None
+        self.monitoring_receiver: Optional[MonitoringRadioReceiver] = None
+
         self.run_dir = os.path.abspath(run_dir)
         self.run_id = run_id
 
@@ -101,7 +106,10 @@ class ParslExecutor(metaclass=ABCMeta):
         Executors should call super().shutdown() as part of their overridden
         implementation.
         """
-        pass
+        if self.monitoring_receiver is not None:
+            logger.info("Starting monitoring receiver shutdown")
+            self.monitoring_receiver.shutdown()
+            logger.info("Done with monitoring receiver shutdown")
 
     def monitor_resources(self) -> bool:
         """Should resource monitoring happen for tasks on running on this executor?

parsl/executors/high_throughput/executor.py
@@ -29,6 +29,8 @@ from parsl.executors.high_throughput.manager_selector import (
 )
 from parsl.executors.status_handling import BlockProviderExecutor
 from parsl.jobs.states import TERMINAL_STATES, JobState, JobStatus
+from parsl.monitoring.radios.base import RadioConfig
+from parsl.monitoring.radios.htex import HTEXRadio
 from parsl.monitoring.radios.zmq_router import ZMQRadioReceiver, start_zmq_receiver
 from parsl.process_loggers import wrap_with_logs
 from parsl.providers import LocalProvider
@@ -260,7 +262,8 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin, UsageIn
                  worker_logdir_root: Optional[str] = None,
                  manager_selector: ManagerSelector = RandomManagerSelector(),
                  block_error_handler: Union[bool, Callable[[BlockProviderExecutor, Dict[str, JobStatus]], None]] = True,
-                 encrypted: bool = False):
+                 encrypted: bool = False,
+                 remote_monitoring_radio: Optional[RadioConfig] = None):
 
         logger.debug("Initializing HighThroughputExecutor")
 
@@ -309,6 +312,12 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin, UsageIn
             self._workers_per_node = 1  # our best guess-- we do not have any provider hints
 
         self._task_counter = 0
+
+        if remote_monitoring_radio is not None:
+            self.remote_monitoring_radio = remote_monitoring_radio
+        else:
+            self.remote_monitoring_radio = HTEXRadio()
+
         self.worker_port = worker_port
         self.worker_port_range = worker_port_range
         self.interchange_proc: Optional[subprocess.Popen] = None
@@ -338,7 +347,6 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin, UsageIn
         self.zmq_monitoring = None
         self.hub_zmq_port = None
 
-    radio_mode = "htex"
     enable_mpi_mode: bool = False
     mpi_launcher: str = "mpiexec"
 

parsl/executors/high_throughput/mpi_executor.py
@@ -15,6 +15,7 @@ from parsl.executors.high_throughput.mpi_prefix_composer import (
 from parsl.executors.status_handling import BlockProviderExecutor
 from parsl.jobs.states import JobStatus
 from parsl.launchers import SimpleLauncher
+from parsl.monitoring.radios.base import RadioConfig
 from parsl.providers import LocalProvider
 from parsl.providers.base import ExecutionProvider
 
@@ -67,7 +68,8 @@ class MPIExecutor(HighThroughputExecutor):
                  worker_logdir_root: Optional[str] = None,
                  mpi_launcher: str = "mpiexec",
                  block_error_handler: Union[bool, Callable[[BlockProviderExecutor, Dict[str, JobStatus]], None]] = True,
-                 encrypted: bool = False):
+                 encrypted: bool = False,
+                 remote_monitoring_radio: Optional[RadioConfig] = None):
         super().__init__(
             # Hard-coded settings
             cores_per_worker=1e-9,  # Ensures there will be at least an absurd number of workers
@@ -94,7 +96,8 @@ class MPIExecutor(HighThroughputExecutor):
             address_probe_timeout=address_probe_timeout,
             worker_logdir_root=worker_logdir_root,
             block_error_handler=block_error_handler,
-            encrypted=encrypted
+            encrypted=encrypted,
+            remote_monitoring_radio=remote_monitoring_radio
         )
         self.enable_mpi_mode = True
         self.mpi_launcher = mpi_launcher

parsl/executors/taskvine/executor.py
@@ -40,6 +40,8 @@ from parsl.executors.taskvine.factory_config import TaskVineFactoryConfig
 from parsl.executors.taskvine.manager import _taskvine_submit_wait
 from parsl.executors.taskvine.manager_config import TaskVineManagerConfig
 from parsl.executors.taskvine.utils import ParslFileToVine, ParslTaskToVine
+from parsl.monitoring.radios.base import RadioConfig
+from parsl.monitoring.radios.filesystem import FilesystemRadio
 from parsl.multiprocessing import SpawnContext
 from parsl.process_loggers import wrap_with_logs
 from parsl.providers import CondorProvider, LocalProvider
@@ -98,8 +100,6 @@ class TaskVineExecutor(BlockProviderExecutor, putils.RepresentationMixin):
             Default is None.
     """
 
-    radio_mode = "filesystem"
-
     @typeguard.typechecked
     def __init__(self,
                  label: str = "TaskVineExecutor",
@@ -108,7 +108,8 @@ class TaskVineExecutor(BlockProviderExecutor, putils.RepresentationMixin):
                  manager_config: TaskVineManagerConfig = TaskVineManagerConfig(),
                  factory_config: TaskVineFactoryConfig = TaskVineFactoryConfig(),
                  provider: Optional[ExecutionProvider] = LocalProvider(init_blocks=1),
-                 storage_access: Optional[List[Staging]] = None):
+                 storage_access: Optional[List[Staging]] = None,
+                 remote_monitoring_radio: Optional[RadioConfig] = None):
 
         # Set worker launch option for this executor
         if worker_launch_method == 'factory' or worker_launch_method == 'manual':
@@ -133,6 +134,11 @@ class TaskVineExecutor(BlockProviderExecutor, putils.RepresentationMixin):
         self.manager_config = manager_config
         self.factory_config = factory_config
         self.storage_access = storage_access
+
+        if remote_monitoring_radio is not None:
+            self.remote_monitoring_radio = remote_monitoring_radio
+        else:
+            self.remote_monitoring_radio = FilesystemRadio()
 
         # Queue to send ready tasks from TaskVine executor process to TaskVine manager process
         self._ready_task_queue: multiprocessing.Queue = SpawnContext.Queue()

parsl/executors/threads.py
@@ -7,6 +7,7 @@ import typeguard
 from parsl.data_provider.staging import Staging
 from parsl.executors.base import ParslExecutor
 from parsl.executors.errors import InvalidResourceSpecification
+from parsl.monitoring.radios.base import RadioConfig
 from parsl.utils import RepresentationMixin
 
 logger = logging.getLogger(__name__)
@@ -28,7 +29,7 @@ class ThreadPoolExecutor(ParslExecutor, RepresentationMixin):
     @typeguard.typechecked
     def __init__(self, label: str = 'threads', max_threads: Optional[int] = 2,
                  thread_name_prefix: str = '', storage_access: Optional[List[Staging]] = None,
-                 working_dir: Optional[str] = None):
+                 working_dir: Optional[str] = None, remote_monitoring_radio: Optional[RadioConfig] = None):
         ParslExecutor.__init__(self)
         self.label = label
         self.max_threads = max_threads
@@ -39,6 +40,8 @@ class ThreadPoolExecutor(ParslExecutor, RepresentationMixin):
         # [] is a list with no storage access in it at all
         self.storage_access = storage_access
         self.working_dir = working_dir
+
+        self.remote_monitoring_radio = remote_monitoring_radio
 
     def start(self):
         self.executor = cf.ThreadPoolExecutor(max_workers=self.max_threads,

parsl/executors/workqueue/executor.py
@@ -31,6 +31,8 @@ from parsl.errors import OptionalModuleMissing
 from parsl.executors.errors import ExecutorError, InvalidResourceSpecification
 from parsl.executors.status_handling import BlockProviderExecutor
 from parsl.executors.workqueue import exec_parsl_function
+from parsl.monitoring.radios.base import RadioConfig
+from parsl.monitoring.radios.filesystem import FilesystemRadio
 from parsl.multiprocessing import SpawnContext, SpawnProcess
 from parsl.process_loggers import wrap_with_logs
 from parsl.providers import CondorProvider, LocalProvider
@@ -227,8 +229,6 @@ class WorkQueueExecutor(BlockProviderExecutor, putils.RepresentationMixin):
             specifiation for each task).
     """
 
-    radio_mode = "filesystem"
-
     @typeguard.typechecked
     def __init__(self,
                  label: str = "WorkQueueExecutor",
@@ -255,7 +255,8 @@ class WorkQueueExecutor(BlockProviderExecutor, putils.RepresentationMixin):
                  worker_executable: str = 'work_queue_worker',
                  function_dir: Optional[str] = None,
                  coprocess: bool = False,
-                 scaling_cores_per_worker: int = 1):
+                 scaling_cores_per_worker: int = 1,
+                 remote_monitoring_radio: Optional[RadioConfig] = None):
         BlockProviderExecutor.__init__(self, provider=provider,
                                        block_error_handler=True)
         if not _work_queue_enabled:
@@ -307,6 +308,11 @@ class WorkQueueExecutor(BlockProviderExecutor, putils.RepresentationMixin):
         self.launch_cmd = ("{package_prefix}python3 exec_parsl_function.py {mapping} {function} {result}")
         if self.init_command != "":
             self.launch_cmd = self.init_command + "; " + self.launch_cmd
+
+        if remote_monitoring_radio is not None:
+            self.remote_monitoring_radio = remote_monitoring_radio
+        else:
+            self.remote_monitoring_radio = FilesystemRadio()
 
     def _get_launch_command(self, block_id):
         # this executor uses different terminology for worker/launch

commit 6d99c36d5413a6ed912bf52a9a367f38293e5fe7

Restore part of #3801 that was regressed in #3892
#3892 did some code movement, which is hard to review via a diff.

In #3801, the exit mechanism for filesystem radio was switched to
event based, rather than hard process termination. Part of that was
lost by this bad #3892 code movement.

This PR restores that.

parsl/monitoring/radios/filesystem_router.py
@@ -12,7 +12,7 @@ from parsl.log_utils import set_file_logger
 from parsl.monitoring.radios.base import MonitoringRadioReceiver
 from parsl.monitoring.radios.multiprocessing import MultiprocessingQueueRadioSender
 from parsl.monitoring.types import TaggedMonitoringMessage
-from parsl.multiprocessing import SpawnEvent, SpawnProcess
+from parsl.multiprocessing import SpawnEvent, SpawnProcess, join_terminate_close_proc
 from parsl.process_loggers import wrap_with_logs
 from parsl.utils import setproctitle
 
@@ -69,6 +69,5 @@ class FilesystemRadioReceiver(MonitoringRadioReceiver):
         logger.info("Started filesystem radio receiver process %s", self.process.pid)
 
     def shutdown(self) -> None:
-        self.process.terminate()
-        self.process.join()
-        self.process.close()
+        self.exit_event.set()
+        join_terminate_close_proc(self.process)