f9fcd9ef15225a55d64d4cc2509fb8201f694e49
Ben Clifford <benc@hawaga.org.uk>
2025-08-03 13:56:47 +0000

    Detach log handler at shutdown of DFK (#3910)
    Prior to this PR, the automatic parsl.log handlers for each DFK will
    accumulate on the "parsl" log topic as new DFKs in one process are
    created, so for example the first created parsl.log will contain the log
    messages for every parsl workflow run in that process.
    
    This is especially noticeable in pytest tests, but real world users also
    occasionally want to run multiple DFKs in one process.
    
    For multiple DFKs in a process that exist completely serially, with one
    being shut down completely before the next starts, this should separate
    the logs from the two DFKs (and components) entirely.
    
    For DFKs that exist concurrently, this PR does not stop overlapping
    logs. The current approach of per-module loggers (rather than
    per-DFK/per-object) makes that hard and this PR does not attempt to
    solve that.
    
    # Changed Behaviour
    
    less logging when using multiple DFKs - mostly that will be a good
    thing, but users relying on parsl.log being written to all the way to
    the end of the process (rather than until the DFK is closed) will have
    to change.
    
    ## Type of change
    
    - New feature

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -91,8 +91,11 @@ class DataFlowKernel:
         self._config = config
         self.run_dir = make_rundir(config.run_dir)
 
+        self._logging_unregister_callback: Optional[Callable[[], None]]
         if config.initialize_logging:
-            parsl.set_file_logger("{}/parsl.log".format(self.run_dir), level=logging.DEBUG)
+            self._logging_unregister_callback = parsl.set_file_logger("{}/parsl.log".format(self.run_dir), level=logging.DEBUG)
+        else:
+            self._logging_unregister_callback = None
 
         logger.info("Starting DataFlowKernel with config\n{}".format(config))
 
@@ -1255,6 +1258,13 @@ class DataFlowKernel:
         else:
             logger.debug("Cleaning up non-default DFK - not unregistering")
 
+        if self._logging_unregister_callback:
+            logger.info("Unregistering log handler")
+            self._logging_unregister_callback()
+            logger.info("Unregistered log handler")
+
+        # This message won't go to the default parsl.log, but other handlers
+        # should still see it.
         logger.info("DFK cleanup complete")
 
     def checkpoint(self, tasks: Optional[Sequence[TaskRecord]] = None) -> str:
