737607fafdf04b422975dc4e376ffdea0c4f1f5b
Ben Clifford <benc@hawaga.org.uk>
2025-09-03 06:00:50 +0000

    fiddling with config for github

--- parsl/tests/test_htex/test_interchange_concurrency_blast.py
+++ parsl/tests/test_htex/test_interchange_concurrency_blast.py
@@ -1,50 +1,55 @@
 # make several threads of blasting concurrency at the interchange
 
-import parsl
-import pytest
+import logging
 import time
+from threading import Event, Thread
 
-from threading import Thread, Event
+import pytest
 
+import parsl
 from parsl.tests.configs.htex_local import fresh_config as local_config
+
+logger = logging.getLogger(__name__)
+
+N_THREADS = 50
+DURATION_S = 10
+
 
 @pytest.mark.local
 def test_concurrency_blast():
-  # create some threads
-  # have them all blast the interchange command channel for stuff for 1 minute
+    # create some threads
+    # have them all blast the interchange command channel for stuff for 1 minute
 
-  cc = parsl.dfk().executors['htex_local'].command_client
+    cc = parsl.dfk().executors['htex_local'].command_client
 
-  n = 2
-  threads = []
+    threads = []
 
-  ok_so_far = True
+    ok_so_far = True
 
-  for _ in range(n):
-    event = Event()
-    thread = Thread(target=blast, args=(cc, event))
-    threads.append( (thread, event) )
+    for _ in range(N_THREADS):
+        event = Event()
+        thread = Thread(target=blast, args=(cc, event))
+        threads.append((thread, event))
 
-  for thread, event in threads:
-    thread.start()
+    for thread, event in threads:
+        thread.start()
 
-  for thread, event in threads:
-    thread.join()
-    if not event.is_set():
-      print("thread should have exited normally, but did not")
-      ok_so_far = False
+    for thread, event in threads:
+        thread.join()
+        if not event.is_set():
+            logger.error("thread should have exited normally, but did not")
+            ok_so_far = False
 
-  assert ok_so_far, "at least one thread did not exit normally"
+    assert ok_so_far, "at least one thread did not exit normally"
+
 
 def blast(cc, e):
-  print("BENC: thread")  
-  duration = 30
-  target_end = time.time() + duration
+    target_end = time.time() + DURATION_S
 
-  while time.time() < target_end:
-    cc.run("WORKERS")
-    cc.run("MANGERs_PACKAGES")
-    cc.run("CONNECTED_BLOCKS")
-    cc.run("WORKER_BINDS")
+    while time.time() < target_end:
+        cc.run("WORKERS")
+        cc.run("MANGERs_PACKAGES")
+        cc.run("CONNECTED_BLOCKS")
+        cc.run("WORKER_BINDS")
 
-  e.set()
+    e.set()
