177a65090e72e6872509a3a1a7e70b4b44f00956
Ben Clifford <benc@hawaga.org.uk>
2025-09-24 18:56:17 +0000

    Format log messages with a bunch of metadata

--- elixirchange/config/config.exs
+++ elixirchange/config/config.exs
@@ -4,3 +4,7 @@ config :logger,
   compile_time_purge_matching: [
      [level_lower_than: :debug]
   ]
+
+config :logger, :default_formatter,
+  format: "$time [$level] $message $metadata\n",
+  metadata: [:mfa, :file, :line, :pid]
