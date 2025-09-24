b5741588d53bd69fe55f3ebd5e6d57714599fab2
Ben Clifford <benc@hawaga.org.uk>
2025-09-24 23:06:11 +0000

    log to file. let log queue get very big.

--- elixirchange/config/config.exs
+++ elixirchange/config/config.exs
@@ -3,8 +3,14 @@ import Config
 config :logger,
   compile_time_purge_matching: [
      [level_lower_than: :debug]
-  ]
+  ],
+  handle_sasl_reports: true
 
 config :logger, :default_formatter,
   format: "$time [$level] $message $metadata\n",
-  metadata: [:mfa, :file, :line, :pid]
+  metadata: [:mfa, :file, :line, :pid],
+  colors: [enabled: false]
+
+config :logger, :default_handler,
+  config: [sync_mode_qlen: 0, drop_mode_qlen: 1000000, flush_qlen: 1000000, burst_limit_enable: false, file: ~c"elixirchange.log"]
+
