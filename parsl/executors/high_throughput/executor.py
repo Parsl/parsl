5572ce39a77a2bef048fd6210d1447cc57c424a2
Ben Clifford <benc@hawaga.org.uk>
2025-09-29 10:26:49 +0000

    longer timeout on interchange startup because compiling idris2 under valgrind is slow

--- parsl/executors/high_throughput/executor.py
+++ parsl/executors/high_throughput/executor.py
@@ -642,7 +642,7 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin, UsageIn
 
         logger.debug("Requesting worker ports")
         try:
-            self.worker_port = self.command_client.run("WORKER_BINDS", timeout_s=120)
+            self.worker_port = self.command_client.run("WORKER_BINDS", timeout_s=600)
         except CommandClientTimeoutError:
             logger.error("Interchange has not completed initialization")
             # TODO: use a parsl exception...
