6111d06e87bb5e16efb371f107e4458df24629b9
Logan Ward <lward@nvidia.com>
2025-10-22 16:41:28 -0400

    Add option to use existing DFK, pin to subset of executors

--- parsl/tests/sites/test_concurrent.py
+++ parsl/tests/sites/test_concurrent.py
@@ -1,8 +1,9 @@
 """Tests of the interfaces to Python's concurrent library"""
-from pytest import mark, warns
+from pytest import mark, warns, raises
 
 from parsl import Config, HighThroughputExecutor
 from parsl.concurrent import ParslPoolExecutor
+from parsl import load
 
 
 def f(x):
@@ -13,15 +14,25 @@ def make_config():
     return Config(
         executors=[
             HighThroughputExecutor(
+                label='test_executor',
                 address="127.0.0.1",
                 max_workers_per_node=2,
                 heartbeat_period=2,
                 heartbeat_threshold=4,
-                encrypted=True,
+                encrypted=False,
             )
         ],
         strategy='none',
     )
+
+
+@mark.local
+def test_init_errors():
+    with load(make_config()) as dfk, raises(ValueError, match='Specify only one of config or dfk'):
+        ParslPoolExecutor(config=make_config(), dfk=dfk)
+
+    with raises(ValueError, match='Must specify one of config or dfk'):
+        ParslPoolExecutor()
 
 
 @mark.local
@@ -44,5 +55,18 @@ def test_executor():
         # Make sure only one function was registered
         assert exc.app_count == 1
 
+    with raises(RuntimeError, match='shut down'):
+        exc.submit(f, 1)
+
     with warns(UserWarning):
         ParslPoolExecutor(make_config()).shutdown(False, cancel_futures=True)
+
+
+@mark.local
+def test_with_dfk():
+    config = make_config()
+
+    with load(config) as dfk, ParslPoolExecutor(dfk=dfk, executors=['test_executor']) as exc:
+        future = exc.submit(f, 1)
+        assert future.result() == 2
+        assert next(iter(exc._app_cache.values())).executors == ['test_executor'], 'Executors were not passed through'
