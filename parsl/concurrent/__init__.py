0f6496e5bf958e609e557c7878f362f0374fa1c2
Logan Ward <WardLT@users.noreply.github.com>
2025-10-23 04:06:30 -0400

    Add option to use existing DFK, pin to subset of executors (#4010)
    # Description
    
    Adds more options for the `concurrent.futures.Executor` interface to
    Parsl (ParslPoolExecutor):
    
    1. Reuse an existing DFK rather than starting a new one
    2. Assigning functions to use only a subset of available executors
    
    Both changes are necessary to allow users to employ multiple types of
    Parsl workers from the concurrent.futures.Executor interface.
    
    ```python
    
     with load(config) as dfk, \
            ParslPoolExecutor(dfk=dfk, executors=['cpu']) as exc_cpu, \
            ParslPoolExecutor(dfk=dfk, executors=['gpu']) as exc_gpu:
    
         # Do something
         future_c = exc_cpu.submit(pre_process, 1)
         future_g = exc_cpu.submit(gpu_work, 2)
    ```
    # Changed Behaviour
    
    Does not change the current route for using ParslPoolExecutor, where one
    provides a Config and runs all functions on all executors.
    
    Does not change the terminology overlap between Parsl and Python.
    
    # Fixes
    
    N/A
    
    ## Type of change
    
    - New feature

--- parsl/concurrent/__init__.py
+++ parsl/concurrent/__init__.py
@@ -1,35 +1,62 @@
 """Interfaces modeled after Python's `concurrent library <https://docs.python.org/3/library/concurrent.html>`_"""
 import time
 from concurrent.futures import Executor
-from typing import Callable, Dict, Iterable, Iterator, Optional
+from contextlib import AbstractContextManager
+from typing import Callable, Dict, Iterable, Iterator, Literal, Optional
 from warnings import warn
 
-from parsl import Config, DataFlowKernel
+from parsl import Config, DataFlowKernel, load
 from parsl.app.python import PythonApp
 
 
-class ParslPoolExecutor(Executor):
+class ParslPoolExecutor(Executor, AbstractContextManager):
     """An executor that uses a pool of workers managed by Parsl
 
     Works just like a :class:`~concurrent.futures.ProcessPoolExecutor` except that tasks
     are distributed across workers that can be on different machines.
-    Create a new executor by supplying a Parsl :class:`~parsl.Config` object to define
-    how to create new workers, Parsl will set up and tear down workers on your behalf.
+
+    Create a new executor using one of two methods:
+
+    1. Supplying a Parsl :class:`~parsl.Config` that defines how to create new workers.
+       The executor will start a new Parsl Data Flow Kernel (DFK) when it is entered as a context manager.
+
+    2. Supplying an already-started Parsl :class:`~parsl.DataFlowKernel` (DFK).
+       The executor assumes you will start and stop the Parsl DFK outside the Executor.
 
     Note: Parsl does not support canceling tasks. The :meth:`map` method does not cancel work
     when one member of the run fails or a timeout is reached
     and :meth:`shutdown` does not cancel work on completion.
     """
 
-    def __init__(self, config: Config):
+    def __init__(self, config: Config | None = None, dfk: DataFlowKernel | None = None, executors: Literal['all'] | list[str] = 'all'):
         """Create the executor
 
         Args:
             config: Configuration for the Parsl Data Flow Kernel (DFK)
+            dfk: DataFlowKernel of an already-started parsl
+            executors: List of executors to use for supplied functions
         """
+        if (config is not None) and (dfk is not None):
+            raise ValueError('Specify only one of config or dfk')
+        if (config is None) and (dfk is None):
+            raise ValueError('Must specify one of config or dfk')
         self._config = config
-        self.dfk = DataFlowKernel(self._config)
         self._app_cache: Dict[Callable, PythonApp] = {}  # Cache specific to this instance: https://stackoverflow.com/questions/33672412
+        self._dfk = dfk
+        self.executors = executors
+
+        # Start workers immediately
+        if self._config is not None:
+            self._dfk = load(self._config)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._dfk is None:  # Nothing has been started, do nothing
+            return
+        elif self._config is not None:  # The executors are being managed by this class, shut them down
+            self.shutdown(wait=True)
+            return
+        else:  # The DFK is managed elsewhere, do nothing
+            return
 
     @property
     def app_count(self):
@@ -46,11 +73,13 @@ class ParslPoolExecutor(Executor):
         """
         if fn in self._app_cache:
             return self._app_cache[fn]
-        app = PythonApp(fn, data_flow_kernel=self.dfk)
+        app = PythonApp(fn, data_flow_kernel=self._dfk, executors=self.executors)
         self._app_cache[fn] = app
         return app
 
     def submit(self, fn, *args, **kwargs):
+        if self._dfk is None:
+            raise RuntimeError('Executor has been shut down.')
         app = self._get_app(fn)
         return app(*args, **kwargs)
 
@@ -78,8 +107,12 @@ class ParslPoolExecutor(Executor):
         return result_iterator()
 
     def shutdown(self, wait: bool = True, *, cancel_futures: bool = False) -> None:
+        if self._dfk is None:
+            return  # Do nothing. Nothing is active
         if cancel_futures:
             warn(message="Canceling on-going tasks is not supported in Parsl")
         if wait:
-            self.dfk.wait_for_current_tasks()
-        self.dfk.cleanup()
+            self._dfk.wait_for_current_tasks()
+        if self._config is not None:  # The executors are being managed
+            self._dfk.cleanup()  # Shutdown the DFK
+        self._dfk = None
