Parsl/parsl

commit 1d9d1124ba671cb0e5e171383337a3816c4b53c4

Make memoizer instance pluggable (#4042)
This is the main breaking change of PR #3535 checkpoint plugins: this PR
reworks construction of the memoizer so that the user now supplies an
instance, plugin style, instead of the DFK constructing an instance.

Memoization and configuration options, which turn out to be specific to
the BasicMemoizer, are now supplied by the user directly to the instance
they construct, rather than being passed through the DFK.

Any checkpoint users will need to alter their Parsl configuration,
although in a fairly straightforward way.

If no memoizer is specified, the DFK constructs a BasicMemozier that
preserves the no-parameters-supplied default behaviour:

Memoization will occur, checkpointing will not occur.

# Changed Behaviour

User facing checkpointing configuration breaks completely and users will
need to rewrite their checkpointing configuration.

## Type of change

- New feature

parsl/dataflow/dflow.py
@@ -165,13 +165,8 @@ class DataFlowKernel:
             self.monitoring_radio.send((MessageType.WORKFLOW_INFO,
                                        workflow_info))
 
-        self.memoizer: Memoizer = BasicMemoizer(memoize=config.app_cache,
-                                                checkpoint_mode=config.checkpoint_mode,
-                                                checkpoint_files=config.checkpoint_files,
-                                                checkpoint_period=config.checkpoint_period)
-        self.memoizer.run_dir = self.run_dir
-
-        self.memoizer.start()
+        self.memoizer: Memoizer = config.memoizer if config.memoizer is not None else BasicMemoizer()
+        self.memoizer.start(run_dir=self.run_dir)
 
         # this must be set before executors are added since add_executors calls
         # job_status_poller.add_executors.

parsl/dataflow/memoization.py
@@ -180,6 +180,17 @@ class Memoizer(metaclass=ABCMeta):
         raise NotImplementedError
 
     @abstractmethod
+    def start(self, *, run_dir: str) -> None:
+        """Called by the DFK when it starts up.
+
+        This is an opportunity for the memoization/checkpoint system to
+        initialize itself.
+
+        The path to the base run directory is passed as a parameter.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
     def checkpoint_queue(self) -> None:
         """Called by the DFK when the user calls dfk.checkpoint(). This
         indicates that the checkpoint system should explicitly process any
@@ -238,18 +249,26 @@ class BasicMemoizer(Memoizer):
     run_dir: str
 
     def __init__(self, *,
-                 memoize: bool = True,
-                 checkpoint_files: Sequence[str] | None,
-                 checkpoint_period: Optional[str],
-                 checkpoint_mode: Literal['task_exit', 'periodic', 'dfk_exit', 'manual'] | None):
+                 checkpoint_files: Sequence[str] | None = None,
+                 checkpoint_period: Optional[str] = None,
+                 checkpoint_mode: Literal['task_exit', 'periodic', 'dfk_exit', 'manual'] | None = None,
+                 memoize: bool = True):
         """Initialize the memoizer.
 
         KWargs:
-            - memoize (Bool): enable memoization or not.
-            - checkpoint (Dict): A checkpoint loaded as a dict.
-        """
-        self.memoize = memoize
 
+            - checkpoint_files : sequence of str, optional
+                  List of paths to checkpoint files. See :func:`parsl.utils.get_all_checkpoints` and
+                  :func:`parsl.utils.get_last_checkpoint` for helpers. Default is None.
+            - checkpoint_period : str, optional
+                  Time interval (in "HH:MM:SS") at which to checkpoint completed tasks. Only has an effect if
+                  ``checkpoint_mode='periodic'``.
+            - checkpoint_mode : str, optional
+                  Checkpoint mode to use, can be ``'dfk_exit'``, ``'task_exit'``, ``'periodic'`` or ``'manual'``.
+                  If set to `None`, checkpointing will be disabled. Default is None.
+            - memoize : str, enable memoization or not.
+
+        """
         self.checkpointed_tasks = 0
 
         # this lock must be held when:
@@ -264,8 +283,12 @@ class BasicMemoizer(Memoizer):
         self.checkpointable_tasks: List[CheckpointCommand] = []
 
         self._checkpoint_timer: Timer | None = None
+        self.memoize = memoize
 
-    def start(self) -> None:
+    def start(self, *, run_dir: str) -> None:
+
+        self.run_dir = run_dir
+
         if self.checkpoint_files is not None:
             checkpoint_files = self.checkpoint_files
         elif self.checkpoint_files is None and self.checkpoint_mode is not None:

parsl/tests/test_checkpointing/test_python_checkpoint_1.py
@@ -5,13 +5,12 @@ import pytest
 
 import parsl
 from parsl import python_app
-from parsl.tests.configs.local_threads import fresh_config
+from parsl.config import Config
+from parsl.dataflow.memoization import BasicMemoizer
 
 
 def local_config():
-    config = fresh_config()
-    config.checkpoint_mode = "manual"
-    return config
+    return Config(memoizer=BasicMemoizer(checkpoint_mode="manual"))
 
 
 @python_app(cache=True)
@@ -26,9 +25,10 @@ def test_initial_checkpoint_write() -> None:
     """
     uuid_app().result()
 
-    parsl.dfk().checkpoint()
-
     cpt_dir = Path(parsl.dfk().run_dir) / 'checkpoint'
 
     cptpath = cpt_dir / 'tasks.pkl'
-    assert os.path.exists(cptpath), f"Tasks checkpoint missing: {cptpath}"
+
+    assert not os.path.exists(cptpath), f"Tasks checkpoint should not exist yet: {cptpath}"
+    parsl.dfk().checkpoint()
+    assert os.path.exists(cptpath), f"Tasks checkpoint should exist now: {cptpath}"

parsl/config.py
@@ -5,6 +5,7 @@ import typeguard
 from typing_extensions import Literal
 
 from parsl.dataflow.dependency_resolvers import DependencyResolver
+from parsl.dataflow.memoization import Memoizer
 from parsl.dataflow.taskrecord import TaskRecord
 from parsl.errors import ConfigurationError
 from parsl.executors.base import ParslExecutor
@@ -27,17 +28,6 @@ class Config(RepresentationMixin, UsageInformation):
     executors : sequence of ParslExecutor, optional
         List (or other iterable) of `ParslExecutor` instances to use for executing tasks.
         Default is (:class:`~parsl.executors.threads.ThreadPoolExecutor()`,).
-    app_cache : bool, optional
-        Enable app caching. Default is True.
-    checkpoint_files : sequence of str, optional
-        List of paths to checkpoint files. See :func:`parsl.utils.get_all_checkpoints` and
-        :func:`parsl.utils.get_last_checkpoint` for helpers. Default is None.
-    checkpoint_mode : str, optional
-        Checkpoint mode to use, can be ``'dfk_exit'``, ``'task_exit'``, ``'periodic'`` or ``'manual'``.
-        If set to `None`, checkpointing will be disabled. Default is None.
-    checkpoint_period : str, optional
-        Time interval (in "HH:MM:SS") at which to checkpoint completed tasks. Only has an effect if
-        ``checkpoint_mode='periodic'``.
     dependency_resolver: plugin point for custom dependency resolvers. Default: only resolve Futures,
         using the `SHALLOW_DEPENDENCY_RESOLVER`.
     exit_mode: str, optional
@@ -100,14 +90,7 @@ class Config(RepresentationMixin, UsageInformation):
     @typeguard.typechecked
     def __init__(self,
                  executors: Optional[Iterable[ParslExecutor]] = None,
-                 app_cache: bool = True,
-                 checkpoint_files: Optional[Sequence[str]] = None,
-                 checkpoint_mode: Union[None,
-                                        Literal['task_exit'],
-                                        Literal['periodic'],
-                                        Literal['dfk_exit'],
-                                        Literal['manual']] = None,
-                 checkpoint_period: Optional[str] = None,
+                 memoizer: Optional[Memoizer] = None,
                  dependency_resolver: Optional[DependencyResolver] = None,
                  exit_mode: Literal['cleanup', 'skip', 'wait'] = 'cleanup',
                  garbage_collect: bool = True,
@@ -131,21 +114,7 @@ class Config(RepresentationMixin, UsageInformation):
         self._executors: Sequence[ParslExecutor] = executors
         self._validate_executors()
 
-        self.app_cache = app_cache
-        self.checkpoint_files = checkpoint_files
-        self.checkpoint_mode = checkpoint_mode
-        if checkpoint_period is not None:
-            if checkpoint_mode is None:
-                logger.debug('The requested `checkpoint_period={}` will have no effect because `checkpoint_mode=None`'.format(
-                    checkpoint_period)
-                )
-            elif checkpoint_mode != 'periodic':
-                logger.debug("Requested checkpoint period of {} only has an effect with checkpoint_mode='periodic'".format(
-                    checkpoint_period)
-                )
-        if checkpoint_mode == 'periodic' and checkpoint_period is None:
-            checkpoint_period = "00:30:00"
-        self.checkpoint_period = checkpoint_period
+        self.memoizer = memoizer
         self.dependency_resolver = dependency_resolver
         self.exit_mode = exit_mode
         self.garbage_collect = garbage_collect

parsl/configs/ASPIRE1.py
@@ -1,5 +1,6 @@
 from parsl.addresses import address_by_interface
 from parsl.config import Config
+from parsl.dataflow.memoization import BasicMemoizer
 from parsl.executors import HighThroughputExecutor
 from parsl.launchers import MpiRunLauncher
 from parsl.monitoring.monitoring import MonitoringHub
@@ -38,7 +39,6 @@ config = Config(
         ),
         strategy='simple',
         retries=3,
-        app_cache=True,
-        checkpoint_mode='task_exit',
+        memoizer=BasicMemoizer(checkpoint_mode='task_exit'),
         usage_tracking=LEVEL_1,
 )

parsl/tests/configs/htex_local_alternate.py
@@ -22,6 +22,7 @@ from parsl.data_provider.file_noop import NoOpFileStaging
 from parsl.data_provider.ftp import FTPInTaskStaging
 from parsl.data_provider.http import HTTPInTaskStaging
 from parsl.data_provider.zip import ZipFileStaging
+from parsl.dataflow.memoization import BasicMemoizer
 from parsl.executors import HighThroughputExecutor
 from parsl.launchers import SingleNodeLauncher
 
@@ -56,7 +57,7 @@ def fresh_config():
             )
         ],
         strategy='simple',
-        app_cache=True, checkpoint_mode='task_exit',
+        memoizer=BasicMemoizer(memoize=True, checkpoint_mode='task_exit'),
         retries=2,
         monitoring=MonitoringHub(
                         monitoring_debug=False,

parsl/tests/configs/local_threads_checkpoint.py
@@ -1,15 +1,0 @@
-from parsl.config import Config
-from parsl.executors.threads import ThreadPoolExecutor
-
-
-def fresh_config():
-    return Config(
-        executors=[
-            ThreadPoolExecutor(
-                label='local_threads_checkpoint',
-            )
-        ]
-    )
-
-
-config = fresh_config()

parsl/tests/configs/local_threads_checkpoint_dfk_exit.py
@@ -1,4 +1,5 @@
 from parsl.config import Config
+from parsl.dataflow.memoization import BasicMemoizer
 from parsl.executors.threads import ThreadPoolExecutor
 
 config = Config(
@@ -7,5 +8,5 @@ config = Config(
             label='local_threads_checkpoint_dfk_exit',
         )
     ],
-    checkpoint_mode='dfk_exit'
+    memoizer=BasicMemoizer(checkpoint_mode='dfk_exit')
 )

commit e508629b19cd8f850b6ea664a02be9d5a25b2639

Make explicit checkpoint calls on memoizer, not DFK (#4044)
This is a checkpointer-specific behaviour, just like periodic
checkpointing. For example, the upcoming SQLite checkpointer has no
notion of deferring checkpoint updates to later than task completion.

This PR modifies a test case to show how manual checkpointing should be
done now.

# Changed Behaviour

breaks interface for users who do explicit checkpointing. they already
got related breakage from #4042 and here is more for them.

## Type of change

- New feature

parsl/dataflow/dflow.py
@@ -1190,9 +1190,6 @@ class DataFlowKernel:
         # should still see it.
         logger.info("DFK cleanup complete")
 
-    def checkpoint(self) -> None:
-        self.memoizer.checkpoint_queue()
-
     @staticmethod
     def _log_std_streams(task_record: TaskRecord) -> None:
         tid = task_record['id']

parsl/dataflow/memoization.py
@@ -227,14 +227,6 @@ class Memoizer(metaclass=ABCMeta):
         raise NotImplementedError
 
     @abstractmethod
-    def checkpoint_queue(self) -> None:
-        """Called by the DFK when the user calls dfk.checkpoint(). This
-        indicates that the checkpoint system should explicitly process any
-        outstanding checkpoint writes.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
     def check_memo(self, task: TaskRecord) -> Optional[Future[Any]]:
         """Asks the checkpoint system for a result recorded for the described
         task. ``check_memo`` should return a `Future` that will be used as
@@ -537,6 +529,11 @@ class BasicMemoizer(Memoizer):
         with self._checkpoint_lock:
             self._checkpoint_these_tasks(self.checkpointable_tasks)
             self.checkpointable_tasks = []
+
+    def checkpoint(self) -> None:
+        """This is the user-facing interface to manual checkpointing.
+        """
+        self.checkpoint_queue()
 
     def _checkpoint_these_tasks(self, checkpoint_queue: List[CheckpointCommand]) -> None:
         """Play a sequence of CheckpointCommands into a checkpoint file.

parsl/tests/test_checkpointing/test_python_checkpoint_1.py
@@ -9,10 +9,6 @@ from parsl.config import Config
 from parsl.dataflow.memoization import BasicMemoizer
 
 
-def local_config():
-    return Config(memoizer=BasicMemoizer(checkpoint_mode="manual"))
-
-
 @python_app(cache=True)
 def uuid_app():
     import uuid
@@ -20,15 +16,18 @@ def uuid_app():
 
 
 @pytest.mark.local
-def test_initial_checkpoint_write() -> None:
+def test_manual_checkpoint() -> None:
     """1. Launch a few apps and write the checkpoint once a few have completed
     """
-    uuid_app().result()
+    memoizer = BasicMemoizer(checkpoint_mode="manual")
 
-    cpt_dir = Path(parsl.dfk().run_dir) / 'checkpoint'
+    with parsl.load(Config(memoizer=memoizer)):
+        uuid_app().result()
 
-    cptpath = cpt_dir / 'tasks.pkl'
+        cpt_dir = Path(parsl.dfk().run_dir) / 'checkpoint'
 
-    assert not os.path.exists(cptpath), f"Tasks checkpoint should not exist yet: {cptpath}"
-    parsl.dfk().checkpoint()
-    assert os.path.exists(cptpath), f"Tasks checkpoint should exist now: {cptpath}"
+        cptpath = cpt_dir / 'tasks.pkl'
+
+        assert not os.path.exists(cptpath), f"Tasks checkpoint should not exist yet: {cptpath}"
+        memoizer.checkpoint()
+        assert os.path.exists(cptpath), f"Tasks checkpoint should exist now: {cptpath}"