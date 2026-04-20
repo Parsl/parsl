b42c8464cd6016ee2ed280310fc7301c6cf1482a
Ben Clifford <benc@hawaga.org.uk>
2026-04-20 11:10:11 +0000

    Add a JSON format log file configuration
    This is intended for the class of users who regexp their logfiles.
    
    Alongside this, further PRs will add in logging 'extra' dictionaries
    containing machine readable fields such as task IDs.

--- parsl/tests/test_logging/test_initialize_logging.py
+++ parsl/tests/test_logging/test_initialize_logging.py
@@ -1,9 +1,12 @@
+import json
+import logging
 import pathlib
 
 import pytest
 
 import parsl
 from parsl.logconfigs.file import FileLogging
+from parsl.logconfigs.json import JSONLogging
 
 
 @pytest.mark.local
@@ -25,3 +28,32 @@ def test_parsl_log_FileLogging(tmpd_cwd):
     with parsl.load(parsl.Config(run_dir=str(tmpd_cwd), initialize_logging=FileLogging())):
         assert (pathlib.Path(parsl.dfk().run_dir) / "parsl.log").exists(), \
             "With FileLogging, parsl.log should exist in rundir after startup"
+
+
+@pytest.mark.local
+def test_parsl_log_JSONLogging(tmpd_cwd):
+    with parsl.load(parsl.Config(run_dir=str(tmpd_cwd), initialize_logging=JSONLogging())):
+        logger = logging.getLogger(__name__)
+        msg1 = "Test without extra"
+        msg2 = "Test with extra"
+
+        logger.info(msg1)
+        logger.info("Test with extra", extra={"parsl.somekey": "vv"})
+
+        logfile = pathlib.Path(parsl.dfk().run_dir) / "parsl.jsonlog"
+        assert logfile.exists(), \
+            "With JSONLogging, parsl.jsonlog should exist in rundir after startup"
+
+    # close down the DFK before attempt to read log file, so that there aren't any partial
+    # writes.
+    with open(logfile, "r") as f:
+        lines = f.readlines()
+
+        # we should parse every line as JSON, without Exceptions
+        parsed = [json.loads(line) for line in lines]
+
+        assert len([None for p in parsed if p['msg'] == msg1]) == 1, "msg1 should appear once"
+
+        msgs2 = [p for p in parsed if p['msg'] == msg2]
+        assert len(msgs2) == 1, "msg2 should appear once"
+        assert msgs2[0]['parsl.somekey'] == "vv", "extra key should have been stored in record"
