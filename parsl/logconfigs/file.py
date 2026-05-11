e4de4f491b654eaf1f49433ae642fc2af454b42f
Ben Clifford <benc@hawaga.org.uk>
2026-05-11 17:42:17 +0000

    use pathlib mkdir

--- parsl/logconfigs/file.py
+++ parsl/logconfigs/file.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import pathlib
 from typing import Callable
 
@@ -17,7 +16,7 @@ class FileLogging(LogConfig):
 
     def initialize_logging(self, *, log_dir: pathlib.Path, log_name: str) -> Callable[[], None]:
 
-        os.makedirs(log_dir, exist_ok=True)
+        log_dir.mkdir(exist_ok=True)
         filename = log_dir.joinpath(log_name + ".log")
 
         handler = logging.FileHandler(filename)
