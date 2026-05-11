09a32688564c60e8644e6eda8e2347518e2feabe
Ben Clifford <benc@hawaga.org.uk>
2026-05-11 12:55:29 +0000

    Remove UUID from this PR -- it can be introduced later if/when I get round to situations where it is needed

--- parsl/logconfigs/base.py
+++ parsl/logconfigs/base.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import abc
-import uuid
 from collections.abc import Callable
 
 
@@ -16,15 +15,7 @@ class LogConfig(abc.ABC):
     Configurations which might be shared between processes should expect to
     be pickled/unpickled multiple times as they move around a Parsl
     distributed system.
-
-    A log configuration is identified across the distributed system by
-    the `uuid` attribute. This is intended to allow code calling the
-    log_configuration class to only initialise a particular log configuration
-    once per process.
     """
-
-    def __init__(self) -> None:
-        self.uuid = str(uuid.uuid4())
 
     @abc.abstractmethod
     def initialize_logging(self, *, log_dir: str, log_name: str) -> Callable[[], None]:
