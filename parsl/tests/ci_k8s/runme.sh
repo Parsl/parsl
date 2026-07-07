f77a1cc7329a961c2390c2e3ee26fa718db399a6
Ben Clifford <benc@hawaga.org.uk>
2026-07-07 12:26:56 +0000

    add some debugging, towards understanding why taskvine just failed

--- parsl/tests/ci_k8s/runme.sh
+++ parsl/tests/ci_k8s/runme.sh
@@ -4,4 +4,21 @@ source /venv/bin/activate
 
 pytest parsl/tests/ --config parsl/tests/ci_k8s/htex_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order
 
-PYTHONPATH=/usr/lib/python3.11/site-packages/ pytest parsl/tests/ --config parsl/tests/ci_k8s/taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG
+PYTHONPATH=/usr/lib/python3.11/site-packages/ pytest parsl/tests/ --config parsl/tests/ci_k8s/taskvine_k8s_kind.py -k 'not issue3328 and not staging_required and not shared_fs' -x --random-order --log-cli-level=DEBUG || true
+
+# || true to keep running so I can add in debug hooks here
+# TODO: remove/fixup before merge
+
+echo pwd
+pwd
+
+echo find /tmp
+find /tmp
+
+echo find .
+find .
+
+echo ps ax
+ps ax
+
+echo end
